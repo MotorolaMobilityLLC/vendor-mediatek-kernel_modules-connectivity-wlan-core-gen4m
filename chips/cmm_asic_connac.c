1a4aec9daeb6f77168c5f2413779af40148ab584
Snowpin Lee <snowpin.lee@mediatek.com>
2018-06-03 01:39:00 +0800

    [WCNCR00149241] 7663u driver dev
    [Description]
        1. Add DMA Scheduler Init API.
        2. 7663U needs to enable UDMA 1us timer for UDMA RX AGG and TX timeout.
          (UDMA 1us timter default is off for low power requirement.)
    
    MTK-Commit-Id: b470b8db7316b9436de899ff777daf7679271532
    
    Change-Id: I485090b66845871bc1affebe99661321e23cac90
    Feature: 7663/usb
    Signed-off-by: Snowpin Lee <snowpin.lee@mediatek.com>
    CR-Id: WCNCR00149241

--- chips/cmm_asic_connac.c
+++ chips/cmm_asic_connac.c
@@ -71,6 +71,56 @@
 *                              C O N S T A N T S
 ********************************************************************************
 */
+
+
+/*******************************************************************************
+*                            P U B L I C   D A T A
+********************************************************************************
+*/
+
+/*
+* Common Default Setting
+* TODO: This table can be modified by command or cfg file.
+*/
+UINT_8 arAcQIdx2GroupId[MAC_TXQ_NUM] = {
+	GROUP0_INDEX,    /* MAC_TXQ_AC0_INDEX */
+	GROUP1_INDEX,    /* MAC_TXQ_AC1_INDEX */
+	GROUP2_INDEX,    /* MAC_TXQ_AC2_INDEX */
+	GROUP4_INDEX,    /* MAC_TXQ_AC3_INDEX */
+
+	GROUP0_INDEX,    /* MAC_TXQ_AC10_INDEX */
+	GROUP1_INDEX,    /* MAC_TXQ_AC11_INDEX */
+	GROUP2_INDEX,    /* MAC_TXQ_AC12_INDEX */
+	GROUP4_INDEX,    /* MAC_TXQ_AC13_INDEX */
+
+	GROUP0_INDEX,    /* MAC_TXQ_AC20_INDEX */
+	GROUP1_INDEX,    /* MAC_TXQ_AC21_INDEX */
+	GROUP2_INDEX,    /* MAC_TXQ_AC22_INDEX */
+	GROUP4_INDEX,    /* MAC_TXQ_AC23_INDEX */
+
+	GROUP0_INDEX,    /* MAC_TXQ_AC30_INDEX */
+	GROUP1_INDEX,    /* MAC_TXQ_AC31_INDEX */
+	GROUP2_INDEX,    /* MAC_TXQ_AC32_INDEX */
+	GROUP4_INDEX,    /* MAC_TXQ_AC33_INDEX */
+
+	GROUP5_INDEX,    /* MAC_TXQ_ALTX_0_INDEX */
+	GROUP5_INDEX,    /* MAC_TXQ_BMC_0_INDEX */
+	GROUP5_INDEX,    /* MAC_TXQ_BCN_0_INDEX */
+	GROUP5_INDEX,    /* MAC_TXQ_PSMP_0_INDEX */
+
+	GROUP5_INDEX,    /* Reserved */
+	GROUP5_INDEX,    /* Reserved */
+	GROUP5_INDEX,    /* Reserved */
+	GROUP5_INDEX,    /* Reserved */
+
+	GROUP5_INDEX,    /* Reserved */
+	GROUP5_INDEX,    /* Reserved */
+};
+
+/*******************************************************************************
+*                              F U N C T I O N S
+********************************************************************************
+*/
 VOID asicCapInit(IN P_ADAPTER_T prAdapter)
 {
 	P_GLUE_INFO_T prGlueInfo;
@@ -85,7 +135,7 @@ VOID asicCapInit(IN P_ADAPTER_T prAdapter)
 	prChipInfo->u2TxInitCmdPort = 0;
 	prChipInfo->u2TxFwDlPort = 0;
 	prChipInfo->fillHifTxDesc = NULL;
-	prChipInfo->u2ExtraTxByteCount = 0;
+	prChipInfo->u4ExtraTxByteCount = 0;
 
 	switch (prGlueInfo->u4InfType) {
 #if defined(_HIF_PCIE)
@@ -93,6 +143,8 @@ VOID asicCapInit(IN P_ADAPTER_T prAdapter)
 		prChipInfo->u2TxInitCmdPort = TX_RING_FWDL_IDX_3;
 		prChipInfo->u2TxFwDlPort = TX_RING_FWDL_IDX_3;
 		prChipInfo->ucPacketFormat = TXD_PKT_FORMAT_TXD;
+		prChipInfo->u4HifDmaShdlBaseAddr = PCIE_HIF_DMASHDL_BASE;
+		asicPcieDmaShdlInit(prAdapter);
 		break;
 #endif /* _HIF_PCIE */
 #if defined(_HIF_USB)
@@ -102,13 +154,15 @@ VOID asicCapInit(IN P_ADAPTER_T prAdapter)
 		prChipInfo->u2TxInitCmdPort = USB_DATA_BULK_OUT_EP8;
 		prChipInfo->u2TxFwDlPort = USB_DATA_BULK_OUT_EP4;
 		prChipInfo->ucPacketFormat = TXD_PKT_FORMAT_TXD_PAYLOAD;
-		prChipInfo->u2ExtraTxByteCount = EXTRA_TXD_SIZE_FOR_TX_BYTE_COUNT;
+		prChipInfo->u4ExtraTxByteCount = EXTRA_TXD_SIZE_FOR_TX_BYTE_COUNT;
+		prChipInfo->u4HifDmaShdlBaseAddr = USB_HIF_DMASHDL_BASE;
+		asicUsbDmaShdlInit(prAdapter);
 		break;
 #endif /* _HIF_USB */
 #if defined(_HIF_SDIO)
 	case MT_DEV_INF_SDIO:
 		prChipInfo->ucPacketFormat = TXD_PKT_FORMAT_TXD_PAYLOAD;
-		prChipInfo->u2ExtraTxByteCount = EXTRA_TXD_SIZE_FOR_TX_BYTE_COUNT;
+		prChipInfo->u4ExtraTxByteCount = EXTRA_TXD_SIZE_FOR_TX_BYTE_COUNT;
 		break;
 #endif /* _HIF_SDIO */
 	default:
@@ -163,6 +217,59 @@ VOID asicEnableFWDownload(IN P_ADAPTER_T prAdapter, IN BOOL fgEnable)
 	}
 }
 
+VOID asicCmmDmaShdlInit(IN P_ADAPTER_T prAdapter, UINT_32 u4RefillGroup)
+{
+	UINT_32 u4BaseAddr, u4MacVal = 0;
+	struct mt66xx_chip_info *prChipInfo;
+
+	ASSERT(prAdapter);
+
+	prChipInfo = prAdapter->chip_info;
+	u4BaseAddr = prChipInfo->u4HifDmaShdlBaseAddr;
+
+	HAL_MCR_RD(prAdapter, CONN_HIF_DMASHDL_PACKET_MAX_SIZE(u4BaseAddr), &u4MacVal);
+	u4MacVal &= ~(PLE_PKT_MAX_SIZE_MASK | PSE_PKT_MAX_SIZE_MASK);
+	u4MacVal |= PLE_PKT_MAX_SIZE_NUM(0x1);
+	u4MacVal |= PSE_PKT_MAX_SIZE_NUM(0x8);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_PACKET_MAX_SIZE(u4BaseAddr), u4MacVal);
+
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_REFILL_CONTROL(u4BaseAddr), u4RefillGroup);
+
+	u4MacVal = DMASHDL_MIN_QUOTA_NUM(0x3);
+	u4MacVal |= DMASHDL_MAX_QUOTA_NUM(0x1ff);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_GROUP0_CTRL(u4BaseAddr), u4MacVal);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_GROUP1_CTRL(u4BaseAddr), u4MacVal);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_GROUP2_CTRL(u4BaseAddr), u4MacVal);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_GROUP3_CTRL(u4BaseAddr), u4MacVal);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_GROUP4_CTRL(u4BaseAddr), u4MacVal);
+
+	u4MacVal = ((arAcQIdx2GroupId[MAC_TXQ_AC0_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE0_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC1_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE1_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC2_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE2_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC3_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE3_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC10_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE4_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC11_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE5_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC12_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE6_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC13_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE7_MAPPING));
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_Q_MAP0(u4BaseAddr), u4MacVal);
+
+	u4MacVal = ((arAcQIdx2GroupId[MAC_TXQ_AC20_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE8_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC21_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE9_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC22_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE10_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC23_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE11_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC30_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE12_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC31_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE13_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC32_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE14_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_AC33_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING1_QUEUE15_MAPPING));
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_Q_MAP1(u4BaseAddr), u4MacVal);
+
+	u4MacVal = ((arAcQIdx2GroupId[MAC_TXQ_ALTX_0_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING2_QUEUE16_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_BMC_0_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING2_QUEUE17_MAPPING)|
+		(arAcQIdx2GroupId[MAC_TXQ_BCN_0_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING2_QUEUE18_MAPPING) |
+		(arAcQIdx2GroupId[MAC_TXQ_PSMP_0_INDEX] << CONN_HIF_DMASHDL_TOP_QUEUE_MAPPING2_QUEUE19_MAPPING));
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_Q_MAP2(u4BaseAddr), u4MacVal);
+}
+
 VOID fillTxDescAppendByHost(IN P_ADAPTER_T prAdapter, IN P_MSDU_INFO_T prMsduInfo, IN UINT_16 u4MsduId,
 			    IN dma_addr_t rDmaAddr, OUT PUINT_8 pucBuffer)
 {
@@ -198,7 +305,100 @@ VOID fillTxDescAppendByCR4(IN P_ADAPTER_T prAdapter, IN P_MSDU_INFO_T prMsduInfo
 	prHwTxDescAppend->CR4_APPEND.au2BufLen[0] = prMsduInfo->u2FrameLength;
 }
 
+#if defined(_HIF_PCIE)
+VOID asicPcieDmaShdlInit(IN P_ADAPTER_T prAdapter)
+{
+	UINT_32 u4BaseAddr, u4MacVal;
+	struct mt66xx_chip_info *prChipInfo;
+
+	ASSERT(prAdapter);
+
+	prChipInfo = prAdapter->chip_info;
+	u4BaseAddr = prChipInfo->u4HifDmaShdlBaseAddr;
+
+	/*
+	* Enable refill control group 0, 1, 2, 4, 5. (Keep group3 for bypass usage.)
+	* Keep all group low refill priority to prevent low group starvation if we have high group.
+	*/
+	u4MacVal = (CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP3_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP6_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP7_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP8_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP9_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP10_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP11_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP12_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP13_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP14_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP15_REFILL_DISABLE_MASK);
+
+	asicCmmDmaShdlInit(prAdapter, u4MacVal);
+
+	/*
+	* HIF Scheduler Setting
+	* Group15(CMD) is highest priority.
+	*/
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_SHDL_SET0(u4BaseAddr), 0x6012345f);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_SHDL_SET1(u4BaseAddr), 0xedcba987);
+}
+#endif /* _HIF_PCIE */
+
 #if defined(_HIF_USB)
+/* DMS Scheduler Init */
+VOID asicUsbDmaShdlInit(IN P_ADAPTER_T prAdapter)
+{
+	UINT_32 u4BaseAddr, u4MacVal;
+	struct mt66xx_chip_info *prChipInfo;
+
+	ASSERT(prAdapter);
+
+	prChipInfo = prAdapter->chip_info;
+	u4BaseAddr = prChipInfo->u4HifDmaShdlBaseAddr;
+
+	/*
+	* USB Endpoint OUT/DMA Scheduler Group Mapping (HW Define)
+	* EP#4 / Group0  (DATA)
+	* EP#5 / Group1  (DATA)
+	* EP#6 / Group2  (DATA)
+	* EP#7 / Group3  (DATA)
+	* EP#9 / Group4  (DATA)
+	* EP#8 / Group15 (CMD)
+	*/
+	arAcQIdx2GroupId[MAC_TXQ_AC3_INDEX] = GROUP3_INDEX;
+	arAcQIdx2GroupId[MAC_TXQ_AC13_INDEX] = GROUP3_INDEX;
+	arAcQIdx2GroupId[MAC_TXQ_AC23_INDEX] = GROUP3_INDEX;
+	arAcQIdx2GroupId[MAC_TXQ_AC33_INDEX] = GROUP3_INDEX;
+	arAcQIdx2GroupId[MAC_TXQ_ALTX_0_INDEX] = GROUP4_INDEX;
+	arAcQIdx2GroupId[MAC_TXQ_BMC_0_INDEX] = GROUP4_INDEX;
+	arAcQIdx2GroupId[MAC_TXQ_BCN_0_INDEX] = GROUP4_INDEX;
+	arAcQIdx2GroupId[MAC_TXQ_PSMP_0_INDEX] = GROUP4_INDEX;
+
+	/*
+	* Enable refill control group 0, 1, 2, 3, 4.
+	* Keep all group low refill priority to prevent low group starvation if we have high group.
+	*/
+	u4MacVal = (CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP5_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP6_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP7_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP8_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP9_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP10_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP11_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP12_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP13_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP14_REFILL_DISABLE_MASK |
+				CONN_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP15_REFILL_DISABLE_MASK);
+
+	asicCmmDmaShdlInit(prAdapter, u4MacVal);
+
+	/*
+	* HIF Scheduler Setting
+	* Group15(CMD) is highest priority.
+	*/
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_SHDL_SET0(u4BaseAddr), 0x6501234f);
+	HAL_MCR_WR(prAdapter, CONN_HIF_DMASHDL_SHDL_SET1(u4BaseAddr), 0xedcba987);
+}
+
 VOID fillUsbHifTxDesc(IN PUINT_8 * pDest, IN PUINT_16 pInfoBufLen)
 {
 	/*USB TX Descriptor (4 bytes)*/
