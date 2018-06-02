19e6a3b84c136d66988ba4baa8b725626e75197a
Owen Lee <owen.lee@mediatek.com>
2018-06-03 04:18:45 +0800

    [ALPS03684002] wlan: Fix coding style warning
    [Detail]
    Fix checkpatch warning and error in following files
    	include/mgmt/privacy.h
    	include/mgmt/rsn.h
    	include/mgmt/tdls.h
    	include/mgmt/roaming_fsm.h
    	include/mgmt/aa_fsm.h
    	include/mgmt/assoc.h
    	include/mgmt/auth.h
    	include/mgmt/rlm.h
    	include/mgmt/rlm_domain.h
    	include/mgmt/rlm_obss.h
    	include/mgmt/rlm_protection.h
    	include/mgmt/rlm_txpwr_init.h
    	include/mgmt/wapi.h
    	include/mgmt/ais_fsm.h
    	include/mgmt/bss.h
    	include/mgmt/mib.h
    	include/mgmt/rate.h
    	include/mgmt/wnm.h
    	mgmt/privacy.c
    	mgmt/tdls.c
    	mgmt/rsn.c
    	mgmt/tkip_mic.c
    	mgmt/roaming_fsm.c
    
    [Solution]
    Use aStyle and revise the implementation
    
    MTK-Commit-Id: b4adff3d493b0983981e46f99b1d9da69c7dd165
    
    Change-Id: I025757d6ccd0fea4a89fe6fa266dfcc35ff68d1b
    CR-Id: ALPS03684002
    Feature: Wi-Fi
    Signed-off-by: Owen Lee <owen.lee@mediatek.com>
    Reviewed-on: http://gerrit.mediatek.inc:8080/1265650
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Terry Wu <terry.wu@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: Tzufan Hsu <tzufan.hsu@mediatek.com>
    Test: srv_mspautosanity@mediatek.com
    (cherry picked from commit 024cee89e546f3b31255b4efd5acbecfbb5b7307)

--- include/mgmt/rate.h
+++ include/mgmt/rate.h
@@ -50,76 +50,83 @@
  *
  *****************************************************************************/
 /*
-** Id: //Department/DaVinci/BRANCHES/MT6620_WIFI_DRIVER_V2_3/include/mgmt/rate.h#1
-*/
+ * Id: //Department/DaVinci/BRANCHES/MT6620_WIFI_DRIVER_V2_3/
+ *							include/mgmt/rate.h#1
+ */
 
 /*! \file  rate.h
  *  \brief This file contains the rate utility function of
  *         IEEE 802.11 family for MediaTek 802.11 Wireless LAN Adapters.
  */
 
-
 #ifndef _RATE_H
 #define _RATE_H
 
 /*******************************************************************************
  *                         C O M P I L E R   F L A G S
- ********************************************************************************
+ *******************************************************************************
  */
 
 /*******************************************************************************
  *                    E X T E R N A L   R E F E R E N C E S
- ********************************************************************************
+ *******************************************************************************
  */
 
 /*******************************************************************************
  *                              C O N S T A N T S
- ********************************************************************************
+ *******************************************************************************
  */
 
 /*******************************************************************************
  *                         D A T A   T Y P E S
- ********************************************************************************
+ *******************************************************************************
  */
 
 /*******************************************************************************
  *                            P U B L I C   D A T A
- ********************************************************************************
+ *******************************************************************************
  */
 
 /*******************************************************************************
  *                           P R I V A T E   D A T A
- ********************************************************************************
+ *******************************************************************************
  */
 
 /*******************************************************************************
  *                                 M A C R O S
- ********************************************************************************
+ *******************************************************************************
  */
 
 /*******************************************************************************
  *                  F U N C T I O N   D E C L A R A T I O N S
- ********************************************************************************
+ *******************************************************************************
  */
 /*----------------------------------------------------------------------------*/
 /* Routines in rate.c                                                         */
 /*----------------------------------------------------------------------------*/
 void
-rateGetRateSetFromIEs(IN struct IE_SUPPORTED_RATE *prIeSupportedRate, IN struct IE_EXT_SUPPORTED_RATE *prIeExtSupportedRate, OUT
-		      uint16_t *pu2OperationalRateSet, OUT uint16_t *pu2BSSBasicRateSet,
+rateGetRateSetFromIEs(IN struct IE_SUPPORTED_RATE
+		      *prIeSupportedRate,
+		      IN struct IE_EXT_SUPPORTED_RATE *prIeExtSupportedRate, OUT
+		      uint16_t *pu2OperationalRateSet,
+		      OUT uint16_t *pu2BSSBasicRateSet,
 		      OUT u_int8_t *pfgIsUnknownBSSBasicRate);
 
 void
-rateGetDataRatesFromRateSet(IN uint16_t u2OperationalRateSet, IN uint16_t u2BSSBasicRateSet, OUT uint8_t *pucDataRates, OUT
+rateGetDataRatesFromRateSet(IN uint16_t
+			    u2OperationalRateSet, IN uint16_t u2BSSBasicRateSet,
+			    OUT uint8_t *pucDataRates, OUT
 			    uint8_t *pucDataRatesLen);
 
-u_int8_t rateGetHighestRateIndexFromRateSet(IN uint16_t u2RateSet, OUT uint8_t *pucHighestRateIndex);
+u_int8_t rateGetHighestRateIndexFromRateSet(
+	IN uint16_t u2RateSet, OUT uint8_t *pucHighestRateIndex);
 
-u_int8_t rateGetLowestRateIndexFromRateSet(IN uint16_t u2RateSet, OUT uint8_t *pucLowestRateIndex);
+u_int8_t rateGetLowestRateIndexFromRateSet(
+	IN uint16_t u2RateSet, OUT uint8_t *pucLowestRateIndex);
 
 /*******************************************************************************
  *                              F U N C T I O N S
- ********************************************************************************
+ *******************************************************************************
  */
 
 #endif /* _RATE_H */
