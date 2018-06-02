a0aedd006e799127a02119c5130fb28105d3b6dd
Simfex Chiu <simfex.chiu@mediatek.com>
2018-06-03 04:20:35 +0800

    [ALPS03714382] Wlan: Connac change ICAP flow
    [Detail]
    1. Mass RX data event missing causes ICAP data incomplete
    2. Packet buffer not enough to receive all ICAP data
    
    [Solution]
    1. Change 1 command and 256 RX event flow to
       1 command 1 flow (256 rounds)
    2. Change receive packets number from 192 to 256 for freerun
    
    MTK-Commit-Id: 2c5db30a36ab428a1490814d3315c896edb0ad48
    
    Change-Id: I7f55a96a5bf821f13107435bcdfc4c29cfa2a0b1
    Signed-off-by: Simfex Chiu <simfex.chiu@mediatek.com>
    CR-Id: ALPS03714382
    Feature: Wi-Fi Chipset Capability

--- os/linux/include/gl_qa_agent.h
+++ os/linux/include/gl_qa_agent.h
@@ -102,7 +102,7 @@ extern unsigned long long gConEmiSize;
 #define HQA_CHIP_ID_7668	0x7668
 
 /* (4096(Samples/Bank) * 6Banks * 3(IQSamples/Sample) * 32bits)/96bits */
-#define MAX_ICAP_IQ_DATA_CNT					(4096 * 6)
+#define MAX_ICAP_IQ_DATA_CNT					(4096 * 8)
 #define ICAP_EVENT_DATA_SAMPLE					256
 
 
