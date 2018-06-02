a59e8f32301373deca3fa1d51985585731c5d03c
Wayne-TW Chen <wayne-tw.chen@mediatek.com>
2018-06-03 05:09:09 +0800

    [ALPS03868198] wlan: Revise vendor roaming_capa
    [Detail]
    Revise mtk_cfg80211_vendor_get_roaming_capabilities.
    Make max blacklist and whitelist send to HAL seprately.
    
    MTK-Commit-Id: a8b72c9240553fe0082897c977c8a7b8e3b81e8e
    
    Change-Id: I5c82a92fd1e1f150c88c76357650b3789a1e487c
    Signed-off-by: Wayne-TW Chen <wayne-tw.chen@mediatek.com>
    CR-Id: ALPS03868198
    Feature: Wi-Fi Chipset Capability
    (cherry picked from commit 8c86a31392e1de2e21b7e3e21ccb3803af6a88d5)

--- os/linux/gl_vendor.c
+++ os/linux/gl_vendor.c
@@ -367,14 +367,17 @@ int mtk_cfg80211_vendor_get_roaming_capabilities(struct wiphy *wiphy,
 		"Get roaming capabilities: max black/whitelist=%d/%d",
 		maxNumOfList[0], maxNumOfList[1]);
 
-	skb = cfg80211_vendor_cmd_alloc_reply_skb(wiphy, sizeof(maxNumOfList));
+	skb = cfg80211_vendor_cmd_alloc_reply_skb(wiphy, sizeof(uint32_t) * 2);
 	if (!skb) {
 		DBGLOG(REQ, ERROR, "Allocate skb failed\n");
 		return -ENOMEM;
 	}
 
 	if (unlikely(nla_put(skb, WIFI_ATTRIBUTE_ROAMING_CAPABILITIES,
-				sizeof(maxNumOfList), maxNumOfList) < 0))
+				sizeof(uint32_t), &maxNumOfList[0]) < 0))
+		goto nla_put_failure;
+	if (unlikely(nla_put(skb, WIFI_ATTRIBUTE_ROAMING_CAPABILITIES,
+				sizeof(uint32_t), &maxNumOfList[1]) < 0))
 		goto nla_put_failure;
 
 	return cfg80211_vendor_cmd_reply(skb);
