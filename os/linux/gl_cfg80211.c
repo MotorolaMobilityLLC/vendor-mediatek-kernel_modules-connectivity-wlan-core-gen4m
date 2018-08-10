68403e3bc8b79d32ffd9c96576c88dfc4fa8fbfe
mtk06658 <tzufan.hsu@mediatek.com>
2018-08-10 23:55:09 +0800

    [ALPS04052562] wlan: PMF modification
    [Detail]
    our DUT is configured to MFP DISABLD, association process failed
    since DUT bring an unexpected RSN IE with group mgmt cipher = 0
    Root cause: driver change mfp parameter from DISABLED to OPTIONAL
    and fill unexpected group mgmt cipher when
    peer device is MFP OPTIONAL since upper layer can only bring
    mfp parameter = DISABLE/REQUIRED to driver, this mfp mode change is
    for passing PMF certification
    
    our DUT is configured to MFP OPTIONAL, association process failed
    (when tested with Samsung S8 phone.
    other device may have the same behavior)
    since DUT bring RSN IE with unnecessary field PMKID Count field = 0
    (from BORA00017389)
    
    [Solution]
    For issue1: when upper layer bring mfp parameter to driver,
    driver change it from DISABLED to OPTIONAL
    if MFPC = 1 in desired RSN IE from upper layer.
    Then driver dont need to change mfp parameter
    depends on peers mfp configuration ever.
    
    For issue2: Follow supplicant flow to fill PMKID Count field = 0
    only when Group Management Cipher field need to be filled
    
    [UT]
    Passed 9 PMF test cases:
    (DUT STA is PMF DISALED/OPTAIONAL/REQUIRED) x
    (Peer AP is PMF DISALED/OPTAIONAL/REQUIRED)
    
    MTK-Commit-Id: 51b541e874a0d3a66f02632f98b6bf011f9c0ed8
    
    Issue1: When peer device is configured to MFP OPTIONAL and
    Issue2: When peer device is configured to MFP DISABLED and
    Change-Id: Ib775d7503c6d222638a12fb6a819d14a2f063412
    Signed-off-by: Tzufan Hsu <Tzufan.Hsu@mediatek.com>
    CR-Id: ALPS04052562
    Feature: Wifi Driver
    Reviewed-on: http://gerrit.mediatek.inc:8080/1595551
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Tested-by: Tzufan Hsu <tzufan.hsu@mediatek.com>
    Reviewed-by: ShaominChiu <shaomin.chiu@mediatek.com>
    Reviewed-by: Weihung Su <wh.su@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Zeno Yang <zeno.yang@mediatek.com>
    Reviewed-by: Owen Lee <owen.lee@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com
    Signed-off-by: Owen Lee <owen.lee@mediatek.com>

--- os/linux/gl_cfg80211.c
+++ os/linux/gl_cfg80211.c
@@ -1156,32 +1156,22 @@ int mtk_cfg80211_connect(struct wiphy *wiphy,
 
 	/* after set operation mode, key table are cleared */
 
-	/* reset wpa info */
-	prGlueInfo->rWpaInfo.u4WpaVersion = IW_AUTH_WPA_VERSION_DISABLED;
-	prGlueInfo->rWpaInfo.u4KeyMgmt = 0;
-	prGlueInfo->rWpaInfo.u4CipherGroup = IW_AUTH_CIPHER_NONE;
-	prGlueInfo->rWpaInfo.u4CipherPairwise = IW_AUTH_CIPHER_NONE;
-	prGlueInfo->rWpaInfo.u4AuthAlg = IW_AUTH_ALG_OPEN_SYSTEM;
 #if CFG_SUPPORT_REPLAY_DETECTION
 	/* reset Detect replay information */
 	prDetRplyInfo = &prGlueInfo->prDetRplyInfo;
 	kalMemZero(prDetRplyInfo, sizeof(struct GL_DETECT_REPLAY_INFO));
 #endif
 
+	/* <1> Reset WPA info */
+	prGlueInfo->rWpaInfo.u4WpaVersion = IW_AUTH_WPA_VERSION_DISABLED;
+	prGlueInfo->rWpaInfo.u4KeyMgmt = 0;
+	prGlueInfo->rWpaInfo.u4CipherGroup = IW_AUTH_CIPHER_NONE;
+	prGlueInfo->rWpaInfo.u4CipherPairwise = IW_AUTH_CIPHER_NONE;
+	prGlueInfo->rWpaInfo.u4AuthAlg = IW_AUTH_ALG_OPEN_SYSTEM;
+	prGlueInfo->rWpaInfo.fgPrivacyInvoke = FALSE;
 #if CFG_SUPPORT_802_11W
 	prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_DISABLED;
-	switch (sme->mfp) {
-	case NL80211_MFP_NO:
-		prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_DISABLED;
-		break;
-	case NL80211_MFP_REQUIRED:
-		prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_REQUIRED;
-		break;
-	default:
-		prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_DISABLED;
-		break;
-	}
-	/* DBGLOG(SCN, INFO, ("MFP=%d\n", prGlueInfo->rWpaInfo.u4Mfp)); */
+	prGlueInfo->rWpaInfo.ucRSNMfpCap = RSN_AUTH_MFP_DISABLED;
 #endif
 
 	if (sme->crypto.wpa_versions & NL80211_WPA_VERSION_1)
@@ -1443,6 +1433,7 @@ int mtk_cfg80211_connect(struct wiphy *wiphy,
 
 			if (rsnParseRsnIE(prGlueInfo->prAdapter,
 			    (struct RSN_INFO_ELEM *)prDesiredIE, &rRsnInfo)) {
+#if CFG_SUPPORT_802_11W
 				if (rRsnInfo.u2RsnCap & ELEM_WPA_CAP_MFPC) {
 					prGlueInfo->rWpaInfo.ucRSNMfpCap =
 							RSN_AUTH_MFP_OPTIONAL;
@@ -1452,7 +1443,9 @@ int mtk_cfg80211_connect(struct wiphy *wiphy,
 						.ucRSNMfpCap =
 							RSN_AUTH_MFP_REQUIRED;
 				} else
-					prGlueInfo->rWpaInfo.ucRSNMfpCap = 0;
+					prGlueInfo->rWpaInfo.ucRSNMfpCap =
+							RSN_AUTH_MFP_DISABLED;
+#endif
 			}
 		}
 
@@ -1493,6 +1486,36 @@ int mtk_cfg80211_connect(struct wiphy *wiphy,
 		kalMemZero(&prGlueInfo->aucWSCAssocInfoIE, 200);
 		prGlueInfo->u2WSCAssocInfoIELen = 0;
 	}
+
+	/* Fill WPA info - mfp setting */
+	/* Must put after paring RSNE from upper layer
+	* for prGlueInfo->rWpaInfo.ucRSNMfpCap assignment
+	*/
+#if CFG_SUPPORT_802_11W
+	switch (sme->mfp) {
+	case NL80211_MFP_NO:
+		prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_DISABLED;
+		/* Change Mfp parameter from DISABLED to OPTIONAL
+		* if upper layer set MFPC = 1 in RSNE
+		* since upper layer can't bring MFP OPTIONAL information
+		* to driver by sme->mfp
+		*/
+		if (prGlueInfo->rWpaInfo.ucRSNMfpCap == RSN_AUTH_MFP_OPTIONAL)
+			prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_OPTIONAL;
+		else if (prGlueInfo->rWpaInfo.ucRSNMfpCap ==
+					RSN_AUTH_MFP_REQUIRED)
+			DBGLOG(REQ, WARN,
+				"mfp parameter(DISABLED) conflict with mfp cap(REQUIRED)\n");
+		break;
+	case NL80211_MFP_REQUIRED:
+		prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_REQUIRED;
+		break;
+	default:
+		prGlueInfo->rWpaInfo.u4Mfp = IW_AUTH_MFP_DISABLED;
+		break;
+	}
+	/* DBGLOG(REQ, INFO, ("MFP=%d\n", prGlueInfo->rWpaInfo.u4Mfp)); */
+#endif
 
 	rStatus = kalIoctl(prGlueInfo, wlanoidSetAuthMode, &eAuthMode,
 			sizeof(eAuthMode), FALSE, FALSE, FALSE, &u4BufLen);
