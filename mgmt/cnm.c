d44c10885fe1f98a41868031815c58b8c533273a
Ben Lai <ben.lai@mediatek.com>
2018-10-01 12:48:41 +0800

    [ALPS04123906] wlan: Remove error handling for add key command
    Remove handling for add key command.
    
    MTK-Commit-Id: f4098bedc231f93be13176e0371ad056c2e0a7f3
    
    Change-Id: I63c2cc854b073319bf230c2c6f43aea6052246da
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS04123906
    Feature: MTBF(*)

--- mgmt/cnm.c
+++ mgmt/cnm.c
@@ -1825,8 +1825,6 @@ struct BSS_INFO *cnmGetBssInfoAndInit(struct ADAPTER *prAdapter,
 		prBssInfo->fgIsPNOEnable = FALSE;
 		prBssInfo->fgIsNetRequestInActive = FALSE;
 #endif
-		/* init add key action */
-		prBssInfo->eKeyAction = SEC_TX_KEY_COMMAND;
 
 		/* initialize wlan id and status for keys */
 		prBssInfo->ucBMCWlanIndex = WTBL_RESERVED_ENTRY;
@@ -1914,8 +1912,6 @@ struct BSS_INFO *cnmGetBssInfoAndInit(struct ADAPTER *prAdapter,
 		prBssInfo->fgIsPNOEnable = FALSE;
 		prBssInfo->fgIsNetRequestInActive = FALSE;
 #endif
-		/* init add key action */
-		prBssInfo->eKeyAction = SEC_TX_KEY_COMMAND;
 
 		/* initialize wlan id and status for keys */
 		prBssInfo->ucBMCWlanIndex = WTBL_RESERVED_ENTRY;
