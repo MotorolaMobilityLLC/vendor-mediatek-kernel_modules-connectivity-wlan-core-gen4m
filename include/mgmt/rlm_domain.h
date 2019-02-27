5748f0063cb3b1d98e4bd2af9c13de58e0ff4349
Li-Wei Chen <li-wei.chen@mediatek.com>
2019-02-27 22:13:31 +0800

    commit I8178a1e4559 ([ALPS02457833] WIFI:Revert change)
    [Detail]
    Revert change .
    Change supported 5g channels list
    
    MTK-Commit-Id: b681db95038efda6f2b815611e239260d422859a
    
    CR-Id: ALPS02457833
    Signed-off-by: Li-Wei Chen <li-wei.chen@mediatek.com>
    Feature: [Android Default] Wi-Fi Driver for SoC
    Change-Id: I8178a1e45593562cbc9a6e0ed4aae4d57476f7c1

--- include/mgmt/rlm_domain.h
+++ include/mgmt/rlm_domain.h
@@ -356,7 +356,7 @@
 #endif
 
 #if (CFG_SUPPORT_SINGLE_SKU == 1)
-#define MAX_SUPPORTED_CH_COUNT MAX_CHN_NUM
+#define MAX_SUPPORTED_CH_COUNT (53) /*ARRAY_SIZE(mtk_2ghz_channels) + ARRAY_SIZE(mtk_5ghz_channels)*/
 #define REG_RULE_LIGHT(start, end, bw, reg_flags) REG_RULE(start, end, bw, 0, 0, reg_flags)
 #endif
 /*******************************************************************************
