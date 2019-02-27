ccf6f7969449cf644f3a8ebb54616c6698c0021c
Jimbo Chi <jimbo.chi@mediatek.com>
2019-02-28 00:13:20 +0800

    [ALPS03805770] wlan:when off, not to handle fw event interrupt
    [Detail]
    When Wi-Fi off polling ready bit, driver will handle pending fw event
    interrupt. It will cause Wi-Fi off timeout.
    
    [Solution]
    Due to PCIE or AXI interface has sleep protection mechanism,
    its not necessary for driver to handle fw event
    interrupt when polling ready bit.
    But when chip interface is USB or SDIO, driver still need to
    handle fw event interrupt.
    
    MTK-Commit-Id: 56179cbcdcafcb4537819040dcf067fa0d68c84b
    
    Change-Id: Ie88f44e11223904bea6520c29b666b13d047281d
    Signed-off-by: Jimbo Chi <jimbo.chi@mediatek.com>
    CR-Id: ALPS03805770
    Feature: Wi-Fi AOSP
    (cherry picked from commit c7dd157abfe385979b9050e9383340944d9d258d)

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -876,8 +876,10 @@ uint32_t wlanCheckWifiFunc(IN struct ADAPTER *prAdapter, IN u_int8_t fgRdyChk)
 		else {
 			HAL_WIFI_FUNC_OFF_CHECK(prAdapter, ready_bits/*WIFI_FUNC_READY_BITS*/, &fgResult);
 
+#if defined(_HIF_USB) || defined(_HIF_SDIO)
 			if (nicProcessIST(prAdapter) != WLAN_STATUS_NOT_INDICATING)
 				DBGLOG(INIT, INFO, "Handle pending interrupt\n");
+#endif /* _HIF_USB or _HIF_SDIO */
 		}
 		u4CurTime = kalGetTimeTick();
 
