9baf40a5fda4954fca1ae1dd7fd36bce57bd95ee
Desmond Lin <desmond.lin@mediatek.com>
2019-02-28 00:50:57 +0800

    [ALPS04057261] perf: Fix malloc with GFP_KERNEL in interrupt context
    [Description]
    Fix calling malloc with flag GFP_KERNEL may in interrupt context.
    That could happened when CFG_TX_DIRECT_USB=1 or CFG_RX_DIRECT_USB=1
    
    (from WCNCR00147619)
    
    MTK-Commit-Id: c39c587cb6a4dfddcaaba5c3e498e20c10130c43
    
    Change-Id: I370789f4e8d1cf421d0d836369325e456014a225
    Signed-off-by: Desmond Lin <desmond.lin@mediatek.com>
    Feature: perf
    Reviewed-on: http://gerrit.mediatek.inc:8080/1606598
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Black Chen <black-ch.chen@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com
    Signed-off-by: Black Chen <black-ch.chen@mediatek.com>
    CR-Id: ALPS04057261
    Feature: [Module]Wi-Fi Driver

--- nic/nic_rx.c
+++ nic/nic_rx.c
@@ -3688,12 +3688,10 @@ void nicRxReturnRFB(IN struct ADAPTER *prAdapter,
 	}
 	KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_RX_FREE_QUE);
 
-#if CFG_USB_RX_HANDLE_IN_HIF_THREAD
 	/* Trigger Rx if there are free SwRfb */
 	if (halIsPendingRx(prAdapter)
 	    && (prRxCtrl->rFreeSwRfbList.u4NumElem > 0))
 		kalSetIntEvent(prAdapter->prGlueInfo);
-#endif
 }				/* end of nicRxReturnRFB() */
 
 /*----------------------------------------------------------------------------*/
