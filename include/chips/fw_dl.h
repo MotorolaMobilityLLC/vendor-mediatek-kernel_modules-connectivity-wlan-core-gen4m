8d5d3b4da7c9fee1ac00ef97d12c7f5883337022
Holiday Hao <holiday.hao@mediatek.com>
2019-02-27 23:34:28 +0800

    [ALPS03428288] CONNAC: fix build error
    [Detail]
    1. Add EMI compiler option
    2. 6632 need CFG_MTK_ANDROID_WMT = 1
    
    MTK-Commit-Id: f336f8a4759d647e55c7621f96286edcd008321c
    
    Change-Id: Ifdaa4f552f1917c0519444ddeb4cfba886bfcbe6
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    CR-Id: ALPS03428288
    Feature: WIFI

--- include/chips/fw_dl.h
+++ include/chips/fw_dl.h
@@ -82,7 +82,7 @@
 #define COMPRESSION_OPTION_MASK     BIT(4)
 #endif
 
-#if CFG_MTK_ANDROID_WMT
+#if CFG_MTK_ANDROID_EMI
 #define WIFI_EMI_ADDR_MASK     0xFFFFFF
 extern phys_addr_t gConEmiPhyBase;
 extern unsigned long long gConEmiSize;
