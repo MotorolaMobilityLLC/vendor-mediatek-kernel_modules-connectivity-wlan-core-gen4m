b1c9f1ee16033c272c2de41e3e49c4da188277f1
Dom Lin <dom.lin@mediatek.com>
2019-02-27 23:32:35 +0800

    [ALPS03684070] wlan: Fix CNM and Scan checkpatch warning and error
    [Detail]
    Fix all checkpatch warning and error in following files
        - include/mgmt/cnm_mem.h
        - include/mgmt/cnm_timer.h
        - include/mgmt/scan.h
        - include/mgmt/wlan_typedef.h
        - mgmt/cnm_timer.c
        - mgmt/scan.c
        - mgmt/scan_fsm.c
    
    MTK-Commit-Id: a2e419ce3411a77a36d64063360c9849648fdbc0
    
    Change-Id: I19aa00742d97e48a7a618e0b2c41c80e28a53925
    CR-Id: ALPS03684070
    Feature: Wi-Fi
    Signed-off-by: Dom Lin <dom.lin@mediatek.com>
    Reviewed-on: http://gerrit.mediatek.inc:8080/1247423
    Reviewed-by: ShaominChiu <shaomin.chiu@mediatek.com>
    Tested-by: ShaominChiu <shaomin.chiu@mediatek.com>
    Reviewed-by: Eva Wang <eva.wang@mediatek.com>
    Reviewed-by: Jimbo Chi <jimbo.chi@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com
    Reviewed-by: YZ Huang <yz.huang@mediatek.com>
    CheckPatch: YZ Huang <yz.huang@mediatek.com>

--- include/mgmt/cnm.h
+++ include/mgmt/cnm.h
@@ -50,39 +50,40 @@
  *
  *****************************************************************************/
 /*
-** Id: //Department/DaVinci/BRANCHES/MT6620_WIFI_DRIVER_V2_3/include/mgmt/cnm.h#1
-*/
+ * Id: //Department/DaVinci/BRANCHES/MT6620_WIFI_DRIVER_V2_3
+ *     /include/mgmt/cnm.h#1
+ */
 
 /*! \file   "cnm.h"
-*    \brief
-*/
+ *    \brief
+ */
 
 
 #ifndef _CNM_H
 #define _CNM_H
 
 /*******************************************************************************
-*                         C O M P I L E R   F L A G S
-********************************************************************************
-*/
+ *                         C O M P I L E R   F L A G S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                    E X T E R N A L   R E F E R E N C E S
-********************************************************************************
-*/
+ *                    E X T E R N A L   R E F E R E N C E S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                              C O N S T A N T S
-********************************************************************************
-*/
+ *                              C O N S T A N T S
+ *******************************************************************************
+ */
 #if CFG_SUPPORT_DBDC
 #define DBDC_5G_WMM_INDEX	0
 #define DBDC_2G_WMM_INDEX	1
 #endif
 /*******************************************************************************
-*                             D A T A   T Y P E S
-********************************************************************************
-*/
+ *                             D A T A   T Y P E S
+ *******************************************************************************
+ */
 
 enum ENUM_CH_REQ_TYPE {
 	CH_REQ_TYPE_JOIN,
@@ -102,7 +103,10 @@ struct MSG_CH_REQ {
 	uint8_t ucPrimaryChannel;
 	enum ENUM_CHNL_EXT eRfSco;
 	enum ENUM_BAND eRfBand;
-	enum ENUM_CHANNEL_WIDTH eRfChannelWidth;	/* To support 80/160MHz bandwidth */
+
+	/* To support 80/160MHz bandwidth */
+	enum ENUM_CHANNEL_WIDTH eRfChannelWidth;
+
 	uint8_t ucRfCenterFreqSeg1;	/* To support 80/160MHz bandwidth */
 	uint8_t ucRfCenterFreqSeg2;	/* To support 80/160MHz bandwidth */
 	enum ENUM_CH_REQ_TYPE eReqType;
@@ -124,7 +128,10 @@ struct MSG_CH_GRANT {
 	uint8_t ucPrimaryChannel;
 	enum ENUM_CHNL_EXT eRfSco;
 	enum ENUM_BAND eRfBand;
-	enum ENUM_CHANNEL_WIDTH eRfChannelWidth;	/* To support 80/160MHz bandwidth */
+
+	/* To support 80/160MHz bandwidth */
+	enum ENUM_CHANNEL_WIDTH eRfChannelWidth;
+
 	uint8_t ucRfCenterFreqSeg1;	/* To support 80/160MHz bandwidth */
 	uint8_t ucRfCenterFreqSeg2;	/* To support 80/160MHz bandwidth */
 	enum ENUM_CH_REQ_TYPE eReqType;
@@ -139,7 +146,10 @@ struct MSG_CH_REOCVER {
 	uint8_t ucPrimaryChannel;
 	enum ENUM_CHNL_EXT eRfSco;
 	enum ENUM_BAND eRfBand;
-	enum ENUM_CHANNEL_WIDTH eRfChannelWidth;	/* To support 80/160MHz bandwidth */
+
+	/*  To support 80/160MHz bandwidth */
+	enum ENUM_CHANNEL_WIDTH eRfChannelWidth;
+
 	uint8_t ucRfCenterFreqSeg1;	/* To support 80/160MHz bandwidth */
 	uint8_t ucRfCenterFreqSeg2;	/* To support 80/160MHz bandwidth */
 	enum ENUM_CH_REQ_TYPE eReqType;
@@ -154,8 +164,8 @@ struct CNM_INFO {
 #if CFG_ENABLE_WIFI_DIRECT
 /* Moved from p2p_fsm.h */
 struct DEVICE_TYPE {
-	uint16_t u2CategoryId;	/* Category ID */
-	uint8_t aucOui[4];	/* OUI */
+	uint16_t u2CategoryId;		/* Category ID */
+	uint8_t aucOui[4];		/* OUI */
 	uint16_t u2SubCategoryId;	/* Sub Category ID */
 } __KAL_ATTRIB_PACKED__;
 #endif
@@ -168,18 +178,34 @@ struct CNM_DBDC_CAP {
 };
 
 enum ENUM_CNM_DBDC_MODE {
-	ENUM_DBDC_MODE_DISABLED,	/* A/G traffic separate by WMM, but both TRX on band 0, CANNOT enable DBDC */
-	ENUM_DBDC_MODE_STATIC,		/* A/G traffic separate by WMM, WMM0/1 TRX on band 0/1, CANNOT disable DBDC */
-	ENUM_DBDC_MODE_DYNAMIC,		/* Automatically enable/disable DBDC, setting just like static/disable mode */
+	/* A/G traffic separate by WMM, but both
+	 * TRX on band 0, CANNOT enable DBDC
+	 */
+	ENUM_DBDC_MODE_DISABLED,
+
+	/* A/G traffic separate by WMM, WMM0/1
+	 * TRX on band 0/1, CANNOT disable DBDC
+	 */
+	ENUM_DBDC_MODE_STATIC,
+
+	/* Automatically enable/disable DBDC,
+	 * setting just like static/disable mode
+	 */
+	ENUM_DBDC_MODE_DYNAMIC,
+
 	ENUM_DBDC_MODE_NUM
 };
 
 enum ENUM_CNM_DBDC_SWITCH_MECHANISM { /* When DBDC available in dynamic DBDC */
-	ENUM_DBDC_SWITCH_MECHANISM_LATENCY_MODE,		/* Switch to DBDC when available (less latency) */
-	ENUM_DBDC_SWITCH_MECHANISM_THROUGHPUT_MODE,	/* Switch to DBDC when DBDC T-put > MCC T-put */
+	/* Switch to DBDC when available (less latency) */
+	ENUM_DBDC_SWITCH_MECHANISM_LATENCY_MODE,
+
+	/* Switch to DBDC when DBDC T-put > MCC T-put */
+	ENUM_DBDC_SWITCH_MECHANISM_THROUGHPUT_MODE,
+
 	ENUM_DBDC_SWITCH_MECHANISM_NUM
 };
-#endif /*CFG_SUPPORT_DBDC*/
+#endif	/* CFG_SUPPORT_DBDC */
 
 enum ENUM_CNM_NETWORK_TYPE_T {
 	ENUM_CNM_NETWORK_TYPE_OTHER,
@@ -190,53 +216,60 @@ enum ENUM_CNM_NETWORK_TYPE_T {
 };
 
 /*******************************************************************************
-*                            P U B L I C   D A T A
-********************************************************************************
-*/
+ *                            P U B L I C   D A T A
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                           P R I V A T E   D A T A
-********************************************************************************
-*/
+ *                           P R I V A T E   D A T A
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                                 M A C R O S
-********************************************************************************
-*/
+ *                                 M A C R O S
+ *******************************************************************************
+ */
 #define CNM_CH_GRANTED_FOR_BSS(_prAdapter, _ucBssIndex) \
 	((_prAdapter)->rCnmInfo.fgChGranted && \
 	 (_prAdapter)->rCnmInfo.ucBssIndex == (_ucBssIndex))
 
 #define IS_CONNECTION_NSS2(prBssInfo, prStaRec) \
-	((((prBssInfo)->ucNss > 1) && ((prStaRec)->aucRxMcsBitmask[1] != 0x00) && \
-	(((prStaRec)->u2HtCapInfo & HT_CAP_INFO_SM_POWER_SAVE) != 0)) || \
-	(((prBssInfo)->ucNss > 1) && ((((prStaRec)->u2VhtRxMcsMap & BITS(2, 3)) >> 2) != BITS(0, 1)) && \
-	((((prStaRec)->ucVhtOpMode & VHT_OP_MODE_RX_NSS) >> VHT_OP_MODE_RX_NSS_OFFSET) > 0)))
+	((((prBssInfo)->ucNss > 1) && ((prStaRec)->aucRxMcsBitmask[1] != 0x00) \
+	&& (((prStaRec)->u2HtCapInfo & HT_CAP_INFO_SM_POWER_SAVE) != 0)) || \
+	(((prBssInfo)->ucNss > 1) && ((((prStaRec)->u2VhtRxMcsMap \
+	& BITS(2, 3)) >> 2) != BITS(0, 1)) && ((((prStaRec)->ucVhtOpMode \
+	& VHT_OP_MODE_RX_NSS) >> VHT_OP_MODE_RX_NSS_OFFSET) > 0)))
 
 /*******************************************************************************
-*                   F U N C T I O N   D E C L A R A T I O N S
-********************************************************************************
-*/
+ *                   F U N C T I O N   D E C L A R A T I O N S
+ *******************************************************************************
+ */
 void cnmInit(struct ADAPTER *prAdapter);
 
 void cnmUninit(struct ADAPTER *prAdapter);
 
-void cnmChMngrRequestPrivilege(struct ADAPTER *prAdapter, struct MSG_HDR *prMsgHdr);
+void cnmChMngrRequestPrivilege(struct ADAPTER *prAdapter,
+	struct MSG_HDR *prMsgHdr);
 
-void cnmChMngrAbortPrivilege(struct ADAPTER *prAdapter, struct MSG_HDR *prMsgHdr);
+void cnmChMngrAbortPrivilege(struct ADAPTER *prAdapter,
+	struct MSG_HDR *prMsgHdr);
 
-void cnmChMngrHandleChEvent(struct ADAPTER *prAdapter, struct WIFI_EVENT *prEvent);
+void cnmChMngrHandleChEvent(struct ADAPTER *prAdapter,
+	struct WIFI_EVENT *prEvent);
 
 #if (CFG_SUPPORT_DFS_MASTER == 1)
-void cnmRadarDetectEvent(struct ADAPTER *prAdapter, struct WIFI_EVENT *prEvent);
+void cnmRadarDetectEvent(struct ADAPTER *prAdapter,
+	struct WIFI_EVENT *prEvent);
 
-void cnmCsaDoneEvent(struct ADAPTER *prAdapter, struct WIFI_EVENT *prEvent);
+void cnmCsaDoneEvent(struct ADAPTER *prAdapter,
+	struct WIFI_EVENT *prEvent);
 #endif
 
-u_int8_t
-cnmPreferredChannel(struct ADAPTER *prAdapter, enum ENUM_BAND *prBand, uint8_t *pucPrimaryChannel, enum ENUM_CHNL_EXT *prBssSCO);
+u_int8_t cnmPreferredChannel(struct ADAPTER *prAdapter, enum ENUM_BAND *prBand,
+	uint8_t *pucPrimaryChannel, enum ENUM_CHNL_EXT *prBssSCO);
 
-u_int8_t cnmAisInfraChannelFixed(struct ADAPTER *prAdapter, enum ENUM_BAND *prBand, uint8_t *pucPrimaryChannel);
+u_int8_t cnmAisInfraChannelFixed(struct ADAPTER *prAdapter,
+	enum ENUM_BAND *prBand, uint8_t *pucPrimaryChannel);
 
 void cnmAisInfraConnectNotify(struct ADAPTER *prAdapter);
 
@@ -254,11 +287,13 @@ uint8_t cnmGetBssMaxBw(struct ADAPTER *prAdapter, uint8_t ucBssIndex);
 
 uint8_t cnmGetBssMaxBwToChnlBW(struct ADAPTER *prAdapter, uint8_t ucBssIndex);
 
-struct BSS_INFO *cnmGetBssInfoAndInit(struct ADAPTER *prAdapter, enum ENUM_NETWORK_TYPE eNetworkType, u_int8_t fgIsP2pDevice);
+struct BSS_INFO *cnmGetBssInfoAndInit(struct ADAPTER *prAdapter,
+	enum ENUM_NETWORK_TYPE eNetworkType, u_int8_t fgIsP2pDevice);
 
 void cnmFreeBssInfo(struct ADAPTER *prAdapter, struct BSS_INFO *prBssInfo);
 #if CFG_SUPPORT_CHNL_CONFLICT_REVISE
-u_int8_t cnmAisDetectP2PChannel(struct ADAPTER *prAdapter, enum ENUM_BAND *prBand, uint8_t *pucPrimaryChannel);
+u_int8_t cnmAisDetectP2PChannel(struct ADAPTER *prAdapter,
+	enum ENUM_BAND *prBand, uint8_t *pucPrimaryChannel);
 #endif
 
 #if CFG_SUPPORT_DBDC
@@ -266,42 +301,45 @@ void cnmInitDbdcSetting(IN struct ADAPTER *prAdapter);
 
 void cnmDbdcOpModeChangeDoneCallback(
 	IN struct ADAPTER *prAdapter,
-	IN uint8_t		ucBssIndex,
-	IN u_int8_t		fgSuccess);
+	IN uint8_t ucBssIndex,
+	IN u_int8_t fgSuccess);
 
 void cnmUpdateDbdcSetting(IN struct ADAPTER *prAdapter, IN u_int8_t fgDbdcEn);
 
 void cnmGetDbdcCapability(
 	IN struct ADAPTER *prAdapter,
-	IN uint8_t				ucBssIndex,
-	IN enum ENUM_BAND			eRfBand,
-	IN uint8_t				ucPrimaryChannel,
-	IN uint8_t				ucNss,
+	IN uint8_t ucBssIndex,
+	IN enum ENUM_BAND eRfBand,
+	IN uint8_t ucPrimaryChannel,
+	IN uint8_t ucNss,
 	OUT struct CNM_DBDC_CAP *prDbdcCap
 );
 
 uint8_t cnmGetDbdcBwCapability(
 	struct ADAPTER *prAdapter,
-	uint8_t		ucBssIndex
+	uint8_t ucBssIndex
 );
 
 void cnmDbdcEnableDecision(
 	IN struct ADAPTER *prAdapter,
-	IN uint8_t		ucChangedBssIndex,
-	IN enum ENUM_BAND	eRfBand
+	IN uint8_t ucChangedBssIndex,
+	IN enum ENUM_BAND eRfBand
 );
 
-void cnmDbdcDisableDecision(IN struct ADAPTER *prAdapter,	IN uint8_t ucChangedBssIndex);
-void cnmDbdcGuardTimerCallback(IN struct ADAPTER *prAdapter, IN unsigned long plParamPtr);
-void cnmDbdcEventHwSwitchDone(IN struct ADAPTER *prAdapter, IN struct WIFI_EVENT *prEvent);
+void cnmDbdcDisableDecision(IN struct ADAPTER *prAdapter,
+	IN uint8_t ucChangedBssIndex);
+void cnmDbdcGuardTimerCallback(IN struct ADAPTER *prAdapter,
+	IN unsigned long plParamPtr);
+void cnmDbdcEventHwSwitchDone(IN struct ADAPTER *prAdapter,
+	IN struct WIFI_EVENT *prEvent);
 #endif /*CFG_SUPPORT_DBDC*/
 
 enum ENUM_CNM_NETWORK_TYPE_T cnmGetBssNetworkType(struct BSS_INFO *prBssInfo);
 
 /*******************************************************************************
-*                              F U N C T I O N S
-********************************************************************************
-*/
+ *                              F U N C T I O N S
+ *******************************************************************************
+ */
 #ifndef _lint
 /* We don't have to call following function to inspect the data structure.
  * It will check automatically while at compile time.
@@ -310,22 +348,37 @@ enum ENUM_CNM_NETWORK_TYPE_T cnmGetBssNetworkType(struct BSS_INFO *prBssInfo);
  */
 static __KAL_INLINE__ void cnmMsgDataTypeCheck(void)
 {
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, rMsgHdr) == 0);
+	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(
+		struct MSG_CH_GRANT, rMsgHdr)
+			== 0);
 
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, rMsgHdr) == OFFSET_OF(struct MSG_CH_REOCVER, rMsgHdr));
+	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(
+		struct MSG_CH_GRANT, rMsgHdr)
+			== OFFSET_OF(struct MSG_CH_REOCVER, rMsgHdr));
 
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, ucBssIndex) == OFFSET_OF(struct MSG_CH_REOCVER, ucBssIndex));
+	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(
+		struct MSG_CH_GRANT, ucBssIndex)
+			== OFFSET_OF(struct MSG_CH_REOCVER, ucBssIndex));
 
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, ucTokenID) == OFFSET_OF(struct MSG_CH_REOCVER, ucTokenID));
+	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(
+		struct MSG_CH_GRANT, ucTokenID)
+			== OFFSET_OF(struct MSG_CH_REOCVER, ucTokenID));
 
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, ucPrimaryChannel) ==
-				      OFFSET_OF(struct MSG_CH_REOCVER, ucPrimaryChannel));
+	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(
+		struct MSG_CH_GRANT, ucPrimaryChannel)
+			== OFFSET_OF(struct MSG_CH_REOCVER, ucPrimaryChannel));
 
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, eRfSco) == OFFSET_OF(struct MSG_CH_REOCVER, eRfSco));
+	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(
+		struct MSG_CH_GRANT, eRfSco)
+			== OFFSET_OF(struct MSG_CH_REOCVER, eRfSco));
 
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, eRfBand) == OFFSET_OF(struct MSG_CH_REOCVER, eRfBand));
+	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(
+		struct MSG_CH_GRANT, eRfBand)
+			== OFFSET_OF(struct MSG_CH_REOCVER, eRfBand));
 
-	DATA_STRUCT_INSPECTING_ASSERT(OFFSET_OF(struct MSG_CH_GRANT, eReqType) == OFFSET_OF(struct MSG_CH_REOCVER, eReqType));
+	DATA_STRUCT_INSPECTING_ASSERT(
+		OFFSET_OF(struct MSG_CH_GRANT, eReqType)
+			== OFFSET_OF(struct MSG_CH_REOCVER, eReqType));
 }
 #endif /* _lint */
 
