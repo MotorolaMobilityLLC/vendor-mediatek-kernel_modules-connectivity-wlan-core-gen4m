f3a64be0a28c79c6d88aae0647efe9335d89e9f0
Chun-Wei Tseng <chun-wei.tseng@mediatek.com>
2019-02-28 01:12:22 +0800

    [ALPS04183477] wlan: Fix EAPOL may queue in driver
    If EAPOL frame received before network active,
    the frame could be queue in driver because the
    TXAllow is false.
    
    Remove skip dequeue if sta_rec is invalid.
    
    MTK-Commit-Id: 7c85e82e2935aca6058c21e8bb8f23b19bf24b37
    
    Change-Id: I272b7b74b702f06a7b9b514084140ea8aa3ffa2a
    Signed-off-by: Chun-Wei Tseng <chun-wei.tseng@mediatek.com>
    CR-Id: ALPS04183477
    Feature: Wi-Fi Driver

--- nic/nic_tx.c
+++ nic/nic_tx.c
@@ -1931,18 +1931,17 @@ nicTxFillDesc(IN struct ADAPTER *prAdapter,
 				      prStaRec)) {
 		prTxDescTemplate =
 			prStaRec->aprTxDescTemplate[prMsduInfo->ucUserPriority];
-
+	}
+	if (prTxDescTemplate) {
 		if (prMsduInfo->ucPacketType == TX_PACKET_TYPE_DATA)
 			kalMemCopy(prTxDesc, prTxDescTemplate,
 				u4TxDescLength + prChipInfo->txd_append_size);
 		else
 			kalMemCopy(prTxDesc, prTxDescTemplate, u4TxDescLength);
-
 		/* Overwrite fields for EOSP or More data */
 		nicTxFillDescByPktOption(prMsduInfo, prTxDesc);
-	}
-	/* Compose TXD by Msdu info */
-	else {
+	} else { /* Compose TXD by Msdu info */
+		DBGLOG_LIMITED(NIC, INFO, "Compose TXD by Msdu info\n");
 #if (UNIFIED_MAC_TX_FORMAT == 1)
 		if (prMsduInfo->eSrc == TX_PACKET_MGMT)
 			prMsduInfo->ucPacketFormat = TXD_PKT_FORMAT_COMMAND;
@@ -1957,7 +1956,6 @@ nicTxFillDesc(IN struct ADAPTER *prAdapter,
 			nicTxComposeDescAppend(prAdapter, prMsduInfo,
 					       prTxDescBuffer + u4TxDescLength);
 	}
-
 	/*
 	 * --------------------------------------------------------------------
 	 * Fill up remaining parts, per-packet variant fields
