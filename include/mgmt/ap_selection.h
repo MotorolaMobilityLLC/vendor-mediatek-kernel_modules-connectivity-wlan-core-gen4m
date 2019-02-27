bff4d5dc07acb35937e6613fb847b091b700f1af
mtk71246 <shuaishuai.kong@mediatek.com>
2019-02-28 01:06:49 +0800

    [ALPS04098168] wlan:implement ap selection for per roaming
    implement ap selection for per roaming
    
    MTK-Commit-Id: f3684e4174faee37e1fa2d2312e1729d4a90a7a9
    
    Change-Id: I669b2c5cdc1b7df55b13874efcfd194940fdffdd
    Signed-off-by: mtk71246 <shuaishuai.kong@mediatek.com>
    CR-Id: ALPS04098168
    Feature: [Module]Wi-Fi Driver

--- include/mgmt/ap_selection.h
+++ include/mgmt/ap_selection.h
@@ -12,6 +12,24 @@
 #ifndef _AP_SELECTION_H
 #define _AP_SELECTION_H
 
+struct weight_config {
+	uint8_t chnl_util_weight;
+	uint8_t snr_weight;
+	uint8_t rssi_weight;
+	uint8_t scan_miss_cnt_weight;
+	uint8_t probe_resp_weight;
+	uint8_t client_cnt_weight;
+	uint8_t ap_num_weight;
+	uint8_t band_weight;
+	uint8_t band_width_weight;
+	uint8_t stbc_weight;
+	uint8_t last_deauth_weight;
+	uint8_t black_list_weight;
+	uint8_t saa_weight;
+	uint8_t chnl_idle_weight;
+	uint8_t opchnl_weight;
+};
+
 /* Support AP Selection */
 struct BSS_DESC *scanSearchBssDescByScoreForAis(struct ADAPTER *prAdapter);
 void scanGetCurrentEssChnlList(struct ADAPTER *prAdapter);
