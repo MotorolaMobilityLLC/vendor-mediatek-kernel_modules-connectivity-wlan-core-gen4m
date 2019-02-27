e35715869d98a6294e53b6d19c2652611375a88b
Wilson Lien <wilson.lien@mediatek.com>
2019-02-28 01:15:21 +0800

    [ALPS04192804] wlan: Avoid swap queue if tx allow state is the same
    When queue management set station record Tx allowed,
    it should check if the Tx allowed state change.
    Only swap pending queue and tx queue when state changed.
    
    Since 1x packet will always enqueue to tx queue instread of
    pending queue, this check will avoild 1x packet being swap to
    pending queue.
    
    MTK-Commit-Id: e22bf671b71f07abfedd530ab8e8c6cd005c05f9
    
    Change-Id: Iaed2f5bbe46820caf0058adc3c45a36fa35cc4df
    Signed-off-by: Wilson Lien <wilson.lien@mediatek.com>
    CR-Id: ALPS04192804
    Feature: Wi-Fi Driver

--- nic/que_mgt.c
+++ nic/que_mgt.c
@@ -1025,31 +1025,33 @@ void qmSetStaRecTxAllowed(IN struct ADAPTER *prAdapter,
 	uint8_t ucIdx;
 	struct QUE *prSrcQ, *prDstQ;
 
-	/* Update Tx queue */
-	for (ucIdx = 0; ucIdx < NUM_OF_PER_STA_TX_QUEUES; ucIdx++) {
-		if (fgIsTxAllowed) {
-			prSrcQ = &prStaRec->arPendingTxQueue[ucIdx];
-			prDstQ = &prStaRec->arTxQueue[ucIdx];
-		} else {
-			prSrcQ = &prStaRec->arTxQueue[ucIdx];
-			prDstQ = &prStaRec->arPendingTxQueue[ucIdx];
+	DBGLOG(QM, INFO, "Set Sta[%u] TxAllowed from [%u] to [%u] %s TxQ\n",
+		prStaRec->ucIndex,
+		prStaRec->fgIsTxAllowed,
+		fgIsTxAllowed,
+		fgIsTxAllowed ? "normal" : "pending");
+
+	/* Update Tx queue when allowed state change*/
+	if (prStaRec->fgIsTxAllowed != fgIsTxAllowed) {
+		for (ucIdx = 0; ucIdx < NUM_OF_PER_STA_TX_QUEUES; ucIdx++) {
+			if (fgIsTxAllowed) {
+				prSrcQ = &prStaRec->arPendingTxQueue[ucIdx];
+				prDstQ = &prStaRec->arTxQueue[ucIdx];
+			} else {
+				prSrcQ = &prStaRec->arTxQueue[ucIdx];
+				prDstQ = &prStaRec->arPendingTxQueue[ucIdx];
+			}
+
+			QUEUE_CONCATENATE_QUEUES_HEAD(prDstQ, prSrcQ);
+			prStaRec->aprTargetQueue[ucIdx] = prDstQ;
 		}
 
-		QUEUE_CONCATENATE_QUEUES_HEAD(prDstQ, prSrcQ);
-		prStaRec->aprTargetQueue[ucIdx] = prDstQ;
-	}
-
-	if (prStaRec->fgIsTxAllowed != fgIsTxAllowed) {
 		if (fgIsTxAllowed)
 			prAdapter->rQM.u4TxAllowedStaCount++;
 		else
 			prAdapter->rQM.u4TxAllowedStaCount--;
 	}
 	prStaRec->fgIsTxAllowed = fgIsTxAllowed;
-
-	DBGLOG(QM, INFO, "Set Sta[%u] TxAllowed[%u] %s TxQ\n",
-		prStaRec->ucIndex, fgIsTxAllowed,
-		fgIsTxAllowed ? "normal" : "pending");
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1596,7 +1598,16 @@ qmDequeueTxPacketsFromPerStaQueues(IN struct ADAPTER *prAdapter,
 						ucAlwaysResetUsedRes & BIT(0)))
 						fgEndThisRound = TRUE;
 					break;
+				} else if (!prStaRec->fgIsValid) {
+					/* In roaming, if the sta_rec doesn't
+					 * active by event 0x0C, it can't
+					 * dequeue data.
+					 */
+					DBGLOG_LIMITED(QM, WARN,
+						"sta_rec is not valid\n");
+					break;
 				}
+
 #if CFG_SUPPORT_SOFT_ACM
 				if (fgAcmFlowCtrl) {
 					uint32_t u4PktTxTime = 0;
