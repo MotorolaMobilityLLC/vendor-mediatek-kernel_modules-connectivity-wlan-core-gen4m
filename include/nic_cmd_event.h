e726d2f1b1fc5fdeb649625f684fddd926d3cd6a
Yi-Cheng Huang <yi-cheng.huang@mediatek.com>
2019-02-27 23:17:39 +0800

    [WCNCR00157090] misc: Change stat CMD format
    [Description]
    Change AGG range format due to HW support further control
    
    MTK-Commit-Id: f326fc09c7a1189a8bb7da23a85a54622a73aae9
    
    Change-Id: Iee314309d5b64ef750e21d947d95d04ed2f8a890
    Signed-off-by: Yi-Cheng Huang <yi-cheng.huang@mediatek.com>
    CR-Id: WCNCR00157090
    Feature: misc
    Reviewed-on: http://gerrit.mediatek.inc:8080/1231788
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: polly chiu <polly.chiu@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- include/nic_cmd_event.h
+++ include/nic_cmd_event.h
@@ -2918,8 +2918,15 @@ typedef struct _EVENT_STA_STATISTICS_T {
 	UINT_8 ucRateEntryIdxPrev;
 	UINT_8 ucTxSgiDetectPassCnt;
 	UINT_8 ucAvePer;
+#if (CFG_SUPPORT_RA_GEN == 0)
 	UINT_8 aucArRatePer[AR_RATE_TABLE_ENTRY_MAX];
 	UINT_8 aucRateEntryIndex[AUTO_RATE_NUM];
+#else
+	UINT_32 u4AggRangeCtrl_0;
+	UINT_32 u4AggRangeCtrl_1;
+	UINT_8 ucRangeType;
+	UINT_8 aucReserved5[24];
+#endif
 	UINT_8 ucArStateCurr;
 	UINT_8 ucArStatePrev;
 	UINT_8 ucArActionType;
@@ -2933,7 +2940,9 @@ typedef struct _EVENT_STA_STATISTICS_T {
 	MIB_INFO_STAT_T rMibInfo[ENUM_BAND_NUM];
 	BOOLEAN fgIsForceTxStream;
 	BOOLEAN fgIsForceSeOff;
-
+#if (CFG_SUPPORT_RA_GEN == 0)
+	UINT_8 aucReserved6[17];
+#else
 	UINT_16 u2RaRunningCnt;
 	UINT_8 ucRaStatus;
 	UINT_8 ucFlag;
@@ -2947,7 +2956,7 @@ typedef struct _EVENT_STA_STATISTICS_T {
 	UINT_8 ucDynamicBWState;
 	UINT_8 ucDynamicGband256QAMState;
 	UINT_8 ucVhtNonSpRateState;
-
+#endif
 	UINT_8 aucReserved[4];
 } EVENT_STA_STATISTICS_T, *P_EVENT_STA_STATISTICS_T;
 
