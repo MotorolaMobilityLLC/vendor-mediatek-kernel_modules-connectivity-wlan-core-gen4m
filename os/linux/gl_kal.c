daec40057e62f0dc3a77a448895a35072ea479d6
Holiday Hao <holiday.hao@mediatek.com>
2019-02-27 23:35:00 +0800

    [ALPS03428288] CONNAC: fix KE
    [Detail]
    fix KE if prBssDesc is NULL
    
    MTK-Commit-Id: 7f1ee59f81b172b88b4b7237298a11058d59cbc8
    
    Change-Id: I4668e2b2dcd818bbfde24c465ceff1475bdf6779
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    CR-Id: ALPS03428288
    Feature: WIFI

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -1018,9 +1018,9 @@ kalIndicateStatusAndComplete(IN struct GLUE_INFO *prGlueInfo, IN uint32_t eStatu
 {
 
 	uint32_t bufLen;
-	struct PARAM_STATUS_INDICATION *pStatus = (struct PARAM_STATUS_INDICATION *) pvBuf;
-	struct PARAM_AUTH_EVENT *pAuth = (struct PARAM_AUTH_EVENT *) pStatus;
-	struct PARAM_PMKID_CANDIDATE_LIST *pPmkid = (struct PARAM_PMKID_CANDIDATE_LIST *) (pStatus + 1);
+	struct PARAM_STATUS_INDICATION *pStatus;
+	struct PARAM_AUTH_EVENT *pAuth;
+	struct PARAM_PMKID_CANDIDATE_LIST *pPmkid;
 	uint8_t arBssid[PARAM_MAC_ADDR_LEN];
 	struct PARAM_SSID ssid;
 	struct ieee80211_channel *prChannel = NULL;
@@ -1030,9 +1030,16 @@ kalIndicateStatusAndComplete(IN struct GLUE_INFO *prGlueInfo, IN uint32_t eStatu
 
 	GLUE_SPIN_LOCK_DECLARATION();
 
+	if (!pvBuf)
+		return;
+
 	kalMemZero(arBssid, MAC_ADDR_LEN);
 
 	ASSERT(prGlueInfo);
+
+	pStatus = (struct PARAM_STATUS_INDICATION *) pvBuf;
+	pAuth = (struct PARAM_AUTH_EVENT *) pStatus;
+	pPmkid = (struct PARAM_PMKID_CANDIDATE_LIST *) (pStatus + 1);
 
 	switch (eStatus) {
 	case WLAN_STATUS_ROAM_OUT_FIND_BEST:
@@ -1321,11 +1328,15 @@ kalIndicateStatusAndComplete(IN struct GLUE_INFO *prGlueInfo, IN uint32_t eStatu
 		{
 			struct BSS_DESC *prBssDesc = prGlueInfo->prAdapter->rWifiVar.rAisFsmInfo.prTargetBssDesc;
 
-			DBGLOG(INIT, INFO, "JOIN Failure: u2JoinStatus=%d", prBssDesc->u2JoinStatus);
-			if (prBssDesc)
+			if (prBssDesc) {
+				DBGLOG(INIT, INFO,
+				       "JOIN Failure: u2JoinStatus=%d",
+				       prBssDesc->u2JoinStatus);
 				COPY_MAC_ADDR(arBssid, prBssDesc->aucBSSID);
-			else
+			} else {
+				DBGLOG(INIT, INFO, "JOIN Failure");
 				COPY_MAC_ADDR(arBssid, prGlueInfo->prAdapter->rWifiVar.rConnSettings.aucBSSID);
+			}
 			if (prBssDesc && prBssDesc->u2JoinStatus
 				&& prBssDesc->u2JoinStatus != STATUS_CODE_AUTH_TIMEOUT
 				&& prBssDesc->u2JoinStatus != STATUS_CODE_ASSOC_TIMEOUT)
