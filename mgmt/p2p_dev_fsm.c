907723dc859ff9fc28addcae2a9ad745edc60597
Zeno Yang <zeno.yang@mediatek.com>
2019-04-13 16:35:59 +0800

    [ALPS04354911] wlan: converity scan by codex checker
    1. Add null check handling
    2. Check returned value for kalIoctl
    
    MTK-Commit-Id: b1c91de5aef61427f10129dbd7aee8cb2bb20680
    
    Change-Id: Ie06a4cdf8a0f8cf14b6f2df9d98a80801c27b319
    Feature: Wi-Fi Direct
    CR-Id: ALPS04354911
    Signed-off-by: Zeno Yang <zeno.yang@mediatek.com>
    (cherry picked from commit a23dcf3aa666b8b307fecd66fd916c083a8be49d)

--- mgmt/p2p_dev_fsm.c
+++ mgmt/p2p_dev_fsm.c
@@ -985,7 +985,7 @@ p2pDevFsmRunEventChnlGrant(IN struct ADAPTER *prAdapter,
 		}
 	} while (FALSE);
 
-	if (prMsgHdr)
+	if (prAdapter && prMsgHdr)
 		cnmMemFree(prAdapter, prMsgHdr);
 }				/* p2pDevFsmRunEventChnlGrant */
 
