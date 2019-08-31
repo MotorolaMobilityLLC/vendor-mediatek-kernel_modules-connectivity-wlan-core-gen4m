e543748b2b613dc4f7bcd041d5eacc3b7d0ee359
Simfex Chiu <simfex.chiu@mediatek.com>
2019-08-31 09:22:51 +0800

    [ALPS04764217] Wlan: Fix use pointer before check
    [Detail]
    prCmd is not check before using it
    
    [Solution]
    return error if prCmd is NULL
    
    MTK-Commit-Id: 8731650fddd6b68b7201ef05efbb021ccd437349
    
    Change-Id: I8a08a96f774fa7ab989c2908fdb45df68c9ba4ed
    Signed-off-by: Simfex Chiu <simfex.chiu@mediatek.com>
    CR-Id: ALPS04764217
    Feature: [Module]Wi-Fi Driver
    (cherry picked from commit 47b80376130b7a4f551425ba94c0f38af00437d5)

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -11923,6 +11923,11 @@ wlanoidSetTxPower(IN struct ADAPTER *prAdapter,
 
 	prCmd = cnmMemAlloc(prAdapter, RAM_TYPE_BUF,
 			    sizeof(struct SET_TXPWR_CTRL));
+
+	if (!prCmd) {
+		DBGLOG(REQ, ERROR, "prCmd not available\n");
+		return WLAN_STATUS_FAILURE;
+	}
 	kalMemZero(prCmd, sizeof(struct SET_TXPWR_CTRL));
 	prCmd->c2GLegacyStaPwrOffset = pTxPwr->c2GLegacyStaPwrOffset;
 	prCmd->c2GHotspotPwrOffset = pTxPwr->c2GHotspotPwrOffset;
