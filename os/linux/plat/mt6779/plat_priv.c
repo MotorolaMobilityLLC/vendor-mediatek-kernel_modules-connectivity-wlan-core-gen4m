4ca3dd659f7134abdc14cabf7bca2e67a0620ddc
Facer Pei <facer.pei@mediatek.com>
2020-03-03 02:02:36 +0800

    [ALPS04385998] wlan: revert code for build error
    Revert 2501286 for mt6779 build error.
    
    MTK-Commit-Id: 58962cd6658d2841b666dc4808a03e36e9b9a3d7
    
    Signed-off-by: Facer Pei <facer.pei@mediatek.com>
    CR-Id: ALPS04385998
    Feature: [Module]Wi-Fi Driver
    Change-Id: Ib7af55190134fb5d561d95210f37dac3643589c7
    Reviewed-on: http://gerrit.mediatek.inc:8080/2537441
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    CheckCourse: srv_hbgsm001 <srv_hbgsm001@mediatek.com>
    Reviewed-by: Austin Chou (周欽弘) <austin.chou@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Soul Huang <soul.huang@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Chun-Yi Lin <chun-yi.lin@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- os/linux/plat/mt6779/plat_priv.c
+++ os/linux/plat/mt6779/plat_priv.c
@@ -9,14 +9,6 @@
 * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 */
 
-#include <cpu_ctrl.h>
-#include <sched_ctl.h>
-#include <eas_ctrl.h>
-#include <topo_ctrl.h>
-#ifdef WLAN_FORCE_DDR_OPP
-#include <linux/pm_qos.h>
-#endif
-
 #include "precomp.h"
 
 #ifdef CONFIG_MTK_EMI
@@ -24,108 +16,6 @@
 #define WIFI_EMI_MEM_OFFSET    0x220000
 #define WIFI_EMI_MEM_SIZE      0xf0000
 #endif
-
-
-#define MAX_CPU_FREQ (3 * 1024 * 1024) /* in kHZ */
-#define MAX_CLUSTER_NUM  3
-
-uint32_t kalGetCpuBoostThreshold(void)
-{
-	DBGLOG(SW4, TRACE, "enter kalGetCpuBoostThreshold\n");
-	/*  8, stands for 450Mbps */
-	return 8;
-}
-
-int32_t kalBoostCpu(IN struct ADAPTER *prAdapter,
-		    IN uint32_t u4TarPerfLevel,
-		    IN uint32_t u4BoostCpuTh)
-{
-	struct ppm_limit_data freq_to_set[MAX_CLUSTER_NUM];
-	int32_t i = 0, i4Freq = -1;
-
-#if defined(CONFIG_UCLAMP_TASK) && defined(CONFIG_UCLAMP_TASK_GROUP)
-	int32_t ret = 0;
-	struct GLUE_INFO *prGlueInfo;
-#endif /* CONFIG_UCLAMP_TASK & CONFIG_UCLAMP_TASK_GROUP */
-
-#ifdef WLAN_FORCE_DDR_OPP
-	static struct pm_qos_request wifi_qos_request;
-	static u_int8_t fgRequested;
-#endif
-	uint32_t u4ClusterNum = topo_ctrl_get_nr_clusters();
-
-	ASSERT(prAdapter);
-	ASSERT(u4ClusterNum <= MAX_CLUSTER_NUM);
-	/* ACAO, we dont have to set core number */
-
-#if defined(CONFIG_UCLAMP_TASK) && defined(CONFIG_UCLAMP_TASK_GROUP)
-	/*  5, stands for 250Mbps */
-	if (u4TarPerfLevel >= 5) {
-		prGlueInfo = prAdapter->prGlueInfo;
-		ASSERT(prGlueInfo);
-
-		/* Set driver threads uclamp */
-		if (prGlueInfo->u4RxThreadPid != 0xffffffff) {
-			ret = set_task_uclamp(prGlueInfo->u4RxThreadPid, 100);
-			if (ret != 0)
-				DBGLOG(SW4, WARN, "set uclamp Rx failed\n");
-		}
-		if (prGlueInfo->u4HifThreadPid != 0xffffffff) {
-			ret = set_task_uclamp(prGlueInfo->u4HifThreadPid, 100);
-			if (ret != 0)
-				DBGLOG(SW4, WARN, "set uclamp Hif failed\n");
-		}
-		if (prGlueInfo->u4TxThreadPid != 0xffffffff) {
-			ret = set_task_uclamp(prGlueInfo->u4TxThreadPid, 100);
-			if (ret != 0)
-				DBGLOG(SW4, WARN, "set uclamp Tx failed\n");
-		}
-		/* Set Top APP threads uclamp */
-		update_eas_uclamp_min(EAS_UCLAMP_KIR_WIFI, CGROUP_TA, 100);
-
-	} else {
-		set_task_uclamp(prAdapter->prGlueInfo->u4RxThreadPid, 0);
-		set_task_uclamp(prAdapter->prGlueInfo->u4HifThreadPid, 0);
-		set_task_uclamp(prAdapter->prGlueInfo->u4TxThreadPid, 0);
-		update_eas_uclamp_min(EAS_UCLAMP_KIR_WIFI, CGROUP_TA, 0);
-	}
-#endif /* CONFIG_UCLAMP_TASK & CONFIG_UCLAMP_TASK_GROUP */
-	/*  Default u4BoostCpuTh set as 8, stands for 450Mbps */
-	if (u4TarPerfLevel >= u4BoostCpuTh) {
-		/* Boost CPU freq */
-		i4Freq = MAX_CPU_FREQ;
-
-		/* Prefer big core */
-		set_sched_boost(SCHED_ALL_BOOST);
-
-	} else {
-		i4Freq = -1;
-		set_sched_boost(SCHED_NO_BOOST);
-	}
-	/* update cpu freq */
-	for (i = 0; i < u4ClusterNum; i++) {
-		freq_to_set[i].min = i4Freq;
-		freq_to_set[i].max = i4Freq;
-	}
-	update_userlimit_cpu_freq(CPU_KIR_WIFI, u4ClusterNum, freq_to_set);
-
-#ifdef WLAN_FORCE_DDR_OPP
-	if (u4TarPerfLevel >= u4BoostCpuTh) {
-		if (!fgRequested) {
-			fgRequested = 1;
-			pm_qos_add_request(&wifi_qos_request,
-					   PM_QOS_DDR_OPP,
-					   DDR_OPP_0);
-		}
-		pm_qos_update_request(&wifi_qos_request, DDR_OPP_0);
-	} else if (fgRequested) {
-		pm_qos_update_request(&wifi_qos_request, DDR_OPP_UNREQ);
-		pm_qos_remove_request(&wifi_qos_request);
-		fgRequested = 0;
-	}
-#endif
-	return 0;
-}
 
 #ifdef CONFIG_MTK_EMI
 void kalSetEmiMpuProtection(phys_addr_t emiPhyBase, bool enable)
