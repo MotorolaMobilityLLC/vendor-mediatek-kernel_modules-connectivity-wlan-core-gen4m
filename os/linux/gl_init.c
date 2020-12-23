d1c6dd943ec98fae69d3570d916d264f95961207
liaoxl <liaoxl@motorola.com>
2020-12-23 10:12:52 +0800

    wlan: disable 2.4GHz ch14 for JAPAN
    We do not enable CH14 for JAPAN on all of our products
    and have disabled it on QCom platform as well.
    
    Change-Id: I47b61fffbc7745a8568b964b0f7ca6adcdaa4493
    Reviewed-on: https://gerrit.mot.com/1835322
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- os/linux/gl_init.c
+++ os/linux/gl_init.c
@@ -244,7 +244,9 @@ static struct ieee80211_channel mtk_2ghz_channels[] = {
 	CHAN2G(11, 2462, 0),
 	CHAN2G(12, 2467, 0),
 	CHAN2G(13, 2472, 0),
+#ifdef WLAN_ENABLE_JP_CH14
 	CHAN2G(14, 2484, 0),
+#endif
 };
 
 #define CHAN5G(_channel, _flags)					\
