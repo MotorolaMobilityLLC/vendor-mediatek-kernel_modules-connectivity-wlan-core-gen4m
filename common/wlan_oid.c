f9659d6bee9a92d28fb9df7eeaf12282d51d6c08
Sticky Chen <sticky.chen@mediatek.com>
2021-01-23 12:59:50 +0800

    [ALPS05485153] Make sure WEP key is removed
    Normally, key is installed after connection establishes.
    However, WEK key is also set when connection is triggered.
    If the WEP connection fails, the key wont be properly removed.
    
    To solve it, we check whether WEP key should be removed
    if join failure occurs.
    
    MTK-Commit-Id: 423825e541bb7a72dd54dbebd44ca08525f451d3
    
    Change-Id: Ifa41be4e9a5fecd65af0eb9398661d18817b7286
    CR-Id: ALPS05485153
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Sticky Chen <sticky.chen@mediatek.com>

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -3174,6 +3174,9 @@ wlanoidSetRemoveKey(IN struct ADAPTER *prAdapter,
 			}
 			ASSERT(prBssInfo->wepkeyWlanIdx < WTBL_SIZE);
 			ucRemoveBCKeyAtIdx = prBssInfo->wepkeyWlanIdx;
+			secPrivacyFreeForEntry(prAdapter,
+					prBssInfo->wepkeyWlanIdx);
+			prBssInfo->wepkeyWlanIdx = WTBL_RESERVED_ENTRY;
 		} else {
 			DBGLOG(RSN, INFO, "Remove group key id = %d",
 			       u4KeyIndex);
@@ -3191,6 +3194,8 @@ wlanoidSetRemoveKey(IN struct ADAPTER *prAdapter,
 				ucRemoveBCKeyAtIdx =
 					prBssInfo->ucBMCWlanIndexS[u4KeyIndex];
 
+				secPrivacyFreeForEntry(prAdapter,
+				    prBssInfo->ucBMCWlanIndexS[u4KeyIndex]);
 				prBssInfo->ucBMCWlanIndexSUsed[u4KeyIndex]
 					= FALSE;
 				prBssInfo->ucBMCWlanIndexS[u4KeyIndex]
