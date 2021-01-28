c489dbc9c6e8c7c2881b1ea2902a18fbc011a9f9
Chun-Wei Tseng <chun-wei.tseng@mediatek.com>
2021-01-28 20:35:05 +0800

    [ALPS04264977] wlan: Fix KE when u4TxHeadRoomSize is unexpected
    Fix KE when skb_realloc_headroom fail.
    We will drop this packet.
    
    MTK-Commit-Id: d02372a53a12df0e3ba5360d8889380a5c373521
    
    Signed-off-by: Chun-Wei Tseng <chun-wei.tseng@mediatek.com>
    CR-Id: ALPS04264977
    Feature: [Module]Wi-Fi Driver
    Change-Id: I1b6cfdd72126424367e69a105bb16d44ca1bc149
    (cherry picked from commit 959d5d6589581f7db5457ace461ba70912892c40)
    Reviewed-on: http://gerrit.mediatek.inc:8080/2045946
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    CheckCourse: srv_hbgsm001 <srv_hbgsm001@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Wilson Lien <wilson.lien@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -1861,9 +1861,15 @@ kalHardStartXmit(struct sk_buff *prOrgSkb,
 	struct sk_buff *prSkbNew = NULL;
 	struct sk_buff *prSkb = NULL;
 	uint32_t u4SkbLen = 0;
+	struct mt66xx_chip_info *prChipInfo;
+	uint32_t u4TxHeadRoomSize = 0;
 
 	ASSERT(prOrgSkb);
 	ASSERT(prGlueInfo);
+
+	prChipInfo = prGlueInfo->prAdapter->chip_info;
+	u4TxHeadRoomSize = NIC_TX_DESC_AND_PADDING_LENGTH +
+		prChipInfo->txd_append_size;
 
 	if (prGlueInfo->ulFlag & GLUE_FLAG_HALT) {
 		DBGLOG(INIT, INFO, "GLUE_FLAG_HALT skip tx\n");
@@ -1877,15 +1883,21 @@ kalHardStartXmit(struct sk_buff *prOrgSkb,
 		return WLAN_STATUS_NOT_ACCEPTED;
 	}
 
-	if (skb_headroom(prOrgSkb) < NIC_TX_HEAD_ROOM) {
+	if (skb_headroom(prOrgSkb) < u4TxHeadRoomSize) {
 		/*
 		 * Should not happen
 		 * kernel crash may happen as skb shared info
 		 * channged.
 		 * offer an change for lucky anyway
 		 */
-		prSkbNew = skb_realloc_headroom(prOrgSkb, NIC_TX_HEAD_ROOM);
-		ASSERT(prSkbNew);
+		prSkbNew = skb_realloc_headroom(prOrgSkb, u4TxHeadRoomSize);
+		if (!prSkbNew) {
+			dev_kfree_skb(prOrgSkb);
+			DBGLOG(INIT, ERROR,
+				"prChipInfo = %pM, u4TxHeadRoomSize: %u\n",
+				prChipInfo, u4TxHeadRoomSize);
+			return WLAN_STATUS_NOT_ACCEPTED;
+		}
 		dev_kfree_skb(prOrgSkb);
 		prSkb = prSkbNew;
 	} else
