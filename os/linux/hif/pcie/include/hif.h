32036279853118162560a5575a657b4d02e58768
Holiday Hao <holiday.hao@mediatek.com>
2021-01-28 17:16:11 +0800

    [WCNCR00149535] change bus2chip CR mapping table to bus info
    [Description]
    change bus2chip CR mapping table to bus_info to support mulit-chips
    
    MTK-Commit-Id: 1d2d6c2d7f62acb901fd42aef636ed1f0ff92ed6
    
    Feature: Wi-Fi
    CR-Id: WCNCR00149535
    Change-Id: Iac9551a81d23a2dcb13a2aeb5130d2d174c273a3
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>

--- os/linux/hif/pcie/include/hif.h
+++ os/linux/hif/pcie/include/hif.h
@@ -170,6 +170,7 @@ typedef struct _GL_HIF_INFO_T {
 typedef struct _BUS_INFO {
 	const unsigned int top_cfg_base;	/* TOP_CFG_BASE address */
 	const unsigned char is_pcie_32dw_read;	/* diff PDMA config */
+	const PCIE_CHIP_CR_MAPPING *bus2chip;
 	const unsigned int tx_ring_cmd_idx;
 	const unsigned int tx_ring_fwdl_idx;
 	const unsigned int tx_ring_data_idx;
@@ -237,9 +238,9 @@ VOID halReturnMsduToken(IN P_ADAPTER_T prAdapter, UINT_32 u4TokenNum);
 VOID halTxUpdateCutThroughDesc(P_GLUE_INFO_T prGlueInfo, P_MSDU_INFO_T prMsduInfo,
 	P_MSDU_TOKEN_ENTRY_T prToken);
 BOOLEAN halIsStaticMapBusAddr(IN UINT_32 u4Addr);
-BOOLEAN halChipToStaticMapBusAddr(IN UINT_32 u4ChipAddr, OUT PUINT_32 pu4BusAddr);
-BOOLEAN halGetDynamicMapReg(IN P_GL_HIF_INFO_T prHifInfo, IN UINT_32 u4ChipAddr, OUT PUINT_32 pu4Value);
-BOOLEAN halSetDynamicMapReg(IN P_GL_HIF_INFO_T prHifInfo, IN UINT_32 u4ChipAddr, IN UINT_32 u4Value);
+BOOLEAN halChipToStaticMapBusAddr(IN P_BUS_INFO prBusInfo, IN UINT_32 u4ChipAddr, OUT PUINT_32 pu4BusAddr);
+BOOLEAN halGetDynamicMapReg(IN P_GLUE_INFO_T prGlueInfo, IN UINT_32 u4ChipAddr, OUT PUINT_32 pu4Value);
+BOOLEAN halSetDynamicMapReg(IN P_GLUE_INFO_T prGlueInfo, IN UINT_32 u4ChipAddr, IN UINT_32 u4Value);
 VOID halEnhancedWpdmaConfig(P_GLUE_INFO_T prGlueInfo, BOOLEAN enable);
 VOID halWpdmaConfig(P_GLUE_INFO_T prGlueInfo, BOOLEAN enable);
 
