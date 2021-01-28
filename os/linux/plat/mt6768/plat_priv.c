1f45c0732a697f7ae844fdd36d86c0c25cad676b
Facer Pei <facer.pei@mediatek.com>
2021-01-28 21:03:44 +0800

    [ALPS04385998] wlan: sync platform code to neptune
    Sync latest platform code to neptune trunk.
    
    MTK-Commit-Id: 08181d2ec734c6bfdf3d5c933e4c5aae41658e61
    
    Change-Id: I65f1effe2b39a7bc93eab08fbfae1071437180fb
    Signed-off-by: Facer Pei <facer.pei@mediatek.com>
    CR-Id: ALPS04385998
    Feature: [Module]Wi-Fi Driver
    Reviewed-on: http://gerrit.mediatek.inc:8080/2501286
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    CheckCourse: srv_hbgsm001 <srv_hbgsm001@mediatek.com>
    Reviewed-by: Chun-Yi Lin <chun-yi.lin@mediatek.com>
    Reviewed-by: Austin Chou (周欽弘) <austin.chou@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Soul Huang <soul.huang@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- os/linux/plat/mt6768/plat_priv.c
+++ os/linux/plat/mt6768/plat_priv.c
@@ -10,8 +10,6 @@
 */
 
 #include <cpu_ctrl.h>
-#include <sched_ctl.h>
-#include <eas_ctrl.h>
 #include <topo_ctrl.h>
 #ifdef WLAN_FORCE_DDR_OPP
 #include <linux/pm_qos.h>
@@ -21,19 +19,20 @@
 
 #ifdef CONFIG_MTK_EMI
 #include <mt_emi_api.h>
-#define WIFI_EMI_MEM_OFFSET    0x220000
-#define WIFI_EMI_MEM_SIZE      0xf0000
+#define WIFI_EMI_MEM_OFFSET    0x177000
+#define WIFI_EMI_MEM_SIZE      0xBD000
 #endif
 
 
 #define MAX_CPU_FREQ (3 * 1024 * 1024) /* in kHZ */
 #define MAX_CLUSTER_NUM  3
 
+
 uint32_t kalGetCpuBoostThreshold(void)
 {
 	DBGLOG(SW4, TRACE, "enter kalGetCpuBoostThreshold\n");
-	/*  8, stands for 450Mbps */
-	return 8;
+	/*  3, stands for 100Mbps */
+	return 3;
 }
 
 int32_t kalBoostCpu(IN struct ADAPTER *prAdapter,
@@ -42,71 +41,20 @@ int32_t kalBoostCpu(IN struct ADAPTER *prAdapter,
 {
 	struct ppm_limit_data freq_to_set[MAX_CLUSTER_NUM];
 	int32_t i = 0, i4Freq = -1;
-
-#if defined(CONFIG_UCLAMP_TASK) && defined(CONFIG_UCLAMP_TASK_GROUP)
-	int32_t ret = 0;
-	struct GLUE_INFO *prGlueInfo;
-#endif /* CONFIG_UCLAMP_TASK & CONFIG_UCLAMP_TASK_GROUP */
-
 #ifdef WLAN_FORCE_DDR_OPP
 	static struct pm_qos_request wifi_qos_request;
 	static u_int8_t fgRequested;
 #endif
 	uint32_t u4ClusterNum = topo_ctrl_get_nr_clusters();
 
-	ASSERT(prAdapter);
 	ASSERT(u4ClusterNum <= MAX_CLUSTER_NUM);
 	/* ACAO, we dont have to set core number */
-
-#if defined(CONFIG_UCLAMP_TASK) && defined(CONFIG_UCLAMP_TASK_GROUP)
-	/*  5, stands for 250Mbps */
-	if (u4TarPerfLevel >= 5) {
-		prGlueInfo = prAdapter->prGlueInfo;
-		ASSERT(prGlueInfo);
-
-		/* Set driver threads uclamp */
-		if (prGlueInfo->u4RxThreadPid != 0xffffffff) {
-			ret = set_task_uclamp(prGlueInfo->u4RxThreadPid, 100);
-			if (ret != 0)
-				DBGLOG(SW4, WARN, "set uclamp Rx failed\n");
-		}
-		if (prGlueInfo->u4HifThreadPid != 0xffffffff) {
-			ret = set_task_uclamp(prGlueInfo->u4HifThreadPid, 100);
-			if (ret != 0)
-				DBGLOG(SW4, WARN, "set uclamp Hif failed\n");
-		}
-		if (prGlueInfo->u4TxThreadPid != 0xffffffff) {
-			ret = set_task_uclamp(prGlueInfo->u4TxThreadPid, 100);
-			if (ret != 0)
-				DBGLOG(SW4, WARN, "set uclamp Tx failed\n");
-		}
-		/* Set Top APP threads uclamp */
-		update_eas_uclamp_min(EAS_UCLAMP_KIR_WIFI, CGROUP_TA, 100);
-
-	} else {
-		set_task_uclamp(prAdapter->prGlueInfo->u4RxThreadPid, 0);
-		set_task_uclamp(prAdapter->prGlueInfo->u4HifThreadPid, 0);
-		set_task_uclamp(prAdapter->prGlueInfo->u4TxThreadPid, 0);
-		update_eas_uclamp_min(EAS_UCLAMP_KIR_WIFI, CGROUP_TA, 0);
-	}
-#endif /* CONFIG_UCLAMP_TASK & CONFIG_UCLAMP_TASK_GROUP */
-	/*  Default u4BoostCpuTh set as 8, stands for 450Mbps */
-	if (u4TarPerfLevel >= u4BoostCpuTh) {
-		/* Boost CPU freq */
-		i4Freq = MAX_CPU_FREQ;
-
-		/* Prefer big core */
-		set_sched_boost(SCHED_ALL_BOOST);
-
-	} else {
-		i4Freq = -1;
-		set_sched_boost(SCHED_NO_BOOST);
-	}
-	/* update cpu freq */
+	i4Freq = (u4TarPerfLevel >= u4BoostCpuTh) ? MAX_CPU_FREQ : -1;
 	for (i = 0; i < u4ClusterNum; i++) {
 		freq_to_set[i].min = i4Freq;
 		freq_to_set[i].max = i4Freq;
 	}
+
 	update_userlimit_cpu_freq(CPU_KIR_WIFI, u4ClusterNum, freq_to_set);
 
 #ifdef WLAN_FORCE_DDR_OPP
@@ -136,7 +84,7 @@ void kalSetEmiMpuProtection(phys_addr_t emiPhyBase, bool enable)
 	region_info.start = emiPhyBase + WIFI_EMI_MEM_OFFSET;
 	region_info.end = emiPhyBase + WIFI_EMI_MEM_OFFSET
 		+ WIFI_EMI_MEM_SIZE - 1;
-	region_info.region = 26;
+	region_info.region = 25;
 
 	SET_ACCESS_PERMISSION(region_info.apc, enable ? LOCK : UNLOCK,
 			      FORBIDDEN, FORBIDDEN,
@@ -155,7 +103,7 @@ void kalSetDrvEmiMpuProtection(phys_addr_t emiPhyBase, uint32_t offset,
 	/*set MPU for EMI share Memory */
 	region_info.start = emiPhyBase + offset;
 	region_info.end = emiPhyBase + offset + size - 1;
-	region_info.region = 18;
+	region_info.region = 29;
 	SET_ACCESS_PERMISSION(region_info.apc, LOCK, FORBIDDEN, FORBIDDEN,
 			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
 			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
@@ -164,3 +112,9 @@ void kalSetDrvEmiMpuProtection(phys_addr_t emiPhyBase, uint32_t offset,
 	emi_mpu_set_protection(&region_info);
 }
 #endif
+
+int32_t kalGetFwFlavor(uint8_t *flavor)
+{
+	*flavor = 'a';
+	return 1;
+}
