120882d69ffa75ab36d4f55c79392dc582b9ee74
TF Huang <tf.huang@mediatek.com>
2021-01-28 22:39:14 +0800

    [ALPS05200926] wlan: Porting CPU boost mechanism for 6779
    Only poring CPU boost mechanism
    
    TODOs:
    (1) Porting adjustment of DRAM and uclamp(based on common kernel)
    
    MTK-Commit-Id: b661b0660bbea4de148c93dceeda3a131d0ba023
    
    Change-Id: Ie57485fd7e3c61c540fdc85d757aac3053819541
    Signed-off-by: TF Huang <tf.huang@mediatek.com>
    CR-Id: ALPS05200926
    Feature: Wi-Fi Driver

--- os/linux/plat/mt6779/plat_priv.c
+++ os/linux/plat/mt6779/plat_priv.c
@@ -9,6 +9,8 @@
 * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 */
 
+#include <cpu_ctrl.h>
+#include <topo_ctrl.h>
 #include "precomp.h"
 #include "wmt_exp.h"
 
@@ -17,6 +19,38 @@
 #define WIFI_EMI_MEM_OFFSET    0x1D0000
 #define WIFI_EMI_MEM_SIZE      0x140000
 #endif
+
+#define MAX_CPU_FREQ (3 * 1024 * 1024) /* in kHZ */
+#define MAX_CLUSTER_NUM  3
+
+uint32_t kalGetCpuBoostThreshold(void)
+{
+	DBGLOG(SW4, TRACE, "enter kalGetCpuBoostThreshold\n");
+	/*  5, stands for 250Mbps */
+	return 5;
+}
+
+int32_t kalBoostCpu(IN struct ADAPTER *prAdapter,
+		    IN uint32_t u4TarPerfLevel,
+		    IN uint32_t u4BoostCpuTh)
+{
+	struct cpu_ctrl_data freq_to_set[MAX_CLUSTER_NUM];
+	int32_t i = 0, i4Freq = -1;
+
+	uint32_t u4ClusterNum = topo_ctrl_get_nr_clusters();
+
+	ASSERT(u4ClusterNum <= MAX_CLUSTER_NUM);
+	/* ACAO, we dont have to set core number */
+	i4Freq = (u4TarPerfLevel >= u4BoostCpuTh) ? MAX_CPU_FREQ : -1;
+	for (i = 0; i < u4ClusterNum; i++) {
+		freq_to_set[i].min = i4Freq;
+		freq_to_set[i].max = i4Freq;
+	}
+
+	update_userlimit_cpu_freq(CPU_KIR_WIFI, u4ClusterNum, freq_to_set);
+
+	return 0;
+}
 
 #ifdef CONFIG_MTK_EMI
 void kalSetEmiMpuProtection(phys_addr_t emiPhyBase, bool enable)
