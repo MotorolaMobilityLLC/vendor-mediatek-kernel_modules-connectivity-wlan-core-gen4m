6219f010f09a09d93a1e97e145a566d4c8d89f61
Ben Lai <ben.lai@mediatek.com>
2021-01-28 22:34:30 +0800

    [ALPS05172450] wlan: Restore free pending msdu mechanism
    Restore free pending msdu mechanism.
    This reverts commit ALPS05090865.
    
    MTK-Commit-Id: 6aacfe856ac7ce7882a850fcac6c4cbacf4a01a2
    
    Change-Id: I7ef02bba2785bfe5c674da11de25d595f36493fd
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS05172450
    Feature: [Module]Wi-Fi Driver

--- os/linux/include/gl_kal.h
+++ os/linux/include/gl_kal.h
@@ -136,8 +136,7 @@ extern struct delayed_work sched_workq;
 	(GLUE_FLAG_HALT | GLUE_FLAG_INT | GLUE_FLAG_HIF_TX | \
 	GLUE_FLAG_HIF_TX_CMD | GLUE_FLAG_HIF_FW_OWN | \
 	GLUE_FLAG_HIF_PRT_HIF_DBG_INFO | \
-	GLUE_FLAG_UPDATE_WMM_QUOTA | \
-	GLUE_FLAG_HIF_RECYCLE_MGMT_TX_QUEUE)
+	GLUE_FLAG_UPDATE_WMM_QUOTA)
 
 #define GLUE_FLAG_RX_PROCESS (GLUE_FLAG_HALT | GLUE_FLAG_RX_TO_OS)
 #else
@@ -237,7 +236,6 @@ enum ENUM_SPIN_LOCK_CATEGORY_E {
 	SPIN_LOCK_CMD_SEQ_NUM,
 	SPIN_LOCK_TX_MSDU_INFO_LIST,
 	SPIN_LOCK_TXING_MGMT_LIST,
-	SPIN_LOCK_RECYCLING_MGMT_LIST,
 	SPIN_LOCK_TX_SEQ_NUM,
 	SPIN_LOCK_TX_COUNT,
 	SPIN_LOCK_TXS_COUNT,
@@ -1492,8 +1490,6 @@ void kalSetTxCmdEvent2Hif(struct GLUE_INFO *pr);
 void kalSetTxCmdDoneEvent(struct GLUE_INFO *pr);
 
 void kalSetRxProcessEvent(struct GLUE_INFO *pr);
-
-void kalSetMgmtTxRecyclingEvent2Hif(struct GLUE_INFO *pr);
 #endif
 /*----------------------------------------------------------------------------*/
 /* NVRAM/Registry Service                                                     */
