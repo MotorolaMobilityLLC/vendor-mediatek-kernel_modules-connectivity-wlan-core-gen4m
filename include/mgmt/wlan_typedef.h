e1237af375018ffc355aed87a207bd60646ec476
Dom Lin <dom.lin@mediatek.com>
2021-01-28 19:11:06 +0800

    [ALPS03684070] wlan: Fix CNM and Scan checkpatch warning and error
    [Detail]
    Fix all checkpatch warning and error in following files
        - include/mgmt/cnm_mem.h
        - include/mgmt/cnm_timer.h
        - include/mgmt/scan.h
        - include/mgmt/wlan_typedef.h
        - mgmt/cnm_timer.c
        - mgmt/scan.c
        - mgmt/scan_fsm.c
    
    MTK-Commit-Id: a2e419ce3411a77a36d64063360c9849648fdbc0
    
    Change-Id: I19aa00742d97e48a7a618e0b2c41c80e28a53925
    CR-Id: ALPS03684070
    Feature: Wi-Fi
    Signed-off-by: Dom Lin <dom.lin@mediatek.com>
    Reviewed-on: http://gerrit.mediatek.inc:8080/1247423
    Reviewed-by: ShaominChiu <shaomin.chiu@mediatek.com>
    Tested-by: ShaominChiu <shaomin.chiu@mediatek.com>
    Reviewed-by: Eva Wang <eva.wang@mediatek.com>
    Reviewed-by: Jimbo Chi <jimbo.chi@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com
    Reviewed-by: YZ Huang <yz.huang@mediatek.com>
    CheckPatch: YZ Huang <yz.huang@mediatek.com>

--- include/mgmt/wlan_typedef.h
+++ include/mgmt/wlan_typedef.h
@@ -50,41 +50,43 @@
  *
  *****************************************************************************/
 /*
-** Id: //Department/DaVinci/BRANCHES/MT6620_WIFI_DRIVER_V2_3/include/mgmt/wlan_typedef.h#1
-*/
+ * Id: //Department/DaVinci/BRANCHES/MT6620_WIFI_DRIVER_V2_3
+ *     /include/mgmt/wlan_typedef.h#1
+ */
 
 /*! \file   wlan_typedef.h
-*    \brief  Declaration of data type and return values of internal protocol stack.
-*
-*    In this file we declare the data type and return values which will be exported
-*    to all MGMT Protocol Stack.
-*/
+ *    \brief  Declaration of data type and return values of internal protocol
+ *    stack.
+ *
+ *    In this file we declare the data type and return values which will be
+ *    exported to all MGMT Protocol Stack.
+ */
 
 
 #ifndef _WLAN_TYPEDEF_H
 #define _WLAN_TYPEDEF_H
 
 /*******************************************************************************
-*                         C O M P I L E R   F L A G S
-********************************************************************************
-*/
+ *                         C O M P I L E R   F L A G S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                    E X T E R N A L   R E F E R E N C E S
-********************************************************************************
-*/
+ *                    E X T E R N A L   R E F E R E N C E S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                              C O N S T A N T S
-********************************************************************************
-*/
+ *                              C O N S T A N T S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                             D A T A   T Y P E S
-********************************************************************************
-*/
-/* Type definition for BSS_INFO_T structure, to describe the attributes used in a
- * common BSS.
+ *                             D A T A   T Y P E S
+ *******************************************************************************
+ */
+/* Type definition for BSS_INFO structure, to describe the
+ * attributes used in a common BSS.
  */
 struct BSS_INFO;	/* declare BSS_INFO_T */
 struct BSS_INFO;	/* declare P2P_DEV_INFO_T */
@@ -97,8 +99,10 @@ struct BOW_SPECIFIC_BSS_INFO;	/* declare BOW_SPECIFIC_BSS_INFO_T */
 struct WFD_CFG_SETTINGS;	/* declare WFD_CFG_SETTINGS_T */
 
 /* BSS related structures */
-/* Type definition for BSS_DESC_T structure, to describe parameter sets of a particular BSS */
-struct BSS_DESC;	/* declare BSS_DESC_T */
+/* Type definition for BSS_DESC structure, to describe parameter
+ * sets of a particular BSS
+ */
+struct BSS_DESC;	/* declare BSS_DESC */
 
 #if CFG_SUPPORT_ROAMING_SKIP_ONE_AP
 struct ROAM_BSS_DESC;	/* declare ROAM_BSS_DESC_T */
@@ -109,27 +113,27 @@ struct HS20_INFO;	/* declare HS20_INFO_T */
 #endif /* CFG_SUPPORT_PASSPOINT */
 
 /*******************************************************************************
-*                            P U B L I C   D A T A
-********************************************************************************
-*/
+ *                            P U B L I C   D A T A
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                           P R I V A T E   D A T A
-********************************************************************************
-*/
+ *                           P R I V A T E   D A T A
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                                 M A C R O S
-********************************************************************************
-*/
+ *                                 M A C R O S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                  F U N C T I O N   D E C L A R A T I O N S
-********************************************************************************
-*/
+ *                  F U N C T I O N   D E C L A R A T I O N S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                              F U N C T I O N S
-********************************************************************************
-*/
+ *                              F U N C T I O N S
+ *******************************************************************************
+ */
 #endif /* _WLAN_TYPEDEF_H */
