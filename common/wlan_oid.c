66452ddfed30cf00659a774b31781cf40cbe2391
Black Chen <black-ch.chen@mediatek.com>
2021-01-28 19:54:05 +0800

    [ALPS03830377] wlan: refine logs for stress onoff
    [Detail]
    stress onff 5 times
    
    [Solution]
    merge log and change to TRACE
    
    MTK-Commit-Id: 4498ffa01c96c5c28458c65757ce2284d824610c
    
    Change-Id: I9745abebdbe6886d6365165e5aa8a2ad2259bcbe
    Signed-off-by: Black Chen <black-ch.chen@mediatek.com>
    CR-Id: ALPS03830377
    Feature: 2G/3G/4G FDD R10R11
    (cherry picked from commit cdb1704756f822350116821e1123acda8df7533f)
    Reviewed-on: http://gerrit.mediatek.inc:8080/1543772
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Simfex Chiu <simfex.chiu@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -8533,7 +8533,7 @@ wlanoidSetCurrentPacketFilter(IN struct ADAPTER *prAdapter,
 	/* Set the new packet filter. */
 	u4NewPacketFilter = *(uint32_t *) pvSetBuffer;
 
-	DBGLOG(REQ, INFO, "New packet filter: %#08x\n",
+	DBGLOG(REQ, TRACE, "New packet filter: %#08x\n",
 	       u4NewPacketFilter);
 
 	if (prAdapter->rAcpiState == ACPI_STATE_D3) {
