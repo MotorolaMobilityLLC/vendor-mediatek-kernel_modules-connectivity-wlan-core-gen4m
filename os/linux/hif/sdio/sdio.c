7a5bfbe0c18eba40613dce8ee7345dc7f6b60a22
Allan Wang <Allan.Wang@mediatek.com>
2021-01-28 20:21:37 +0800

    [WCNCR00147877] wowlan: Add WoWL for SDIO
    [Description]
    Add WoWL for SDIO
      1. Make sure CMD_ID_SET_PF_CAPABILITY and
         CMD_ID_SET_WOWLAN sent out
      2. Allow forced FW OWN after WOW process in SDIO suspend Cb
    
    MTK-Commit-Id: ab722aae26f505f2e1e4b5c2258256534e0e6ece
    
    Feature: wowlan
    Change-Id: I5a8877082f7cdf5fb85818ad3a3115c61b113e41
    Signed-off-by: Allan Wang <Allan.Wang@mediatek.com>
    CR-Id: WCNCR00147877
    Feature: wowlan
    Reviewed-on: http://gerrit.mediatek.inc:8080/1658179
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Tested-by: Deren Wu <deren.wu@mediatek.com>
    Reviewed-by: Sarick Jiang <sarick.jiang@mediatek.com>
    Reviewed-by: Deren Wu <deren.wu@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com
    (cherry picked from commit aaa63d8b4524dc122107dfa49895915af841cf5b)
    Reviewed-on: http://gerrit.mediatek.inc:8080/1680841
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Sheng-Wen Hsu <nina.hsu@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Holiday Hao <holiday.hao@mediatek.com>

--- os/linux/hif/sdio/sdio.c
+++ os/linux/hif/sdio/sdio.c
@@ -433,22 +433,48 @@ static int mtk_sdio_pm_suspend(struct device *pDev)
 	func = dev_to_sdio_func(pDev);
 	prGlueInfo = sdio_get_drvdata(func);
 
+	DBGLOG(REQ, STATE, "Wow:%d, WowEnable:%d, state:%d\n",
+		prGlueInfo->prAdapter->rWifiVar.ucWow,
+		prGlueInfo->prAdapter->rWowCtrl.fgWowEnable,
+		kalGetMediaStateIndicated(prGlueInfo));
+
+	/* 1) wifi cfg "Wow" is true
+	*  2) wow is enable
+	*  3) WIfI connected => execute WOW flow
+	*/
+	if (prGlueInfo->prAdapter->rWifiVar.ucWow &&
+		prGlueInfo->prAdapter->rWowCtrl.fgWowEnable &&
+		(kalGetMediaStateIndicated(prGlueInfo) ==
+		PARAM_MEDIA_STATE_CONNECTED)) {
+		DBGLOG(HAL, STATE, "enter WOW flow\n");
+		kalWowProcess(prGlueInfo, TRUE);
+	}
+
+	prGlueInfo->rHifInfo.fgForceFwOwn = TRUE;
+
 	/* Wait for
 	*  1. The other unfinished ownership handshakes
 	*  2. FW own back
 	*/
 	while (1) {
-		if (wait > LP_OWN_BACK_FAILED_LOG_SKIP_MS) {
-			DBGLOG(HAL, ERROR, "Timeout !!\n");
+		if (prGlueInfo->prAdapter->u4PwrCtrlBlockCnt == 0
+				&& prGlueInfo->prAdapter->fgIsFwOwn == TRUE) {
+			DBGLOG(HAL, STATE, "************************\n");
+			DBGLOG(HAL, STATE, "* Entered SDIO Supsend *\n");
+			DBGLOG(HAL, STATE, "************************\n");
+			DBGLOG(HAL, INFO, "wait = %d\n\n", wait);
 			break;
 		}
-		if (prGlueInfo->prAdapter->u4PwrCtrlBlockCnt == 0
-			&& prGlueInfo->prAdapter->fgIsFwOwn == TRUE) {
-			DBGLOG(HAL, STATE, "\n Entered SDIO Supsend ");
-			break;
+
+		ACQUIRE_POWER_CONTROL_FROM_PM(prGlueInfo->prAdapter);
+		kalMsleep(10);
+		RECLAIM_POWER_CONTROL_TO_PM(prGlueInfo->prAdapter, FALSE);
+
+		if (wait > 100) {
+			DBGLOG(HAL, ERROR, "Timeout !!\n");
+			return -EAGAIN;
 		}
 		wait++;
-		kalMsleep(LP_OWN_BACK_LOOP_DELAY_MS);
 	}
 
 	pm_caps = sdio_get_host_pm_caps(func);
@@ -481,8 +507,30 @@ static int mtk_sdio_pm_suspend(struct device *pDev)
 
 static int mtk_sdio_pm_resume(struct device *pDev)
 {
+	struct sdio_func *func;
+	struct GLUE_INFO *prGlueInfo = NULL;
+
 	DBGLOG(HAL, STATE, "==>\n");
 
+	func = dev_to_sdio_func(pDev);
+	prGlueInfo = sdio_get_drvdata(func);
+
+	DBGLOG(REQ, STATE, "Wow:%d, WowEnable:%d, state:%d\n",
+		prGlueInfo->prAdapter->rWifiVar.ucWow,
+		prGlueInfo->prAdapter->rWowCtrl.fgWowEnable,
+		kalGetMediaStateIndicated(prGlueInfo));
+
+	prGlueInfo->rHifInfo.fgForceFwOwn = FALSE;
+
+	if (prGlueInfo->prAdapter->rWifiVar.ucWow &&
+		prGlueInfo->prAdapter->rWowCtrl.fgWowEnable &&
+		(kalGetMediaStateIndicated(prGlueInfo) ==
+		PARAM_MEDIA_STATE_CONNECTED)) {
+		DBGLOG(HAL, STATE, "leave WOW flow\n");
+		kalWowProcess(prGlueInfo, FALSE);
+	}
+
+	DBGLOG(HAL, STATE, "<==\n");
 	return 0;
 }
 
