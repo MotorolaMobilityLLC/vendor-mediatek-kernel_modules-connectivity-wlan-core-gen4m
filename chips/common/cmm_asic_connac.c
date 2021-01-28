72556b70cb9e5b2c696473c7aad329a1c57aac38
Ben Lai <ben.lai@mediatek.com>
2021-01-28 22:07:03 +0800

    [ALPS05059532] wlan: Change log level from sleep mode
    Change log level from sleep mode.
    
    MTK-Commit-Id: 322a91a7511bd66ecec406e82bc8ab0bce6ced08
    
    Change-Id: If6216069be09266a1ac92f66457bca98b13b70b4
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS05059532
    Feature: [Module]Wi-Fi Driver

--- chips/common/cmm_asic_connac.c
+++ chips/common/cmm_asic_connac.c
@@ -902,11 +902,11 @@ void asicCheckDummyReg(struct GLUE_INFO *prGlueInfo)
 	DBGLOG(HAL, INFO, "Weakup from sleep mode\n");
 
 	if (halWpdmaGetRxDmaDoneCnt(prGlueInfo, RX_RING_EVT_IDX_1)) {
-		DBGLOG(HAL, INFO, "Force to read RX event\n");
+		DBGLOG(HAL, TRACE, "Force to read RX event\n");
 		prAdapter->u4NoMoreRfb |= BIT(RX_RING_EVT_IDX_1);
 	}
 	if (halWpdmaGetRxDmaDoneCnt(prGlueInfo, RX_RING_DATA_IDX_0)) {
-		DBGLOG(HAL, INFO, "Force to read RX data\n");
+		DBGLOG(HAL, TRACE, "Force to read RX data\n");
 		prAdapter->u4NoMoreRfb |= BIT(RX_RING_DATA_IDX_0);
 	}
 	/* Write sleep mode magic num to dummy reg */
