c965cd87a6f6ce82bab8f37278e45a88d851afa1
Ben Lai <ben.lai@mediatek.com>
2021-01-28 22:00:55 +0800

    [ALPS05038778] wlan: MPU porting
    MPU porting.
    
    MTK-Commit-Id: 20263e064f327abfb879f704e4336079256a37a0
    
    Change-Id: I2066f8b63835dd69e92e750d7444b5884ff6381a
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS05038778
    Feature: [Module]Wi-Fi Driver

--- os/linux/plat/mt6873/plat_priv.c
+++ os/linux/plat/mt6873/plat_priv.c
@@ -5,47 +5,72 @@
 
 #include "precomp.h"
 
-#ifdef CONFIG_MTK_EMI
-#include <mt_emi_api.h>
+#ifdef CONFIG_MEDIATEK_EMI
+#include <memory/mediatek/emi.h>
 #define WIFI_EMI_MEM_OFFSET    0x1D0000
 #define WIFI_EMI_MEM_SIZE      0x140000
+#define	DOMAIN_AP	0
+#define	DOMAIN_CONN	2
 #endif
 
-#ifdef CONFIG_MTK_EMI
+#ifdef CONFIG_MEDIATEK_EMI
 void kalSetEmiMpuProtection(phys_addr_t emiPhyBase, bool enable)
 {
-	struct emi_region_info_t region_info;
+	struct emimpu_region_t region;
+	unsigned long long start = emiPhyBase + WIFI_EMI_MEM_OFFSET;
+	unsigned long long end = emiPhyBase + WIFI_EMI_MEM_OFFSET +
+			WIFI_EMI_MEM_SIZE - 1;
+	int ret;
 
-	/*set MPU for EMI share Memory */
-	region_info.start = emiPhyBase + WIFI_EMI_MEM_OFFSET;
-	region_info.end = emiPhyBase + WIFI_EMI_MEM_OFFSET
-		+ WIFI_EMI_MEM_SIZE - 1;
-	region_info.region = 26;
+	DBGLOG(INIT, INFO, "emiPhyBase: 0x%p, enable: %d\n",
+				emiPhyBase, enable);
 
-	SET_ACCESS_PERMISSION(region_info.apc, enable ? LOCK : UNLOCK,
-			      FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION,
-			      FORBIDDEN, enable ? FORBIDDEN : NO_PROTECTION);
-	emi_mpu_set_protection(&region_info);
+	ret = mtk_emimpu_init_region(&region, 26);
+	if (ret) {
+		DBGLOG(INIT, ERROR, "mtk_emimpu_init_region failed, ret: %d\n",
+				ret);
+		return;
+	}
+	mtk_emimpu_set_addr(&region, start, end);
+	mtk_emimpu_set_apc(&region, DOMAIN_AP, MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_set_apc(&region, DOMAIN_CONN, MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_lock_region(&region,
+			enable ? MTK_EMIMPU_LOCK : MTK_EMIMPU_UNLOCK);
+	ret = mtk_emimpu_set_protection(&region);
+	if (ret)
+		DBGLOG(INIT, ERROR,
+			"mtk_emimpu_set_protection failed, ret: %d\n",
+			ret);
+	mtk_emimpu_free_region(&region);
 }
 
 void kalSetDrvEmiMpuProtection(phys_addr_t emiPhyBase, uint32_t offset,
 			       uint32_t size)
 {
-	struct emi_region_info_t region_info;
+	struct emimpu_region_t region;
+	unsigned long long start = emiPhyBase + offset;
+	unsigned long long end = emiPhyBase + offset + size - 1;
+	int ret;
 
-	/*set MPU for EMI share Memory */
-	region_info.start = emiPhyBase + offset;
-	region_info.end = emiPhyBase + offset + size - 1;
-	region_info.region = 18;
-	SET_ACCESS_PERMISSION(region_info.apc, LOCK, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION,
-			      FORBIDDEN, NO_PROTECTION);
-	emi_mpu_set_protection(&region_info);
+	DBGLOG(INIT, INFO, "emiPhyBase: 0x%p, offset: %d, size: %d\n",
+				emiPhyBase, offset, size);
+
+	ret = mtk_emimpu_init_region(&region, 18);
+	if (ret) {
+		DBGLOG(INIT, ERROR, "mtk_emimpu_init_region failed, ret: %d\n",
+				ret);
+		return;
+	}
+	mtk_emimpu_set_addr(&region, start, end);
+	mtk_emimpu_set_apc(&region, DOMAIN_AP, MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_set_apc(&region, DOMAIN_CONN, MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_lock_region(&region, MTK_EMIMPU_LOCK);
+	ret = mtk_emimpu_set_protection(&region);
+	if (ret)
+		DBGLOG(INIT, ERROR,
+			"mtk_emimpu_set_protection failed, ret: %d\n",
+			ret);
+	mtk_emimpu_free_region(&region);
 }
 #endif
 
