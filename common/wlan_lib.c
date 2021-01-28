03d308d1a5d6db69de977350a93450c2e2b008e4
Dean Chiang <dean.chiang@mediatek.com>
2021-01-28 22:38:58 +0800

    [ALPS05177139] wlan: Process interrupt before sending power off cmd
    After driver send power off cmd to FW, FW reset host WFDMA which may
    cause driver not process interrupt normally.
    Therefore, driver process interrupt before sending power off cmd.
    
    MTK-Commit-Id: 69ed142b60aace89da6ea67113bd118bda48bcb2
    
    Change-Id: Ia42c1b18cfed0028aafa727262e6312ba98608e5
    Signed-off-by: Dean Chiang <dean.chiang@mediatek.com>
    CR-Id: ALPS05177139
    Feature: [Module]Wi-Fi Driver

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -1596,11 +1596,6 @@ uint32_t wlanCheckWifiFunc(IN struct ADAPTER *prAdapter,
 				WLAN_STATUS_NOT_INDICATING)
 				DBGLOG_LIMITED(INIT, INFO,
 				       "Handle pending interrupt\n");
-#elif defined(_HIF_AXI)
-			if (nicProcessISTWithSpecifiedCount(prAdapter, 5) !=
-				WLAN_STATUS_NOT_INDICATING)
-				DBGLOG(INIT, INFO,
-				       "Handle pending interrupt\n");
 #endif /* _HIF_USB or _HIF_SDIO */
 		}
 		u4CurTime = kalGetTimeTick();
