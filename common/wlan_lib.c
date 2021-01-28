b729e538738f75241a356231813af524a2e9c7b5
Alonso Hsu <alonso.hsu@mediatek.com>
2021-01-28 21:29:21 +0800

    [ALPS04788202] wlan: extending range development
    [Peturs][wifi][Gen4m] extending range development
    
    MTK-Commit-Id: a0e08fc3e4679b41c6e0aca79498c45a1804beef
    
    Change-Id: I1731dc62603b239842a4c2b5ec06b49f5a6bfbe5
    CR-Id: ALPS04788202
    Feature: [Module]Wi-Fi driver
    Signed-off-by: mtk11045 <alonso.hsu@mediatek.com>

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -7135,6 +7135,12 @@ void wlanInitFeatureOption(IN struct ADAPTER *prAdapter)
 			prAdapter, "TrafficThreshold",
 			TRAFFIC_RHRESHOLD);
 #endif
+
+#if CFG_SUPPORT_HE_ER
+	prWifiVar->u4ExtendedRange = (uint32_t) wlanCfgGetUint32(
+			prAdapter, "ExtendedRange",
+			FEATURE_ENABLED);
+#endif
 }
 
 void wlanCfgSetSwCtrl(IN struct ADAPTER *prAdapter)
