971903daaeb68989cf803f9320f22a336322b809
Juju Sung <juju.sung@mediatek.com>
2021-01-28 21:56:59 +0800

    [ALPS04957424] wlan: refine wakeup & emi
    wakeup_source API refactory.
    This patch replace wakeup_source_init to
    wakeup_source_register and disable EMI_MPU
    to prevent build break.
    
    MTK-Commit-Id: 92a9c8be34c9de05d36c8b271be6546ae00c6943
    
    Change-Id: I5ac3c6f483ace91e4bbe5d74e3f1707f0f1a2c9b
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS04957424
    Feature:[Module]Kernel Maintenance

--- os/linux/include/gl_kal.h
+++ os/linux/include/gl_kal.h
@@ -297,7 +297,9 @@ enum ENUM_KAL_MEM_ALLOCATION_TYPE_E {
 };
 
 #ifdef CONFIG_ANDROID		/* Defined in Android kernel source */
-#if (KERNEL_VERSION(4, 9, 0) <= LINUX_VERSION_CODE)
+#if (KERNEL_VERSION(4, 19, 0) <= CFG80211_VERSION_CODE)
+#define KAL_WAKE_LOCK_T struct wakeup_source
+#elif (KERNEL_VERSION(4, 9, 0) <= CFG80211_VERSION_CODE)
 #define KAL_WAKE_LOCK_T struct wakeup_source
 #else
 #define KAL_WAKE_LOCK_T struct wake_lock
@@ -611,7 +613,7 @@ static inline void kalCfg80211ScanDone(struct cfg80211_scan_request *request,
 #if (KERNEL_VERSION(4, 9, 0) <= LINUX_VERSION_CODE)
 #if (KERNEL_VERSION(4, 19, 0) <= LINUX_VERSION_CODE)
 #define KAL_WAKE_LOCK_INIT(_prAdapter, _prWakeLock, _pcName) \
-	_prWakeLock = wakeup_source_register(NULL, _pcName);
+	_prWakeLock = wakeup_source_register(_pcName);
 
 #define KAL_WAKE_LOCK_DESTROY(_prAdapter, _prWakeLock) \
 { \
