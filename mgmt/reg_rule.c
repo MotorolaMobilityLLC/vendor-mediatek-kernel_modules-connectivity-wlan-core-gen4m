48427247ae818797d6473936222c90732c830339
Neil Chen <yn.chen@mediatek.com>
2021-01-28 20:03:37 +0800

    [WCNCR00164248] regdb: Add local regulatory db of US, CN, JP, and TR
    [Description]
    Add local regulatory db of US, CN, JP, and TR
    Reference to the latest db.txt from
     - http://git.kernel.org/?p=linux/kernel/git/sforshee/wireless-regdb.git
     - commit 9b3317bd4b31 ("wireless-regdb: Add 60 GHz rule for Taiwan (TW)")
    
    Add channel flags NO_OFDM, DFS, and AUTO_BW
    
    MTK-Commit-Id: 05a6b48b32be417a730882f1a20fa824db782964
    
    Change-Id: Ib5085e3f81ec19f636f4cab27cb356dc0048886b
    Signed-off-by: Neil Chen <yn.chen@mediatek.com>
    CR-Id: WCNCR00164248
    Feature: regdb
    Reviewed-on: http://gerrit.mediatek.inc:8080/1584918
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Sarick Jiang <sarick.jiang@mediatek.com>
    Reviewed-by: Zeno Yang <zeno.yang@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- mgmt/reg_rule.c
+++ mgmt/reg_rule.c
@@ -191,26 +191,41 @@ const struct ieee80211_regdomain regdom_us01 = {
 	/* channels 36..48 */
 	REG_RULE_LIGHT(5180-10, 5240+10, 40, 0),
 	/* channels 56..64 */
-	REG_RULE_LIGHT(5260-10, 5320+10, 40, NL80211_RRF_DFS),
+	REG_RULE_LIGHT(5260-10, 5320+10, 40, KAL_RRF_DFS),
 	/* channels 100..118 */
-	REG_RULE_LIGHT(5500-10, 5590+10, 40, NL80211_RRF_DFS),
+	REG_RULE_LIGHT(5500-10, 5590+10, 40, KAL_RRF_DFS),
 	/* channels 132..140 */
-	REG_RULE_LIGHT(5660-10, 5700+10, 40, NL80211_RRF_DFS),
+	REG_RULE_LIGHT(5660-10, 5700+10, 40, KAL_RRF_DFS),
 	/* channels 149..165 */
 	REG_RULE_LIGHT(5745-10, 5825+10, 40, 0) }
 };
 
-const struct ieee80211_regdomain regdom_cn = {
+const struct ieee80211_regdomain regdom_us = {
 	.n_reg_rules = 5,
+	.dfs_region = NL80211_DFS_FCC,
 	.reg_rules = {
 	/* channels 1..11 */
 	REG_RULE_LIGHT(2412-10, 2462+10, 40, 0),
-	/* channels 12,13 */
-	REG_RULE_LIGHT(2467-10, 2472+10, 40, 0),
 	/* channels 36..48 */
-	REG_RULE_LIGHT(5180-10, 5240+10, 80, 0),
+	REG_RULE_LIGHT(5180-10, 5240+10, 80, KAL_RRF_AUTO_BW),
 	/* channels 52..64 */
-	REG_RULE_LIGHT(5260-10, 5320+10, 80, NL80211_RRF_DFS),
+	REG_RULE_LIGHT(5260-10, 5320+10, 80, KAL_RRF_DFS | KAL_RRF_AUTO_BW),
+	/* channels 100..140 */
+	REG_RULE_LIGHT(5500-10, 5720+10, 160, KAL_RRF_DFS),
+	/* channels 149..165 */
+	REG_RULE_LIGHT(5745-10, 5825+10, 80, 0) }
+};
+
+const struct ieee80211_regdomain regdom_cn = {
+	.n_reg_rules = 4,
+	.dfs_region = NL80211_DFS_FCC,
+	.reg_rules = {
+	/* channels 1..13 */
+	REG_RULE_LIGHT(2412-10, 2472+10, 40, 0),
+	/* channels 36..48 */
+	REG_RULE_LIGHT(5180-10, 5240+10, 80, KAL_RRF_AUTO_BW),
+	/* channels 52..64 */
+	REG_RULE_LIGHT(5260-10, 5320+10, 80, KAL_RRF_DFS | KAL_RRF_AUTO_BW),
 	/* channels 149..165 */
 	REG_RULE_LIGHT(5745-10, 5825+10, 80, 0) }
 };
@@ -225,9 +240,43 @@ const struct ieee80211_regdomain regdom_cz_nl = {
 	/* channels 36..48 */
 	REG_RULE_LIGHT(5180-10, 5240+10, 80, 0),
 	/* channels 52..64 */
-	REG_RULE_LIGHT(5260-10, 5320+10, 80, NL80211_RRF_DFS),
+	REG_RULE_LIGHT(5260-10, 5320+10, 80, KAL_RRF_DFS),
 	/* channels 100..140 */
-	REG_RULE_LIGHT(5500-10, 5700+10, 160, NL80211_RRF_DFS) }
+	REG_RULE_LIGHT(5500-10, 5700+10, 160, KAL_RRF_DFS) }
+};
+
+const struct ieee80211_regdomain regdom_jp = {
+	.n_reg_rules = 7,
+	.dfs_region = NL80211_DFS_JP,
+	.reg_rules = {
+	/* channels 1..13 */
+	REG_RULE_LIGHT(2412-10, 2472+10, 40, 0),
+	/* channels 14 */
+	REG_RULE_LIGHT(2484-10, 2484+10, 20, KAL_RRF_NO_OFDM),
+	/* channels 184..196 */
+	REG_RULE_LIGHT(4920-10, 4980+10, 40, 0),
+	/* channels 8..16 */
+	REG_RULE_LIGHT(5040-10, 5080+10, 40, 0),
+	/* channels 36..48 */
+	REG_RULE_LIGHT(5180-10, 5240+10, 80, KAL_RRF_AUTO_BW),
+	/* channels 52..64 */
+	REG_RULE_LIGHT(5260-10, 5320+10, 80, KAL_RRF_DFS | KAL_RRF_AUTO_BW),
+	/* channels 100..140 */
+	REG_RULE_LIGHT(5500-10, 5700+10, 160, KAL_RRF_DFS) }
+};
+
+const struct ieee80211_regdomain regdom_tr = {
+	.n_reg_rules = 4,
+	.dfs_region = NL80211_DFS_ETSI,
+	.reg_rules = {
+	/* channels 1..13 */
+	REG_RULE_LIGHT(2412-10, 2472+10, 40, 0),
+	/* channels 36..48 */
+	REG_RULE_LIGHT(5180-10, 5240+10, 80, KAL_RRF_AUTO_BW),
+	/* channels 52..64 */
+	REG_RULE_LIGHT(5260-10, 5320+10, 80, KAL_RRF_DFS | KAL_RRF_AUTO_BW),
+	/* channels 100..140 */
+	REG_RULE_LIGHT(5500-10, 5700+10, 160, KAL_RRF_DFS) }
 };
 
 /*
@@ -240,7 +289,7 @@ const struct mtk_regdomain my_regdom_us01 = {
 
 const struct mtk_regdomain my_regdom_us = {
 	.country_code = "US",
-	.prRegdRules = &regdom_us01
+	.prRegdRules = &regdom_us
 };
 
 const struct mtk_regdomain my_regdom_cn = {
@@ -258,6 +307,16 @@ const struct mtk_regdomain my_regdom_cz = {
 	.prRegdRules = &regdom_cz_nl
 };
 
+const struct mtk_regdomain my_regdom_jp = {
+	.country_code = "JP",
+	.prRegdRules = &regdom_jp
+};
+
+const struct mtk_regdomain my_regdom_tr = {
+	.country_code = "TR",
+	.prRegdRules = &regdom_tr
+};
+
 /*
  * Step3. Register to table
  */
@@ -267,6 +326,8 @@ const struct mtk_regdomain *g_prRegRuleTable[] = {
 	&my_regdom_cn,
 	&my_regdom_nl,
 	&my_regdom_cz,
+	&my_regdom_jp,
+	&my_regdom_tr,
 	NULL /* this NULL SHOULD be at the end of the array */
 };
 
