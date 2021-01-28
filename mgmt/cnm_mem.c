7ae40bb762af5789893cccbea8b826644bc0324b
Peiyong Feng <peiyong.feng@mediatek.com>
2021-01-28 18:16:43 +0800

    [ALPS03064821] wlan:p2p: add debug log for AAA/SAA
    [Detail]
    
    1. AAA / SAA debug log not enough for issues
    
    [Solution]
    
    1. Add reason for p2pFuncDisconnect.
    
    2. Add log for AAA / SAA TX failed.
    
    MTK-Commit-Id: c8e33201600da6673cc3b39e9cd0431d6ac5cfe3
    
    Change-Id: I13d26809c3dafd24b39e9b816d349b7b388d97b2
    CR-Id: ALPS03064821
    Feature: MT6632 WIFI driver
    Signed-off-by: Peiyong Feng <peiyong.feng@mediatek.com>

--- mgmt/cnm_mem.c
+++ mgmt/cnm_mem.c
@@ -604,7 +604,7 @@ VOID cnmStaRecFree(P_ADAPTER_T prAdapter, P_STA_RECORD_T prStaRec)
 	if (!prStaRec)
 		return;
 
-	DBGLOG(RSN, INFO, "cnmStaRecFree %d", prStaRec->ucIndex);
+	DBGLOG(RSN, INFO, "cnmStaRecFree %d\n", prStaRec->ucIndex);
 
 	ucStaRecIndex = prStaRec->ucIndex;
 	ucBssIndex = prStaRec->ucBssIndex;
