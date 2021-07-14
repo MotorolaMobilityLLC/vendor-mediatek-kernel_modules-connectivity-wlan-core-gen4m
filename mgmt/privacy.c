47b16b6a2197d1921d394986ac3015cfd9d19d3f
Rachel Tseng <Rachel.Tseng@mediatek.com>
2021-07-14 12:00:56 +0800

    [ALPS05882851] wlan: Assign suitable keyid for IGTK
    To correctly use GTK and IGTK, we need to make sure
    the following parameters:
    1) wlanIdx: GTK > IGTK
    2) keyId: 1~2 for GTK and 3 for IGTK
    
    MTK-Commit-Id: 0155b814b481f39c1a65ac02591c1577d3ba3d2f
    
    Change-Id: Ic068797443d32124dd3b0a5ce3f2cac7eb1d0432
    CR-Id: ALPS05882851
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>

--- mgmt/privacy.c
+++ mgmt/privacy.c
@@ -831,8 +831,8 @@ u_int8_t secPrivacySeekForEntry(
 
 	prWtbl = prAdapter->rWifiVar.arWtbl;
 
-	/* reserve wtbl IDX for GTK: 0-2 for wlan0, 3-5 for wlan1 */
-	ucStartIDX = 6;
+	/* reserve wtbl IDX 0, 1 for BIP*/
+	ucStartIDX = 2;
 	ucMaxIDX = prAdapter->ucTxDefaultWlanIndex - 1;
 
 	for (i = ucStartIDX; i <= ucMaxIDX; i++) {
@@ -1114,27 +1114,23 @@ secPrivacySeekForBcEntry(IN struct ADAPTER *prAdapter,
 		prBSSInfo->eCurrentOPMode, prBSSInfo->eNetworkType,
 		fgCheckKeyId);
 
-	/* reserve wtbl IDX for GTK: 0-2 for wlan0, 3-5 for wlan1 */
-	if (IS_BSS_AIS(prBSSInfo) && ucKeyId >= 1 && ucKeyId <= 3) { /* GTK */
-		ucStartIDX = (ucBssIndex == 0)?0:3;
-		ucMaxIDX = (ucBssIndex == 0)?2:5;
-		DBGLOG(RSN, TRACE,
-			"[Wlan index]: IGTK key install ranging from 0 to 5\n");
+	/* reserve wtbl IDX 0, 1 for BIP*/
+	ucStartIDX = 2;
+	ucMaxIDX = prAdapter->ucTxDefaultWlanIndex - 1;
 
+	if (ucAlg == CIPHER_SUITE_BIP) {
+		ucEntry = ucBssIndex;
 	} else {
-		ucStartIDX = 6;
-		ucMaxIDX = prAdapter->ucTxDefaultWlanIndex - 1;
-	}
-
 		for (i = ucStartIDX; i <= ucMaxIDX; i++) {
 
 			if (prWtbl[i].ucUsed && !prWtbl[i].ucPairwise
 				&& prWtbl[i].ucBssIndex == ucBssIndex) {
+
 				if (!fgCheckKeyId) {
 					ucEntry = i;
 					DBGLOG(RSN, TRACE,
-					    "[Wlan index]: Reuse entry #%d for open/wep/wpi\n",
-					    i);
+						"[Wlan index]: Reuse entry #%d for open/wep/wpi\n",
+						i);
 					break;
 				}
 
@@ -1143,8 +1139,8 @@ secPrivacySeekForBcEntry(IN struct ADAPTER *prAdapter,
 						|| prWtbl[i].ucKeyId == 0xFF)) {
 					ucEntry = i;
 					DBGLOG(RSN, TRACE,
-					    "[Wlan index]: Reuse entry #%d\n",
-					    i);
+						"[Wlan index]: Reuse entry #%d\n",
+						i);
 					break;
 				}
 			}
@@ -1155,12 +1151,13 @@ secPrivacySeekForBcEntry(IN struct ADAPTER *prAdapter,
 				if (prWtbl[i].ucUsed == FALSE) {
 					ucEntry = i;
 					DBGLOG(RSN, TRACE,
-					    "[Wlan index]: Assign entry #%d\n",
-					    i);
+					       "[Wlan index]: Assign entry #%d\n",
+						i);
 					break;
 				}
 			}
 		}
+	}
 
 	if (ucEntry < prAdapter->ucTxDefaultWlanIndex) {
 		prWtbl[ucEntry].ucUsed = TRUE;
