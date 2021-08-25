063bdfe0c23888842d7e183e29fd4fc84b3320ea
Candice Gan <candice.gan@mediatek.com>
2021-08-25 21:21:24 +0800

    [ALPS06076525] support Smart Gear
    1. disable Smart Gear
    
    MTK-Commit-Id: fb498af54127279143462da87e786e9d035e347d
    
    Change-Id: I5650f6d299f92c5ac4323ae16566731ede444f91
    CR-Id: ALPS06076525
    Signed-off-by: Candice.Gan <candice.gan@mediatek.com>
    Feature: [Module]Wi-Fi Driver

--- include/config.h
+++ include/config.h
@@ -1492,7 +1492,7 @@
  *------------------------------------------------------------------------------
 */
 #ifndef CFG_SUPPORT_SMART_GEAR
-#define CFG_SUPPORT_SMART_GEAR 1
+#define CFG_SUPPORT_SMART_GEAR 0
 #endif
 
 #define CFG_SUPPORT_WIFI_RNR  1
