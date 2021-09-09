28a9cd24706c8e86c671ac5a0341f7fa26ffad30
CS Kuo <cs.kuo@mediatek.com>
2021-09-09 15:51:03 +0800

    [ALPS06120898] wlan: fix build error when ARP_MONITER_ENABLE is off
    Fix build errors when ARP_MONITER_ENABLE is turned off.
    
    MTK-Commit-Id: 17f241210e8d5e7fdcfbe197c782afd6763c6c0d
    
    Change-Id: I95a0852f3e59f635c8659d98f90e09895419114e
    CR-Id: ALPS06120898
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: CS Kuo <cs.kuo@mediatek.com>

--- nic/nic_rx.c
+++ nic/nic_rx.c
@@ -2154,13 +2154,12 @@ void nicRxProcessDataPacket(IN struct ADAPTER *prAdapter,
 					prStaRec = cnmGetStaRecByIndex(
 						prAdapter,
 						prRetSwRfb->ucStaRecIdx);
+#if ARP_MONITER_ENABLE
 					if (prStaRec &&
 						IS_STA_IN_AIS(prStaRec)) {
-#if ARP_MONITER_ENABLE
 						qmHandleRxArpPackets(
 							prAdapter,
 							prRetSwRfb);
-#endif
 					}
 
 					if (prStaRec) { /* STA or GC */
@@ -2168,6 +2167,7 @@ void nicRxProcessDataPacket(IN struct ADAPTER *prAdapter,
 							prAdapter,
 							prRetSwRfb);
 					}
+#endif
 #if CFG_SUPPORT_WIFI_SYSDVT
 #if (CFG_SUPPORT_CONNAC2X == 1)
 					/* Not handle non-CONNAC2X case */
