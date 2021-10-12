ef4af4bda5d00c0e233e75db9e79a76241323f8e
Sticky Chen <sticky.chen@mediatek.com>
2021-10-12 15:14:22 +0800

    [ALPS06184350] Fix BW160 in MCS map
    When filling MCS map, driver should get bandwidth
    by heGetBssBandBw() as other fields do
    
    MTK-Commit-Id: a754485a27a9613fc4080de0a2712a29b907d552
    
    CR-Id: ALPS06184350
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Sticky Chen <sticky.chen@mediatek.com>
    Change-Id: I66c5552048c7e6d24711bf868b17a36a27320d01

--- mgmt/he_rlm.c
+++ mgmt/he_rlm.c
@@ -457,7 +457,6 @@ static void heRlmFillHeCapIE(
 	struct _IE_HE_CAP_T *prHeCap;
 	struct _HE_SUPPORTED_MCS_FIELD *prHeSupportedMcsSet;
 	uint32_t u4OverallLen = OFFSET_OF(struct _IE_HE_CAP_T, aucVarInfo[0]);
-	uint16_t ucMaxBw;
 	u_int8_t fgBfEn = TRUE;
 	uint32_t soundingDim = 0;
 
@@ -482,8 +481,6 @@ static void heRlmFillHeCapIE(
 
 	prHeCap->ucId = ELEM_ID_RESERVED;
 	prHeCap->ucExtId = ELEM_EXT_ID_HE_CAP;
-
-	ucMaxBw = cnmGetBssMaxBw(prAdapter, prBssInfo->ucBssIndex);
 
 	/* MAC capabilities */
 	HE_RESET_MAC_CAP(prHeCap->ucHeMacCap);
@@ -630,14 +627,16 @@ static void heRlmFillHeCapIE(
 	heRlmFillMCSMap(prAdapter, prBssInfo, prHeSupportedMcsSet);
 	u4OverallLen += sizeof(struct _HE_SUPPORTED_MCS_FIELD);
 
-	if (ucMaxBw >= MAX_BW_160MHZ) {
+	if (heGetBssBandBw(prAdapter, prBssInfo, eHePhyCapBand)
+		>= MAX_BW_160MHZ) {
 		prHeSupportedMcsSet = (struct _HE_SUPPORTED_MCS_FIELD *)
 			(((uint8_t *) prHeCap) + u4OverallLen);
 		heRlmFillMCSMap(prAdapter, prBssInfo, prHeSupportedMcsSet);
 		u4OverallLen += sizeof(struct _HE_SUPPORTED_MCS_FIELD);
 	}
 
-	if (ucMaxBw >= MAX_BW_80_80_MHZ) {
+	if (heGetBssBandBw(prAdapter, prBssInfo, eHePhyCapBand)
+		>= MAX_BW_80_80_MHZ) {
 		prHeSupportedMcsSet = (struct _HE_SUPPORTED_MCS_FIELD *)
 			(((uint8_t *) prHeCap) + u4OverallLen);
 		heRlmFillMCSMap(prAdapter, prBssInfo, prHeSupportedMcsSet);
