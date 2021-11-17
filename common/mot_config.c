7c9486139100f22ebe698733c42eb28c3525b3dc
liaoxl <liaoxl@motorola.com>
2021-11-17 16:45:03 +0800

    wlan: use moto bootargs for WiFi sku config
    Linux Kernel MailList not suggest to use saved_command_line
    in DLMK directly, so we use instead of moto bootargs.
    
    Change-Id: I7ffb60580756ef6c42921fc13aeebfc7fabda940
    Reviewed-on: https://gerrit.mot.com/2119025
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- common/mot_config.c
+++ common/mot_config.c
@@ -1,5 +1,7 @@
 #include "mot_config.h"
 #include <linux/string.h>
+/* for reading moto bootargs */
+#include <linux/of.h>
 
 // BEGIN IKSWR-130356
 typedef struct moto_product {
@@ -18,36 +20,77 @@ static moto_product products_list[] = {
 };
 
 
-void read_cmdline(char* value, char* root_cmdline)
+/* use moto bootargs to get device name & radio parameters */
+static char *bootargs_str;
+
+static int mmi_get_bootarg_dt(char *key, char **value, char *prop, char *spl_flag)
 {
-	char format[ARRAY_VALUE_MAX] = { 0 };
-	char* rootfsmtd_ptr = strstr(saved_command_line, root_cmdline);
+	const char *bootargs_tmp = NULL;
+	char *idx = NULL;
+	char *kvpair = NULL;
+	int err = 1;
+	struct device_node *n = of_find_node_by_path("/chosen");
+	size_t bootargs_tmp_len = 0;
 
-	if (rootfsmtd_ptr) {
-		memcpy(format, root_cmdline, strlen(root_cmdline));
-		strcat(format, "%s");
-		DBGLOG(RLM, ERROR, "[MOTO]format:%s\n", format);
+	if (n == NULL)
+		goto err;
 
-		sscanf(rootfsmtd_ptr, format, value);
+	if (of_property_read_string(n, prop, &bootargs_tmp) != 0)
+		goto putnode;
 
-		DBGLOG(RLM, ERROR, "[MOTO]read_cmdline:%s%s\n", root_cmdline, value);
+	bootargs_tmp_len = strlen(bootargs_tmp);
+	if (!bootargs_str) {
+		/* The following operations need a non-const
+		 * version of bootargs
+		 */
+		bootargs_str = kzalloc(bootargs_tmp_len + 1, GFP_KERNEL);
+		if (!bootargs_str)
+			goto putnode;
+	}
+	strlcpy(bootargs_str, bootargs_tmp, bootargs_tmp_len + 1);
+
+	idx = strnstr(bootargs_str, key, strlen(bootargs_str));
+	if (idx) {
+		kvpair = strsep(&idx, " ");
+		if (kvpair)
+			if (strsep(&kvpair, "=")) {
+				*value = strsep(&kvpair, spl_flag);
+				if (*value)
+					err = 0;
+			}
 	}
 
+putnode:
+	of_node_put(n);
+err:
+	return err;
+}
+
+int mmi_get_bootarg(char *key, char **value)
+{
+#ifdef CONFIG_BOOT_CONFIG
+	return mmi_get_bootarg_dt(key, value, "mmi,bootconfig", "\n");
+#else
+	return mmi_get_bootarg_dt(key, value, "bootargs", " ");
+#endif
 }
 
 void get_moto_config_file_name(char* name, WIFI_CFG_ENUM index)
 {
 	char device[ARRAY_VALUE_MAX] = { 0 };
 	char radio[ARRAY_VALUE_MAX] = { 0 };
-
 	int num = 0;
 	int i = 0;
+	char *s;
 
-	DBGLOG(RLM, ERROR, "[MOTO]get_moto_config_file_name.\n");
-
-	read_cmdline(device, "androidboot.device=");
-	read_cmdline(radio, "androidboot.radio=");
-
+	if (mmi_get_bootarg("androidboot.device=", &s) == 0) {
+		memcpy(device, s, strlen(s));
+		DBGLOG(RLM, INFO, "[MOTO]bootargs get device: %s\n", device);
+	}
+	if (mmi_get_bootarg("androidboot.radio=", &s) == 0) {
+		memcpy(radio, s, strlen(s));
+		DBGLOG(RLM, INFO, "[MOTO]bootargs get radio: %s\n", radio);
+	}
 
     num = sizeof(products_list) / sizeof(moto_product);
     for (i = 0; i < num; i++) {
