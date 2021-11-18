ff42cb2e6c76d24853f23195187007ccba56eb58
rachel.tseng <rachel.tseng@mediatek.com>
2021-11-19 01:16:25 +0800

    [ALPS06375101] Revert abort add key when disconnected
    This reverts patch "abort add key when disconnected"
    
    MTK-Commit-Id: 4425a356e0b70832eb4381f7fc8ccd8f128b18a9
    
    Reason for revert: WEP AP connection fail
    Change-Id: I24963e530c9e23541a2ecacce7a17cb565b0eb57
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    CR-Id: ALPS06375101
    Feature: [Module]Wi-Fi Driver

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -2804,12 +2804,10 @@ wlanoidSetAddKey(IN struct ADAPTER *prAdapter, IN void *pvSetBuffer,
 			/* ASSERT(FALSE); */
 		}
 	} else {
-		if (!IS_BSS_ACTIVE(prBssInfo)) {
+		if (!IS_BSS_ACTIVE(prBssInfo))
 			DBGLOG_LIMITED(REQ, INFO,
 				"[wlan] BSS info (%d) not active yet!",
 				prNewKey->ucBssIdx);
-			return WLAN_STATUS_SUCCESS;
-		}
 	}
 	prCmdKey->ucBssIdx = prBssInfo->ucBssIndex;
 	prCmdKey->ucKeyId = (uint8_t) (prNewKey->u4KeyIndex & 0xff);
