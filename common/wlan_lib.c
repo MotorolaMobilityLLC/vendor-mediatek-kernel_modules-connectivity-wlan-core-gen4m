d83732501999b28096979e0a252a04c2038d5160
devin.yu <devin.yu@mediatek.com>
2022-01-11 19:28:51 +0800

    [ALPS06490417] Change log level from INFO to TRACE
    [Detail] Use TRACE level to decrease log times
    
    MTK-Commit-Id: d1285084458fc09c9d202bb7c24c0b0f5aaad23e
    
    Signed-off-by: devin.yu <devin.yu@mediatek.com>
    CR-Id: ALPS06490417
    Feature: MTBF(*)
    Change-Id: I9737559bda42f18a4615ddafc714b548ad56f9a1
    (cherry picked from commit 9698588674dee7f79ede476888d5a18ffaa52b98)

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -11598,7 +11598,7 @@ wlanGetSpeIdx(IN struct ADAPTER *prAdapter,
 			ucRetValSpeIdx = wlanAntPathFavorSelect(prAdapter,
 				eWfPathFavor);
 	}
-	DBGLOG(INIT, INFO, "SpeIdx:%d,D:%d,G=%d,B=%d,Bss=%d\n",
+	DBGLOG(INIT, TRACE, "SpeIdx:%d,D:%d,G=%d,B=%d,Bss=%d\n",
 	       ucRetValSpeIdx, prAdapter->rWifiVar.fgDbDcModeEn,
 	       prBssInfo->fgIsGranted, eBand, ucBssIndex);
 #endif
