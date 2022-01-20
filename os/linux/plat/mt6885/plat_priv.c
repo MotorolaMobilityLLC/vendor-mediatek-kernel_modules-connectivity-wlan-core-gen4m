d34b3165c35fb5783b9145d0f7722c5eaedb967a
Wyatt Sun <wyatt.sun@mediatek.com>
2022-01-20 11:17:20 +0800

    [ALPS06522029] Linux kernel 4.19 support for mt6885
    Apply the same patch from 6893.
    
    MTK-Commit-Id: ef3ea28e3273b9ad0f7423473190dce22a317cec
    
    CR-Id: ALPS06522029
    Feature: Wi-Fi Driver Gen2
    Change-Id: Ie5780caa5c8d85a41a493be7c29d2259d8aa9f6d
    Signed-off-by: Wyatt Sun <wyatt.sun@mediatek.com>

--- os/linux/plat/mt6885/plat_priv.c
+++ os/linux/plat/mt6885/plat_priv.c
@@ -3,12 +3,34 @@
  * Copyright (c) 2019 MediaTek Inc.
  */
 
+#include "gl_os.h"
+
+#if KERNEL_VERSION(5, 4, 0) <= CFG80211_VERSION_CODE
+#include <uapi/linux/sched/types.h>
+#include <linux/sched/task.h>
+#include <linux/cpufreq.h>
+#elif KERNEL_VERSION(4, 19, 0) <= CFG80211_VERSION_CODE
+#include <cpu_ctrl.h>
+#include <topo_ctrl.h>
+#include <linux/soc/mediatek/mtk-pm-qos.h>
+#include <helio-dvfsrc-opp.h>
+#define pm_qos_add_request(_req, _class, _value) \
+		mtk_pm_qos_add_request(_req, _class, _value)
+#define pm_qos_update_request(_req, _value) \
+		mtk_pm_qos_update_request(_req, _value)
+#define pm_qos_remove_request(_req) \
+		mtk_pm_qos_remove_request(_req)
+#define pm_qos_request mtk_pm_qos_request
+#define PM_QOS_DDR_OPP MTK_PM_QOS_DDR_OPP
+#define ppm_limit_data cpu_ctrl_data
+#else
+
 #include <cpu_ctrl.h>
 #include <topo_ctrl.h>
 
 #include <linux/pm_qos.h>
 #include <helio-dvfsrc-opp-mt6885.h>
-
+#endif
 #include "precomp.h"
 
 #ifdef CFG_MTK_ANDROID_EMI
