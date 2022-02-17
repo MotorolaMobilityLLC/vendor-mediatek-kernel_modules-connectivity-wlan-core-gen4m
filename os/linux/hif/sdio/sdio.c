eb82d605cba85fc47f527fcf423443fa8e6ce750
Terry Wu <terry.wu@mediatek.com>
2022-02-17 16:07:43 +0800

    [ALPS03042000] WLAN: Add Rx Error Handling
    [Detail]
    1. Add Rx invalid packet error handling
    2. Add Rx underflow error handling
    
    [Solution]
    1. Add Rx de-aggregation format check
    2. Drop all format error Rx packet to avoid KE
    3. Skip all Rx if Rx underflow occurred
    
    MTK-Commit-Id: 85f7eca606545f57df39338718e3f83dc545ea68
    
    Change-Id: I7ee69e390fb6b50a86917ca23d9aa1b1da036075
    CR-Id: ALPS03042000
    Feature: MT6632 Driver
    Signed-off-by: Terry Wu <terry.wu@mediatek.com>
    (cherry picked from commit df19217808312e5df0ec299448de22ef515e99ad)

--- os/linux/hif/sdio/sdio.c
+++ os/linux/hif/sdio/sdio.c
@@ -642,6 +642,7 @@ VOID glSetHifInfo(P_GLUE_INFO_T prGlueInfo, ULONG ulCookie)
 	mutex_init(&prHif->rRxFreeBufQueMutex);
 	mutex_init(&prHif->rRxDeAggQueMutex);
 
+	prHif->fgSkipRx = FALSE;
 	prGlueInfo->u4InfType = MT_DEV_INF_SDIO;
 }				/* end of glSetHifInfo() */
 
