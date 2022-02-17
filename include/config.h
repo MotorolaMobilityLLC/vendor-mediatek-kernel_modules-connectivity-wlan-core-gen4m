959aff055b63a95115d5a7109e2ea2632eefeb19
perry.liao <perry.liao@mediatek.com>
2022-02-17 16:59:40 +0800

    [ALPS05587429] mddp: support AOR
    MDDP AOR support on SOC2 1x1
    Use chip id to check
    6855: support AOR
    6833: not support AOR
    
    MTK-Commit-Id: f86bd08051c997f9db677e0c1339f42fe7cf4095
    
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Perry Liao <perry.liao@mediatek.com>
    CR-Id: ALPS05587429
    Change-Id: I07586239282b40cb51f516b96f81b6497da0217d
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/5438297
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Reviewed-by: tf.huang <tf.huang@mediatek.com>
    Tested-by: herman.yang <herman.yang@mediatek.com>
    Build: herman.yang <herman.yang@mediatek.com>
    Test: herman.yang <herman.yang@mediatek.com>

--- include/config.h
+++ include/config.h
@@ -1538,6 +1538,10 @@
 #define CFG_ROM_PATCH_NO_SEM_CTRL 0
 #endif
 
+#ifndef CFG_SUPPORT_MDDP_AOR
+#define CFG_SUPPORT_MDDP_AOR 0
+#endif
+
 /*------------------------------------------------------------------------------
  * Flags of Disconnect with disable channel based on REGD update
  *------------------------------------------------------------------------------
