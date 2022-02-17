35042d7a0b6a588f216a49b34916efd911630570
Chun-Wei Tseng <chun-wei.tseng@mediatek.com>
2022-02-17 16:50:55 +0800

    [ALPS06075093] Support EHT IE
    Support EHT IE
    
    MTK-Commit-Id: 9741e320ddb5c51b768c3c362d49fb5c34fb97f4
    
    Signed-off-by: Chun-Wei Tseng <chun-wei.tseng@mediatek.com>
    Change-Id: I036415c5df424d65dba78caf22e6d4ab22cbc798
    CR-Id: ALPS06075093
    Feature: [Module]Wi-Fi Driver

--- include/mgmt/eht_ie.h
+++ include/mgmt/eht_ie.h
@@ -21,7 +21,7 @@
 #define EHT_MAC_CAP_BYTE_NUM                            (2)
 #define EHT_PHY_CAP_BYTE_NUM                            (8)
 
-struct _IE_EHT_CAP_T {
+struct IE_EHT_CAP {
 	u_int8_t  ucId;
 	u_int8_t  ucLength;
 	u_int8_t  ucExtId;
@@ -117,7 +117,7 @@ struct _IE_EHT_OP_T {
 /* EHT PHY Capabilities Information field */
 
 /* Support Of MCS 15 subfield */
-enum eht_mru_set_mcs15 {
+enum EHT_MRU_SET_MCS15 {
 	MRU_106TONE_OR_52TONE_W_26TONE = 1,
 	MRU_484TONE_W_242TONE_IN_80M = (1 << 1),
 	MRU_996TONE_TO_242TONE_IN_160M = (1 << 2),
@@ -125,7 +125,7 @@ enum eht_mru_set_mcs15 {
 };
 
 /* Common Nominal Packet Padding */
-enum eht_common_nominal_padd_us {
+enum EHT_COMMON_NOMINAL_PADD_US {
 	COMMON_NOMINAL_PAD_0_US,
 	COMMON_NOMINAL_PAD_8_US,
 	COMMON_NOMINAL_PAD_16_US,
@@ -133,12 +133,12 @@ enum eht_common_nominal_padd_us {
 };
 
 /* Maximum Number Of Supported EHT-LTFs */
-enum eht_max_ltf {
+enum EHT_MAX_LTF {
 	MAX_EHT_LTF_NUM_4,
 	MAX_EHT_LTF_NUM_8,
 };
 
-enum eht_bf_caps {
+enum EHT_BF_CAPS {
 	EHT_SU_BFER = 1,
 	EHT_SU_BFEE = 1 << 1,
 	EHT_MU_BFER = 1 << 2,
@@ -150,19 +150,19 @@ enum eht_bf_caps {
 	EHT_TRIGED_MU_BF_PATIAL_BW_FEEDBACK = 1 << 8,
 };
 
-enum eht_gi_caps {
+enum EHT_GI_CAPS {
 	EHT_NDP_4X_EHT_LTF_3DOT2US_GI = 1,
 	EHT_MU_PPDU_4X_EHT_LTF_DOT8US_GI = 1 << 1,
 };
 
-enum eht_mcs15_mru_support {
+enum EHT_MCS15_MRU_SUPPORT {
 	EHT_MCS15_MRU_106_or_52_w_26_tone = 1,
 	EHT_MCS15_MRU_484_w_242_tone_80M = 1 << 1,
 	EHT_MCS15_MRU_996_to_242_tone_160M = 1 << 2,
 	EHT_MCS15_MRU_3x996_tone_320M = 1 << 3,
 };
 
-enum eht_phy_caps {
+enum EHT_PHY_CAPS {
 	EHT_320M_6G = 1,
 	EHT_242_TONE_RU_WT_20M = 1 << 1,
 	EHT_PARTIAL_BW_UL_MI_MIMO  = 1 << 2,
@@ -179,8 +179,8 @@ enum eht_phy_caps {
 	EHT_20M_RX_NDP_W_WIDER_BW = 1 << 13,
 };
 
-struct eht_bf_info {
-	enum eht_bf_caps bf_cap;
+struct EHT_BF_INFO {
+	enum EHT_BF_CAPS bf_cap;
 	uint8_t bfee_ss_le_eq_bw80;
 	uint8_t bfee_ss_bw160;
 	uint8_t bfee_ss_bw320;
@@ -192,9 +192,22 @@ struct eht_bf_info {
 	uint8_t max_ltf_num_mu;
 };
 
-struct eht_phy_capinfo {
+struct EHT_PHY_CAP_INFO {
 	uint32_t phy_capinfo_1;
 	uint32_t phy_capinfo_2;
+} __KAL_ATTRIB_PACKED__;
+
+struct EHT_SUPPORTED_MCS_BW20_FIELD {
+	u_int8_t eht_bw20_mcs_0_7;
+	u_int8_t eht_bw20_mcs_8_9;
+	u_int8_t eht_bw20_mcs_10_11;
+	u_int8_t eht_bw20_mcs_12_13;
+} __KAL_ATTRIB_PACKED__;
+
+struct EHT_SUPPORTED_MCS_BW80_160_320_FIELD {
+	u_int8_t eht_mcs_0_9;
+	u_int8_t eht_mcs_10_11;
+	u_int8_t eht_mcs_12_13;
 } __KAL_ATTRIB_PACKED__;
 
 #define SET_CAP_BITS(_cap, _capinfo, _value)\
@@ -218,7 +231,6 @@ struct eht_phy_capinfo {
 #define DOT11BE_PHY_CAP_PARTIAL_BW_UL_MU_MIMO BIT(4)
 #define DOT11BE_PHY_CAP_SU_BFER BIT(5)
 #define DOT11BE_PHY_CAP_SU_BFEE BIT(6)
-#define DOT11BE_PHY_CAP_MU_BFER BIT(7)
 #define DOT11BE_PHY_CAP_BFEE_SS_LE_EQ_80M_BITS 3
 #define DOT11BE_PHY_CAP_BFEE_SS_LE_EQ_80M_SHIFT 7
 #define DOT11BE_PHY_CAP_BFEE_SS_LE_EQ_80M_MASK	\
@@ -267,22 +279,23 @@ struct eht_phy_capinfo {
 	SET_CAP_BITS(DOT11BE_PHY_CAP_SOUND_DIM_NUM_320M, _capinfo, _value)
 #define GET_DOT11BE_PHY_CAP_SOUND_DIM_NUM_320M(_capinfo) \
 	GET_CAP_BITS(DOT11BE_PHY_CAP_SOUND_DIM_NUM_320M, _capinfo)
-#define DOT11BE_PHY_CAP_NG16_SU_FEEDBACK BIT(26)
+#define DOT11BE_PHY_CAP_NG16_SU_FEEDBACK BIT(25)
 /* TODO: name and definition do not match, table 9-322ap */
-#define DOT11BE_PHY_CAP_NG32_SU_FEEDBACK BIT(27)
-#define DOT11BE_PHY_CAP_CODEBOOK_4_2_SU_FEEDBACK BIT(28)
+#define DOT11BE_PHY_CAP_NG16_MU_FEEDBACK BIT(26)
+#define DOT11BE_PHY_CAP_CODEBOOK_4_2_SU_FEEDBACK BIT(27)
 /* TODO: name and definition do not match, table 9-322ap */
-#define DOT11BE_PHY_CAP_CODEBOOK_7_5_SU_FEEDBACK BIT(29)
-#define DOT11BE_PHY_CAP_TRIGED_SU_BF_FEEDBACK BIT(30)
-#define DOT11BE_PHY_CAP_TRIGED_MU_BF_PARTIAL_BW_FEEDBACK BIT(31)
+#define DOT11BE_PHY_CAP_CODEBOOK_7_5_MU_FEEDBACK BIT(28)
+#define DOT11BE_PHY_CAP_TRIGED_SU_BF_FEEDBACK BIT(29)
+#define DOT11BE_PHY_CAP_TRIGED_MU_BF_PARTIAL_BW_FEEDBACK BIT(30)
+#define DOT11BE_PHY_CAP_TRIGED_CQI_FEEDBACK BIT(31)
+
 
 /* phy_capinfo_2: bit32..bit63 */
-#define DOT11BE_PHY_CAP_TRIGED_CQI_FEEDBACK BIT(0)
-#define DOT11BE_PHY_CAP_PARTIAL_BW_DL_MU_MIMO BIT(1)
-#define DOT11BE_PHY_CAP_PSR_BASED_SR BIT(2)
-#define DOT11BE_PHY_CAP_POWER_BOOST_FACTOR BIT(3)
-#define DOT11BE_PHY_CAP_EHT_MU_PPDU_4X_EHT_LTF_DOT8US_GI BIT(4)
-#define DOT11BE_PHY_CAP_MAX_NC_BITS 3
+#define DOT11BE_PHY_CAP_PARTIAL_BW_DL_MU_MIMO BIT(0)
+#define DOT11BE_PHY_CAP_PSR_BASED_SR BIT(1)
+#define DOT11BE_PHY_CAP_POWER_BOOST_FACTOR BIT(2)
+#define DOT11BE_PHY_CAP_EHT_MU_PPDU_4X_EHT_LTF_DOT8US_GI BIT(3)
+#define DOT11BE_PHY_CAP_MAX_NC_BITS 4
 #define DOT11BE_PHY_CAP_MAX_NC_SHIFT 4
 #define DOT11BE_PHY_CAP_MAX_NC_MASK \
 	CAP_BITS_MASK(DOT11BE_PHY_CAP_MAX_NC)
@@ -295,32 +308,23 @@ struct eht_phy_capinfo {
 #define DOT11BE_PHY_CAP_RX_1024QAM_4096QAM_LE_242_TONE_RU BIT(10)
 #define DOT11BE_PHY_CAP_PPE_THRLD_PRESENT BIT(11)
 #define DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD_BITS 2
-#define DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD_SHIFT 11
+#define DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD_SHIFT 12
 #define DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD_MASK \
 	CAP_BITS_MASK(DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD)
 #define SET_DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD(_capinfo, _value) \
 	SET_CAP_BITS(DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD, _capinfo, _value)
 #define GET_DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD(_capinfo) \
-	GET_CAP_BITS(DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD, _capinfo)
-#define DOT11BE_PHY_CAP_EXTRA_MAX_EHT_LTF_NUM_SU BIT(13)
-#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU_BITS 2
-#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU_SHIFT 14
-#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU_MASK \
-	CAP_BITS_MASK(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU)
-#define SET_DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU(_capinfo, _value) \
-	SET_CAP_BITS(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU, _capinfo, _value)
-#define GET_DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU(_capinfo) \
-	GET_CAP_BITS(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SU, _capinfo)
-#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU_BITS 2
-#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU_SHIFT 16
-#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU_MASK \
-	CAP_BITS_MASK(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU)
-#define SET_DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU(_capinfo, _value) \
-	SET_CAP_BITS(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU, _capinfo, _value)
-#define GET_DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU(_capinfo) \
-	GET_CAP_BITS(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MU, _capinfo)
+	GET_CAP_BITS(DOT11BE_PHY_CAP_COMMON_NOMINAL_PKT_PAD, _capinfo)  
+#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_BITS 5
+#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_SHIFT 14
+#define DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM_MASK \
+	CAP_BITS_MASK(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM)
+#define SET_DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM(_capinfo, _value) \
+	SET_CAP_BITS(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM, _capinfo, _value)
+#define GET_DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM(_capinfo) \
+	GET_CAP_BITS(DOT11BE_PHY_CAP_MAX_EHT_LTF_NUM, _capinfo)
 #define DOT11BE_PHY_CAP_MCS_15_BITS 4
-#define DOT11BE_PHY_CAP_MCS_15_SHIFT 18
+#define DOT11BE_PHY_CAP_MCS_15_SHIFT 19
 #define DOT11BE_PHY_CAP_MCS_15_MASK \
 	CAP_BITS_MASK(DOT11BE_PHY_CAP_MCS_15)
 #define SET_DOT11BE_PHY_CAP_MCS_15(_capinfo, _value) \
@@ -329,6 +333,18 @@ struct eht_phy_capinfo {
 	GET_CAP_BITS(DOT11BE_PHY_CAP_MCS_15, _capinfo)
 #define DOT11BE_PHY_CAP_EHT_DUP_6G BIT(23)
 #define DOT11BE_PHY_CAP_20M_RX_NDP_W_WIDER_BW BIT(24)
+#define DOT11BE_PHY_CAP_NON_OFDMA_UL_MU_MIMO_80M BIT(25)
+#define DOT11BE_PHY_CAP_NON_OFDMA_UL_MU_MIMO_160M BIT(26)
+#define DOT11BE_PHY_CAP_NON_OFDMA_UL_MU_MIMO_320M BIT(27)
+#define DOT11BE_PHY_CAP_MU_BFER_80M BIT(28)
+#define DOT11BE_PHY_CAP_MU_BFER_160M BIT(29)
+#define DOT11BE_PHY_CAP_MU_BFER_320M BIT(30)
+
+#define MAX_SS_MASK 0xF
+#define GET_TX_MAX_SS(_x) ((_x>>4)&MAX_SS_MASK)
+#define GET_RX_MAX_SS(_x) (_x&MAX_SS_MASK)
+#define SET_TX_MAX_SS(_mcs, _x) (_mcs |= ((_x&MAX_SS_MASK) << 4))
+#define SET_RX_MAX_SS(_mcs, _x) (_mcs |= (_x&MAX_SS_MASK))
 
 #endif /* CFG_SUPPORT_802_11BE == 1 */
 #endif /* !_EHT_IE_H */
