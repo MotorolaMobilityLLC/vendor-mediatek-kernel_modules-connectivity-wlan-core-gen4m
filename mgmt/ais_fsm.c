829086a89ceac33f3e7f2498b4cdcf1c0162ea20
Xianpu Liang <xianpu.liang@mediatek.com>
2022-02-17 16:09:19 +0800

    [ALPS03273519] wlan: support APs with same bssid but different SSID
    [Detail]
    If two or more AP share the same bssid, but are set differenct SSID,
    connecting to the AP may fail.
    [Solution]
    Search AP by bssid and SSID in wlan driver
    
    MTK-Commit-Id: e2ef3f903cca1e84cee99b8b30a1e238f9f47a07
    
    Change-Id: Ib82b57fec1044087179b2749b7aa5c3d09d113dc
    Signed-off-by: Xianpu Liang <xianpu.liang@mediatek.com>
    CR-Id: ALPS03273519
    Feature: Others
    (cherry picked from commit e37ae0a18db579b0ea3bf49fc8c2aa939d8d904c)
    Reviewed-on: http://gerrit.mediatek.inc:8080/1222736
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Tested-by: Wayne-TW Chen <wayne-tw.chen@mediatek.com>
    Reviewed-by: Terry Wu <terry.wu@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- mgmt/ais_fsm.c
+++ mgmt/ais_fsm.c
@@ -727,7 +727,8 @@ VOID aisFsmStateAbort_JOIN(IN P_ADAPTER_T prAdapter)
 	prJoinAbortMsg->ucSeqNum = prAisFsmInfo->ucSeqNumOfReqMsg;
 	prJoinAbortMsg->prStaRec = prAisFsmInfo->prTargetStaRec;
 
-	scanRemoveConnFlagOfBssDescByBssid(prAdapter, prAisFsmInfo->prTargetStaRec->aucMacAddr);
+	prAisFsmInfo->prTargetBssDesc->fgIsConnected = FALSE;
+	prAisFsmInfo->prTargetBssDesc->fgIsConnecting = FALSE;
 
 	mboxSendMsg(prAdapter, MBOX_ID_0, (P_MSG_HDR_T) prJoinAbortMsg, MSG_SEND_METHOD_BUF);
 
@@ -2041,7 +2042,7 @@ enum _ENUM_AIS_STATE_T aisFsmJoinCompleteAction(IN struct _ADAPTER_T *prAdapter,
 				/* 3.2 reset local variable */
 				prAisFsmInfo->fgIsInfraChannelFinished = TRUE;
 
-				prBssDesc = scanSearchBssDescByBssid(prAdapter, prStaRec->aucMacAddr);
+				prBssDesc = prAisFsmInfo->prTargetBssDesc;
 
 				if (prBssDesc == NULL)
 					break;
@@ -2593,7 +2594,7 @@ VOID aisUpdateBssInfoForJOIN(IN P_ADAPTER_T prAdapter, P_STA_RECORD_T prStaRec, 
 	prAisBssInfo->fgIsQBSS = prStaRec->fgIsQoS;
 
 	/* 3 <4> Update BSS_INFO_T from BSS_DESC_T */
-	prBssDesc = scanSearchBssDescByBssid(prAdapter, prAssocRspFrame->aucBSSID);
+	prBssDesc = prAisFsmInfo->prTargetBssDesc;
 	if (prBssDesc) {
 		prBssDesc->fgIsConnecting = FALSE;
 		prBssDesc->fgIsConnected = TRUE;
@@ -2961,6 +2962,8 @@ VOID aisFsmDisconnect(IN P_ADAPTER_T prAdapter, IN BOOLEAN fgDelayIndication)
 			}
 		} else {
 			scanRemoveConnFlagOfBssDescByBssid(prAdapter, prAisBssInfo->aucBSSID);
+			prAdapter->rWifiVar.rAisFsmInfo.prTargetBssDesc->fgIsConnected = FALSE;
+			prAdapter->rWifiVar.rAisFsmInfo.prTargetBssDesc->fgIsConnecting = FALSE;
 		}
 
 		if (fgDelayIndication) {
@@ -3822,8 +3825,17 @@ VOID aisFsmRoamingDisconnectPrevAP(IN P_ADAPTER_T prAdapter, IN P_STA_RECORD_T p
 	/* rlmBssAborted(prAdapter, prAisBssInfo); */
 
 	/* 4 <3> Unset the fgIsConnected flag of BSS_DESC_T and send Deauth if needed. */
-	if (prAisBssInfo->eConnectionState == PARAM_MEDIA_STATE_CONNECTED)
-		scanRemoveConnFlagOfBssDescByBssid(prAdapter, prAisBssInfo->aucBSSID);
+	if (prAisBssInfo->eConnectionState == PARAM_MEDIA_STATE_CONNECTED) {
+		PARAM_SSID_T rSsid;
+		P_BSS_DESC_T prBssDesc = NULL;
+
+		COPY_SSID(rSsid.aucSsid, rSsid.u4SsidLen, prAisBssInfo->aucSSID, prAisBssInfo->ucSSIDLen);
+		prBssDesc = scanSearchBssDescByBssidAndSsid(prAdapter, prAisBssInfo->aucBSSID, TRUE, &rSsid);
+		if (prBssDesc) {
+			prBssDesc->fgIsConnected = FALSE;
+			prBssDesc->fgIsConnecting = FALSE;
+		}
+	}
 
 	/* 4 <4> Change Media State immediately. */
 	aisChangeMediaState(prAdapter, PARAM_MEDIA_STATE_DISCONNECTED);
