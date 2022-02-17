2150381dab3f367372d2aa27ffd1dfdcdb33d590
Zeno Yang <zeno.yang@mediatek.com>
2022-02-17 16:12:02 +0800

    [ALPS03645093] wlan: Fix check patch issues
    [Detail]
    Fix check patch warnings and errors.
    (Patch 4)
    
    [Solution]
    Refine the coding style
    
    MTK-Commit-Id: e4d85504c3a6d2303183424b267ac1558067f91e
    
    Feature: Driver
    CR-Id: ALPS03645093
    Change-Id: I8740b757aa4c27b07ff6499d5dca176b7301cf33
    Signed-off-by: Zeno Yang <zeno.yang@mediatek.com>
    Reviewed-on: http://gerrit.mediatek.inc:8080/1287204
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Weihung Su <wh.su@mediatek.com>
    Reviewed-by: Ben Lai <ben.lai@mediatek.com>
    Reviewed-by: George Kuo <george.kuo@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- mgmt/p2p_rlm_obss.c
+++ mgmt/p2p_rlm_obss.c
@@ -50,98 +50,106 @@
  *
  *****************************************************************************/
 /*
-** Id: @(#) gl_p2p_cfg80211.c@@
-*/
+ ** Id: @(#) gl_p2p_cfg80211.c@@
+ */
 
 /*! \file   gl_p2p_cfg80211.c
-*    \brief  Main routines of Linux driver interface for Wi-Fi Direct
-*	    using cfg80211 interface
-*
-*    This file contains the main routines of Linux driver for MediaTek Inc. 802.11
-*    Wireless LAN Adapters.
-*/
+ *    \brief  Main routines of Linux driver interface for Wi-Fi Direct
+ *	    using cfg80211 interface
+ *
+ *    This file contains the main routines of Linux driver
+ *    for MediaTek Inc. 802.11
+ *    Wireless LAN Adapters.
+ */
 
-/*******************************************************************************
-*                         C O M P I L E R   F L A G S
-********************************************************************************
-*/
+/******************************************************************************
+ *                         C O M P I L E R   F L A G S
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                    E X T E R N A L   R E F E R E N C E S
-********************************************************************************
-*/
+/******************************************************************************
+ *                    E X T E R N A L   R E F E R E N C E S
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                              C O N S T A N T S
-********************************************************************************
-*/
+/******************************************************************************
+ *                              C O N S T A N T S
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                             D A T A   T Y P E S
-********************************************************************************
-*/
+/******************************************************************************
+ *                             D A T A   T Y P E S
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                            P U B L I C   D A T A
-********************************************************************************
-*/
+/******************************************************************************
+ *                            P U B L I C   D A T A
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                           P R I V A T E   D A T A
-********************************************************************************
-*/
+/******************************************************************************
+ *                           P R I V A T E   D A T A
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                                 M A C R O S
-********************************************************************************
-*/
+/******************************************************************************
+ *                                 M A C R O S
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                   F U N C T I O N   D E C L A R A T I O N S
-********************************************************************************
-*/
+/******************************************************************************
+ *                   F U N C T I O N   D E C L A R A T I O N S
+ ******************************************************************************
+ */
 
-/*******************************************************************************
-*                              F U N C T I O N S
-********************************************************************************
-*/
+/******************************************************************************
+ *                              F U N C T I O N S
+ ******************************************************************************
+ */
 
 #include "precomp.h"
 
-static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo, uint8_t ucPriChannel, enum ENUM_CHNL_EXT eExtend);
+static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo,
+		uint8_t ucPriChannel, enum ENUM_CHNL_EXT eExtend);
 
-static uint8_t rlmObssChnlLevelIn5G(struct BSS_INFO *prBssInfo, uint8_t ucPriChannel, enum ENUM_CHNL_EXT eExtend);
+static uint8_t rlmObssChnlLevelIn5G(struct BSS_INFO *prBssInfo,
+		uint8_t ucPriChannel, enum ENUM_CHNL_EXT eExtend);
 
 /*----------------------------------------------------------------------------*/
 /*!
-* \brief Different concurrent network has itself channel lists, and
-*        concurrent networks should have been recorded in channel lists.
-*        If role of active P2P is GO, assume associated AP of AIS will
-*        record our Beacon for P2P GO because of same channel.
-*
-*        Note: If we have scenario of different channel in the future,
-*              the internal FW communication channel shall be established.
-*
-* \param[in]
-*
-* \return none
-*/
+ * \brief Different concurrent network has itself channel lists, and
+ *        concurrent networks should have been recorded in channel lists.
+ *        If role of active P2P is GO, assume associated AP of AIS will
+ *        record our Beacon for P2P GO because of same channel.
+ *
+ *        Note: If we have scenario of different channel in the future,
+ *              the internal FW communication channel shall be established.
+ *
+ * \param[in]
+ *
+ * \return none
+ */
 /*----------------------------------------------------------------------------*/
-uint8_t rlmObssChnlLevel(struct BSS_INFO *prBssInfo, enum ENUM_BAND eBand, uint8_t ucPriChannel, enum ENUM_CHNL_EXT eExtend)
+uint8_t rlmObssChnlLevel(struct BSS_INFO *prBssInfo,
+		enum ENUM_BAND eBand,
+		uint8_t ucPriChannel,
+		enum ENUM_CHNL_EXT eExtend)
 {
 	uint8_t ucChannelLevel;
 
 	ASSERT(prBssInfo);
 
 	if (eBand == BAND_2G4) {
-		ucChannelLevel = rlmObssChnlLevelIn2G4(prBssInfo, ucPriChannel, eExtend);
+		ucChannelLevel =
+			rlmObssChnlLevelIn2G4(prBssInfo, ucPriChannel, eExtend);
 
 		/* (TBD) If concurrent networks permit different channel, extra
 		 *       channel judgement should be added. Please refer to
 		 *       previous version of this file.
 		 */
 	} else if (eBand == BAND_5G) {
-		ucChannelLevel = rlmObssChnlLevelIn5G(prBssInfo, ucPriChannel, eExtend);
+		ucChannelLevel =
+			rlmObssChnlLevelIn5G(prBssInfo, ucPriChannel, eExtend);
 
 		/* (TBD) If concurrent networks permit different channel, extra
 		 *       channel judgement should be added. Please refer to
@@ -156,14 +164,16 @@ uint8_t rlmObssChnlLevel(struct BSS_INFO *prBssInfo, enum ENUM_BAND eBand, uint8
 
 /*----------------------------------------------------------------------------*/
 /*!
-* \brief
-*
-* \param[in]
-*
-* \return none
-*/
+ * \brief
+ *
+ * \param[in]
+ *
+ * \return none
+ */
 /*----------------------------------------------------------------------------*/
-static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo, uint8_t ucPriChannel, enum ENUM_CHNL_EXT eExtend)
+static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo,
+		uint8_t ucPriChannel,
+		enum ENUM_CHNL_EXT eExtend)
 {
 	uint8_t i, ucChannelLevel;
 	uint8_t ucSecChannel, ucCenterChannel;
@@ -186,17 +196,24 @@ static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo, uint8_t ucPriCh
 	ASSERT(ucCenterChannel >= 1 && ucCenterChannel <= 14);
 
 	/* Calculated low/upper channels in affected freq range */
-	ucAffectedChnl_L = (ucCenterChannel <= AFFECTED_CHNL_OFFSET) ? 1 : (ucCenterChannel - AFFECTED_CHNL_OFFSET);
+	ucAffectedChnl_L =
+		(ucCenterChannel <= AFFECTED_CHNL_OFFSET)
+		? 1
+		: (ucCenterChannel - AFFECTED_CHNL_OFFSET);
 
-	ucAffectedChnl_H = (ucCenterChannel >= (14 - AFFECTED_CHNL_OFFSET)) ?
-	    14 : (ucCenterChannel + AFFECTED_CHNL_OFFSET);
+	ucAffectedChnl_H = (ucCenterChannel >= (14 - AFFECTED_CHNL_OFFSET))
+		? 14
+		: (ucCenterChannel + AFFECTED_CHNL_OFFSET);
 
 	/* Check intolerant (Non-HT) channel list */
 	ASSERT(prBssInfo->auc2G_NonHtChnlList[0] <= CHNL_LIST_SZ_2G);
-	for (i = 1; i <= prBssInfo->auc2G_NonHtChnlList[0] && i <= CHNL_LIST_SZ_2G; i++) {
-		if ((prBssInfo->auc2G_NonHtChnlList[i] >= ucAffectedChnl_L &&
-		     prBssInfo->auc2G_NonHtChnlList[i] <= ucAffectedChnl_H) &&
-		    prBssInfo->auc2G_NonHtChnlList[i] != ucPriChannel) {
+	for (i = 1; i <= prBssInfo->auc2G_NonHtChnlList[0]
+		&& i <= CHNL_LIST_SZ_2G; i++) {
+		if ((prBssInfo->auc2G_NonHtChnlList[i] >= ucAffectedChnl_L
+			&&
+			prBssInfo->auc2G_NonHtChnlList[i] <= ucAffectedChnl_H)
+			&&
+			prBssInfo->auc2G_NonHtChnlList[i] != ucPriChannel) {
 
 			ucChannelLevel = CHNL_LEVEL0;
 			goto L_2G4_level_end;
@@ -205,9 +222,12 @@ static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo, uint8_t ucPriCh
 
 	/* Check 20M BW request channel list */
 	ASSERT(prBssInfo->auc2G_20mReqChnlList[0] <= CHNL_LIST_SZ_2G);
-	for (i = 1; i <= prBssInfo->auc2G_20mReqChnlList[0] && i <= CHNL_LIST_SZ_2G; i++) {
-		if ((prBssInfo->auc2G_20mReqChnlList[i] >= ucAffectedChnl_L &&
-		     prBssInfo->auc2G_20mReqChnlList[i] <= ucAffectedChnl_H)) {
+	for (i = 1; i <= prBssInfo->auc2G_20mReqChnlList[0]
+		&& i <= CHNL_LIST_SZ_2G; i++) {
+		if ((prBssInfo->auc2G_20mReqChnlList[i] >= ucAffectedChnl_L
+			&&
+			prBssInfo->auc2G_20mReqChnlList[i]
+			<= ucAffectedChnl_H)) {
 
 			ucChannelLevel = CHNL_LEVEL0;
 			goto L_2G4_level_end;
@@ -216,10 +236,11 @@ static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo, uint8_t ucPriCh
 
 	/* Check 2.4G primary channel list */
 	ASSERT(prBssInfo->auc2G_PriChnlList[0] <= CHNL_LIST_SZ_2G);
-	for (i = 1; i <= prBssInfo->auc2G_PriChnlList[0] && i <= CHNL_LIST_SZ_2G; i++) {
-		if ((prBssInfo->auc2G_PriChnlList[i] >= ucAffectedChnl_L &&
-		     prBssInfo->auc2G_PriChnlList[i] <= ucAffectedChnl_H) &&
-		    prBssInfo->auc2G_PriChnlList[i] != ucPriChannel) {
+	for (i = 1; i <= prBssInfo->auc2G_PriChnlList[0]
+		&& i <= CHNL_LIST_SZ_2G; i++) {
+		if ((prBssInfo->auc2G_PriChnlList[i] >= ucAffectedChnl_L
+			&& prBssInfo->auc2G_PriChnlList[i] <= ucAffectedChnl_H)
+			&& prBssInfo->auc2G_PriChnlList[i] != ucPriChannel) {
 
 			ucChannelLevel = CHNL_LEVEL0;
 			goto L_2G4_level_end;
@@ -228,10 +249,11 @@ static uint8_t rlmObssChnlLevelIn2G4(struct BSS_INFO *prBssInfo, uint8_t ucPriCh
 
 	/* Check 2.4G secondary channel list */
 	ASSERT(prBssInfo->auc2G_SecChnlList[0] <= CHNL_LIST_SZ_2G);
-	for (i = 1; i <= prBssInfo->auc2G_SecChnlList[0] && i <= CHNL_LIST_SZ_2G; i++) {
-		if ((prBssInfo->auc2G_SecChnlList[i] >= ucAffectedChnl_L &&
-		     prBssInfo->auc2G_SecChnlList[i] <= ucAffectedChnl_H) &&
-		    prBssInfo->auc2G_SecChnlList[i] != ucSecChannel) {
+	for (i = 1; i <= prBssInfo->auc2G_SecChnlList[0]
+		&& i <= CHNL_LIST_SZ_2G; i++) {
+		if ((prBssInfo->auc2G_SecChnlList[i] >= ucAffectedChnl_L
+			&& prBssInfo->auc2G_SecChnlList[i] <= ucAffectedChnl_H)
+			&& prBssInfo->auc2G_SecChnlList[i] != ucSecChannel) {
 
 			ucChannelLevel = CHNL_LEVEL0;
 			goto L_2G4_level_end;
@@ -245,14 +267,16 @@ L_2G4_level_end:
 
 /*----------------------------------------------------------------------------*/
 /*!
-* \brief
-*
-* \param[in]
-*
-* \return none
-*/
+ * \brief
+ *
+ * \param[in]
+ *
+ * \return none
+ */
 /*----------------------------------------------------------------------------*/
-static uint8_t rlmObssChnlLevelIn5G(struct BSS_INFO *prBssInfo, uint8_t ucPriChannel, enum ENUM_CHNL_EXT eExtend)
+static uint8_t rlmObssChnlLevelIn5G(struct BSS_INFO *prBssInfo,
+		uint8_t ucPriChannel,
+		enum ENUM_CHNL_EXT eExtend)
 {
 	uint8_t i, ucChannelLevel;
 	uint8_t ucSecChannel;
@@ -272,7 +296,8 @@ static uint8_t rlmObssChnlLevelIn5G(struct BSS_INFO *prBssInfo, uint8_t ucPriCha
 
 	/* Check 5G primary channel list */
 	ASSERT(prBssInfo->auc5G_PriChnlList[0] <= CHNL_LIST_SZ_5G);
-	for (i = 1; i <= prBssInfo->auc5G_PriChnlList[0] && i <= CHNL_LIST_SZ_5G; i++) {
+	for (i = 1; i <= prBssInfo->auc5G_PriChnlList[0]
+		&& i <= CHNL_LIST_SZ_5G; i++) {
 		if (prBssInfo->auc5G_PriChnlList[i] == ucSecChannel) {
 
 			ucChannelLevel = CHNL_LEVEL0;
@@ -284,7 +309,8 @@ static uint8_t rlmObssChnlLevelIn5G(struct BSS_INFO *prBssInfo, uint8_t ucPriCha
 
 	/* Check non-HT channel list */
 	ASSERT(prBssInfo->auc5G_NonHtChnlList[0] <= CHNL_LIST_SZ_5G);
-	for (i = 1; i <= prBssInfo->auc5G_NonHtChnlList[0] && i <= CHNL_LIST_SZ_5G; i++) {
+	for (i = 1; i <= prBssInfo->auc5G_NonHtChnlList[0]
+		&& i <= CHNL_LIST_SZ_5G; i++) {
 		if (prBssInfo->auc5G_NonHtChnlList[i] == ucSecChannel) {
 
 			ucChannelLevel = CHNL_LEVEL0;
@@ -296,7 +322,8 @@ static uint8_t rlmObssChnlLevelIn5G(struct BSS_INFO *prBssInfo, uint8_t ucPriCha
 
 	/* Check secondary channel list */
 	ASSERT(prBssInfo->auc5G_SecChnlList[0] <= CHNL_LIST_SZ_5G);
-	for (i = 1; i <= prBssInfo->auc5G_SecChnlList[0] && i <= CHNL_LIST_SZ_5G; i++) {
+	for (i = 1; i <= prBssInfo->auc5G_SecChnlList[0]
+		&& i <= CHNL_LIST_SZ_5G; i++) {
 		if (prBssInfo->auc5G_SecChnlList[i] == ucPriChannel) {
 
 			ucChannelLevel = CHNL_LEVEL0;
@@ -311,19 +338,23 @@ L_5G_level_end:
 
 /*----------------------------------------------------------------------------*/
 /*!
-* \brief
-*
-* \param[in]
-*
-* \return none
-*/
+ * \brief
+ *
+ * \param[in]
+ *
+ * \return none
+ */
 /*----------------------------------------------------------------------------*/
-void rlmObssScanExemptionRsp(struct ADAPTER *prAdapter, struct BSS_INFO *prBssInfo, struct SW_RFB *prSwRfb)
+void rlmObssScanExemptionRsp(struct ADAPTER *prAdapter,
+		struct BSS_INFO *prBssInfo,
+		struct SW_RFB *prSwRfb)
 {
 	struct MSDU_INFO *prMsduInfo;
 	struct ACTION_20_40_COEXIST_FRAME *prTxFrame;
 
-	/* To do: need an algorithm to do judgement. Now always reject request */
+	/* To do: need an algorithm to do judgement.
+	 * Now always reject request
+	 */
 
 	prMsduInfo = (struct MSDU_INFO *)
 	    cnmMgtPktAlloc(prAdapter, PUBLIC_ACTION_MAX_LEN);
@@ -335,9 +366,13 @@ void rlmObssScanExemptionRsp(struct ADAPTER *prAdapter, struct BSS_INFO *prBssIn
 	prTxFrame = (struct ACTION_20_40_COEXIST_FRAME *) prMsduInfo->prPacket;
 
 	prTxFrame->u2FrameCtrl = MAC_FRAME_ACTION;
-	COPY_MAC_ADDR(prTxFrame->aucDestAddr, ((struct ACTION_20_40_COEXIST_FRAME *) prSwRfb->pvHeader)->aucSrcAddr);
-	COPY_MAC_ADDR(prTxFrame->aucSrcAddr, prBssInfo->aucOwnMacAddr);
-	COPY_MAC_ADDR(prTxFrame->aucBSSID, prBssInfo->aucBSSID);
+	COPY_MAC_ADDR(prTxFrame->aucDestAddr,
+		((struct ACTION_20_40_COEXIST_FRAME *)
+			prSwRfb->pvHeader)->aucSrcAddr);
+	COPY_MAC_ADDR(prTxFrame->aucSrcAddr,
+		prBssInfo->aucOwnMacAddr);
+	COPY_MAC_ADDR(prTxFrame->aucBSSID,
+		prBssInfo->aucBSSID);
 
 	prTxFrame->ucCategory = CATEGORY_PUBLIC_ACTION;
 	prTxFrame->ucAction = ACTION_PUBLIC_20_40_COEXIST;
@@ -353,7 +388,8 @@ void rlmObssScanExemptionRsp(struct ADAPTER *prAdapter, struct BSS_INFO *prBssIn
 		     prMsduInfo,
 		     prBssInfo->ucBssIndex,
 		     prSwRfb->ucStaRecIdx,
-		     WLAN_MAC_MGMT_HEADER_LEN, WLAN_MAC_MGMT_HEADER_HTC_LEN + 5, NULL, MSDU_RATE_MODE_AUTO);
+		     WLAN_MAC_MGMT_HEADER_LEN, WLAN_MAC_MGMT_HEADER_HTC_LEN + 5,
+		     NULL, MSDU_RATE_MODE_AUTO);
 
 	/* Send them to HW queue */
 	nicTxEnqueueMsdu(prAdapter, prMsduInfo);
