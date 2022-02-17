2c10b4e32480614a5c78f04508ed7eb0ac09d2ae
Ives Hung <ives.hung@mediatek.com>
2022-02-17 16:12:59 +0800

    [ALPS03645093] wlan gen4m: Kenerl Standardization: Check Patch Refactoring
    [Detail]
    WARNING:LONG_LINE_COMMENT: line over 80 characters
    
    [Solution]
    wrap the lines.
    
    MTK-Commit-Id: ce87f2dccfba5f8f7f9efdc171efc783660993b0
    
    Signed-off-by: Ives Hung <ives.hung@mediatek.com>
    CR-Id: ALPS03645093
    Feature: Wi-Fi Chipset Capability
    Change-Id: I9ac9c49abd47b7003cd6d4eb50bbd8b049090e17
    Reviewed-on: http://gerrit.mediatek.inc:8080/1334319
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Black Chen <black-ch.chen@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: TH3 Huang <th3.huang@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- os/linux/include/gl_qa_agent.h
+++ os/linux/include/gl_qa_agent.h
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*******************************************************************************
  *
  * This file is provided under a dual license.  When you use or
  * distribute this software, you may choose to be licensed under
@@ -48,10 +48,10 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *****************************************************************************/
+ ******************************************************************************/
 /*! \file   gl_qa_agent.h
-*    \brief  This file includes private ioctl support.
-*/
+ *    \brief  This file includes private ioctl support.
+ */
 
 #ifndef _GL_QA_AGENT_H
 #define _GL_QA_AGENT_H
@@ -59,38 +59,40 @@
 #if CFG_SUPPORT_QA_TOOL
 
 /*******************************************************************************
-*                         C O M P I L E R   F L A G S
-********************************************************************************
-*/
+ *                         C O M P I L E R   F L A G S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                    E X T E R N A L   R E F E R E N C E S
-********************************************************************************
-*/
+ *                    E X T E R N A L   R E F E R E N C E S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                              C O N S T A N T S
-********************************************************************************
-*/
+ *                              C O N S T A N T S
+ *******************************************************************************
+ */
 
 /* Trigger Event */
-#define CAP_FREE_RUN							0
+#define CAP_FREE_RUN		0
 
 /* Ring Mode */
-#define CAP_RING_MODE_ENABLE					1
-#define CAP_RING_MODE_DISABLE					0
+#define CAP_RING_MODE_ENABLE	1
+#define CAP_RING_MODE_DISABLE	0
 
 /* Capture Bit Width */
-#define CAP_96BIT								0
+#define CAP_96BIT		0
 #define CAP_128BIT
 
 /* I/Q Type */
-#define CAP_I_TYPE								0
-#define CAP_Q_TYPE								1
-#define NUM_OF_CAP_TYPE							2
+#define CAP_I_TYPE		0
+#define CAP_Q_TYPE		1
+#define NUM_OF_CAP_TYPE		2
 
 /* ACTION */
-#define ACTION_SWITCH_TO_RFTEST 0 /* to switch firmware mode between normal mode or rf test mode */
+#define ACTION_SWITCH_TO_RFTEST 0 /* to switch firmware mode between normal mode
+				   * or rf test mode
+				   */
 #define ACTION_IN_RFTEST        1
 
 #define HQA_CMD_MAGIC_NO 0x18142880
@@ -113,78 +115,82 @@
 #endif
 #define MAX_EEPROM_BUFFER_SIZE	1200
 
-/******************************************************************************
-*                    E X T E R N A L   R E F E R E N C E S
-*******************************************************************************
-*/
+/*******************************************************************************
+ *                    E X T E R N A L   R E F E R E N C E S
+ *******************************************************************************
+ */
 
 extern uint8_t uacEEPROMImage[MAX_EEPROM_BUFFER_SIZE];
 
-/******************************************************************************
-*                             D A T A   T Y P E S
-*******************************************************************************
-*/
+/*******************************************************************************
+ *                             D A T A   T Y P E S
+ *******************************************************************************
+ */
 
 #if 0
 struct PARAM_RX_STAT {
-	uint32_t MacFCSErr;	/* Y            0x820F_D014 */
-	uint32_t MacMdrdy;	/* Y            0x820F_D030 */
-	uint32_t FCSErr_CCK;	/* Y            0x8207_021C     [15:00] */
-	uint32_t FCSErr_OFDM;	/* Y            0x8207_021C     [31:16] */
-	uint32_t CCK_PD;		/* Y            0x8207_020C     [15:00] */
-	uint32_t OFDM_PD;	/* Y            0x8207_020C     [15:00] */
-	uint32_t CCK_SIG_Err;	/* Y            0x8207_0210     [31:16] */
-	uint32_t CCK_SFD_Err;	/* Y            0x8207_0210     [15:00] */
-	uint32_t OFDM_SIG_Err;	/* Y            0x8207_0214     [31:16] */
-	uint32_t OFDM_TAG_Err;	/* Y            0x8207_0214     [15:00] */
-	uint32_t WB_RSSSI0;	/* Y            0x8207_21A8     [23:16] */
-	uint32_t IB_RSSSI0;	/* Y            0x8207_21A8     [31:24] */
-	uint32_t WB_RSSSI1;	/* Y            0x8207_21A8     [07:00] */
-	uint32_t IB_RSSSI1;	/* Y            0x8207_21A8     [15:08] */
-	uint32_t PhyMdrdyCCK;	/* Y            0x8207_0220     [15:00] */
-	uint32_t PhyMdrdyOFDM;	/* Y            0x8207_0220     [31:16] */
-	uint32_t DriverRxCount;	/* Y            FW Counter Band0 */
-	uint32_t RCPI0;		/* Y            RXV4            [07:00] */
-	uint32_t RCPI1;		/* Y            RXV4            [15:08] */
-	uint32_t FreqOffsetFromRX;	/* Y            RXV5            MISC1[24:00]    OFDM:[11:00]    CCK:[10:00] */
-	uint32_t RSSI0;		/* N */
-	uint32_t RSSI1;		/* N */
-	uint32_t rx_fifo_full;	/* N */
-	uint32_t RxLenMismatch;	/* N */
-	uint32_t MacFCSErr_band1;	/* Y            0x820F_D214 */
-	uint32_t MacMdrdy_band1;	/* Y            0x820F_D230 */
-	/* Y            RXV3            [23:16] (must set 0x8207066C[1:0] = 0x0 ~ 0x3) */
+	uint32_t MacFCSErr;		/* Y 0x820F_D014 */
+	uint32_t MacMdrdy;		/* Y 0x820F_D030 */
+	uint32_t FCSErr_CCK;		/* Y 0x8207_021C [15:00] */
+	uint32_t FCSErr_OFDM;		/* Y 0x8207_021C [31:16] */
+	uint32_t CCK_PD;		/* Y 0x8207_020C [15:00] */
+	uint32_t OFDM_PD;		/* Y 0x8207_020C [15:00] */
+	uint32_t CCK_SIG_Err;		/* Y 0x8207_0210 [31:16] */
+	uint32_t CCK_SFD_Err;		/* Y 0x8207_0210 [15:00] */
+	uint32_t OFDM_SIG_Err;		/* Y 0x8207_0214 [31:16] */
+	uint32_t OFDM_TAG_Err;		/* Y 0x8207_0214 [15:00] */
+	uint32_t WB_RSSSI0;		/* Y 0x8207_21A8 [23:16] */
+	uint32_t IB_RSSSI0;		/* Y 0x8207_21A8 [31:24] */
+	uint32_t WB_RSSSI1;		/* Y 0x8207_21A8 [07:00] */
+	uint32_t IB_RSSSI1;		/* Y 0x8207_21A8 [15:08] */
+	uint32_t PhyMdrdyCCK;		/* Y 0x8207_0220 [15:00] */
+	uint32_t PhyMdrdyOFDM;		/* Y 0x8207_0220 [31:16] */
+	uint32_t DriverRxCount;		/* Y FW Counter Band0 */
+	uint32_t RCPI0;			/* Y RXV4 [07:00] */
+	uint32_t RCPI1;			/* Y RXV4 [15:08] */
+	uint32_t FreqOffsetFromRX;	/* Y RXV5 MISC1[24:00] OFDM:[11:00]
+					 *   CCK:[10:00]
+					 */
+	uint32_t RSSI0;			/* N */
+	uint32_t RSSI1;			/* N */
+	uint32_t rx_fifo_full;		/* N */
+	uint32_t RxLenMismatch;		/* N */
+	uint32_t MacFCSErr_band1;	/* Y 0x820F_D214 */
+	uint32_t MacMdrdy_band1;	/* Y 0x820F_D230 */
+	/* Y RXV3 [23:16] (must set 0x8207066C[1:0] = 0x0 ~ 0x3) */
 	uint32_t FAGC_IB_RSSSI[4];
-	/* Y            RXV3            [31:24] (must set 0x8207066C[1:0] = 0x0 ~ 0x3) */
+	/* Y RXV3 [31:24] (must set 0x8207066C[1:0] = 0x0 ~ 0x3) */
 	uint32_t FAGC_WB_RSSSI[4];
-	/* Y            0x8207_21A8     [31:24] [15:08] 0x8207_29A8     [31:24] [15:08] */
+	/* Y 0x8207_21A8 [31:24] [15:08] 0x8207_29A8 [31:24] [15:08] */
 	uint32_t Inst_IB_RSSSI[4];
-	/* Y            0x8207_21A8     [23:16] [07:00] 0x8207_29A8     [23:16] [07:00] */
+	/* Y 0x8207_21A8 [23:16] [07:00] 0x8207_29A8 [23:16] [07:00] */
 	uint32_t Inst_WB_RSSSI[4];
-	uint32_t ACIHitLow;	/* Y            0x8207_21B0     [18] */
-	uint32_t ACIHitHigh;	/* Y            0x8207_29B0     [18] */
-	uint32_t DriverRxCount1;	/* Y            FW Counter Band1 */
-	uint32_t RCPI2;		/* Y            RXV4            [23:16] */
-	uint32_t RCPI3;		/* Y            RXV4            [31:24] */
-	uint32_t RSSI2;		/* N */
-	uint32_t RSSI3;		/* N */
-	uint32_t SNR0;		/* Y            RXV5            (MISC1 >> 19) - 16 */
-	uint32_t SNR1;		/* N */
-	uint32_t SNR2;		/* N */
-	uint32_t SNR3;		/* N */
+	uint32_t ACIHitLow;		/* Y 0x8207_21B0 [18] */
+	uint32_t ACIHitHigh;		/* Y 0x8207_29B0 [18] */
+	uint32_t DriverRxCount1;	/* Y FW Counter Band1 */
+	uint32_t RCPI2;			/* Y RXV4 [23:16] */
+	uint32_t RCPI3;			/* Y RXV4 [31:24] */
+	uint32_t RSSI2;			/* N */
+	uint32_t RSSI3;			/* N */
+	uint32_t SNR0;			/* Y RXV5 (MISC1 >> 19) - 16 */
+	uint32_t SNR1;			/* N */
+	uint32_t SNR2;			/* N */
+	uint32_t SNR3;			/* N */
 	uint32_t rx_fifo_full_band1;	/* N */
 	uint32_t RxLenMismatch_band1;	/* N */
-	uint32_t CCK_PD_band1;	/* Y            0x8207_040C     [15:00] */
-	uint32_t OFDM_PD_band1;	/* Y            0x8207_040C     [31:16] */
-	uint32_t CCK_SIG_Err_band1;	/* Y            0x8207_0410     [31:16] */
-	uint32_t CCK_SFD_Err_band1;	/* Y            0x8207_0410     [15:00] */
-	uint32_t OFDM_SIG_Err_band1;	/* Y            0x8207_0414     [31:16] */
-	uint32_t OFDM_TAG_Err_band1;	/* Y            0x8207_0414     [15:00] */
-	uint32_t PhyMdrdyCCK_band1;	/* Y            0x8207_0420     [15:00] */
-	uint32_t PhyMdrdyOFDM_band1;	/* Y            0x8207_0420     [31:16] */
-	uint32_t CCK_FCS_Err_band1;	/* Y            0x8207_041C     [15:00] */
-	uint32_t OFDM_FCS_Err_band1;	/* Y            0x8207_041C     [31:16] */
-	uint32_t MuPktCount;	/* Y            MT_ATEUpdateRxStatistic RXV1_2ND_CYCLE->GroupId */
+	uint32_t CCK_PD_band1;		/* Y 0x8207_040C [15:00] */
+	uint32_t OFDM_PD_band1;		/* Y 0x8207_040C [31:16] */
+	uint32_t CCK_SIG_Err_band1;	/* Y 0x8207_0410 [31:16] */
+	uint32_t CCK_SFD_Err_band1;	/* Y 0x8207_0410 [15:00] */
+	uint32_t OFDM_SIG_Err_band1;	/* Y 0x8207_0414 [31:16] */
+	uint32_t OFDM_TAG_Err_band1;	/* Y 0x8207_0414 [15:00] */
+	uint32_t PhyMdrdyCCK_band1;	/* Y 0x8207_0420 [15:00] */
+	uint32_t PhyMdrdyOFDM_band1;	/* Y 0x8207_0420 [31:16] */
+	uint32_t CCK_FCS_Err_band1;	/* Y 0x8207_041C [15:00] */
+	uint32_t OFDM_FCS_Err_band1;	/* Y 0x8207_041C [31:16] */
+	uint32_t MuPktCount;		/* Y MT_ATEUpdateRxStatistic
+					 *   RXV1_2ND_CYCLE->GroupId
+					 */
 };
 #else
 struct PARAM_RX_STAT {
@@ -284,8 +290,10 @@ struct HQA_CMD_FRAME {
 	uint8_t Data[2048];
 } __KAL_ATTRIB_PACKED__;
 
-typedef int32_t(*HQA_CMD_HANDLER) (struct net_device *prNetDev,
-				  IN union iwreq_data *prIwReqData, struct HQA_CMD_FRAME *HqaCmdFrame);
+typedef int32_t(*HQA_CMD_HANDLER) (struct net_device
+				   *prNetDev,
+				   IN union iwreq_data *prIwReqData,
+				   struct HQA_CMD_FRAME *HqaCmdFrame);
 
 struct HQA_CMD_TABLE {
 	HQA_CMD_HANDLER *CmdSet;
@@ -293,30 +301,41 @@ struct HQA_CMD_TABLE {
 	uint32_t CmdOffset;
 };
 
-/******************************************************************************
-*                   F U N C T I O N   D E C L A R A T I O N S
-*******************************************************************************
-*/
+/*******************************************************************************
+ *                   F U N C T I O N   D E C L A R A T I O N S
+ *******************************************************************************
+ */
 
-int HQA_CMDHandler(struct net_device *prNetDev, IN union iwreq_data *prIwReqData, struct HQA_CMD_FRAME *HqaCmdFrame);
+int HQA_CMDHandler(struct net_device *prNetDev,
+		   IN union iwreq_data *prIwReqData,
+		   struct HQA_CMD_FRAME *HqaCmdFrame);
 
 int priv_qa_agent(IN struct net_device *prNetDev,
-		  IN struct iw_request_info *prIwReqInfo, IN union iwreq_data *prIwReqData, IN char *pcExtra);
+		  IN struct iw_request_info *prIwReqInfo,
+		  IN union iwreq_data *prIwReqData, IN char *pcExtra);
 
-int32_t mt6632SetICapStart(struct GLUE_INFO *prGlueInfo, uint32_t u4Trigger, uint32_t u4RingCapEn,
-			  uint32_t u4Event, uint32_t u4Node, uint32_t u4Len, uint32_t u4StopCycle,
-			  uint32_t u4BW, uint32_t u4MacTriggerEvent, uint32_t u4SourceAddrLSB,
-			  uint32_t u4SourceAddrMSB, uint32_t u4Band);
+int32_t mt6632SetICapStart(struct GLUE_INFO *prGlueInfo,
+			   uint32_t u4Trigger, uint32_t u4RingCapEn,
+			   uint32_t u4Event, uint32_t u4Node, uint32_t u4Len,
+			   uint32_t u4StopCycle,
+			   uint32_t u4BW, uint32_t u4MacTriggerEvent,
+			   uint32_t u4SourceAddrLSB,
+			   uint32_t u4SourceAddrMSB, uint32_t u4Band);
 int32_t mt6632GetICapStatus(struct GLUE_INFO *prGlueInfo);
 
-int32_t connacSetICapStart(struct GLUE_INFO *prGlueInfo, uint32_t u4Trigger, uint32_t u4RingCapEn,
-			  uint32_t u4Event, uint32_t u4Node, uint32_t u4Len, uint32_t u4StopCycle,
-			  uint32_t u4BW, uint32_t u4MacTriggerEvent, uint32_t u4SourceAddrLSB,
-			  uint32_t u4SourceAddrMSB, uint32_t u4Band);
+int32_t connacSetICapStart(struct GLUE_INFO *prGlueInfo,
+			   uint32_t u4Trigger, uint32_t u4RingCapEn,
+			   uint32_t u4Event, uint32_t u4Node, uint32_t u4Len,
+			   uint32_t u4StopCycle,
+			   uint32_t u4BW, uint32_t u4MacTriggerEvent,
+			   uint32_t u4SourceAddrLSB,
+			   uint32_t u4SourceAddrMSB, uint32_t u4Band);
 int32_t connacGetICapStatus(struct GLUE_INFO *prGlueInfo);
 
-int32_t commonGetICapIQData(struct GLUE_INFO *prGlueInfo, uint8_t *pData, uint32_t u4IQType, uint32_t u4WFNum);
-int32_t connacGetICapIQData(struct GLUE_INFO *prGlueInfo, uint8_t *pData, uint32_t u4IQType, uint32_t u4WFNum);
+int32_t commonGetICapIQData(struct GLUE_INFO *prGlueInfo,
+			uint8_t *pData, uint32_t u4IQType, uint32_t u4WFNum);
+int32_t connacGetICapIQData(struct GLUE_INFO *prGlueInfo,
+			uint8_t *pData, uint32_t u4IQType, uint32_t u4WFNum);
 
 
 #endif /*CFG_SUPPORT_QA_TOOL */
