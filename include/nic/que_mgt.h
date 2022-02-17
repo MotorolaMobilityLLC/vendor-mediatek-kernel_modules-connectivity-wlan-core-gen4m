05d897801f1a7637c4a280503f983a86173144c4
Wilson Lien <wilson.lien@mediatek.com>
2022-02-17 16:15:14 +0800

    [ALPS03869576] p2p: Oshare patch
    [Detail]
    1. Fall behind packets will be dropped by driver.
    2. Sometimes that will lead to file send fail by oshare.
    
    [Solution]
    Dont abandon fall behind packets about tcp and udp for oshare
    
    MTK-Commit-Id: d4e31330e97f43788a4e2f713fcf86a9a0cfc627
    
    Change-Id: If914490b8bbe8b9521a6470873f90cfbc81a44b3
    Signed-off-by: Wilson Lien <wilson.lien@mediatek.com>
    CR-Id: ALPS03869576
    Feature: Wi-Fi Direct
    (cherry picked from commit b1413583c4d55b82c16550802d2a0346a265f1af)
    Reviewed-on: http://gerrit.mediatek.inc:8080/1513119
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: GaryCC Chen <garycc.chen@mediatek.com>
    Reviewed-by: TH3 Huang <th3.huang@mediatek.com>
    Reviewed-by: Black Chen <black-ch.chen@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- include/nic/que_mgt.h
+++ include/nic/que_mgt.h
@@ -1158,9 +1158,11 @@ u_int8_t qmHandleRxReplay(struct ADAPTER *prAdapter,
 			  struct SW_RFB *prSwRfb);
 #endif
 
-#if CFG_SUPPORT_LOWLATENCY_MODE
-u_int8_t qmIsNoDropPacket(IN struct ADAPTER *prAdapter, IN uint8_t *pucData);
+#if CFG_SUPPORT_LOWLATENCY_MODE || CFG_SUPPORT_OSHARE
+u_int8_t
+qmIsNoDropPacket(IN struct ADAPTER *prAdapter, IN struct SW_RFB *prSwRfb);
 #endif /* CFG_SUPPORT_LOWLATENCY_MODE */
+
 /*******************************************************************************
  *                              F U N C T I O N S
  *******************************************************************************
