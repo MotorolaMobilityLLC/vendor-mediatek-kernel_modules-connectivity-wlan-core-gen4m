e8dee780a77216ae7a94295e508518a554c1d0a3
Holiday Hao <holiday.hao@mediatek.com>
2022-02-17 16:18:21 +0800

    [ALPS03891706] CONNAC: Driver UT framework
    [Description]
    UT framework for WiFi driver
    1.  ioctl path
    2.  callback function for tx_thread
    3.  UT framework
    4.  testcase example
    5.  Fix checkpatch error in test case folder
    6.  Build pass and runnable on alps-mp-o1.mp7 phone
        - Add option to check kernel version
        - Fix checkpatch warning & error
    7.  Executable on PC environment with kernel-4.8
        - Fix kernel version check
        - Fix checkpatch error
        - Fix EMI download process on PC
    8.  Fix checkpatch error & warning by auto script
    9.  Fix errors and warnings found by checkpatch
    10. porting to alps-mp-p0.mp1
        verified on alps-mp-p0.mp1-of.p15
    
    [Original Patch Link]
    branch: personal/mtk02176/wifi-ut-framework-dev
    http://gerrit.mediatek.inc:8080/1686727/
    
    [Step for Phone]
    1.add following repo to the path
        repo: neptune/wlan_driver/gen4m_ut
        path: vendor/mediatek/kernel_modules/connectivity/wlan/core/gen4m_ut
    2.remove "insmod /vendor/lib/modules/wlan_drv_${ro.vendor.wlan.gen}.ko"
        in /vendor/etc/init/init.wlan_drv.rc
    3.enable option MTK_WLAN_UT_TEST_MODE
        build out wlan_drv_gen4m.ko for unit test
    4.push wlan_drv_gen4m.ko to /vendor/lib/modules/
    5.Push "wireless-tool" binary to /system/bin/
    6.adb reboot and insmod .ko manually
        $ adb shell insmod /vendor/lib/modules/wlan_drv_gen4m.ko
    7.run UT
        $ adb root
        $ adb shell
        $ /system/bin/iwpriv wlan0 driver "ut all"
    8. check the log/result
        $ adb shell dmesg > klog.txt
        $ adb shell cat /sdcard/wifi_ut.log
    
    MTK-Commit-Id: a6527aae0751d84afa24e14c81b916fb15b8e9df
    
    Change-Id: I8720e06afa0aeacffa89d68b6aee00e1b0dbb68a
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    CR-Id: ALPS03891706
    Feature: [Module]Wi-Fi Driver
    Tested-by: YL Wang <yl.wang@mediatek.com>
    Tested-by: Willy Yu <willy.yu@mediatek.com>
    Tested-by: Johnny Yao <johnny.yao@mediatek.com>
    Reviewed-by: Willy Yu <willy.yu@mediatek.com>
    Reviewed-by: Tim Chang <tim.chang@mediatek.com>
    Reviewed-by: YL Wang <yl.wang@mediatek.com>
    Reviewed-on: http://gerrit.mediatek.inc:8080/1693664
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Tested-by: YZ Huang <yz.huang@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -79,8 +79,16 @@
 #if CFG_SUPPORT_AGPS_ASSIST
 #include <net/netlink.h>
 #endif
+
 #if CFG_TC1_FEATURE
 #include <tc1_partition.h>
+#endif
+
+#if CFG_SUPPORT_WAKEUP_REASON_DEBUG
+/* #include <pcm_def.h> */
+#ifndef CFG_BUILD_X86_PLATFORM
+#include <mtk_sleep.h>
+#endif
 #endif
 /*******************************************************************************
  *                              C O N S T A N T S
@@ -3539,6 +3547,9 @@ int main_thread(void *data)
 	       KAL_GET_CURRENT_THREAD_NAME(), KAL_GET_CURRENT_THREAD_ID());
 
 	while (TRUE) {
+#ifdef UT_TEST_MODE
+		testThreadBegin(prGlueInfo->prAdapter);
+#endif
 
 #if CFG_ENABLE_WIFI_DIRECT
 		/*run p2p multicast list work. */
@@ -3666,7 +3677,9 @@ int main_thread(void *data)
 		}
 #endif
 		/* transfer ioctl to OID request */
-
+#ifdef UT_TEST_MODE
+		testProcessOid(prGlueInfo->prAdapter);
+#endif
 		do {
 			if (test_and_clear_bit(GLUE_FLAG_OID_BIT,
 					       &prGlueInfo->ulFlag)) {
@@ -3715,11 +3728,18 @@ int main_thread(void *data)
 		 *
 		 */
 
+#ifdef UT_TEST_MODE
+		testProcessTxReq(prGlueInfo->prAdapter);
+#endif
+
 		if (test_and_clear_bit(GLUE_FLAG_TXREQ_BIT,
 				       &prGlueInfo->ulFlag))
 			kalProcessTxReq(prGlueInfo, &fgNeedHwAccess);
 #if CFG_SUPPORT_MULTITHREAD
 		/* Process RX */
+#ifdef UT_TEST_MODE
+		testProcessRFBs(prGlueInfo->prAdapter);
+#endif
 		if (test_and_clear_bit(GLUE_FLAG_RX_BIT,
 				       &prGlueInfo->ulFlag))
 			nicRxProcessRFBs(prGlueInfo->prAdapter);
@@ -3741,12 +3761,18 @@ int main_thread(void *data)
 			wlanReleasePowerControl(prGlueInfo->prAdapter);
 #endif
 		/* handle cnmTimer time out */
+#ifdef UT_TEST_MODE
+		testTimeoutCheck(prGlueInfo->prAdapter);
+#endif
 		if (test_and_clear_bit(GLUE_FLAG_TIMEOUT_BIT,
 				       &prGlueInfo->ulFlag))
 			wlanTimerTimeoutCheck(prGlueInfo->prAdapter);
 #if CFG_SUPPORT_SDIO_READ_WRITE_PATTERN
 		if (prGlueInfo->fgEnSdioTestPattern == TRUE)
 			kalSetEvent(prGlueInfo);
+#endif
+#ifdef UT_TEST_MODE
+		testThreadEnd(prGlueInfo->prAdapter);
 #endif
 	}
 
