019d6ef22606f343db04e9cab562f2f922316e18
TF Huang <tf.huang@mediatek.com>
2022-02-17 16:56:19 +0800

    [ALPS05656763] Fix build error
    Remove unnecessary attribute policy
    
    MTK-Commit-Id: ba59bf88f8f6ddfef24bc048bdf2e0f30a3ad84f
    
    Signed-off-by: TF Huang <tf.huang@mediatek.com>
    CR-Id: ALPS05656763
    Feature: [Module]Wi-Fi Driver
    Change-Id: Id4877e316b8ee9f0c0c29a95c47617982ca0e7e2
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/5436076
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Tested-by: herman.yang <herman.yang@mediatek.com>
    Build: herman.yang <herman.yang@mediatek.com>
    Test: herman.yang <herman.yang@mediatek.com>

--- os/linux/gl_vendor_nan.c
+++ os/linux/gl_vendor_nan.c
@@ -65,13 +65,6 @@
  *                              F U N C T I O N S
  *******************************************************************************
  */
-const struct nla_policy mtk_wlan_vendor_nan_policy[NL80211_ATTR_MAX + 1] = {
-#if KERNEL_VERSION(5, 4, 0) <= CFG80211_VERSION_CODE
-	[NL80211_ATTR_VENDOR_DATA] = NLA_POLICY_MIN_LEN(0),
-#else
-	[NL80211_ATTR_VENDOR_DATA] = {.type = NLA_MIN_LEN, .len = 0},
-#endif
-};
 
 /* Helper function to Write and Read TLV called in indication as well as */
 /*  request */
