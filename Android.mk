32d25ddba2e0ae00c8bbedb28ce123e6017b63b2
chris.hong <chris.hong@mediatek.com>
2022-02-17 16:48:11 +0800

    [ALPS05889910] mt6879: wlan: code porting
    Add 6879 config in makefile
    
    MTK-Commit-Id: b042fedd432e29c7d7193eb233b10e349d8ce58c
    
    Change-Id: I581f1c8f4f3dd51338c60d2b68174ca3c3e6b8f5
    Signed-off-by: chris.hong <chris.hong@mediatek.com>
    CR-Id: ALPS05889910
    Feature: [Module]Wi-Fi Driver

--- Android.mk
+++ Android.mk
@@ -19,6 +19,14 @@ ifeq ($(WLAN_BUILD_COMMON), true)
 	WIFI_NAME := wlan_drv_gen4m_6983
 	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
 	include $(LOCAL_PATH)/build_wlan_drv.mk
+
+	WLAN_CHIP_ID := 6879
+	WIFI_CHIP := CONNAC2X2_SOC7_0
+	WIFI_IP_SET := 1
+	CONNAC_VER := 2_0
+	WIFI_NAME := wlan_drv_gen4m_6879
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
 else
 	WIFI_NAME := wlan_drv_gen4m
 	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi.ko
