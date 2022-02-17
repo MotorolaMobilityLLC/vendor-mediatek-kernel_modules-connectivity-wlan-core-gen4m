4e37e2a4dda92ebd2f8e70878459f87bade872a1
Zeno Yang <zeno.yang@mediatek.com>
2022-02-17 16:11:33 +0800

    [ALPS03645093] wlan: Fix check patch issues
    [Detail]
    Fix check patch warnings and errors.
    
    [Solution]
    Refine the coding style
    
    MTK-Commit-Id: 56c2f67dbdfb22c951714bb738676b81620cdb31
    
    Change-Id: Icd7194278423b5b56b38d9244d75ef8de950aa9f
    CR-Id: ALPS03645093
    Signed-off-by: Zeno Yang <zeno.yang@mediatek.com>
    Feature: Driver
    Reviewed-on: http://gerrit.mediatek.inc:8080/1272825
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: George Kuo <george.kuo@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- include/mgmt/p2p_fsm.h
+++ include/mgmt/p2p_fsm.h
@@ -50,8 +50,8 @@
  *
  *****************************************************************************/
 /*
-** Id: //Department/DaVinci/TRUNK/WiFi_P2P_Driver/include/mgmt/p2p_fsm.h#23
-*/
+ ** Id: //Department/DaVinci/TRUNK/WiFi_P2P_Driver/include/mgmt/p2p_fsm.h#23
+ */
 
 /*! \file   p2p_fsm.h
  *  \brief  Declaration of functions and finite state machine for P2P Module.
@@ -63,19 +63,25 @@
 #ifndef _P2P_FSM_H
 #define _P2P_FSM_H
 
-void p2pFsmRunEventScanRequest(IN struct ADAPTER *prAdapter, IN struct MSG_HDR *prMsgHdr);
+void p2pFsmRunEventScanRequest(IN struct ADAPTER *prAdapter,
+		IN struct MSG_HDR *prMsgHdr);
 
-void p2pFsmRunEventChGrant(IN struct ADAPTER *prAdapter, IN struct MSG_HDR *prMsgHdr);
+void p2pFsmRunEventChGrant(IN struct ADAPTER *prAdapter,
+		IN struct MSG_HDR *prMsgHdr);
 
-void p2pFsmRunEventNetDeviceRegister(IN struct ADAPTER *prAdapter, IN struct MSG_HDR *prMsgHdr);
+void p2pFsmRunEventNetDeviceRegister(IN struct ADAPTER *prAdapter,
+		IN struct MSG_HDR *prMsgHdr);
 
-void p2pFsmRunEventUpdateMgmtFrame(IN struct ADAPTER *prAdapter, IN struct MSG_HDR *prMsgHdr);
+void p2pFsmRunEventUpdateMgmtFrame(IN struct ADAPTER *prAdapter,
+		IN struct MSG_HDR *prMsgHdr);
 
 #if CFG_SUPPORT_WFD
-void p2pFsmRunEventWfdSettingUpdate(IN struct ADAPTER *prAdapter, IN struct MSG_HDR *prMsgHdr);
+void p2pFsmRunEventWfdSettingUpdate(IN struct ADAPTER *prAdapter,
+		IN struct MSG_HDR *prMsgHdr);
 #endif
 
-void p2pFsmRunEventScanDone(IN struct ADAPTER *prAdapter, IN struct MSG_HDR *prMsgHdr);
+void p2pFsmRunEventScanDone(IN struct ADAPTER *prAdapter,
+		IN struct MSG_HDR *prMsgHdr);
 
 
 #endif /* _P2P_FSM_H */
