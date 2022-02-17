f12d04957a1739cbdf0c7c062b409adfead6ad42
Mason Lai <mason.lai@mediatek.com>
2022-02-17 16:11:24 +0800

    [BORA00012379] Wlan: Fix check patch issues
    [Detail]
    Fix check patch warnings and errors.
    
    [Solution]
    Fix check patch warnings and errors.
    
    MTK-Commit-Id: 3fc438f0aab07d4faa3dbdca31ee0aea849765ef
    
    CR-Id: BORA00012379
    Feature: Driver
    Signed-off-by: Mason Lai <mason.lai@mediatek.com>
    Change-Id: I2cb239f84bb92a54dcfc61ae510a06d02e73c36e
    Reviewed-on: http://gerrit.mediatek.inc:8080/1273352
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Chun-Wei Tseng <chun-wei.tseng@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- include/nic/cmd_buf.h
+++ include/nic/cmd_buf.h
@@ -50,39 +50,39 @@
  *
  *****************************************************************************/
 /*
-** Id:
-*/
+ * Id:
+ */
 
 /*! \file   "cmd_buf.h"
  *   \brief  In this file we define the structure for Command Packet.
  *
- *		In this file we define the structure for Command Packet and the control unit
- *   of MGMT Memory Pool.
-*/
+ *		In this file we define the structure for Command Packet and the
+ *    control unit of MGMT Memory Pool.
+ */
 
 
 #ifndef _CMD_BUF_H
 #define _CMD_BUF_H
 
 /*******************************************************************************
-*                         C O M P I L E R   F L A G S
-********************************************************************************
-*/
+ *                         C O M P I L E R   F L A G S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                    E X T E R N A L   R E F E R E N C E S
-********************************************************************************
-*/
+ *                    E X T E R N A L   R E F E R E N C E S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                              C O N S T A N T S
-********************************************************************************
-*/
+ *                              C O N S T A N T S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                             D A T A   T Y P E S
-********************************************************************************
-*/
+ *                             D A T A   T Y P E S
+ *******************************************************************************
+ */
 
 enum COMMAND_TYPE {
 	COMMAND_TYPE_GENERAL_IOCTL,
@@ -92,11 +92,15 @@ enum COMMAND_TYPE {
 	COMMAND_TYPE_NUM
 };
 
-typedef void(*PFN_CMD_DONE_HANDLER) (IN struct ADAPTER *prAdapter, IN struct CMD_INFO *prCmdInfo, IN uint8_t *pucEventBuf);
+typedef void(*PFN_CMD_DONE_HANDLER) (IN struct ADAPTER
+	*prAdapter, IN struct CMD_INFO *prCmdInfo,
+	IN uint8_t *pucEventBuf);
 
-typedef void(*PFN_CMD_TIMEOUT_HANDLER) (IN struct ADAPTER *prAdapter, IN struct CMD_INFO *prCmdInfo);
+typedef void(*PFN_CMD_TIMEOUT_HANDLER) (IN struct ADAPTER
+	*prAdapter, IN struct CMD_INFO *prCmdInfo);
 
-typedef void(*PFN_HIF_TX_CMD_DONE_CB) (IN struct ADAPTER *prAdapter, IN struct CMD_INFO *prCmdInfo);
+typedef void(*PFN_HIF_TX_CMD_DONE_CB) (IN struct ADAPTER
+	*prAdapter, IN struct CMD_INFO *prCmdInfo);
 
 struct CMD_INFO {
 	struct QUE_ENTRY rQueEntry;
@@ -105,7 +109,8 @@ struct CMD_INFO {
 
 	uint16_t u2InfoBufLen;	/* This is actual CMD buffer length */
 	uint8_t *pucInfoBuffer;	/* May pointer to structure in prAdapter */
-	struct MSDU_INFO *prMsduInfo;	/* only valid when it's a security/MGMT frame */
+	struct MSDU_INFO
+		*prMsduInfo;	/* only valid when it's a security/MGMT frame */
 	void *prPacket;	/* only valid when it's a security frame */
 
 	PFN_CMD_DONE_HANDLER pfCmdDoneHandler;
@@ -135,29 +140,31 @@ struct CMD_INFO {
 };
 
 /*******************************************************************************
-*                            P U B L I C   D A T A
-********************************************************************************
-*/
+ *                            P U B L I C   D A T A
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                           P R I V A T E   D A T A
-********************************************************************************
-*/
+ *                           P R I V A T E   D A T A
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                                 M A C R O S
-********************************************************************************
-*/
+ *                                 M A C R O S
+ *******************************************************************************
+ */
 
 /*******************************************************************************
-*                   F U N C T I O N   D E C L A R A T I O N S
-********************************************************************************
-*/
+ *                   F U N C T I O N   D E C L A R A T I O N S
+ *******************************************************************************
+ */
 void cmdBufInitialize(IN struct ADAPTER *prAdapter);
 
-struct CMD_INFO *cmdBufAllocateCmdInfo(IN struct ADAPTER *prAdapter, IN uint32_t u4Length);
+struct CMD_INFO *cmdBufAllocateCmdInfo(IN struct ADAPTER
+				       *prAdapter, IN uint32_t u4Length);
 
-void cmdBufFreeCmdInfo(IN struct ADAPTER *prAdapter, IN struct CMD_INFO *prCmdInfo);
+void cmdBufFreeCmdInfo(IN struct ADAPTER *prAdapter,
+		       IN struct CMD_INFO *prCmdInfo);
 
 /*----------------------------------------------------------------------------*/
 /* Routines for CMDs                                                          */
@@ -171,7 +178,8 @@ wlanSendSetQueryCmd(IN struct ADAPTER *prAdapter,
 		    PFN_CMD_DONE_HANDLER pfCmdDoneHandler,
 		    PFN_CMD_TIMEOUT_HANDLER pfCmdTimeoutHandler,
 		    uint32_t u4SetQueryInfoLen,
-		    uint8_t *pucInfoBuffer, OUT void *pvSetQueryBuffer, IN uint32_t u4SetQueryBufferLen);
+		    uint8_t *pucInfoBuffer, OUT void *pvSetQueryBuffer,
+		    IN uint32_t u4SetQueryBufferLen);
 
 #if CFG_SUPPORT_TX_BF
 uint32_t
@@ -184,20 +192,24 @@ wlanSendSetQueryExtCmd(IN struct ADAPTER *prAdapter,
 		       PFN_CMD_DONE_HANDLER pfCmdDoneHandler,
 		       PFN_CMD_TIMEOUT_HANDLER pfCmdTimeoutHandler,
 		       uint32_t u4SetQueryInfoLen,
-		       uint8_t *pucInfoBuffer, OUT void *pvSetQueryBuffer, IN uint32_t u4SetQueryBufferLen);
+		       uint8_t *pucInfoBuffer, OUT void *pvSetQueryBuffer,
+		       IN uint32_t u4SetQueryBufferLen);
 #endif
 
-void cmdBufDumpCmdQueue(struct QUE *prQueue, int8_t *queName);
+void cmdBufDumpCmdQueue(struct QUE *prQueue,
+			int8_t *queName);
 #if (CFG_SUPPORT_TRACE_TC4 == 1)
 void wlanDebugTC4Init(void);
 void wlanDebugTC4Uninit(void);
-void wlanTraceReleaseTcRes(struct ADAPTER *prAdapter, uint32_t u4TxRlsCnt, uint32_t u4Available);
+void wlanTraceReleaseTcRes(struct ADAPTER *prAdapter,
+			   uint32_t u4TxRlsCnt, uint32_t u4Available);
 void wlanTraceTxCmd(struct CMD_INFO *prCmd);
-void wlanDumpTcResAndTxedCmd(uint8_t *pucBuf, uint32_t maxLen);
+void wlanDumpTcResAndTxedCmd(uint8_t *pucBuf,
+			     uint32_t maxLen);
 #endif
 
 /*******************************************************************************
-*                              F U N C T I O N S
-********************************************************************************
-*/
+ *                              F U N C T I O N S
+ *******************************************************************************
+ */
 #endif /* _CMD_BUF_H */
