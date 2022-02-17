d756dcf7d3991b9be5b8a6da609b85e24372ddae
Awk Jiang <awk.jiang@mediatek.com>
2022-02-17 16:01:49 +0800

    [WCNCR00146538] softap: Change ACS 5G ch range and ACS alg for 2.4G APs
    [Description]
    Change ACS 5G ch range and ACS alg for 2.4G APs
    
    i. In original ACS algorithm for 2.4g APs we only
    select one adjacent channel to calculate the dirtiness,
    now we select two adjacent channels to enhance the
    dirtiness estimation result.
    
    ii. The 5g channels range is wrong, we take the 5g
    channels in mtk_5ghz_channels array as reference to
    correct the related channel range mapping code for ACS.
    
    MTK-Commit-Id: 445a4833b246fe5b5aa00d96102e4c22e6e5c9e5
    
    Change-Id: Ic16e58317e1da50acab4285af476c74cfc63ffb2
    Feature: softap
    Signed-off-by: Awk Jiang <awk.jiang@mediatek.com>
    CR-Id: WCNCR00146538
    (cherry picked from commit 1597bef925cb22cc602f84bcd8c5ffe4292568c5)

--- include/config.h
+++ include/config.h
@@ -437,7 +437,7 @@
   * Auto Channel Selection maximun channel number
   *------------------------------------------------------------------------------
   */
-#define MAX_CHN_NUM                             39 /* CH1~CH14, CH36~CH48, CH52~CH64, CH100~CH144, CH149~CH165 */
+#define MAX_CHN_NUM                             49 /* ARRAY_SIZE(mtk_5ghz_channels) + ARRAY_SIZE(mtk_2ghz_channels) */
 #define MAX_2G_BAND_CHN_NUM                     14
 #define MAX_5G_BAND_CHN_NUM                     (MAX_CHN_NUM - MAX_2G_BAND_CHN_NUM)
 
