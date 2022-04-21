9f1f0d2914570002ecfdd1af9c7695a06135bc86
TS Hsu <ts.hsu@mediatek.com>
2022-04-22 00:56:36 +0800

    [ALPS07108107] Fix coverity
    Fix coverity.
    
    MTK-Commit-Id: 107fb30f400300be3d7f846aab9daaa34ec80a88
    
    Change-Id: If04b142e25af681b6aee69dbe742671ad553ca98
    CR-Id: ALPS07108107
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: TS Hsu <ts.hsu@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/5819916
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: tf.huang <tf.huang@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity

--- mgmt/rlm.c
+++ mgmt/rlm.c
@@ -3698,6 +3698,7 @@ static void rlmRecAssocRespIeInfoForClient(struct ADAPTER *prAdapter,
 	ASSERT(pucIE);
 
 	prStaRec = prBssInfo->prStaRecOfAP;
+	kalMemZero(&rSsid, sizeof(rSsid));
 
 	if (!prStaRec)
 		return;
@@ -6207,6 +6208,7 @@ void rlmCsaTimeout(IN struct ADAPTER *prAdapter,
 		return;
 	}
 
+	kalMemZero(&rSsid, sizeof(rSsid));
 	prCSAParams = &prBssInfo->CSAParams;
 	prBssInfo->ucPrimaryChannel = prCSAParams->ucCsaNewCh;
 	if (prCSAParams->eCsaBand != BAND_NULL)
--- mgmt/scan.c
+++ mgmt/scan.c
@@ -3388,6 +3388,8 @@ uint32_t scanAddScanResult(IN struct ADAPTER *prAdapter,
 	ASSERT(prSwRfb);
 
 	prScanInfo = &(prAdapter->rWifiVar.rScanInfo);
+	kalMemZero(&rConfiguration, sizeof(rConfiguration));
+	kalMemZero(&rSsid, sizeof(rSsid));
 
 	if (prBssDesc->eBand == BAND_2G4) {
 		if ((prBssDesc->u2OperationalRateSet & RATE_SET_OFDM)
