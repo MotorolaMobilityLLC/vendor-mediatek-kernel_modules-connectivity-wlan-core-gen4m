2389d6883b0b5ffd0dacca1faa88d08d08323ae6
Cheng Sung <cheng.sung@mediatek.com>
2022-06-16 10:52:10 +0800

    [ALPS07150055] [Coverity][ALPS]CERT-C Characters and Strings: 10949681
        Fix coverity issue
    
    MTK-Commit-Id: c2955f94576aa7a44959d4eb65f87f0f607cfa94
    
    Signed-off-by: Cheng Sung <cheng.sung@mediatek.com>
    CR-Id: ALPS07150055
    Feature: Wi-Fi NAN
    Change-Id: If2c6d413c17bc514b75fa08dc25c270c2d0ecec8
    (cherry picked from commit bfa3e0659276cbf76557a636eee1ee322474fc54)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6042600
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: fred.shen <fred.shen@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_mspautosanity

--- nan/nanDiscovery.c
+++ nan/nanDiscovery.c
@@ -287,7 +287,7 @@ nanPublishRequest(struct ADAPTER *prAdapter, struct NanPublishRequest *msg) {
 	struct _CMD_EVENT_TLV_COMMOM_T *prTlvCommon = NULL;
 	struct _CMD_EVENT_TLV_ELEMENT_T *prTlvElement = NULL;
 	struct NanFWPublishRequest *prPublishReq = NULL;
-	char aucServiceName[NAN_FW_MAX_SERVICE_NAME_LEN];
+	char aucServiceName[NAN_FW_MAX_SERVICE_NAME_LEN + 1];
 	struct nan_rdf_sha256_state r_SHA_256_state;
 	uint8_t auc_tk[32];
 	uint32_t u4Idx;
@@ -590,7 +590,7 @@ nanSubscribeRequest(struct ADAPTER *prAdapter,
 	struct _CMD_EVENT_TLV_ELEMENT_T *prTlvElement = NULL;
 	struct NanFWSubscribeRequest *prSubscribeReq = NULL;
 
-	char aucServiceName[NAN_FW_MAX_SERVICE_NAME_LEN];
+	char aucServiceName[NAN_FW_MAX_SERVICE_NAME_LEN + 1];
 	struct nan_rdf_sha256_state r_SHA_256_state;
 	uint8_t auc_tk[32];
 	uint32_t u4Idx;
