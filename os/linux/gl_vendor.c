70328fed22005af6eedb1b2d32506c25a9fa0744
Rachel Tseng <Rachel.Tseng@mediatek.com>
2022-09-24 12:23:28 +0800

    [ALPS07603096] wlan: fix OOB-Read in gl_vendor
    [Description]
    Add null pointer check and length check for vendor command attributes
    to avoid OOB read.
    
    MTK-Commit-Id: ddd816bd074745055444dde8ea5d4ab5fb53caa3
    
    Change-Id: I5df00c106a3b63868fd4ee7855e63680138aba43
    CR-Id: ALPS07603096
    Feature: [Module] Wi-Fi Driver
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    (cherry picked from commit 79c4b8d814a41782bb7122b030a5129d8089748f)
    (cherry-pick from f149a86d91d61490082dd72329c310984430f9b5)

--- os/linux/gl_vendor.c
+++ os/linux/gl_vendor.c
@@ -824,6 +824,8 @@ int mtk_cfg80211_vendor_enable_roaming(struct wiphy *wiphy,
 
 	ASSERT(wiphy);	/* change to if (wiphy == NULL) then return? */
 	ASSERT(wdev);	/* change to if (wiphy == NULL) then return? */
+	if ((data == NULL) || (data_len == 0))
+		return -EINVAL;
 
 	WIPHY_PRIV(wiphy, prGlueInfo);
 	if (!prGlueInfo)
@@ -2111,6 +2113,8 @@ int mtk_cfg80211_vendor_set_tx_power_scenario(struct wiphy *wiphy,
 
 	ASSERT(wiphy);
 	ASSERT(wdev);
+	if ((data == NULL) || (data_len == 0))
+		return -EINVAL;
 
 	WIPHY_PRIV(wiphy, prGlueInfo);
 
@@ -2187,6 +2191,8 @@ int mtk_cfg80211_vendor_set_multista_primary_connection(struct wiphy *wiphy,
 
 	ASSERT(wiphy);
 	ASSERT(wdev);
+	if ((data == NULL) || (data_len == 0))
+		return -EINVAL;
 
 	prGlueInfo = (struct GLUE_INFO *) wiphy_priv(wiphy);
 	if (!prGlueInfo)
@@ -2241,6 +2247,8 @@ int mtk_cfg80211_vendor_set_multista_use_case(
 
 	ASSERT(wiphy);
 	ASSERT(wdev);
+	if ((data == NULL) || (data_len == 0))
+		return -EINVAL;
 
 	prGlueInfo = (struct GLUE_INFO *) wiphy_priv(wiphy);
 	if (!prGlueInfo)
