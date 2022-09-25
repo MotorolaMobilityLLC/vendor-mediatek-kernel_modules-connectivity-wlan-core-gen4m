b3f3a47611f6135e16d68b012b51020efff0e04d
Ben Lai <ben.lai@mediatek.com>
2022-09-25 13:35:49 +0800

    [ALPS07584345] init: Add wfsys lock trace for debug
    Add wfsys lock trace for debug.
    
    MTK-Commit-Id: e52c3985ec7d5635044b1872c683de98d8443220
    
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS07584345
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I96b5594ffccac55cd04d1dad8413589aea84c402

--- os/linux/include/gl_sys_lock.h
+++ os/linux/include/gl_sys_lock.h
@@ -3,6 +3,20 @@
  * Copyright (c) 2020 MediaTek Inc.
  */
 
+#define WFSYS_LOCK_MAX_TRACE		10
+#define WFSYS_LOCK_MAX_HOLD_TIME	10
+#define WFSYS_LOCK_PRINT_PERIOD		1
+
+struct wfsys_lock_dbg_t {
+	struct task_struct *task;
+	int pid;
+	uint64_t start_time;
+	uint64_t start_time_sec;
+	uint64_t start_time_nsec;
+	unsigned long addrs[WFSYS_LOCK_MAX_TRACE];
+	unsigned int  nr_entries;
+};
+
 void wfsys_lock(void);
 int wfsys_trylock(void);
 void wfsys_unlock(void);
