e5dcf0a62d7802a7cee11c0d28630cef2625665b
Ben Lai <ben.lai@mediatek.com>
2022-11-15 20:01:13 +0800

    [ALPS07693080] rst: Unset bus hang flag in reset end flow
    Unset bus hang flag in reset end flow.
    
    MTK-Commit-Id: 460874f91958c90143b2080170cac0148e629946
    
    Change-Id: I5cfa2d461eb64d311cbe4e847f6920c77fe6e910
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS07693080
    Feature: Wi-Fi Driver CONNAC

--- os/linux/gl_rst.c
+++ os/linux/gl_rst.c
@@ -484,8 +484,12 @@ void glResetTrigger(struct ADAPTER *prAdapter,
 
 	dump_stack();
 	DBGLOG(INIT, ERROR,
-		"Trigger chip reset in %s line %u reason %s\n",
-		pucFile, u4Line, apucRstReason[eResetReason]);
+		"Trigger chip reset in %s line %u bushang %d flag 0x%x reason %s\n",
+		pucFile,
+		u4Line,
+		g_IsWfsysBusHang,
+		u4RstFlag,
+		apucRstReason[eResetReason]);
 
 	prAdapter->u4HifDbgFlag |= DEG_HIF_DEFAULT_DUMP;
 	halPrintHifDbgInfo(prAdapter);
@@ -1718,6 +1722,7 @@ void glResetSubsysRstProcedure(struct RESET_STRUCT *rst,
 			glResetMsgHandler(ENUM_RST_MSG_L04_END);
 		} else {
 			glResetUpdateFlag(FALSE);
+			g_IsWfsysBusHang = FALSE;
 			DBGLOG(INIT, INFO,
 				"Don't trigger subsys reset due to driver is not ready\n");
 		}
