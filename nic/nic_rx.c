98ef0a7d49470ead9993635b94030b15f6a54ec4
devin.yu <devin.yu@mediatek.com>
2022-11-15 20:01:46 +0800

    [ALPS07692338] Fix coverity issue
    [Coverity][ALPS]Read from pointer after free: 11374395
    [Coverity][ALPS]CERT-C Memory Management: 11374394
    
    MTK-Commit-Id: f38e56d5e1f11be052b774308056881e133818e2
    
    CR-Id: ALPS07692338
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I30e0337658695155e25e181e951ea9880ad6bf36
    Signed-off-by: devin.yu <devin.yu@mediatek.com>

--- nic/nic_rx.c
+++ nic/nic_rx.c
@@ -1387,7 +1387,7 @@ void nicRxProcessForwardPkt(struct ADAPTER *prAdapter,
 			prSwRfb->pvHeader,
 			(uint32_t) prSwRfb->u2PacketLen,
 			prSwRfb->aeCSUM) == WLAN_STATUS_SUCCESS) {
-
+		uint8_t ucTmpTid = 0;
 		/* parsing forward frame */
 		wlanProcessTxFrame(prAdapter, (void *) (prSwRfb->pvPacket));
 		/* pack into MSDU_INFO_T */
@@ -1401,6 +1401,7 @@ void nicRxProcessForwardPkt(struct ADAPTER *prAdapter,
 
 		/* release RX buffer (to rIndicatedRfbList) */
 		prSwRfb->pvPacket = NULL;
+		ucTmpTid = prSwRfb->ucTid;
 		nicRxReturnRFB(prAdapter, prSwRfb);
 
 		/* Handle if prMsduInfo out of bss index range*/
@@ -1421,7 +1422,7 @@ void nicRxProcessForwardPkt(struct ADAPTER *prAdapter,
 		/* add resource control for WMM forward packet */
 		GLUE_INC_REF_CNT(prTxCtrl
 			->i4PendingFwdFrameWMMCount[
-			aucACI2TxQIdx[aucTid2ACI[prSwRfb->ucTid]]]);
+			aucACI2TxQIdx[aucTid2ACI[ucTmpTid]]]);
 
 		/* send into TX queue */
 		KAL_ACQUIRE_SPIN_LOCK(prAdapter, SPIN_LOCK_QM_TX_QUEUE);
