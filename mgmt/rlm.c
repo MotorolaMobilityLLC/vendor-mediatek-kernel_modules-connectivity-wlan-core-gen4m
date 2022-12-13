3d7f33b9e5f074ef8b01108014c02f231364cb5e
Rachel Tseng <Rachel.Tseng@mediatek.com>
2022-12-13 15:34:26 +0800

    [ALPS07748992] wlan: indicate correct interface when channel switch
    [Description]
    Fix warining log in cfg80211_ch_switch_notify. Driver should report
    to correct interface when channel switch done, instead of always
    reporting to wlan0.
    
    MTK-Commit-Id: 7c6ef6e29e82d4488d048f487463a80c4ced62d7
    
    Change-Id: Ifbd9184c9da4d9ca7ad3186c03f77e48e0148a7e
    CR-Id: ALPS07748992
    Feature: [Module] Wi-Fi Driver
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6843215
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: tf.huang <tf.huang@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity

--- mgmt/rlm.c
+++ mgmt/rlm.c
@@ -6377,7 +6377,8 @@ void rlmCsaTimeout(struct ADAPTER *prAdapter,
 				prAdapter->prGlueInfo,
 				prBssInfo->eBssSCO,
 				prBssDesc->ucChannelNum,
-				prBssDesc->eBand);
+				prBssDesc->eBand,
+				prBssInfo->ucBssIndex);
 	} else {
 		DBGLOG(RLM, INFO,
 		       "DFS: BSS: " MACSTR " Desc is not found\n ",
--- os/linux/include/gl_kal.h
+++ os/linux/include/gl_kal.h
@@ -1718,7 +1718,8 @@ kalRemainOnChannelExpired(struct GLUE_INFO *prGlueInfo,
 void
 kalIndicateChannelSwitch(struct GLUE_INFO *prGlueInfo,
 			enum ENUM_CHNL_EXT eSco,
-			uint8_t ucChannelNum, enum ENUM_BAND eBand);
+			uint8_t ucChannelNum, enum ENUM_BAND eBand,
+			uint8_t ucBssIndex);
 #endif
 
 void
