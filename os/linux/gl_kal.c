0f3447a3050699334672eaabc6dd7155bb2d004e
Rachel Tseng <Rachel.Tseng@mediatek.com>
2022-12-13 15:34:06 +0800

    [ALPS07723995] wlan: fix FT roaming fail
    [Description]
    1. Not support Resource Request Protocol. Thus, should disable it to
    avoid sending second AUTH and being rejected by AP with status 14 or
    53.
    2. FT IE is cleared only when indicating DISCONNECT. However, STA may
    be disconnected by indicating JOIN_FAILURE, and FT IE wont be cleared.
    FT IE should be cleared not only when indicating DISCONNECT, but also
    indicating JOIN_FAILURE.
    
    MTK-Commit-Id: e1cd60ca3c27942fad242ce26da36a36f2d91013
    
    Change-Id: I8d4f78114e985b7cea8701220240177ad8aa68ae
    CR-Id: ALPS07723995
    Feature: [Module] Wi-Fi Driver
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    (cherry picked from commit b42adc2c8003bca65cf5ef16e00dea4764628a3e)
    (cherry picked from commit fc2ef506f3c4f88d3d32b9f9422f2199f3b0ad3f)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6793603
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_mspautosanity
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: tf.huang <tf.huang@mediatek.com>

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -2926,6 +2926,16 @@ kalIndicateStatusAndComplete(struct GLUE_INFO
 				u2JoinStatus,
 				GFP_KERNEL);
 #endif
+
+		prFtIEs = aisGetFtIe(prAdapter, ucBssIndex);
+		if (prFtIEs) {
+			kalMemFree(prFtIEs->pucIEBuf,
+				VIR_MEM_TYPE,
+				prFtIEs->u4IeLength);
+			kalMemZero(prFtIEs,
+				sizeof(*prFtIEs));
+		}
+
 		kalSetMediaStateIndicated(prGlueInfo,
 			MEDIA_STATE_DISCONNECTED,
 			ucBssIndex);
