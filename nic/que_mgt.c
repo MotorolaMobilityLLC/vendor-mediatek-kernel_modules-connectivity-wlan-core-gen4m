45bd3b6aa5591f7e86cb567e3dbb5c6f47c468cf
jiaxin.chen <jiaxin.chen@mediatek.com>
2022-12-13 15:34:39 +0800

    [ALPS07732352] Revert "[ALPS07694799] Check StaRec IsValid"
    This reverts commit 58c01d4b034361e0ccb67dbf0437d0cb44bb1a00.
    
    Reason for revert: <side effect: 1X TX drop for no STA_REC>
    
    MTK-Commit-Id: e070484b9a9c2450faa00f291c796e9e1b924d0c
    
    Signed-off-by: Jiaxin Chen <jiaxin.chen@mediatek.com>
    CR-Id: ALPS07732352
    Feature: Wi-Fi AOSP
    Change-Id: I58718b9ef7c9a2c4466af9b0906f847c6f631706
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6847961
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Test: srv_pf_nep_sanity
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Test: srv_mspautosanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: wilson.lien <wilson.lien@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>

--- nic/que_mgt.c
+++ nic/que_mgt.c
@@ -1459,7 +1459,7 @@ void qmDetermineStaRecIndex(struct ADAPTER *prAdapter,
 				       prTempStaRec->fgIsInUse,
 				       prTempStaRec->eStaType);
 
-				if (prTempStaRec->fgIsValid) {
+				if (prTempStaRec->fgIsInUse) {
 					prMsduInfo->ucStaRecIndex =
 						prTempStaRec->ucIndex;
 					DBGLOG(QM, LOUD, "TX with AP_STA[%u]\n",
@@ -1483,7 +1483,7 @@ void qmDetermineStaRecIndex(struct ADAPTER *prAdapter,
 	prTempStaRec = cnmGetStaRecByAddress(prAdapter,
 			prMsduInfo->ucBssIndex,
 			prMsduInfo->aucEthDestAddr);
-	if (prTempStaRec && prTempStaRec->fgIsValid) {
+	if (prTempStaRec) {
 		prMsduInfo->ucStaRecIndex = prTempStaRec->ucIndex;
 		DBGLOG(QM, LOUD, "TX with STA[%u]\n",
 			prTempStaRec->ucIndex);
