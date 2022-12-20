db5591616b0d263a5a5d02b0c73a533e9412839b
Steven-sw Wang <steven-sw.wang@mediatek.com>
2022-12-20 14:07:33 +0800

    [ALPS07705684] wlan: Disable SDIO related cfg
    Disable CFG_SUPPORT_SDIO_READ_WRITE_PATTERN
    
    MTK-Commit-Id: fe5566ac20d6e9d14f32bc442827bed92ed7ba9a
    
    CR-Id: ALPS07705684
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: perry.liao <perry.liao@mediatek.com>
    Change-Id: I7edf7da69708f6fecb764580e20694008092ce08
    (cherry picked from commit 513237e4c36303752498af10960f268b293ee8b9)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6691921
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: tommy.yen <tommy.yen@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Tested-by: harry.liang <harry.liang@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: harry.liang <harry.liang@mediatek.com>
    Tested-by: tommy.yen <tommy.yen@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    (cherry picked from commit 7e9ea8cfa749cb818957dc6a39ffc5de9525a8c9)
    (cherry-pick from 49f56ef07ab1a5a4216efb15171847185ec36b57)
    (cherry-pick from b47c9fc13d659340cfb024a7a9d16ae2310311e7)
    (cherry-pick from 50f17d689d7c4db1659821184a80aeda911eb36b)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6593920
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Tested-by: srv_ssm2_admin <srv_ssm2_admin@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: wilson.lien <wilson.lien@mediatek.com>

--- include/config.h
+++ include/config.h
@@ -1065,7 +1065,7 @@
  * Flags of SDIO test pattern support
  *------------------------------------------------------------------------------
  */
-#define CFG_SUPPORT_SDIO_READ_WRITE_PATTERN 1
+#define CFG_SUPPORT_SDIO_READ_WRITE_PATTERN 0
 
 /*------------------------------------------------------------------------------
  * Flags of Workaround
