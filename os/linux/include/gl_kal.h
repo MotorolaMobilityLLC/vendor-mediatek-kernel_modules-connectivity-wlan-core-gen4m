c377fa4741b16afc45c57bcd463471a04c106ae4
Rachel Tseng <Rachel.Tseng@mediatek.com>
2023-01-18 17:20:45 +0800

    [ALPS07833066] wlan: disable wifi standalone log feature
    [Description]
    Add config for wifi standalone log feature, and default
    disable it.
    
    MTK-Commit-Id: 8f8f107a1b2c78d80d7f1fe6bac394d8ccc62a6d
    
    Change-Id: I97d6734dae22e9b272ca172b6b9a5b0aa55d93e4
    CR-Id: ALPS07833066
    Feature: [Module] Wi-Fi Driver
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7024241
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>

--- os/linux/include/gl_kal.h
+++ os/linux/include/gl_kal.h
@@ -251,8 +251,10 @@ extern bool fgIsTxPowerDecreased;
 #define TRAFFIC_RHRESHOLD	150
 #endif
 
+#if CFG_SUPPORT_SA_LOG
 #define WIFI_LOG_MSG_MAX	(512)
 #define WIFI_LOG_MSG_BUFFER	(WIFI_LOG_MSG_MAX * 2)
+#endif
 
 #if (CFG_SUPPORT_POWER_THROTTLING == 1)
 #define PWR_LEVEL_STAT_UPDATE_INTERVAL	60	/* sec */
@@ -1056,18 +1058,20 @@ int8_t atoi(uint8_t ch);
 
 #define kalGetTimeTick()                jiffies_to_msecs(jiffies)
 
-#define kalPrintLogLimited(fmt, ...)					\
+#if CFG_SUPPORT_SA_LOG
+#define kalPrintSALogLimited(fmt, ...)					\
 ({									\
 	static DEFINE_RATELIMIT_STATE(_rs,				\
 		DEFAULT_RATELIMIT_INTERVAL, DEFAULT_RATELIMIT_BURST);	\
 									\
 	if (__ratelimit(&_rs))						\
-		kalPrintLog(fmt, ##__VA_ARGS__);			\
+		kalPrintSALog(fmt, ##__VA_ARGS__);			\
 })
+#endif
 
 #define WLAN_TAG                        "[wlan]"
-#define kalPrint               kalPrintLog
-#define kalPrintLimited(_Fmt...) kalPrintLogLimited(WLAN_TAG _Fmt)
+#define kalPrint(_Fmt...)               pr_info(WLAN_TAG _Fmt)
+#define kalPrintLimited(_Fmt...)        pr_info_ratelimited(WLAN_TAG _Fmt)
 
 #define kalBreakPoint() \
 do { \
@@ -1961,8 +1965,9 @@ void kalSyncTimeToFWByIoctl(void);
 void kalUpdateCompHdlrRec(IN struct ADAPTER *prAdapter,
 	IN PFN_OID_HANDLER_FUNC pfnOidHandler, IN struct CMD_INFO *prCmdInfo);
 
-extern uint32_t get_wifi_standalone_log_mode(void);
-void kalPrintLog(const char *fmt, ...);
+#if CFG_SUPPORT_SA_LOG
+void kalPrintSALog(const char *fmt, ...);
+#endif
 
 #if (CFG_SUPPORT_POWER_THROTTLING == 1)
 void kalPwrLevelHdlrRegister(IN struct ADAPTER *prAdapter,
