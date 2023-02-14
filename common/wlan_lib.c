e86ee2add838325909cc86167c0dc903659c0d7a
Sandy Hu <sandy.hu@mediatek.com>
2023-02-14 09:40:45 +0800

    [ALPS07794476] fix null dereference
    check prStaRec
    
    MTK-Commit-Id: e4c458b488530dcf8cc1976996037b65b102d157
    
    CR-Id: ALPS07794476
    Feature: [Module]Wi-Fi Driver
    Change-Id: I0a62f0aecb9935cb3098e7bd6a6db1aa0f3bfd34
    Signed-off-by: Sandy Hu <sandy.hu@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7077741
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: wyatt.sun <wyatt.sun@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -6212,6 +6212,9 @@ wlanQueryStaStatistics(struct ADAPTER *prAdapter,
 			}
 		}
 
+		if (!prStaRec)
+			return WLAN_STATUS_INVALID_DATA;
+
 		/* 4 6. Ensure FW supports get station link status */
 		rQueryCmdStaStatistics.ucIndex = prStaRec->ucIndex;
 		COPY_MAC_ADDR(rQueryCmdStaStatistics.aucMacAddr,
