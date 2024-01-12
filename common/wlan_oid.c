e8e18306937e4108330a76ddb62726a688f8e1a2
Rachel Tseng <Rachel.Tseng@mediatek.com>
2024-01-12 22:26:28 +0800

    [ALPS06720434] ais: stop AUTH or ASSOC retry flow if turning off wifi
    During ais AUTH or ASSOC flow, if wifi is turning off, SAA state machine
    cant notice it and may keep retrying sending the packet. Therefore, SAA
    should check wfi status when retrying.
    
    MTK-Commit-Id: 5114d809c125c546a61a422173a1d5a94196e45b
    
    Change-Id: I0fc6a3fd5f5e8731785583fb254af69377f6297d
    CR-Id: ALPS06720434
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7942226
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    (cherry picked from commit af653e00551085268b045da3809aaff7d98817cc)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7957370
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -3742,8 +3742,6 @@ wlanoidSetPmkid(struct ADAPTER *prAdapter,
 {
 	struct PARAM_PMKID *prPmkid;
 
-	DBGLOG(REQ, INFO, "wlanoidSetPmkid\n");
-
 	ASSERT(prAdapter);
 	ASSERT(pu4SetInfoLen);
 	ASSERT(pvSetBuffer);
