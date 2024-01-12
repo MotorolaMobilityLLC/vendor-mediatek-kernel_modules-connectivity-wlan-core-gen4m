cd9149ba59c01481f66a1133f10dd82bee173a2e
mtk09010 <johnny.chiang@mediatek.com>
2024-01-12 22:24:11 +0800

    [ALPS08316135] sap: add pointer check for p2p fole fsm
    add pointer check for p2p fole fsm
    
    MTK-Commit-Id: 70281abc89a68a2836d413f3f5cd4cfe468d15e9
    
    CR-Id: ALPS08316135
    Signed-off-by: mtk09010 <johnny.chiang@mediatek.com>
    Feature: Wi-Fi Driver
    Change-Id: I4d9768d343edb94f7e8c548bd33427938b668c4f
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7930646
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    (cherry picked from commit e0ceb5a0a84bc090f3cd8596bb4a0280c2f58970)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7940457
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: brian-kl.lin <brian-kl.lin@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_mspautosanity

--- mgmt/p2p_role_fsm.c
+++ mgmt/p2p_role_fsm.c
@@ -5131,6 +5131,10 @@ void p2pRoleFsmRunEventAcs(struct ADAPTER *prAdapter,
 	prMsgAcsRequest = (struct MSG_P2P_ACS_REQUEST *) prMsgHdr;
 	prP2pRoleFsmInfo = P2P_ROLE_INDEX_2_ROLE_FSM_INFO(prAdapter,
 			prMsgAcsRequest->ucRoleIdx);
+
+	if (!prP2pRoleFsmInfo)
+		goto exit;
+
 	prP2pRoleFsmInfo->fgIsChannelSelectByAcs = TRUE;
 	prAcsReqInfo = &prP2pRoleFsmInfo->rAcsReqInfo;
 
@@ -5178,7 +5182,9 @@ void p2pRoleFsmRunEventAcs(struct ADAPTER *prAdapter,
 	if (prAcsReqInfo->eHwMode == P2P_VENDOR_ACS_HW_MODE_11ANY) {
 		struct BSS_INFO *prAisBssInfo;
 		prAisBssInfo = aisGetDefaultLinkBssInfo(prAdapter);
-		if (prAisBssInfo->eConnectionState == MEDIA_STATE_CONNECTED &&
+		if (prAisBssInfo &&
+			prAisBssInfo->eConnectionState ==
+			MEDIA_STATE_CONNECTED &&
 			(!p2pFuncIsDualAPMode(prAdapter) ||
 			(p2pFuncIsDualAPMode(prAdapter) &&
 			prAisBssInfo->eBand > BAND_2G4))) {
