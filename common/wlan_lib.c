4796800b34c7434307b2b440d68621a9d261f757
Juien Chang <jui-en.chang@mediatek.com>
2024-01-12 22:08:47 +0800

    [WCNCR00283023][Falcon] NAN Default BW
    [Description]
    ** For CE only **
    CE Platform only support two WMM concurrent
    Need decide mapping WMM first
    (Wanted Channel of Bssidxs WMM should used WMM0)
    
    [ToDo]
    Need discuss general case of three networks concurrent flow on CE platform
    
    MTK-Commit-Id: 511d78c93b0bd9d5c8aaae8a69a8c663cf51d16d
    
    Signed-off-by: jui-en.chang <jui-en.chang@mediatek.com>
    CR-Id: WCNCR00283023
    Feature: Wi-Fi NAN
    Change-Id: I5dc8f657bdff205105116d47cf26c3998e5c5300
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6686586
    Reviewed-by: chiahsuan.chuang <chiahsuan.chuang@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Test: srv_mspautosanity
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: fred.shen <fred.shen@mediatek.com>
    Reviewed-by: longlay.ji <longlay.ji@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: wh.su <wh.su@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -7906,7 +7906,8 @@ void wlanInitFeatureOptionImpl(struct ADAPTER *prAdapter, uint8_t *pucKey)
 	INIT_UINT(prWifiVar->ucNan5gBandwidth, "Nan5gBw", MAX_BW_80MHZ);
 	INIT_UINT(prWifiVar->ucNdlFlowCtrlVer,
 		"NanNdlFlowCtrlVer", CFG_SUPPORT_NAN_ADVANCE_DATA_CONTROL);
-	INIT_UINT(prWifiVar->fgNanWmmSeq, "NanWmmSeq", 1);
+	INIT_UINT(prWifiVar->fgNanWmmSeq, "NanWmmSeq",
+		(prWifiVar->ucNanFixChnl < 36) ? 0:1);
 #endif
 
 	INIT_UINT(prWifiVar->fgReuseRSNIE, "ReuseRSNIE", FEATURE_DISABLED);
