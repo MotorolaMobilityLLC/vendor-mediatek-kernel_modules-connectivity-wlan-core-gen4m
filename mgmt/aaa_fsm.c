cc6147ba69ab4a08c107c114c00ad2aff869133f
chris.hong <chris.hong@mediatek.com>
2024-01-12 22:10:59 +0800

    [ALPS07777103] p2p: prevent skip auth
    Since P2P connection may send multiple auth frame.
    It need to process all auth to make sure connection success.
    
    MTK-Commit-Id: 405d583e0ad983309183693daf33477587f4d237
    
    Signed-off-by: chris.hong <chris.hong@mediatek.com>
    CR-Id: ALPS07777103
    Feature: Wi-Fi Direct
    Change-Id: I214e44ca689a9d2114759e3075df6d38268f7eae
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6895707
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: zeno.yang <zeno.yang@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Test: srv_mspautosanity
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    (cherry picked from commit c26c7cf877e8229483015d32e4d88b24417e1f2b)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6931878
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Reviewed-by: brian-kl.lin <brian-kl.lin@mediatek.com>

--- mgmt/aaa_fsm.c
+++ mgmt/aaa_fsm.c
@@ -256,6 +256,7 @@ void aaaFsmRunEventRxAuth(struct ADAPTER *prAdapter,
 	u_int8_t fgReplyAuth = FALSE;
 	struct WLAN_AUTH_FRAME *prAuthFrame = (struct WLAN_AUTH_FRAME *) NULL;
 	uint32_t rStatus;
+	struct P2P_CONNECTION_SETTINGS *prP2pConnSettings = NULL;
 
 	ASSERT(prAdapter);
 
@@ -428,13 +429,21 @@ bow_proc:
 					uint32_t rAuthTime;
 					uint32_t rTimeDiff;
 
+					prP2pConnSettings =
+						prAdapter->rWifiVar
+						.prP2PConnSettings[
+						prBssInfo->u4PrivateData];
+
 					DBGLOG(AAA, WARN,
 						"Prev AAState (%d) != IDLE.\n",
 						prStaRec->eAuthAssocState);
 					GET_CURRENT_SYSTIME(&rAuthTime);
 					rTimeDiff = rAuthTime -
 						prStaRec->rUpdateTime;
-					if (prAuthFrame->u2AuthTransSeqNo ==
+					if (p2pFuncIsAPMode(
+						prP2pConnSettings) &&
+						prAuthFrame
+						->u2AuthTransSeqNo ==
 						AUTH_TRANSACTION_SEQ_1 &&
 						rTimeDiff <
 						MIN_AUTH_TIME_DIFF) {
