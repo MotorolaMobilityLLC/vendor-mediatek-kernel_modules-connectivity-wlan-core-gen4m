7a5774b96259ac8048b99bb28988d138eaeed9a3
Herman Yang <herman.yang@mediatek.com>
2024-01-12 21:22:53 +0800

    [ALPS05217376] perf: monitor tput every second
    NIC decides enabling gro by current rx tput but rx tput is updated
    only when perf monitor is started. Thus, when screen off which results
    in perf monitor stopped, gro doesnt work.Therefore, we need correct
    tput even when perf monitor is not enabled. To achieve this, update
    tput in main_threads main loop periodicaly.
    
    MTK-Commit-Id: 300e125a4d07c7328e5617fba8ebb710b26cd934
    
    Change-Id: Iff94683ef563d449f88357f88c25a93b072f9d01
    CR-Id: ALPS05217376
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Herman Yang <herman.yang@mediatek.com>

--- os/none/include/gl_kal.h
+++ os/none/include/gl_kal.h
@@ -177,9 +177,10 @@ extern int g_u4WlanInitFlag;
 #endif
 
 /* performance monitor feature */
-#define PERF_MON_DISABLE_BIT    (0)
-#define PERF_MON_STOP_BIT       (1)
-#define PERF_MON_RUNNING_BIT    (2)
+#define PERF_MON_INIT_BIT       (0)
+#define PERF_MON_DISABLE_BIT    (1)
+#define PERF_MON_STOP_BIT       (2)
+#define PERF_MON_RUNNING_BIT    (3)
 
 #define PERF_MON_UPDATE_INTERVAL (1000)
 #define PERF_MON_TP_MAX_THRESHOLD (10)
