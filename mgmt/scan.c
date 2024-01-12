b26b34ae0575cd4b73a6047898bd910be44bd6e6
TS Hsu <ts.hsu@mediatek.com>
2024-01-12 22:22:30 +0800

    [ALPS07921829] Fix weird AP carry abnormal RNR IE issue
    1.
    If AP carry RNR IE with invalid length, we might enter
    infinite loop. Because calculated length is UINT8, might be
    overflow and therefore smaller than RNR IE_LEN.
    
    2.
    Enlarge scan done print info buffer size to avoid
    out of boundary access when channel count is too many.
    
    MTK-Commit-Id: a40364d0b88d0f5b3b521cb88397f7f436f4ecc9
    
    Change-Id: I6ad52e56dbf58bb718bcb64b302861c006bc9c92
    CR-Id: ALPS07921829
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: TS Hsu <ts.hsu@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7596059
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit 9c5eeeda7148be581c54ec3e0e5531833a522517)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7760961
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: ffbs.lin <ffbs.lin@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: jeff-kk.zhang <jeff-kk.zhang@mediatek.com>
    Tested-by: ffbs.lin <ffbs.lin@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: efane.wu <efane.wu@mediatek.com>

--- mgmt/scan.c
+++ mgmt/scan.c
@@ -1772,10 +1772,10 @@ void scanParsingRnrElement(struct ADAPTER *prAdapter,
 {
 	uint8_t i = 0, j = 0, ucNewLink = FALSE, ucRnrChNum;
 	uint8_t ucShortSsidOffset, ucBssParamOffset, ucMldParamOffset;
-	uint8_t ucBssidNum = 0, ucCurrentLength = 0, ucShortSsidNum = 0;
+	uint8_t ucBssidNum = 0, ucShortSsidNum = 0;
 	uint8_t ucHasBssid = FALSE, ucScanEnable = TRUE, ucOpClass = 0;
 	uint8_t aucNullAddr[] = NULL_MAC_ADDR;
-	uint16_t u2TbttInfoCount, u2TbttInfoLength;
+	uint16_t u2TbttInfoCount, u2TbttInfoLength, u2CurrentLength = 0;
 	uint8_t ucHasMlo = FALSE, ucNeedMlo = FALSE, ucHasSameCh = FALSE;
 	struct NEIGHBOR_AP_INFO *prNeighborAPInfo = NULL;
 	struct NEIGHBOR_AP_INFO_FIELD *prNeighborAPInfoField;
@@ -1796,9 +1796,9 @@ void scanParsingRnrElement(struct ADAPTER *prAdapter,
 	ucNeedMlo = (prAdapter->rWifiVar.ucMldLinkMax > 1);
 #endif
 
-	while (ucCurrentLength < IE_LEN(pucIE)) {
+	while (u2CurrentLength < IE_LEN(pucIE)) {
 		prNeighborAPInfoField =	(struct NEIGHBOR_AP_INFO_FIELD *)
-					(prRnr->aucInfoField + ucCurrentLength);
+					(prRnr->aucInfoField + u2CurrentLength);
 		ucOpClass = prNeighborAPInfoField->ucOpClass;
 
 		/* get TBTT information count and length for
@@ -1873,9 +1873,9 @@ void scanParsingRnrElement(struct ADAPTER *prAdapter,
 			*/
 			DBGLOG(SCN, WARN,
 				"RNR w/o BSSID, length(%d,%d),TBTT(%d,%d)\n",
-				IE_LEN(pucIE), ucCurrentLength,
+				IE_LEN(pucIE), u2CurrentLength,
 				u2TbttInfoCount, u2TbttInfoLength);
-			ucCurrentLength += 4 +
+			u2CurrentLength += 4 +
 				(u2TbttInfoCount * u2TbttInfoLength);
 			continue;
 		}
@@ -1889,14 +1889,14 @@ void scanParsingRnrElement(struct ADAPTER *prAdapter,
 				ucOpClass, ucNeedMlo, ucHasMlo);
 
 			/* Calculate next NeighborAPInfo's index if exists */
-			ucCurrentLength += 4 +
+			u2CurrentLength += 4 +
 				(u2TbttInfoCount * u2TbttInfoLength);
 			continue;
 		} else {
 			/* RNR bring 6G channel, but chip not support 6G */
 			/* Calculate next NeighborAPInfo's index if exists */
 #if !(CFG_SUPPORT_WIFI_6G) && (CFG_SUPPORT_802_11BE_MLO == 0)
-			ucCurrentLength += 4 +
+			u2CurrentLength += 4 +
 				(u2TbttInfoCount * u2TbttInfoLength);
 			continue;
 #endif
@@ -2015,7 +2015,7 @@ void scanParsingRnrElement(struct ADAPTER *prAdapter,
 			if (ucNewLink)
 				cnmMemFree(prAdapter, prNeighborAPInfo);
 			/* Calculate next NeighborAPInfo's index if exists */
-			ucCurrentLength += 4 +
+			u2CurrentLength += 4 +
 				(u2TbttInfoCount * u2TbttInfoLength);
 			continue;
 		}
@@ -2112,7 +2112,7 @@ void scanParsingRnrElement(struct ADAPTER *prAdapter,
 						prBssDesc, ucBssidNum);
 		}
 		/* Calculate next NeighborAPInfo's index if exists */
-		ucCurrentLength += 4 + (u2TbttInfoCount * u2TbttInfoLength);
+		u2CurrentLength += 4 + (u2TbttInfoCount * u2TbttInfoLength);
 
 		/* Only handle RnR with BSSID */
 		if (ucHasBssid && ucScanEnable) {
