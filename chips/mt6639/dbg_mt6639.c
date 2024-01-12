a737cf6c79c58e39c7cc3438ad9dccb2d22d61b5
Ben Lai <ben.lai@mediatek.com>
2024-01-12 22:07:13 +0800

    [ALPS07629795] wlan: Integration with connv3 driver
    1. power_on_notify support.
    2. conninfra bus dump.
    3. hif dump interface.
    4. efuse flow.
    5. wfsys lock & debug trace.
    
    MTK-Commit-Id: a9784320674137da6258653c958b5a3c2a903c3e
    
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS07629795
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ib6423fa38e0e235a544bae951744ef994d13cc4b
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6210133
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: jeff.hsu <jeff.hsu@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_mspautosanity

--- chips/mt6639/dbg_mt6639.c
+++ chips/mt6639/dbg_mt6639.c
@@ -1816,18 +1816,6 @@ void mt6639_dumpWfBusReg(struct ADAPTER *ad)
 
 static void mt6639_dumpConninfraBus(struct ADAPTER *ad)
 {
-#if IS_ENABLED(CFG_MTK_WIFI_CONNV3_SUPPORT)
-	struct connv3_cr_cb cb;
-#endif
-
-#if IS_ENABLED(CFG_MTK_WIFI_CONNV3_SUPPORT)
-	kalMemZero(&cb, sizeof(cb));
-	cb.read = wf_reg_read_wrapper;
-	cb.write = wf_reg_write_wrapper;
-	cb.write_mask = wf_reg_write_mask_wrapper;
-
-	connv3_conninfra_bus_dump(CONNV3_DRV_TYPE_WIFI, &cb, ad);
-#endif
 }
 
 void mt6639_DumpBusHangCr(struct ADAPTER *ad)
