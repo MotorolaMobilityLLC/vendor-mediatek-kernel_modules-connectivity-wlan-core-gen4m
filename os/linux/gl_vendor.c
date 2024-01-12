8c41879a054befa23fa255449cabebe5a3478a58
Kok Suan Lim <ks.lim@mediatek.com>
2024-01-12 22:13:54 +0800

    [ALPS07380104] radiotap: bugfix patch sync
    1. enable CONFIG_SNIFFER_RADIOTAP on smartphone
    2. fix rx tput degrade issue due to pdma scatter
    3. fix mode switching fail issue
    4. add debug counter for sniffer mode
    5. fix KE when sniffer mode is off
    6. allow kalPerMonUpdate/halDumpHifStats to dump log when sniffer
       mode on
    7. prevent P2P scan when sniffer mode is on
    8. fix radiotap headroom zero issue
    9. fix KE after change to monitor mode
    10. fix sniffer mode schedule bug due to dynamic pagepool feature
    
    CL: 6303998, 6759027
    
    MTK-Commit-Id: 30df2decfc34f15fc76038ead41ffc092b9be478
    
    Signed-off-by: Kok Suan Lim <ks.lim@mediatek.com>
    CR-Id: ALPS07380104
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ibbb9f7103a3ada6f1f622327ca2d52b9cf2679cb
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7127142
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: allan.wang <allan.wang@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Test: srv_pf_nep_sanity

--- os/linux/gl_vendor.c
+++ os/linux/gl_vendor.c
@@ -1810,6 +1810,11 @@ int mtk_cfg80211_vendor_llstats_get_info(struct wiphy *wiphy,
 	prAdapter = prGlueInfo->prAdapter;
 
 	ucBssIdx = wlanGetBssIdx(wdev->netdev);
+	if (ucBssIdx >= MAX_BSSID_NUM) {
+		DBGLOG(REQ, ERROR, "Invalid BSS Index ucBssIdx=%u\n", ucBssIdx);
+		return -EFAULT;
+	}
+
 	if (prAdapter->ucLinkStatsBssNum == BSSID_NUM)
 		band_hint = 0x00; /* select band/radio by Bss HW band index */
 
