44d38362357ed46902fabfd4da591f4d99e7c56b
Facer Pei <facer.pei@mediatek.com>
2024-01-12 22:19:54 +0800

    [ALPS07243622] wlan: rename fw_entry
    Rename fw_entry to avoid duplicate symbol problem when modify to
    build-in driver.
    
    MTK-Commit-Id: 00424c634db310cc820ec3a71ced655278e08e1e
    
    Change-Id: I52bc061002cbbf5aaafb8aaf8481bd6677aff1e9
    Signed-off-by: Facer Pei <facer.pei@mediatek.com>
    CR-Id: ALPS07243622
    Feature: [Module]Wi-Fi Driver
    (cherry picked from commit c95da1f7d1eefbcc82293dde969f663280b8a205)
    (cherry picked from commit e5f221f092e2a146643111f59789ea47323f4614)
    (cherry picked from commit 6290e76c83e3c6f6754c1e4c6ade277840b44f50)
    (cherry picked from commit 03d2e5cc0d5e5afb47a7e42d264e5c2c47b7caa4)
    (cherry picked from commit 216b26a9df12e0e3d40aa7645a8a02911816c604)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7539989
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -218,7 +218,7 @@ static int32_t kalWorkGetCpu(struct GLUE_INFO *pr,
 #define  KGIDT_VALUE(v) v
 #endif
 
-const struct firmware *fw_entry;
+const struct firmware *wlan_fw_entry;
 
 /* Default */
 static uint8_t *apucFwName[] = {
@@ -296,7 +296,8 @@ uint32_t kalFirmwareOpen(struct GLUE_INFO *prGlueInfo,
 		 *               "/firmware/image"
 		 * Linux path: "/lib/firmware", "/lib/firmware/update"
 		 */
-		ret = _kalRequestFirmware(&fw_entry, apucNameTable[ucNameIdx],
+		ret = _kalRequestFirmware(&wlan_fw_entry,
+				       apucNameTable[ucNameIdx],
 				       prGlueInfo->prDev);
 
 		if (ret) {
@@ -340,7 +341,7 @@ error_open:
 /*----------------------------------------------------------------------------*/
 uint32_t kalFirmwareClose(struct GLUE_INFO *prGlueInfo)
 {
-	release_firmware(fw_entry);
+	release_firmware(wlan_fw_entry);
 
 	return WLAN_STATUS_SUCCESS;
 }
@@ -365,12 +366,12 @@ uint32_t kalFirmwareLoad(struct GLUE_INFO *prGlueInfo,
 	ASSERT(pu4Size);
 	ASSERT(prBuf);
 
-	if ((fw_entry == NULL) || (fw_entry->size == 0)
-	    || (fw_entry->data == NULL)) {
+	if ((wlan_fw_entry == NULL) || (wlan_fw_entry->size == 0)
+	    || (wlan_fw_entry->data == NULL)) {
 		goto error_read;
 	} else {
-		memcpy(prBuf, fw_entry->data, fw_entry->size);
-		*pu4Size = fw_entry->size;
+		memcpy(prBuf, wlan_fw_entry->data, wlan_fw_entry->size);
+		*pu4Size = wlan_fw_entry->size;
 	}
 
 	return WLAN_STATUS_SUCCESS;
@@ -398,7 +399,7 @@ uint32_t kalFirmwareSize(struct GLUE_INFO *prGlueInfo,
 	ASSERT(prGlueInfo);
 	ASSERT(pu4Size);
 
-	*pu4Size = fw_entry->size;
+	*pu4Size = wlan_fw_entry->size;
 
 	return WLAN_STATUS_SUCCESS;
 }
