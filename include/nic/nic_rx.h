75622a8251eee66f070e7d4b8fcb9399cafd95ed
Kok Suan Lim <ks.lim@mediatek.com>
2024-01-12 22:14:11 +0800

    [ALPS07859434] nic: fix bug of RXM_IS_FROM_DS_TO_DS
    fix bug of RXM_IS_FROM_DS_TO_DS
    
    MTK-Commit-Id: fbe2e4edc200c559f9d6f1b3c4d057e048a504c6
    
    Change-Id: Ic8dca3c8c225a10c9f138f157a986da4dc822573
    Signed-off-by: Kok Suan Lim <ks.lim@mediatek.com>
    Feature: Wi-Fi Driver CONNAC
    CR-Id: ALPS07859434
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7113747
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Test: srv_pf_nep_sanity
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>

--- include/nic/nic_rx.h
+++ include/nic/nic_rx.h
@@ -1538,7 +1538,8 @@ struct ACTION_FRAME_SIZE_MAP {
 		TRUE : FALSE)
 
 #define RXM_IS_FROM_DS_TO_DS(_u2FrameCtrl) \
-	(RXM_IS_TO_DS(_u2FrameCtrl) && RXM_IS_FROM_DS(_u2FrameCtrl))
+	(((_u2FrameCtrl & MASK_TO_DS_FROM_DS) == MASK_TO_DS_FROM_DS) ?\
+		TRUE : FALSE)
 
 /*******************************************************************************
  *                   F U N C T I O N   D E C L A R A T I O N S
