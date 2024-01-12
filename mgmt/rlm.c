10626ccab007b57ae4be6742e00036ff9a4ac7b2
Sticky Chen <sticky.chen@mediatek.com>
2024-01-12 22:21:33 +0800

    [ALPS07573030] Add BtmCap config
    Add BtmCap config for SAE-5.8.x certification
    
    MTK-Commit-Id: ee8202201b8fa58c5061fbacfc798705b15af9c4
    
    CR-Id: ALPS07573030
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Sticky Chen <sticky.chen@mediatek.com>
    Change-Id: I81173ddada5df4e8ea70cc4c346b3c280b71d6f9
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7585487
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    (cherry picked from commit 16d6c398813fa24808c22d4bf53592eb2513a261)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7687283
    Reviewed-by: efane.wu <efane.wu@mediatek.com>
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    Reviewed-by: han.hu <han.hu@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_mspautosanity

--- mgmt/rlm.c
+++ mgmt/rlm.c
@@ -1329,12 +1329,11 @@ static void rlmFillExtCapIE(struct ADAPTER *prAdapter,
 
 	/* Disable BTM cap for WPA3 cert. */
 	if (IS_BSS_AIS(prBssInfo)
-		&& prAdapter->rWifiVar.u4SwTestMode ==
-		ENUM_SW_TEST_MODE_SIGMA_WPA3) {
+		&& IS_FEATURE_DISABLED(prAdapter->rWifiVar.ucBtmCap)) {
 		CLEAR_EXT_CAP(prExtCap->aucCapabilities,
 					ELEM_MAX_LEN_EXT_CAP,
 					ELEM_EXT_CAP_BSS_TRANSITION_BIT);
-		DBGLOG(RLM, INFO, "Disable BTM cap in WPA3 testmode!");
+		DBGLOG(RLM, INFO, "Disable BTM cap due to wifi.cfg");
 	}
 
 	while (prExtCap->ucLength > 0 &&
