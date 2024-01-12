fd4fc179eb4c26d3225fd7d36952cd494ce950e6
Kok Suan Lim <ks.lim@mediatek.com>
2024-01-12 22:04:00 +0800

    [ALPS07381291] wlan: fix napi memleak issue
    1. use dummy device when register napi, break the link of napi with
    normal netdev.
    2. move napi init/enable flow to wlan on/off flow.
    3. use kalRxGroInit to register GRO feature instead.
    
    MTK-Commit-Id: 6befb34dd99f9094d45c3f153fbd4a39623c2a8d
    
    Signed-off-by: Kok Suan Lim <ks.lim@mediatek.com>
    CR-Id: ALPS07381291
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I285097b8fd08cdc0c596e8744c10ed5a94fe5932
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6160252
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>

--- os/linux/gl_init.c
+++ os/linux/gl_init.c
@@ -2026,6 +2026,29 @@ static void glTaskletUninit(struct GLUE_INFO *prGlueInfo)
 #endif /* CFG_SUPPORT_TASKLET_FREE_MSDU */
 }
 
+static void glRxInit(struct GLUE_INFO *prGlueInfo)
+{
+#if CFG_SUPPORT_RX_GRO
+	kalNapiInit(prGlueInfo);
+#if CFG_SUPPORT_RX_NAPI
+	kalNapiRxDirectInit(prGlueInfo);
+	kalNapiEnable(prGlueInfo);
+#endif /* CFG_SUPPORT_RX_NAPI */
+#endif /* CFG_SUPPORT_RX_GRO */
+	glTaskletInit(prGlueInfo);
+}
+
+static void glRxUninit(struct GLUE_INFO *prGlueInfo)
+{
+	glTaskletUninit(prGlueInfo);
+#if CFG_SUPPORT_RX_GRO
+#if CFG_SUPPORT_RX_NAPI
+	kalNapiDisable(prGlueInfo);
+	kalNapiRxDirectUninit(prGlueInfo);
+#endif /* CFG_SUPPORT_RX_NAPI */
+#endif /* CFG_SUPPORT_RX_GRO */
+}
+
 static void wlanFreeNetDev(void)
 {
 	uint32_t u4Idx = 0;
@@ -2571,7 +2594,7 @@ static int wlanInit(struct net_device *prDev)
 #endif
 
 #if CFG_SUPPORT_RX_GRO
-	kalNapiInit(prDev);
+	kalRxGroInit(prDev);
 #endif /* CFG_SUPPORT_RX_GRO */
 	return 0;		/* success */
 }				/* end of wlanInit() */
@@ -2688,13 +2711,6 @@ static int wlanOpen(struct net_device *prDev)
 	ASSERT(prGlueInfo);
 #endif /* fos_change begin */
 
-#if CFG_SUPPORT_RX_GRO
-#if CFG_SUPPORT_RX_NAPI
-	kalNapiRxDirectInit(prDev);
-	kalNapiEnable(prDev);
-#endif /* CFG_SUPPORT_RX_NAPI */
-#endif /* CFG_SUPPORT_RX_GRO */
-
 	netif_tx_start_all_queues(prDev);
 /* fos_change begin */
 #if CFG_SUPPORT_WAKEUP_STATISTICS
@@ -2771,13 +2787,6 @@ static int wlanStop(struct net_device *prDev)
 	}
 
 	netif_tx_stop_all_queues(prDev);
-
-#if CFG_SUPPORT_RX_GRO
-#if CFG_SUPPORT_RX_NAPI
-	kalNapiDisable(prDev);
-	kalNapiRxDirectUninit(prDev);
-#endif /* CFG_SUPPORT_RX_NAPI */
-#endif /* CFG_SUPPORT_RX_GRO */
 
 	return 0;		/* success */
 }				/* end of wlanStop() */
@@ -6177,6 +6186,8 @@ int32_t wlanOffAtReset(void)
 #endif
 	glBusFreeIrq(prDev, prGlueInfo);
 
+	glRxUninit(prGlueInfo);
+
 #if (CFG_SUPPORT_TRACE_TC4 == 1)
 	wlanDebugTC4Uninit();
 #endif
@@ -6274,6 +6285,12 @@ int32_t wlanOnAtReset(void)
 		QUEUE_INITIALIZE(&prGlueInfo->rTxQueue);
 
 		glResetHifInfo(prGlueInfo);
+
+		/*
+		 * interrupt may come in after setup irq
+		 * we need to make sure that rx is ready before it
+		 */
+		glRxInit(prGlueInfo);
 
 		rStatus = glBusSetIrq(prDev, NULL, prGlueInfo);
 		if (rStatus != WLAN_STATUS_SUCCESS) {
@@ -6509,9 +6526,9 @@ static int32_t wlanProbe(void *pvData, void *pvDriverData)
 
 		/*
 		 * interrupt may come in after setup irq
-		 * we need to make sure that tasklet is ready before it
+		 * we need to make sure that rx is ready before it
 		 */
-		glTaskletInit(prGlueInfo);
+		glRxInit(prGlueInfo);
 
 		i4Status = glBusSetIrq(prWdev->netdev, NULL, prGlueInfo);
 
@@ -6723,7 +6740,7 @@ static int32_t wlanProbe(void *pvData, void *pvDriverData)
 						netdev_priv(prWdev->netdev)));
 		/* fallthrough */
 		case BUS_SET_IRQ_FAIL:
-			glTaskletUninit(prGlueInfo);
+			glRxUninit(prGlueInfo);
 			if (prChipInfo && prChipInfo->fw_dl_ops->mcu_deinit)
 				prChipInfo->fw_dl_ops->mcu_deinit(prAdapter);
 		/* fallthrough */
@@ -7046,6 +7063,8 @@ static void wlanRemove(void)
 	/* 4 <x> Stopping handling interrupt and free IRQ */
 	glBusFreeIrq(prDev, prGlueInfo);
 
+	glRxUninit(prGlueInfo);
+
 	/* 4 <5> Release the Bus */
 	glBusRelease(prDev);
 
@@ -7066,8 +7085,6 @@ static void wlanRemove(void)
 	/* 4 <7> Destroy the device */
 	wlanNetDestroy(prDev->ieee80211_ptr);
 	prDev = NULL;
-
-	glTaskletUninit(prGlueInfo);
 
 	/* 4 <8> Unregister early suspend callback */
 #if CFG_ENABLE_EARLY_SUSPEND
