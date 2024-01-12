c0e70dacf9dbe408a8d01c795485019ae81a6ef1
iris.liao <iris.liao@mediatek.com>
2024-01-12 21:12:01 +0800

    [ALPS04738790] [WiFi] DMASHDL early porting
    [Detail]
     DMASHDL early porting for one ring
    
    MTK-Commit-Id: dcb2aba5fae794628a6b2c744a10f8859952d91a
    
    Change-Id: I8ad70a81b24b9dd07ad2b2481095e7a383235c81
    CR-Id: ALPS04738790
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: iris.liao <iris.liao@mediatek.com>

---
+++ chips/soc3_0/hal_dmashdl_soc3_0.c
@@ -0,0 +1,361 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+/*! \file   hal_dmashdl_mt6885.c
+*    \brief  DMASHDL HAL API for MT6885
+*
+*    This file contains all routines which are exported
+     from MediaTek 802.11 Wireless LAN driver stack to GLUE Layer.
+*/
+
+#ifdef SOC3_0
+#if defined(_HIF_PCIE) || defined(_HIF_AXI)
+
+/*******************************************************************************
+*                         C O M P I L E R   F L A G S
+********************************************************************************
+*/
+
+/*******************************************************************************
+*                    E X T E R N A L   R E F E R E N C E S
+********************************************************************************
+*/
+
+#include "precomp.h"
+#include "soc3_0.h"
+#include "coda/soc3_0/wf_hif_dmashdl_top.h"
+#include "hal_dmashdl_soc3_0.h"
+
+/*******************************************************************************
+*                              C O N S T A N T S
+********************************************************************************
+*/
+
+/*******************************************************************************
+*                                 M A C R O S
+********************************************************************************
+*/
+
+/*******************************************************************************
+*                   F U N C T I O N   D E C L A R A T I O N S
+********************************************************************************
+*/
+
+/*******************************************************************************
+*                              F U N C T I O N S
+********************************************************************************
+*/
+
+
+/*******************************************************************************
+*                            P U B L I C   D A T A
+********************************************************************************
+*/
+
+struct MT6885_DMASHDL_CFG rMT6885DmashdlCfg = {
+	.fgSlotArbiterEn = MT6885_DMASHDL_SLOT_ARBITER_EN,
+
+	.u2PktPleMaxPage = MT6885_DMASHDL_PKT_PLE_MAX_PAGE,
+
+	.u2PktPseMaxPage = MT6885_DMASHDL_PKT_PSE_MAX_PAGE,
+
+	.afgRefillEn = {
+		MT6885_DMASHDL_GROUP_0_REFILL_EN,
+		MT6885_DMASHDL_GROUP_1_REFILL_EN,
+		MT6885_DMASHDL_GROUP_2_REFILL_EN,
+		MT6885_DMASHDL_GROUP_3_REFILL_EN,
+		MT6885_DMASHDL_GROUP_4_REFILL_EN,
+		MT6885_DMASHDL_GROUP_5_REFILL_EN,
+		MT6885_DMASHDL_GROUP_6_REFILL_EN,
+		MT6885_DMASHDL_GROUP_7_REFILL_EN,
+		MT6885_DMASHDL_GROUP_8_REFILL_EN,
+		MT6885_DMASHDL_GROUP_9_REFILL_EN,
+		MT6885_DMASHDL_GROUP_10_REFILL_EN,
+		MT6885_DMASHDL_GROUP_11_REFILL_EN,
+		MT6885_DMASHDL_GROUP_12_REFILL_EN,
+		MT6885_DMASHDL_GROUP_13_REFILL_EN,
+		MT6885_DMASHDL_GROUP_14_REFILL_EN,
+		MT6885_DMASHDL_GROUP_15_REFILL_EN,
+	},
+
+	.au2MaxQuota = {
+		MT6885_DMASHDL_GROUP_0_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_1_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_2_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_3_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_4_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_5_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_6_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_7_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_8_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_9_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_10_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_11_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_12_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_13_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_14_MAX_QUOTA,
+		MT6885_DMASHDL_GROUP_15_MAX_QUOTA,
+	},
+
+	.au2MinQuota = {
+		MT6885_DMASHDL_GROUP_0_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_1_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_2_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_3_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_4_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_5_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_6_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_7_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_8_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_9_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_10_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_11_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_12_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_13_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_14_MIN_QUOTA,
+		MT6885_DMASHDL_GROUP_15_MIN_QUOTA,
+	},
+
+	.aucQueue2Group = {
+		MT6885_DMASHDL_QUEUE_0_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_1_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_2_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_3_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_4_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_5_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_6_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_7_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_8_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_9_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_10_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_11_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_12_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_13_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_14_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_15_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_16_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_17_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_18_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_19_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_20_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_21_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_22_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_23_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_24_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_25_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_26_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_27_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_28_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_29_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_30_TO_GROUP,
+		MT6885_DMASHDL_QUEUE_31_TO_GROUP,
+	},
+
+	.aucPriority2Group = {
+		MT6885_DMASHDL_PRIORITY0_GROUP,
+		MT6885_DMASHDL_PRIORITY1_GROUP,
+		MT6885_DMASHDL_PRIORITY2_GROUP,
+		MT6885_DMASHDL_PRIORITY3_GROUP,
+		MT6885_DMASHDL_PRIORITY4_GROUP,
+		MT6885_DMASHDL_PRIORITY5_GROUP,
+		MT6885_DMASHDL_PRIORITY6_GROUP,
+		MT6885_DMASHDL_PRIORITY7_GROUP,
+		MT6885_DMASHDL_PRIORITY8_GROUP,
+		MT6885_DMASHDL_PRIORITY9_GROUP,
+		MT6885_DMASHDL_PRIORITY10_GROUP,
+		MT6885_DMASHDL_PRIORITY11_GROUP,
+		MT6885_DMASHDL_PRIORITY12_GROUP,
+		MT6885_DMASHDL_PRIORITY13_GROUP,
+		MT6885_DMASHDL_PRIORITY14_GROUP,
+		MT6885_DMASHDL_PRIORITY15_GROUP,
+	},
+};
+
+void mt6885HalDmashdlSetPlePktMaxPage(struct ADAPTER *prAdapter,
+				      uint16_t u2MaxPage)
+{
+	uint32_t u4Val;
+
+	HAL_MCR_RD(prAdapter, WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_ADDR, &u4Val);
+
+	u4Val &= ~WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_PLE_PACKET_MAX_SIZE_MASK;
+	u4Val |= (u2MaxPage <<
+		  WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_PLE_PACKET_MAX_SIZE_SHFT) &
+		 WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_PLE_PACKET_MAX_SIZE_MASK;
+
+	HAL_MCR_WR(prAdapter, WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_ADDR, u4Val);
+}
+
+void mt6885HalDmashdlSetPsePktMaxPage(struct ADAPTER *prAdapter,
+				      uint16_t u2MaxPage)
+{
+	uint32_t u4Val;
+
+	HAL_MCR_RD(prAdapter, WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_ADDR, &u4Val);
+
+	u4Val &= ~WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_PSE_PACKET_MAX_SIZE_MASK;
+	u4Val |= (u2MaxPage <<
+		  WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_PSE_PACKET_MAX_SIZE_SHFT) &
+		 WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_PSE_PACKET_MAX_SIZE_MASK;
+
+	HAL_MCR_WR(prAdapter, WF_HIF_DMASHDL_TOP_PACKET_MAX_SIZE_ADDR, u4Val);
+}
+
+void mt6885HalDmashdlSetRefill(struct ADAPTER *prAdapter, uint8_t ucGroup,
+			       u_int8_t fgEnable)
+{
+	uint32_t u4Val, u4Mask;
+
+	if (ucGroup >= ENUM_MT6885_DMASHDL_GROUP_NUM)
+		ASSERT(0);
+
+	u4Mask = WF_HIF_DMASHDL_TOP_REFILL_CONTROL_GROUP0_REFILL_DISABLE_MASK
+		<< ucGroup;
+
+	HAL_MCR_RD(prAdapter, WF_HIF_DMASHDL_TOP_REFILL_CONTROL_ADDR, &u4Val);
+
+	if (fgEnable)
+		u4Val &= ~u4Mask;
+	else
+		u4Val |= u4Mask;
+
+	HAL_MCR_WR(prAdapter, WF_HIF_DMASHDL_TOP_REFILL_CONTROL_ADDR, u4Val);
+}
+
+void mt6885HalDmashdlSetMaxQuota(struct ADAPTER *prAdapter, uint8_t ucGroup,
+				 uint16_t u2MaxQuota)
+{
+	uint32_t u4Addr, u4Val;
+
+	if (ucGroup >= ENUM_MT6885_DMASHDL_GROUP_NUM)
+		ASSERT(0);
+
+	u4Addr = WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_ADDR + (ucGroup << 2);
+
+	HAL_MCR_RD(prAdapter, u4Addr, &u4Val);
+
+	u4Val &= ~WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_GROUP0_MAX_QUOTA_MASK;
+	u4Val |= (u2MaxQuota <<
+		  WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_GROUP0_MAX_QUOTA_SHFT) &
+		 WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_GROUP0_MAX_QUOTA_MASK;
+
+	HAL_MCR_WR(prAdapter, u4Addr, u4Val);
+}
+
+void mt6885HalDmashdlSetMinQuota(struct ADAPTER *prAdapter, uint8_t ucGroup,
+				 uint16_t u2MinQuota)
+{
+	uint32_t u4Addr, u4Val;
+
+	if (ucGroup >= ENUM_MT6885_DMASHDL_GROUP_NUM)
+		ASSERT(0);
+
+	u4Addr = WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_ADDR + (ucGroup << 2);
+
+	HAL_MCR_RD(prAdapter, u4Addr, &u4Val);
+
+	u4Val &= ~WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_GROUP0_MIN_QUOTA_MASK;
+	u4Val |= (u2MinQuota <<
+		  WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_GROUP0_MIN_QUOTA_SHFT) &
+		 WF_HIF_DMASHDL_TOP_GROUP0_CONTROL_GROUP0_MIN_QUOTA_MASK;
+
+	HAL_MCR_WR(prAdapter, u4Addr, u4Val);
+}
+
+void mt6885HalDmashdlSetQueueMapping(struct ADAPTER *prAdapter, uint8_t ucQueue,
+				     uint8_t ucGroup)
+{
+	uint32_t u4Addr, u4Val, u4Mask, u4Shft;
+
+	if (ucQueue >= 32)
+		ASSERT(0);
+
+	if (ucGroup >= ENUM_MT6885_DMASHDL_GROUP_NUM)
+		ASSERT(0);
+
+	u4Addr = WF_HIF_DMASHDL_TOP_QUEUE_MAPPING0_ADDR +
+		 ((ucQueue >> 3) << 2);
+	u4Mask = WF_HIF_DMASHDL_TOP_QUEUE_MAPPING0_QUEUE0_MAPPING_MASK <<
+		 ((ucQueue % 8) << 2);
+	u4Shft = (ucQueue % 8) << 2;
+
+	HAL_MCR_RD(prAdapter, u4Addr, &u4Val);
+
+	u4Val &= ~u4Mask;
+	u4Val |= (ucGroup << u4Shft) & u4Mask;
+
+	HAL_MCR_WR(prAdapter, u4Addr, u4Val);
+}
+
+void mt6885HalDmashdlSetSlotArbiter(struct ADAPTER *prAdapter,
+				    u_int8_t fgEnable)
+{
+	uint32_t u4Val;
+
+	HAL_MCR_RD(prAdapter, WF_HIF_DMASHDL_TOP_PAGE_SETTING_ADDR, &u4Val);
+
+	if (fgEnable)
+		u4Val |=
+		 WF_HIF_DMASHDL_TOP_PAGE_SETTING_GROUP_SEQUENCE_ORDER_TYPE_MASK;
+	else
+		u4Val &=
+		~WF_HIF_DMASHDL_TOP_PAGE_SETTING_GROUP_SEQUENCE_ORDER_TYPE_MASK;
+
+	HAL_MCR_WR(prAdapter, WF_HIF_DMASHDL_TOP_PAGE_SETTING_ADDR, u4Val);
+}
+
+void mt6885HalDmashdlSetUserDefinedPriority(struct ADAPTER *prAdapter,
+					    uint8_t ucPriority, uint8_t ucGroup)
+{
+	uint32_t u4Addr, u4Val, u4Mask, u4Shft;
+
+	ASSERT(ucPriority < 16);
+	ASSERT(ucGroup < ENUM_MT6885_DMASHDL_GROUP_NUM);
+
+	u4Addr = WF_HIF_DMASHDL_TOP_HIF_SCHEDULER_SETTING0_ADDR +
+		((ucPriority >> 3) << 2);
+	u4Mask = WF_HIF_DMASHDL_TOP_HIF_SCHEDULER_SETTING0_PRIORITY0_GROUP_MASK
+		 << ((ucPriority % 8) << 2);
+	u4Shft = (ucPriority % 8) << 2;
+
+	HAL_MCR_RD(prAdapter, u4Addr, &u4Val);
+
+	u4Val &= ~u4Mask;
+	u4Val |= (ucGroup << u4Shft) & u4Mask;
+
+	HAL_MCR_WR(prAdapter, u4Addr, u4Val);
+}
+
+void mt6885DmashdlInit(struct ADAPTER *prAdapter)
+{
+	uint32_t idx;
+
+	mt6885HalDmashdlSetPlePktMaxPage(prAdapter,
+					 rMT6885DmashdlCfg.u2PktPleMaxPage);
+
+	for (idx = 0; idx < ENUM_MT6885_DMASHDL_GROUP_NUM; idx++) {
+		mt6885HalDmashdlSetRefill(prAdapter, idx,
+					  rMT6885DmashdlCfg.afgRefillEn[idx]);
+
+		mt6885HalDmashdlSetMaxQuota(prAdapter, idx,
+					    rMT6885DmashdlCfg.au2MaxQuota[idx]);
+
+		mt6885HalDmashdlSetMinQuota(prAdapter, idx,
+					    rMT6885DmashdlCfg.au2MinQuota[idx]);
+	}
+
+	for (idx = 0; idx < 32; idx++)
+		mt6885HalDmashdlSetQueueMapping(prAdapter, idx,
+					 rMT6885DmashdlCfg.aucQueue2Group[idx]);
+
+	for (idx = 0; idx < 16; idx++)
+		mt6885HalDmashdlSetUserDefinedPriority(prAdapter, idx,
+				      rMT6885DmashdlCfg.aucPriority2Group[idx]);
+
+	mt6885HalDmashdlSetSlotArbiter(prAdapter,
+				       rMT6885DmashdlCfg.fgSlotArbiterEn);
+}
+
+#endif /* defined(_HIF_PCIE) || defined(_HIF_AXI) */
+#endif /* SOC3_0*/
