2f9fe8b0d630d8037070ac50e0536652f00f9efc
johnny-cc.chen <johnny-cc.chen@mediatek.com>
2024-01-12 22:18:33 +0800

    [ALPS07901021][[[Gen4m] Fix rx stat print fail]]
    [Description]
    As title
    
    MTK-Commit-Id: f8218b6062fb0744e04d0bd4d6f0d5fd3190e2c1
    
    Signed-off-by: johnny-cc.chen <johnny-cc.chen@mediatek.com>
    Feature: testmode
    CR-Id: ALPS07901021
    Change-Id: Ibc341cb983f085521a39b0d398b8dd7ec431299e
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7453075
    Reviewed-by: ian-cc.chuang <ian-cc.chuang@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: austin.chou <austin.chou@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>

--- os/linux/gl_wext_priv.c
+++ os/linux/gl_wext_priv.c
@@ -19032,7 +19032,7 @@ int8_t *RxStatPerBand[] = {
 #if (CFG_SUPPORT_CONNAC3X == 1) /* band info v1*/
 	"aci_hit_low	: 0x%08x\n",
 	"aci_hit_high	: 0x%08x\n",
-	"phy_pd_alr		: 0x%08x\n"	/* band info v2*/
+	"phy_pd_alr		: 0x%08x\n",	/* band info v2*/
 	"mac_u2m_mpdu_cnt	: 0x%08x\n"	/* band info v2*/
 #endif
 };
