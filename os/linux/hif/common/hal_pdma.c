e2ac9ef08b54c7e152877bf7934dd6b6f97dddd6
Holiday Hao <holiday.hao@mediatek.com>
2024-01-12 22:09:32 +0800

    [ALPS07674851] wlan: hif: fix print log cause fw dl fail
    move print log before check event ring to avoid
    print log time too long cause fw dl fail
    
    MTK-Commit-Id: 898f3a54a76135a93375e0e88ab58fb56f8aa2af
    
    CR-Id: ALPS07674851
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    Change-Id: I05d874608c1c67efc2bfd2aa404356d64a037588
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6808163
    Test: srv_pf_nep_sanity
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: soul.huang <soul.huang@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Test: srv_mspautosanity
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- os/linux/hif/common/hal_pdma.c
+++ os/linux/hif/common/hal_pdma.c
@@ -263,6 +263,10 @@ uint32_t halRxWaitResponse(struct ADAPTER *prAdapter, uint8_t ucPortIdx,
 			return WLAN_STATUS_FAILURE;
 		}
 
+		if (!prGlueInfo->u4ReadyFlag)
+			halCheckRxPollingMode(prAdapter, u4Time,
+					      u4TimeoutValue, u4Cnt);
+
 		fgStatus = kalDevPortRead(
 			prGlueInfo, ucNewPort, u4PktLen,
 			pucRspBuffer, HIF_RX_COALESCING_BUFFER_SIZE);
@@ -272,9 +276,6 @@ uint32_t halRxWaitResponse(struct ADAPTER *prAdapter, uint8_t ucPortIdx,
 			break;
 		}
 
-		if (!prGlueInfo->u4ReadyFlag)
-			halCheckRxPollingMode(prAdapter, u4Time,
-					      u4TimeoutValue, u4Cnt);
 		if (halIsTimeout(u4Time, u4TimeoutValue)) {
 #if IS_ENABLED(CFG_SUPPORT_CONNAC1X)
 			uint32_t u4Value = 0;
