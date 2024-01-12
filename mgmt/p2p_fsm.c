ac280f6198f08374f0e5c03eda2aa39a8574ff28
CS Kuo <cs.kuo@mediatek.com>
2024-01-12 21:17:42 +0800

    [ALPS05012607] p2p: add beacon timeout handler
    When beacon timeout event were reported, we should check
    if p2p Tx/Rx data are still on-going to prevent triggering
    unnecessary disconnection.
    
    MTK-Commit-Id: 89db56bba2d85758879f1b95bcf2ce046fe79d3f
    
    Change-Id: Id7df6484de07b60c2c7728e72e0a8bf7c3624420
    CR-Id: ALPS05012607
    Feature: Wi-Fi Direct
    Signed-off-by: CS Kuo <cs.kuo@mediatek.com>

--- mgmt/p2p_fsm.c
+++ mgmt/p2p_fsm.c
@@ -348,10 +348,13 @@ void p2pFsmRunEventWfdSettingUpdate(IN struct ADAPTER *prAdapter,
 					&(prP2pRoleFsmInfo
 					->rP2pRoleFsmGetStatisticsTimer),
 					(3 * P2P_ROLE_GET_STATISTICS_TIME));
-			else
+			else {
 				cnmTimerStopTimer(prAdapter,
 					&prP2pRoleFsmInfo
 					->rP2pRoleFsmGetStatisticsTimer);
+				/* Reset linkscore */
+				prWfdCfgSettings->u4LinkScore = 0;
+			}
 		}
 #endif
 
