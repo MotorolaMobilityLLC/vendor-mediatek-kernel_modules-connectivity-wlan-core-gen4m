c43aec0d6f61b4f640d2c61a3841a52e71f1f34b
James-cy Chen <james-cy.chen@mediatek.com>
2024-01-12 22:29:37 +0800

    [ALPS08541347] wlan: enable L0 reset if L0.5 happens 3 times in 30s
    Enable L0 reset if L0.5 reset happens 3 times during 30s.
    
    MTK-Commit-Id: f64882d95441b9908d1fb44a3c79318ff4fc6527
    
    CR-Id: ALPS08541347
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: James-cy Chen <james-cy.chen@mediatek.com>
    Change-Id: I80022b85ecd082127979ab67d4946863036937bb
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8257068
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_mspautosanity
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: michael.chung <michael.chung@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- os/linux/gl_rst.c
+++ os/linux/gl_rst.c
@@ -1936,7 +1936,6 @@ void glReset_timeinit(struct timespec64 *rNowTs, struct timespec64 *rLastTs)
 bool IsOverRstTimeThreshold(
 	struct timespec64 *rNowTs, struct timespec64 *rLastTs)
 {
-#if (CFG_SUPPORT_CONNINFRA == 1)
 	struct timespec64 rTimeout, rTime = {0};
 	bool fgIsTimeout = FALSE;
 
@@ -1964,9 +1963,6 @@ bool IsOverRstTimeThreshold(
 			KAL_GET_TIME_OF_USEC_OR_NSEC(rTime));
 	}
 	return fgIsTimeout;
-#else
-	return TRUE;
-#endif
 }
 
 void glResetWholeChipResetTrigger(char *pcReason)
