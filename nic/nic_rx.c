c935f8ff2e74fc32512861c8aa5f08942a58ed9c
Wyatt Sun <wyatt.sun@mediatek.com>
2024-01-12 22:06:07 +0800

    [ALPS07535013] Allocate RFB from heap if no pre-allocated buffer available
    RX worker get all SW RFB to receive data, in the meanwhile,
    MLO handles the RX frames need to duplicate the data to multiple
    handlers.
    Returning NULL in that case will make MLO hard to proceed its state
    transition with error handling.
    The buffer allocated from heap shall be checked and return to heap.
    
    MTK-Commit-Id: bd268ad04fbb8a68db0af06166d08378bd595521
    
    CR-Id: ALPS07535013
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I7c02e1bb8210e594c60faff2f4cf37fb85a7b17b
    Signed-off-by: Wyatt Sun <wyatt.sun@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6423018
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Test: srv_pf_nep_sanity
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>

--- nic/nic_rx.c
+++ nic/nic_rx.c
@@ -2639,11 +2639,26 @@ struct SW_RFB *nicRxAcquireRFB(struct ADAPTER *prAdapter, uint16_t num)
 	if (likely(i == num))
 		return (struct SW_RFB *)QUEUE_GET_HEAD(que);
 
-	/* fail case goes here */
+	/* Fail to get RFB from rFreeSwRfbList */
 	KAL_ACQUIRE_SPIN_LOCK(prAdapter, SPIN_LOCK_RX_FREE_QUE);
 	QUEUE_CONCATENATE_QUEUES(&ctrl->rFreeSwRfbList, que);
 	KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_RX_FREE_QUE);
-	return NULL;
+
+	/* Fallback, allocate from spared */
+	QUEUE_INITIALIZE(que);
+	for (i = 0; i < num; i++) {
+		rfb = kalMemAlloc(sizeof(struct SW_RFB), VIR_MEM_TYPE);
+		if (!rfb) {
+			DBGLOG_LIMITED(RX, WARN,
+				"No RFB from spared caller=%pS\n", KAL_TRACE);
+			break;
+		}
+		nicRxSetupRFB(prAdapter, rfb);
+		QUEUE_INSERT_TAIL(que, &rfb->rQueEntry);
+	}
+
+	/* Assume heap is always sufficient, skip check allocated num */
+	return (struct SW_RFB *)QUEUE_GET_HEAD(que);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -2704,6 +2719,13 @@ uint32_t nicRxCopyRFB(struct ADAPTER *prAdapter,
 	return WLAN_STATUS_SUCCESS;
 }
 
+u_int8_t isRfbFromSpared(struct RX_CTRL *prRxCtrl, struct SW_RFB *prSwRfb)
+{
+	return (uint8_t *)prSwRfb < prRxCtrl->pucRxCached ||
+		(uint8_t *)prSwRfb >
+			prRxCtrl->pucRxCached + prRxCtrl->u4RxCachedSize;
+}
+
 /*----------------------------------------------------------------------------*/
 /*!
  * @brief This routine is called to put a RFB back onto the "RFB with Buffer"
@@ -2735,6 +2757,13 @@ void nicRxReturnRFB(struct ADAPTER *prAdapter,
 
 	ASSERT(prQueEntry);
 
+	if (isRfbFromSpared(prRxCtrl, prSwRfb)) {
+		if (prSwRfb->pvPacket)
+			kalPacketFree(prGlueInfo, prSwRfb->pvPacket);
+		kalMemFree(prSwRfb, VIR_MEM_TYPE, sizeof(struct SW_RFB));
+		goto done;
+	}
+
 	/* The processing on this RFB is done,
 	 * so put it back on the tail of our list
 	 */
@@ -2754,6 +2783,7 @@ void nicRxReturnRFB(struct ADAPTER *prAdapter,
 	}
 	KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_RX_FREE_QUE);
 
+done:
 	/* Trigger Rx if there are free SwRfb */
 	if (halIsPendingRx(prAdapter)
 	    && (prRxCtrl->rFreeSwRfbList.u4NumElem > 0))
