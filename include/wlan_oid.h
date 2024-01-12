21dcd569845adbaa37e23893e3c47947e30428c0
Wyatt Sun <wyatt.sun@mediatek.com>
2024-01-12 22:30:13 +0800

    [ALPS08573280] Fix flexible array member access in set network OID
    1. Change address field in structure from [1] to []
    2. Remove flexiable array member in struct PARAM_NETWORK_ADDRESS_LIST,
       replaced by stide by sizeof and casting to
       (struct PARAM_NETWORK_ADDRESS *).
    3. Fix buffer size calculation
    
    MTK-Commit-Id: 63c64b1c831a66beb757ae0358d2d4d4bcf2e6b2
    
    CR-Id: ALPS08573280
    Feature: [Module]Wi-Fi Driver
    Change-Id: Idd0f66d99aa4704db947b0fabf62c55a05c45beb
    Signed-off-by: Wyatt Sun <wyatt.sun@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8288818
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_mspautosanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: deren.wu <deren.wu@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>

--- include/wlan_oid.h
+++ include/wlan_oid.h
@@ -2622,21 +2622,25 @@ struct PARAM_NETWORK_ADDRESS {
 	uint16_t u2AddressType;	/* type of this address
 				 * (PARAM_PROTOCOL_ID_XXX above)
 				 */
-	uint8_t aucAddress[1];	/* actually AddressLength bytes long */
+	uint8_t aucAddress[];	/* actually AddressLength bytes long */
 };
 
 /* The following is used with OID_GEN_NETWORK_LAYER_ADDRESSES to set network
  * layer addresses on an interface
  */
-
 struct PARAM_NETWORK_ADDRESS_LIST {
 	uint8_t ucBssIdx;
-	uint32_t u4AddressCount;/* number of addresses following */
 	uint16_t u2AddressType;	/* type of this address
 				 * (NDIS_PROTOCOL_ID_XXX above)
 				 */
-	struct PARAM_NETWORK_ADDRESS
-		arAddress[1];	/* actually AddressCount elements long */
+	uint32_t u4AddressCount;/* number of addresses following */
+
+	/**
+	 * Followed by one or more flexible size struct PARAM_NETWORK_ADDRESS,
+	 * the original structure struct PARAM_NETWORK_ADDRESS aucAddress[];
+	 * was removed since declaring a flexible array member here will cause
+	 * arrays of objects containing zero-size array.
+	 */
 };
 
 #if CFG_SLT_SUPPORT
