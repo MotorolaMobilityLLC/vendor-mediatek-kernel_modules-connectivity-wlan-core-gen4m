a001da425ee8d53c912d7ccff967579c3feff6da
mtk09010 <johnny.chiang@mediatek.com>
2024-01-12 22:29:04 +0800

    [ALPS07918466] sap: cross band csa
    cross band csa
    
    MTK-Commit-Id: 9c04b972cdee819e77330b53046743af746b2b74
    
    CR-Id: ALPS07918466
    Signed-off-by: mtk09010 <johnny.chiang@mediatek.com>
    Feature: Wi-Fi Driver
    Change-Id: Ie977f6d79430e688f9e5a2756004c16b0cc9a598
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7700242
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    (cherry picked from commit 63bd701787206e3b1a8a6b3d93a76474e291da6b)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8216880
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: brian-kl.lin <brian-kl.lin@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>

--- include/config.h
+++ include/config.h
@@ -981,10 +981,6 @@
 #define CFG_P2P_DEFAULT_CLIENT_COUNT 0
 #endif
 
-#ifndef CFG_P2P_FORCE_ROC_CSA
-#define CFG_P2P_FORCE_ROC_CSA 1
-#endif
-
 /*------------------------------------------------------------------------------
  * Flags for GTK rekey offload
  *------------------------------------------------------------------------------
