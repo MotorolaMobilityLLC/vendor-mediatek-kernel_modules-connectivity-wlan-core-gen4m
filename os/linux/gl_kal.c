2c7a48491d3d58b3e87f2624f2fa51b3f9ce3e88
Ben Lai <ben.lai@mediatek.com>
2024-01-12 21:52:33 +0800

    [ALPS06646009] init: wifi on integration with conn v3 driver
    wifi on integration with conn v3 driver.
    
    MTK-Commit-Id: 413cb2454d13fd082118b8840c9a017fc5f87a1b
    
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS06646009
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I6043c48ca26823df0b0b8ad84c89ea80a244edd1
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/5695210
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -9459,6 +9459,11 @@ int32_t __weak kalCheckVcoreBoost(IN struct ADAPTER *prAdapter,
 	return FALSE;
 }
 
+int32_t __weak kalPlatOpsInit(void)
+{
+	return 0;
+}
+
 /* mimic store_rps_map as net-sysfs.c does */
 int wlan_set_rps_map(struct netdev_rx_queue *queue, unsigned long rps_value)
 {
