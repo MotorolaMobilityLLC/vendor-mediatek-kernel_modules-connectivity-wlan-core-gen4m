8fcc2534012bba5fd713b8486e429b043e8c2669
Jo-Ping.Li <Jo-Ping.li@mediatek.com>
2024-01-12 22:22:14 +0800

    [ALPS08192433] driver received log when resume
    1. driver received log when resume
    2. enable kernel warning
    
    MTK-Commit-Id: fe7f435d34e550ed069260b31a597cf52450d50f
    
    CR-Id: ALPS08192433
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: "Jo-Ping.Li" <Jo-Ping.li@mediatek.com>
    Change-Id: I62df912666d8187e9fd4d682983abd773590934c
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7703766
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    (cherry-pick from 86329db4b7f0eab1fa83e67cc83be0f67452e550)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7722510
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Reviewed-by: jo-ping.li <jo-ping.li@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Tested-by: srv_ssm2_admin <srv_ssm2_admin@mediatek.com>
    Test: srv_mspautosanity
    Tested-by: jo-ping.li <jo-ping.li@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- os/linux/gl_cfg80211.c
+++ os/linux/gl_cfg80211.c
@@ -5716,6 +5716,7 @@ int mtk_cfg80211_resume(struct wiphy *wiphy)
 		DBGLOG(REQ, WARN, "ScanResultLog error:%x\n",
 		       rStatus);
 	halSetSuspendFlagToFw(prGlueInfo->prAdapter, FALSE);
+	fw_log_handler();
 end:
 	kalHaltUnlock();
 
