0623e49e324ae22998ab637f019e78eba5fe5bb3
Ben Lai <ben.lai@mediatek.com>
2024-01-12 21:30:13 +0800

    [ALPS05669036] wlan: Check sub queue stopped by queue index
    Check sub queue stopped by queue index instead of skb.
    
    MTK-Commit-Id: 2d4bb1404c24a9197a79c0fa7c3d7f6f8397621e
    
    Change-Id: Ibc2577daa7e32868e6bd09dd117e4be525a06df4
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS05669036
    Feature: [Module]Wi-Fi Driver

--- os/linux/gl_init.c
+++ os/linux/gl_init.c
@@ -1046,34 +1046,34 @@ u16 wlanSelectQueue(struct net_device *dev,
 		    struct sk_buff *skb,
 		    struct net_device *sb_dev)
 {
-	return mtk_wlan_ndev_select_queue(skb);
+	return mtk_wlan_ndev_select_queue(dev, skb);
 }
 #elif KERNEL_VERSION(4, 19, 0) <= LINUX_VERSION_CODE
 u16 wlanSelectQueue(struct net_device *dev,
 		    struct sk_buff *skb,
 		    struct net_device *sb_dev, select_queue_fallback_t fallback)
 {
-	return mtk_wlan_ndev_select_queue(skb);
+	return mtk_wlan_ndev_select_queue(dev, skb);
 }
 #elif KERNEL_VERSION(3, 14, 0) <= LINUX_VERSION_CODE
 u16 wlanSelectQueue(struct net_device *dev,
 		    struct sk_buff *skb,
 		    void *accel_priv, select_queue_fallback_t fallback)
 {
-	return mtk_wlan_ndev_select_queue(skb);
+	return mtk_wlan_ndev_select_queue(dev, skb);
 }
 #elif KERNEL_VERSION(3, 13, 0) <= LINUX_VERSION_CODE
 u16 wlanSelectQueue(struct net_device *dev,
 		    struct sk_buff *skb,
 		    void *accel_priv)
 {
-	return mtk_wlan_ndev_select_queue(skb);
+	return mtk_wlan_ndev_select_queue(dev, skb);
 }
 #else
 u16 wlanSelectQueue(struct net_device *dev,
 		    struct sk_buff *skb)
 {
-	return mtk_wlan_ndev_select_queue(skb);
+	return mtk_wlan_ndev_select_queue(dev, skb);
 }
 #endif
 
