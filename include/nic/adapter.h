7aecf40f0bf29e92b007064b65c0cb4f72f3acce
Kok Suan Lim <ks.lim@mediatek.com>
2024-01-12 22:12:02 +0800

    [ALPS07531974] nic: support MLO PS
    support MLO PS
    
    MTK-Commit-Id: 6b96bfe1c047a44b3c486e92ca9532fd96e8e487
    
    Signed-off-by: Kok Suan Lim <ks.lim@mediatek.com>
    CR-Id: ALPS07531974
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I5ffe83f79da572cb11b426ead099930444cc8bce
    (cherry picked from commit c61066be6ad46dfc0c95813a76ebb69706500550)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6592542
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>

--- include/nic/adapter.h
+++ include/nic/adapter.h
@@ -2236,6 +2236,7 @@ struct ADAPTER {
 	struct MLD_BSS_INFO aprMldBssInfo[MAX_BSSID_NUM];
 	struct MLD_STA_RECORD aprMldStarec[CFG_STA_REC_NUM];
 	uint8_t ucBssAbsentBitmap;
+	uint32_t u4StaInPSBitmap;
 #endif
 	uint8_t ucCnmTokenID;
 #if (CFG_SUPPORT_AVOID_DESENSE == 1)
