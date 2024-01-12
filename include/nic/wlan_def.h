1126b8cddf8627fffdfca5b6666e4310e297a217
ch-howard.chang <ch-howard.chang@mediatek.com>
2024-01-12 22:19:03 +0800

    [WCNCR00303310] Fix coverity: Constant expression result
    For right shift 8 bits in WLAN_SET_FIELD_BE16, uint8_t will
    be constant vlaue 0, so cast the value into uint_16 first.
    
    The same concept for other cases.
    
    MTK-Commit-Id: 452dae845df40c5c4348ed1c7dbf87bea305730a
    
    Signed-off-by: ch-howard.chang <ch-howard.chang@mediatek.com>
    CR-Id: WCNCR00303310
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I230ce72d2383c10c25dfe519bbe67fa04d78be9c
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7490048
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: deren.wu <deren.wu@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- include/nic/wlan_def.h
+++ include/nic/wlan_def.h
@@ -1491,62 +1491,69 @@ struct P2P_DEVICE_DESC {
 #define WLAN_SET_FIELD_16(_memAddr_p, _value) \
 	{ \
 		uint8_t *__cp = (uint8_t *)(_memAddr_p); \
-		__cp[0] = (uint8_t)(_value); \
-		__cp[1] = (uint8_t)((_value) >> 8); \
+		uint16_t __value = (uint16_t) _value; \
+		__cp[0] = (uint8_t)(__value); \
+		__cp[1] = (uint8_t)((__value) >> 8); \
 	}
 
 #define WLAN_SET_FIELD_64(_memAddr_p, _value) \
 	{ \
 		uint8_t *__cp = (uint8_t *)(_memAddr_p); \
-		__cp[0] = (uint8_t)((_value) & 0xff); \
-		__cp[1] = (uint8_t)((_value) >> 8); \
-		__cp[2] = (uint8_t)((_value) >> 16); \
-		__cp[3] = (uint8_t)((_value) >> 24); \
-		__cp[4] = (uint8_t)((_value) >> 32); \
-		__cp[5] = (uint8_t)((_value) >> 40); \
-		__cp[6] = (uint8_t)((_value) >> 48); \
-		__cp[7] = (uint8_t)((_value) >> 56); \
+		uint64_t __value = (uint64_t) _value; \
+		__cp[0] = (uint8_t)((__value) & 0xff); \
+		__cp[1] = (uint8_t)((__value) >> 8); \
+		__cp[2] = (uint8_t)((__value) >> 16); \
+		__cp[3] = (uint8_t)((__value) >> 24); \
+		__cp[4] = (uint8_t)((__value) >> 32); \
+		__cp[5] = (uint8_t)((__value) >> 40); \
+		__cp[6] = (uint8_t)((__value) >> 48); \
+		__cp[7] = (uint8_t)((__value) >> 56); \
 	}
 
 #define WLAN_SET_FIELD_BE16(_memAddr_p, _value) \
 	{ \
 		uint8_t *__cp = (uint8_t *)(_memAddr_p); \
-		__cp[0] = (uint8_t)((_value) >> 8); \
-		__cp[1] = (uint8_t)(_value); \
+		uint16_t __value = (uint16_t) _value; \
+		__cp[0] = (uint8_t)((__value) >> 8); \
+		__cp[1] = (uint8_t)(__value); \
 	}
 
 #define WLAN_SET_FIELD_24(_memAddr_p, _value) \
 	{ \
 		uint8_t *__cp = (uint8_t *)(_memAddr_p); \
-		__cp[0] = (uint8_t)(_value); \
-		__cp[1] = (uint8_t)((_value) >> 8); \
-		__cp[2] = (uint8_t)((_value) >> 16); \
+		uint32_t __value = (uint32_t) _value; \
+		__cp[0] = (uint8_t)(__value); \
+		__cp[1] = (uint8_t)((__value) >> 8); \
+		__cp[2] = (uint8_t)((__value) >> 16); \
 	}
 
 #define WLAN_SET_FIELD_32(_memAddr_p, _value) \
 	{ \
 		uint8_t *__cp = (uint8_t *)(_memAddr_p); \
-		__cp[0] = (uint8_t)(_value); \
-		__cp[1] = (uint8_t)((_value) >> 8); \
-		__cp[2] = (uint8_t)((_value) >> 16); \
-		__cp[3] = (uint8_t)((_value) >> 24); \
+		uint32_t __value = (uint32_t) _value; \
+		__cp[0] = (uint8_t)(__value); \
+		__cp[1] = (uint8_t)((__value) >> 8); \
+		__cp[2] = (uint8_t)((__value) >> 16); \
+		__cp[3] = (uint8_t)((__value) >> 24); \
 	}
 
 #define WLAN_SET_FIELD_BE24(_memAddr_p, _value) \
 	{ \
 		uint8_t *__cp = (uint8_t *)(_memAddr_p); \
-		__cp[0] = (uint8_t)((_value) >> 16); \
-		__cp[1] = (uint8_t)((_value) >> 8); \
-		__cp[2] = (uint8_t)(_value); \
+		uint32_t __value = (uint32_t) _value; \
+		__cp[0] = (uint8_t)((__value) >> 16); \
+		__cp[1] = (uint8_t)((__value) >> 8); \
+		__cp[2] = (uint8_t)(__value); \
 	}
 
 #define WLAN_SET_FIELD_BE32(_memAddr_p, _value) \
 	{ \
 		uint8_t *__cp = (uint8_t *)(_memAddr_p); \
-		__cp[0] = (uint8_t)((_value) >> 24); \
-		__cp[1] = (uint8_t)((_value) >> 16); \
-		__cp[2] = (uint8_t)((_value) >> 8); \
-		__cp[3] = (uint8_t)(_value); \
+		uint32_t __value = (uint32_t) _value; \
+		__cp[0] = (uint8_t)((__value) >> 24); \
+		__cp[1] = (uint8_t)((__value) >> 16); \
+		__cp[2] = (uint8_t)((__value) >> 8); \
+		__cp[3] = (uint8_t)(__value); \
 	}
 
 /*******************************************************************************
