a3a66666b92837ee0ab59cf931c878fd2c5b4ad2
Rachel Tseng <Rachel.Tseng@mediatek.com>
2024-01-12 22:24:14 +0800

    [ALPS06720434] rsn: limit pmf usage for Cert. PMF
    Cert. PMF checks first connection with only ELEM_WPA_CAP_MFPC,
    while Cert. WPA3 5.2.1 checks ELEM_WPA_CAP_MFPC and MFPR. If
    MFP setting is optional, only Cert. PMF should disable MFPR.
    
    MTK-Commit-Id: 15657450dd5598a08ce2b3e9ee4f766c7439dbd9
    
    Change-Id: I9d5bfaa341dde8270435c2d070d0a449681e378f
    CR-Id: ALPS06720434
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7929417
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: han.hu <han.hu@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Test: srv_mspautosanity

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -7250,30 +7250,14 @@ u_int8_t kalIsAPmode(struct GLUE_INFO *prGlueInfo)
 uint32_t kalGetMfpSetting(struct GLUE_INFO *prGlueInfo,
 	uint8_t ucBssIndex)
 {
-	uint32_t u4RsnMfp = RSN_AUTH_MFP_DISABLED;
 	struct GL_WPA_INFO *prWpaInfo;
+
+	ASSERT(prGlueInfo);
 
 	prWpaInfo = aisGetWpaInfo(prGlueInfo->prAdapter,
 		ucBssIndex);
 
-	ASSERT(prGlueInfo);
-
-	switch (prWpaInfo->u4Mfp) {
-	case IW_AUTH_MFP_DISABLED:
-		u4RsnMfp = RSN_AUTH_MFP_DISABLED;
-		break;
-	case IW_AUTH_MFP_OPTIONAL:
-		u4RsnMfp = RSN_AUTH_MFP_OPTIONAL;
-		break;
-	case IW_AUTH_MFP_REQUIRED:
-		u4RsnMfp = RSN_AUTH_MFP_REQUIRED;
-		break;
-	default:
-		u4RsnMfp = RSN_AUTH_MFP_DISABLED;
-		break;
-	}
-
-	return u4RsnMfp;
+	return prWpaInfo->u4Mfp;
 }
 
 /*----------------------------------------------------------------------------*/
