fe4cdb70770eb91e96802cee30814133cef4fc3a
Facer Pei <facer.pei@mediatek.com>
2024-01-12 21:16:36 +0800

    [ALPS04949849] wlan: porting kernel 4.19
    Porting kernel 4.19 for gen4m driver:
    1. timer API changed.
    2. wake lock API changed.
    
    MTK-Commit-Id: 7cc67cf136fcf5937d86e5b4a7dc990d3079d4f1
    
    Change-Id: Ic0a128b69d4c6eae1d728cd6c402bb2453ddaf15
    Signed-off-by: Facer Pei <facer.pei@mediatek.com>
    CR-Id: ALPS04949849
    Feature: [Module]Wi-Fi Driver

--- os/linux/include/gl_kal.h
+++ os/linux/include/gl_kal.h
@@ -609,42 +609,105 @@ static inline void kalCfg80211ScanDone(struct cfg80211_scan_request *request,
 #if defined(CONFIG_ANDROID) && (CFG_ENABLE_WAKE_LOCK)
 /* CONFIG_ANDROID is defined in Android kernel source */
 #if (KERNEL_VERSION(4, 9, 0) <= LINUX_VERSION_CODE)
+#if (KERNEL_VERSION(4, 19, 0) <= LINUX_VERSION_CODE)
 #define KAL_WAKE_LOCK_INIT(_prAdapter, _prWakeLock, _pcName) \
-	wakeup_source_init(_prWakeLock, _pcName)
+	_prWakeLock = wakeup_source_register(NULL, _pcName);
 
 #define KAL_WAKE_LOCK_DESTROY(_prAdapter, _prWakeLock) \
-	wakeup_source_trash(_prWakeLock)
+{ \
+	wakeup_source_unregister(_prWakeLock); \
+	_prWakeLock = NULL; \
+}
+#else
+#define KAL_WAKE_LOCK_INIT(_prAdapter, _prWakeLock, _pcName) \
+{ \
+	_prWakeLock = kalMemAlloc(sizeof(KAL_WAKE_LOCK_T), \
+		VIR_MEM_TYPE); \
+	if (!_prWakeLock) { \
+		DBGLOG(HAL, ERROR, \
+			"KAL_WAKE_LOCK_INIT init fail!\n"); \
+	} \
+	else { \
+		wakeup_source_init(_prWakeLock, _pcName); \
+	} \
+}
 
+#define KAL_WAKE_LOCK_DESTROY(_prAdapter, _prWakeLock) \
+{ \
+	if (!_prWakeLock) { \
+		wakeup_source_trash(_prWakeLock); \
+		_prWakeLock = NULL; \
+	} \
+}
+#endif
 #define KAL_WAKE_LOCK(_prAdapter, _prWakeLock) \
-	__pm_stay_awake(_prWakeLock)
+{ \
+	if (!_prWakeLock) { \
+		__pm_stay_awake(_prWakeLock); \
+	} \
+}
 
 #define KAL_WAKE_LOCK_TIMEOUT(_prAdapter, _prWakeLock, _u4Timeout) \
-	__pm_wakeup_event(_prWakeLock, JIFFIES_TO_MSEC(_u4Timeout))
+{ \
+	if (!_prWakeLock) { \
+		__pm_wakeup_event(_prWakeLock, JIFFIES_TO_MSEC(_u4Timeout)); \
+	} \
+}
 
 #define KAL_WAKE_UNLOCK(_prAdapter, _prWakeLock) \
-	__pm_relax(_prWakeLock)
+{ \
+	if (!_prWakeLock) { \
+		__pm_relax(_prWakeLock); \
+	} \
+}
 
 #define KAL_WAKE_LOCK_ACTIVE(_prAdapter, _prWakeLock) \
-	((_prWakeLock)->active)
+	((_prWakeLock) && ((_prWakeLock)->active))
 
 #else
 #define KAL_WAKE_LOCK_INIT(_prAdapter, _prWakeLock, _pcName) \
-	wake_lock_init(_prWakeLock, WAKE_LOCK_SUSPEND, _pcName)
+{ \
+	_prWakeLock = kalMemAlloc(sizeof(KAL_WAKE_LOCK_T), \
+		VIR_MEM_TYPE); \
+	if (!_prWakeLock) { \
+		DBGLOG(HAL, ERROR, \
+			"KAL_WAKE_LOCK_INIT init fail!\n"); \
+	} \
+	else { \
+		wake_lock_init(_prWakeLock, WAKE_LOCK_SUSPEND, _pcName); \
+	} \
+}
 
 #define KAL_WAKE_LOCK_DESTROY(_prAdapter, _prWakeLock) \
-	wake_lock_destroy(_prWakeLock)
+{ \
+	if (!_prWakeLock) { \
+		wake_lock_destroy(_prWakeLock); \
+	} \
+}
 
 #define KAL_WAKE_LOCK(_prAdapter, _prWakeLock) \
-	wake_lock(_prWakeLock)
+{ \
+	if (!_prWakeLock) { \
+		wake_lock(_prWakeLock); \
+	} \
+}
 
 #define KAL_WAKE_LOCK_TIMEOUT(_prAdapter, _prWakeLock, _u4Timeout) \
-	wake_lock_timeout(_prWakeLock, _u4Timeout)
+{ \
+	if (!_prWakeLock) { \
+		wake_lock_timeout(_prWakeLock, _u4Timeout); \
+	} \
+}
 
 #define KAL_WAKE_UNLOCK(_prAdapter, _prWakeLock) \
-	wake_unlock(_prWakeLock)
+{ \
+	if (!_prWakeLock) { \
+		wake_unlock(_prWakeLock); \
+	} \
+}
 
 #define KAL_WAKE_LOCK_ACTIVE(_prAdapter, _prWakeLock) \
-	wake_lock_active(_prWakeLock)
+		((_prWakeLock) && wake_lock_active(_prWakeLock))
 #endif
 
 #else
@@ -1338,7 +1401,11 @@ uint8_t kalUpdateBssTimestamp(IN struct GLUE_INFO *prGlueInfo);
 
 uint32_t kalRandomNumber(void);
 
+#if KERNEL_VERSION(4, 15, 0) <= LINUX_VERSION_CODE
+void kalTimeoutHandler(struct timer_list *timer);
+#else
 void kalTimeoutHandler(unsigned long arg);
+#endif
 
 void kalSetEvent(struct GLUE_INFO *pr);
 
