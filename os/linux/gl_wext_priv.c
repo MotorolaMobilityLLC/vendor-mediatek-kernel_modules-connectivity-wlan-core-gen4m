decc8a11d85614207c1a7f75241d298d9d436e2c
Herman Yang <herman.yang@mediatek.com>
2024-01-12 22:23:46 +0800

    [ALPS08138648] scan: fix ml probe cmd validation
    min argc is 5. extra para 6 for ssid.
    
    MTK-Commit-Id: b00fa34f9afa7495488ec1b388995179fc1b7282
    
    Signed-off-by: Herman Yang <herman.yang@mediatek.com>
    CR-Id: ALPS08138648
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I795aad60bda661b8d0dd1f7ad7091d1fad324911
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7886376
    Test: srv_pf_nep_sanity
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>

--- os/linux/gl_wext_priv.c
+++ os/linux/gl_wext_priv.c
@@ -5384,7 +5384,7 @@ int priv_driver_set_ml_probereq(struct net_device *prNetDev,
 		return -EINVAL;
 	}
 
-	if (rStatus == WLAN_STATUS_SUCCESS && i4Argc >= 4) {
+	if (rStatus == WLAN_STATUS_SUCCESS && i4Argc >= 5) {
 		struct BSS_DESC *prBssDesc;
 		uint32_t u4SsidNum = 0;
 		struct PARAM_SSID rSsid;
@@ -5411,7 +5411,7 @@ int priv_driver_set_ml_probereq(struct net_device *prNetDev,
 			return -1;
 		}
 
-		if (i4Argc >= 5) {
+		if (i4Argc >= 6) {
 			u4SsidNum = 1;
 			COPY_SSID(rSsid.aucSsid, rSsid.u4SsidLen,
 				  apcArgv[5], strlen(apcArgv[5]));
