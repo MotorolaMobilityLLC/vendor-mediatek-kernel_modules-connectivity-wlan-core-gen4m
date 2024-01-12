ec84b2378da4bb5d6b79f4170a68dd0dcdb5906c
Jo-Ping.Li <Jo-Ping.li@mediatek.com>
2024-01-12 22:17:19 +0800

    [ALPS08015965] wlan: add pcie dump before free irq
    1.add pcie dump before free irq
    2.add driver own status dump
    
    MTK-Commit-Id: d51c7166e0d3774f0fde88c291f9d12b903d6656
    
    CR-Id: ALPS08015965
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: jo-ping.li <jo-ping.li@mediatek.com>
    Change-Id: I747c8ab422cca0578c0c2d67722c3936f0350b30
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7346783
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: qizhong.cheng <qizhong.cheng@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Reviewed-by: lucious.chou <lucious.chou@mediatek.com>
    Tested-by: qizhong.cheng <qizhong.cheng@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>

--- os/linux/hif/pcie/pcie.c
+++ os/linux/hif/pcie/pcie.c
@@ -1766,6 +1766,10 @@ static void glBusFreeMsiIrq(struct pci_dev *pdev,
 {
 #if KERNEL_VERSION(4, 8, 0) <= LINUX_VERSION_CODE
 	struct pcie_msi_info *prMsiInfo = NULL;
+#if (CFG_MTK_ANDROID_WMT == 1) && defined(MT6639)
+	struct ADAPTER *prAdapter = NULL;
+	uint32_t u4Value = 0;
+#endif
 	uint8_t dbg[512];
 	uint32_t written = 0;
 	uint8_t i = 0;
@@ -1775,7 +1779,18 @@ static void glBusFreeMsiIrq(struct pci_dev *pdev,
 	prMsiInfo = &prBusInfo->pcie_msi_info;
 	kalMemZero(dbg, sizeof(dbg));
 
+#if (CFG_MTK_ANDROID_WMT == 1) && defined(MT6639)
+	prAdapter = prGlueInfo->prAdapter;
+	/* conninfra CR: 0x7c060010 = 0x70007154: cb-infra CR */
+	HAL_MCR_RD(prAdapter, 0x70007154, &u4Value);
+	DBGLOG(HAL, INFO, "Dump 0x70007154 = 0x%08x\n", u4Value);
+#endif
 	KAL_REC_TIME_START();
+#if (CFG_MTK_ANDROID_WMT == 1) && defined(MT6639)
+#if IS_ENABLED(CFG_MTK_WIFI_PCIE_SUPPORT)
+	mtk_pcie_dump_link_info(0);
+#endif
+#endif
 	for (i = 0; i < prMsiInfo->u4MsiNum; i++) {
 		struct pcie_msi_layout *prMsiLayout =
 			&prMsiInfo->prMsiLayout[i];
