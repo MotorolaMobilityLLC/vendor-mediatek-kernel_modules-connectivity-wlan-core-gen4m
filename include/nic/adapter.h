02589eaa742f4077c87913879c61b6b8259647ed
devin.yu <devin.yu@mediatek.com>
2024-01-12 22:28:09 +0800

    [ALPS08292807] Filter Tx timeout
    Warning Tx timeout if
    1. Moving average idle slot in tx timeout period > threshold (disable)
    2. Tx timeout token is same > 15s
    
    MTK-Commit-Id: e578f0ededcb828da7092e4ca0daad1a446bea41
    
    CR-Id: ALPS08292807
    Feature: WAPI
    Change-Id: I0fd758eba615ac7bd635c28484348c850bb1a054
    Signed-off-by: devin.yu <devin.yu@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8026302
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ks.lim <ks.lim@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8130070
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: wilson.lien <wilson.lien@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- include/nic/adapter.h
+++ include/nic/adapter.h
@@ -1147,7 +1147,11 @@ struct WIFI_VAR {
 	uint32_t u4LTOValue;
 #endif /* CFG_TX_CUSTOMIZE_LTO */
 
-	uint8_t ucWarningTxTimeout;
+	u_int8_t fgWarningTxTimeout;
+	uint32_t u4TxTimeoutWarningThr;
+	u_int8_t fgIgnoreLowIdleSlot;
+	uint32_t u4LowIdleSlotThr;
+	uint32_t u4SameTokenThr;
 
 	uint32_t u4UapsdAcBmp;
 	uint32_t u4MaxSpLen;
@@ -2281,6 +2285,11 @@ struct ADAPTER {
 
 	/* Online Scan Option */
 	u_int8_t fgDisBcnLostDetection;
+
+	uint32_t u4SumIdleSlot;
+	uint32_t u4LastTokenId;
+	uint32_t u4TxTimeoutCnt;
+	uint32_t u4SameTokenCnt;
 
 	/* MAC address */
 	uint8_t rMyMacAddr[PARAM_MAC_ADDR_LEN];
