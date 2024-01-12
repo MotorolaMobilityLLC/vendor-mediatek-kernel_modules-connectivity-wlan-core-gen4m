1fd4b42a261524386e5bfa5c795b1bcae63e8689
ch-howard.chang <ch-howard.chang@mediatek.com>
2024-01-12 22:21:51 +0800

    [ALPS08168229] Dump mld Starec before and after wlanoidSetAddKey
    There is a low probability of null pointer KE or infinitely
    looping on mld StarecList. Dump the list to check what happened.
    
    MTK-Commit-Id: 33ab0c50a67b6b1297774383eaad897264fbafdc
    
    Signed-off-by: ch-howard.chang <ch-howard.chang@mediatek.com>
    CR-Id: ALPS08168229
    Feature: Wi-Fi Driver
    Change-Id: I1a4a865b25fd8fd03c4493fe87d052f3471778c1
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7699718
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_mspautosanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: wyatt.sun <wyatt.sun@mediatek.com>

--- include/mgmt/mlo.h
+++ include/mgmt/mlo.h
@@ -411,4 +411,9 @@ uint8_t mldCheckMldType(struct ADAPTER *prAdapter,
 
 struct STA_RECORD *mldGetStaRecByBandIdx(struct ADAPTER *prAdapter,
 		struct STA_RECORD *prStaRec, uint8_t ucHwBandIdx);
+
+void mldDumpStarecList(struct ADAPTER *prAdapter,
+	struct MLD_STA_RECORD *prMldStarec);
+
+void mldCheckStarecList(struct ADAPTER *prAdapter);
 #endif /* !_MLO_H */
