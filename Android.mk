9f19f8adea4b5f488819e9638bf6e4fdf2e52f20
Jo-Ping.Li <Jo-Ping.li@mediatek.com>
2024-01-12 21:54:23 +0800

    [ALPS07205806] [MT6886 Porting] wlan driver porting
    1. mt6886 plat_priv.c porting from mt6983
    2. Add 6886 config in makefile
    
    MTK-Commit-Id: 77a86598dd4343b220b62019b5c11bbdfbe1ab7e
    
    CR-Id: ALPS07205806
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ia006864329a9cf06fe6b66f84ee825add6107bdd
    Signed-off-by: jo-ping.li <jo-ping.li@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/5921418
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: chris.hong <chris.hong@mediatek.com>
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Test: srv_mspautosanity

--- Android.mk
+++ Android.mk
@@ -48,6 +48,17 @@ ifeq ($(WLAN_BUILD_COMMON), true)
 	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
 	include $(LOCAL_PATH)/build_wlan_drv.mk
 
+	WLAN_CHIP_ID := 6886
+	WIFI_CHIP := CONNAC2X2_SOC7_0
+	WIFI_IP_SET := 1
+	CONNAC_VER := 2_0
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_NAME := wlan_drv_gen4m_6886
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
+
 	WLAN_CHIP_ID := 6855
 	WIFI_CHIP := SOC2_1X1
 	WIFI_IP_SET := 1
