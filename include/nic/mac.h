82069e4a106249a7b4c1cc6461f78ceb5d7964ff
Rachel Tseng <Rachel.Tseng@mediatek.com>
2024-01-12 22:22:23 +0800

    [ALPS06720434] Dont use PMKID if auth type is SAE
    If STA put PMKID in Assoc Req when auth type is SAE, some AP
    will reject with invalid PMKID, event the PMKID is correct.
    Therefore, dont use PMKID if auth type is SAE.
    
    MTK-Commit-Id: ba85fca91e1ec8dbfe6551eb25a3fe36f205aeee
    
    Change-Id: Ie3c3aea6801a9f1b8ef513a544f48bf3364b835a
    CR-Id: ALPS06720434
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <rachel.tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7738337
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    (cherry picked from commit 2356f88752f3c7c9a6b2be2994b440577f8313d2)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7745451
    Test: srv_mspautosanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: han.hu <han.hu@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- include/nic/mac.h
+++ include/nic/mac.h
@@ -627,6 +627,7 @@
 #define AUTH_ALGORITHM_NUM_FILS_SK		4 /* FILS - Shared key */
 #define AUTH_ALGORITHM_NUM_FILS_SK_PFS		5 /* FILS - SK with PFS */
 #define AUTH_ALGORITHM_NUM_FILS_PK		6 /* FILS - Public key */
+#define AUTH_ALGORITHM_NUM                      0xFF
 
 /* 7.3.1.2 Authentication Transaction Sequence Number field */
 #define AUTH_TRANSACTION_SEQENCE_NUM_FIELD_LEN      2
