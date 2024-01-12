87695665d5257d4f860c5b531aa8333119250a08
han.hu <han.hu@mediatek.com>
2024-01-12 22:21:26 +0800

    [WCNCR00320301][OWL] fix coverity
    [Description]
    fix coverity, null point check
    
    [Test]
    pass
    
    MTK-Commit-Id: bacb6f7c3e1065dd90e3ca7258e9aedb3be029aa
    
    Change-Id: Iab30adf43dd8e261f95a8b941dafc0019d6357b6
    Signed-off-by: han.hu <han.hu@mediatek.com>
    CR-Id: WCNCR00320301
    Feature: WIFI
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7663405
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: fred.shen <fred.shen@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>

--- mgmt/mlo.c
+++ mgmt/mlo.c
@@ -3558,12 +3558,13 @@ static void mldStarecUpdateMldId(struct ADAPTER *prAdapter,
 	prMldStarec->u2SecondMldId = 0;
 	LINK_FOR_EACH_ENTRY(prStarec, prStarecList,
 			rLinkEntryMld, struct STA_RECORD) {
+		if (!prStarec)
+			break;
+
 		if (i == 0)
-			prMldStarec->u2PrimaryMldId =
-				prStarec ? prStarec->ucWlanIndex : 0;
+			prMldStarec->u2PrimaryMldId = prStarec->ucWlanIndex;
 		else if (i == 1) {
-			prMldStarec->u2SecondMldId =
-				prStarec ? prStarec->ucWlanIndex : 0;
+			prMldStarec->u2SecondMldId = prStarec->ucWlanIndex;
 			break;
 		}
 		i++;
