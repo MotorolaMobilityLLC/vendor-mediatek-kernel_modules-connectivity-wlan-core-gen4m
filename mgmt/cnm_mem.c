9d0602a41bb0e7f742d3a90c93eba6d8ebd1a459
TF Huang <tf.huang@mediatek.com>
2024-01-12 22:05:10 +0800

    [ALPS07445304] roam: Send command to FW before free StaRec
    Since cnmStaRoutinesForAbort will clear StaRec, and it leads to get
    an unexpected wlan index after that. Move cnmStaSendRemoveCmd before
    cnmStaRoutinesForAbort to make sure the StaRec information correct.
    
    MTK-Commit-Id: 91a602428285a09e2c76e8012c53b9b54f5eefe4
    
    Signed-off-by: TF Huang <tf.huang@mediatek.com>
    CR-Id: ALPS07445304
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ib2bf23689ec9fd3d030b8255b67bcb9de8cba328
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6292791
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Tested-by: herman.yang <herman.yang@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: yuchi.wang <yuchi.wang@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: wyatt.sun <wyatt.sun@mediatek.com>

--- mgmt/cnm_mem.c
+++ mgmt/cnm_mem.c
@@ -783,10 +783,10 @@ void cnmStaRecFree(struct ADAPTER *prAdapter, struct STA_RECORD *prStaRec)
 		nicFreePendingTxMsduInfo(prAdapter, prStaRec->ucWlanIndex,
 				MSDU_REMOVE_BY_WLAN_INDEX);
 
-		cnmStaRoutinesForAbort(prAdapter, prStaRec);
-
 		cnmStaSendRemoveCmd(prAdapter, STA_REC_CMD_ACTION_STA,
 			prStaRec->ucIndex, prStaRec->ucBssIndex);
+
+		cnmStaRoutinesForAbort(prAdapter, prStaRec);
 	} else {
 		log_dbg(CNM, ERROR, "prStaRec is not in use\n");
 	}
@@ -860,14 +860,6 @@ void cnmStaFreeAllStaByNetwork(struct ADAPTER *prAdapter, uint8_t ucBssIndex,
 	if (ucBssIndex > prAdapter->ucHwBssIdNum)
 		return;
 
-	for (i = 0; i < CFG_STA_REC_NUM; i++) {
-		prStaRec = (struct STA_RECORD *) &prAdapter->arStaRec[i];
-
-		if (prStaRec->fgIsInUse && prStaRec->ucBssIndex == ucBssIndex
-			&& i != ucStaRecIndexExcluded)
-			cnmStaRoutinesForAbort(prAdapter, prStaRec);
-	}	/* end of for loop */
-
 	if (ucStaRecIndexExcluded < CFG_STA_REC_NUM)
 		eAction = STA_REC_CMD_ACTION_BSS_EXCLUDE_STA;
 	else
@@ -876,6 +868,14 @@ void cnmStaFreeAllStaByNetwork(struct ADAPTER *prAdapter, uint8_t ucBssIndex,
 	cnmStaSendRemoveCmd(prAdapter,
 		eAction,
 		ucStaRecIndexExcluded, ucBssIndex);
+
+	for (i = 0; i < CFG_STA_REC_NUM; i++) {
+		prStaRec = (struct STA_RECORD *) &prAdapter->arStaRec[i];
+
+		if (prStaRec->fgIsInUse && prStaRec->ucBssIndex == ucBssIndex
+			&& i != ucStaRecIndexExcluded)
+			cnmStaRoutinesForAbort(prAdapter, prStaRec);
+	}	/* end of for loop */
 
 #if CFG_ENABLE_WIFI_DIRECT
 	/* To do: Confirm if it is invoked here or other location, but it should
