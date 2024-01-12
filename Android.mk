1d00186c97f5c361016d8bb9cec650a28c94d146
Facer Pei <facer.pei@mediatek.com>
2024-01-12 21:05:05 +0800

    [ALPS04385998] Modify android.mk for new chips
    Revise android.mk for build new chips on Neptune trunk.
    
    MTK-Commit-Id: e4682a89eab92ef348bb0e633704412fbf2302d4
    
    CR-Id: ALPS04385998
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Facer Pei <facer.pei@mediatek.com>
    Change-Id: I4f2220a34953904956698c5f4ef598602d2c2b7f
    Reviewed-on: http://gerrit.mediatek.inc:8080/2295480
    CheckCourse: srv_hbgsm001 <srv_hbgsm001@mediatek.com>
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Austin Chou (周欽弘) <austin.chou@mediatek.com>
    Reviewed-by: Chun-Yi Lin <chun-yi.lin@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- Android.mk
+++ Android.mk
@@ -34,6 +34,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(PLATFORM_VERSION), 8.1.0)
+
 ###############################################################################
 #################### KERNEL ENVIRONMENT SETUP #################################
 #
@@ -137,7 +139,6 @@ export AUTO_CONF=$(KERNEL_OUT)/include/config/auto.conf
 ###############################################################################
 
 LOCAL_PATH := $(call my-dir)
-
 ifeq ($(MTK_WLAN_SUPPORT), yes)
 
 ifneq (true,$(strip $(TARGET_NO_KERNEL)))
@@ -204,5 +205,49 @@ $(LOCAL_BUILT_MODULE): $(LOCAL_GENERATED_SOURCES) $(KERNEL_OUT)/scripts/sign-fil
 	$(MAKE) -C $(KERNEL_OUT) $(KOPTS) $(WIFI_OPTS)
 	$(hide) $(call sign-kernel-module,$(KERNEL_OUT)/scripts/sign-file,$(CERT_PATH)/ko_prvk.pem,$(CERT_PATH)/ko_pubk.x509.der)
 
+endif
+endif
+else
+ifeq ($(MTK_WLAN_SUPPORT), yes)
+ifneq (true,$(strip $(TARGET_NO_KERNEL)))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wlan_drv_gen4m.ko
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := mtk
+LOCAL_SRC_FILES := $(patsubst $(LOCAL_PATH)/%,%,$(shell find $(LOCAL_PATH) -type f -name '*.[cho]')) Makefile
+LOCAL_REQUIRED_MODULES := wmt_chrdev_wifi.ko
+
+include $(MTK_KERNEL_MODULE)
+
+
+ifeq ($(MTK_WLAN_UT_TEST_MODE), yes)
+LOCAL_SRC_UT_FILES := $(shell find $(LOCAL_PATH)_ut/ -type f -name '*')
+LOCAL_DST_UT_FILES := $(patsubst $(LOCAL_PATH)_ut/%,$(intermediates)/LINKED/%,$(LOCAL_SRC_UT_FILES))
+$(LOCAL_DST_UT_FILES): $(intermediates)/LINKED/% : $(LOCAL_PATH)_ut/%
+	$(copy-file-to-target)
+
+$(linked_module): $(LOCAL_DST_UT_FILES)
+
+WIFI_HIF := ut
+WIFI_CHIP := UT_TEST_MODE
+endif
+
+
+WIFI_NAME := wlan_drv_gen4m
+WIFI_OPTS := CONFIG_MTK_COMBO_WIFI_HIF=$(WIFI_HIF) MODULE_NAME=$(WIFI_NAME) MTK_COMBO_CHIP=$(WIFI_CHIP) WLAN_CHIP_ID=$(WLAN_CHIP_ID) MTK_ANDROID_WMT=$(WIFI_WMT) WIFI_ENABLE_GCOV=$(WIFI_ENABLE_GCOV) WIFI_IP_SET=$(WIFI_IP_SET) MTK_CONSYS_ADIE=$(MTK_CONSYS_ADIE)
+
+#### Copy Module.symvers from $(LOCAL_REQUIRED_MODULES) to this module #######
+#### For symbol link (when CONFIG_MODVERSIONS is defined)
+LOCAL_SRC_EXPORT_SYMBOL := $(subst $(LOCAL_MODULE),$(LOCAL_REQUIRED_MODULES),$(intermediates)/LINKED)/Module.symvers
+$(LOCAL_SRC_EXPORT_SYMBOL): $(subst $(LOCAL_MODULE),$(LOCAL_REQUIRED_MODULES),$(linked_module))
+LOCAL_EXPORT_SYMBOL := $(intermediates)/LINKED/Module.symvers
+$(LOCAL_EXPORT_SYMBOL): $(intermediates)/LINKED/% : $(LOCAL_SRC_EXPORT_SYMBOL)
+	$(copy-file-to-target)
+
+$(linked_module): OPTS += $(WIFI_OPTS)
+$(linked_module): $(LOCAL_EXPORT_SYMBOL)
+
+endif
 endif
 endif
