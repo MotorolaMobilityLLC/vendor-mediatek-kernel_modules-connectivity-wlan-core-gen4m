4e16311ac39b1052338419950507df6ea895f3b3
Wyatt Sun <wyatt.sun@mediatek.com>
2024-01-12 22:11:28 +0800

    [ALPS07485724] Fix HE_MU support for rate table lookup
    Fix on handling mode==HE_MU && nsts==0 (in RXV group 3),
    It shows Unknown rate for [11, 1, 0, 2, 7].
    
    The setting of NSTS value are revised for handling MU-MIMO by checking
    additional field.
    
    MTK-Commit-Id: 811d043a45aba24c43c03854a20820c2f7161356
    
    CR-Id: ALPS07485724
    Feature: Wi-Fi Certification : TGax STA Mode
    Change-Id: I05635d2430cb3354e33437b8b749f302dea05685
    Signed-off-by: Wyatt Sun <wyatt.sun@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6356780
    Test: srv_pf_nep_sanity
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: sn.chen <sn.chen@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Test: srv_mspautosanity
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -12797,9 +12797,6 @@ int wlanQueryRateByTable(uint32_t txmode, uint32_t rate,
 		   txmode == TX_RATE_MODE_HE_MU) { /* AX */
 		uint8_t dcm = 0, ru106 = 0;
 
-		if (txmode == TX_RATE_MODE_HE_MU)
-			nsts--;
-
 		if (nsts == 0 || nsts >= 5) {
 			DBGLOG_LIMITED(SW4, ERROR, "nsts error: %u\n", nsts);
 			return -1;
