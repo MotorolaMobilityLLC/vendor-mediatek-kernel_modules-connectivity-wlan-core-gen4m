a97ed01807eed24c7f6cd3500cc25d6d92ff3705
Chiahsuan Chuang <chiahsuan.chuang@mediatek.com>
2024-01-12 20:33:49 +0800

    [WCNCR00156234] sdio: fix get_drv_mcr failure after FW_OWN
    [Description]
    Fix sdio get_drv_mcr/set_drv_mcr failure after FW_OWN
    
    MTK-Commit-Id: 17bec6a0e97ffa0ccc2fff480cdf1e2194064dd8
    
    Change-Id: I9c365498012fe0ddc66b1ba756824299ae970ee9
    Signed-off-by: Chiahsuan Chuang <chiahsuan.chuang@mediatek.com>
    CR-Id: WCNCR00156234
    Feature: sdio
    (cherry picked from commit 2ba2e8813148313bb7a80fbf4a7aa860b107076f)

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -5901,6 +5901,7 @@ wlanoidQueryDrvMcrRead(IN P_ADAPTER_T prAdapter,
 
 	prMcrRdInfo = (P_PARAM_CUSTOM_MCR_RW_STRUCT_T) pvQueryBuffer;
 
+	ACQUIRE_POWER_CONTROL_FROM_PM(prAdapter);
 	HAL_MCR_RD(prAdapter, (prMcrRdInfo->u4McrOffset & BITS(2, 31)), &prMcrRdInfo->u4McrData);
 
 	DBGLOG(INIT, TRACE, "DRV MCR Read: Offset = %#08lx, Data = %#08lx\n",
@@ -5948,6 +5949,7 @@ wlanoidSetDrvMcrWrite(IN P_ADAPTER_T prAdapter,
 
 	prMcrWrInfo = (P_PARAM_CUSTOM_MCR_RW_STRUCT_T) pvSetBuffer;
 
+	ACQUIRE_POWER_CONTROL_FROM_PM(prAdapter);
 	HAL_MCR_WR(prAdapter, (prMcrWrInfo->u4McrOffset & BITS(2, 31)), prMcrWrInfo->u4McrData);
 
 	DBGLOG(INIT, TRACE, "DRV MCR Write: Offset = %#08lx, Data = %#08lx\n",
