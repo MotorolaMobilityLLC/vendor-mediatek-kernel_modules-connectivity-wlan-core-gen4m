c9467895ef521d08693127e89c4d17bbcd3cbb2e
Holiday Hao <holiday.hao@mediatek.com>
2024-01-12 22:31:08 +0800

    [ALPS08274671] mddp: fix mddp msi issue
    dont set mddp on address if mddp is disable
    
    MTK-Commit-Id: 0fc8b6a580b22f493f11b248671b627bfeabfa34
    
    CR-Id: ALPS08274671
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    Change-Id: I4edb5a23e74161cfd6f446434a122254fd777157
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8384502
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- chips/mt6653/mt6653.c
+++ chips/mt6653/mt6653.c
@@ -60,6 +60,7 @@
 
 #if CFG_MTK_MDDP_SUPPORT
 #include "mddp_export.h"
+#include "mddp.h"
 #endif
 
 #if CFG_MTK_CCCI_SUPPORT
@@ -3561,7 +3562,12 @@ static int mt6653ConnacPccifOn(struct ADAPTER *prAdapter)
 	writel(0x4D434D4D, vir_addr + 0x7C);
 
 	u4Val = readl(vir_addr + MCIF_EMI_BASE_OFFSET);
-	HAL_MCR_WR(prAdapter, MT6653_MCIF_MD_STATE_WHEN_WIFI_ON_ADDR, u4Val);
+
+	if (mddpIsSupportMcifWifi()) {
+		HAL_MCR_WR(prAdapter,
+			   MT6653_MCIF_MD_STATE_WHEN_WIFI_ON_ADDR,
+			   u4Val);
+	}
 
 	DBGLOG(INIT, TRACE, "MCIF_EMI_BASE_OFFSET=[0x%08x]\n", u4Val);
 	DBGLOG_MEM128(HAL, TRACE, vir_addr, MCIF_EMI_MEMORY_SIZE);
