22dae511320db69abb7c99f363f241591485b45b
Xianpu Liang <xianpu.liang@mediatek.com>
2024-01-12 20:38:00 +0800

    [ALPS03554452] wlan: use max RSSI as the real RSSI instead of WF0
    [Detail]
    The RSSI currently used is detected by WF0, it does not exactly
    represent the right RSSI of the packet
    [Solution]
    Use the max RSSI as the real RSSI of the packet
    
    MTK-Commit-Id: 1d6e4207f8b621b98062f3e598786ec9cae334ba
    
    Change-Id: I2b582ccb34d9771f861155b6158530564f0190cb
    Signed-off-by: Xianpu Liang <xianpu.liang@mediatek.com>
    CR-Id: ALPS03554452
    Feature: Others
    (cherry picked from commit 216ba5052462b88a21b5f2f6e27c4f3979ad144f)
    (cherry picked from commit 072f49f2d054ebc1bfc1f3853aeb2239655802d2)

--- mgmt/saa_fsm.c
+++ mgmt/saa_fsm.c
@@ -909,7 +909,7 @@ WLAN_STATUS saaFsmRunEventRxAssoc(IN P_ADAPTER_T prAdapter, IN P_SW_RFB_T prSwRf
 
 			/* update RCPI */
 			ASSERT(prSwRfb->prRxStatusGroup3);
-			prStaRec->ucRCPI = nicRxGetRcpiValueFromRxv(RCPI_MODE_WF0, prSwRfb);
+			prStaRec->ucRCPI = nicRxGetRcpiValueFromRxv(RCPI_MODE_MAX, prSwRfb);
 
 			eNextState = AA_STATE_IDLE;
 
