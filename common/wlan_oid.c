0efb174da8c3baef43744fb269ea1a917236038a
TS Hsu <ts.hsu@mediatek.com>
2024-01-12 21:39:31 +0800

    [ALPS06206511] scan: Refine 6G scan
    1. Fix 6G oob scan/hidden ap issue
       - Report scandone to kernel after scan all non-PSC channel
       - Only clear 6G SSID if received beacon/probe response is
         the first one in the current scan. (u2RawLength = 0)
         Because 20TU unsolicited probe response may clear probe response
         with SSID.
    2. Fix potential null access and non-declared variable usage.
    
    MTK-Commit-Id: 4a9110de626ad699d77e188522addad53c430429
    
    CR-Id: ALPS06206511
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: TS Hsu <ts.hsu@mediatek.com>
    Change-Id: Ia3bc5bc07858fca5b1c56671da4beca3dc405588
    (cherry picked from commit 7d4fe0eb6b43ca551fee803ea32c24814c161056)

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -12694,7 +12694,7 @@ wlanoidSetStopSchedScan(IN struct ADAPTER *prAdapter,
 	if (scnFsmSchedScanStopRequest(prAdapter) == TRUE) {
 		kalMemFree(prSchedScanRequest->pucIE,
 			   VIR_MEM_TYPE,
-			   prGlueInfo->prSchedScanRequest->u4IELength);
+			   prSchedScanRequest->u4IELength);
 		kalMemFree(prSchedScanRequest,
 			   VIR_MEM_TYPE,
 			   sizeof(struct PARAM_SCHED_SCAN_REQUEST));
