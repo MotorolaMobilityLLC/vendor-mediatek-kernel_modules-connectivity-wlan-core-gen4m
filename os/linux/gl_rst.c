eb34a943dfc2efb7ee03449ebaf8308d63e99e1c
Michael.Chung <michael.chung@mediatek.com>
2024-01-12 22:29:09 +0800

    [ALPS08557211][[OWL]fix gen4m reset string display error]
    [Description]
    [OWL]fix gen4m reset string display error
    
    MTK-Commit-Id: b63b37b68439a12808849126efbb41ecd4a56745
    
    Signed-off-by: Michael.Chung <michael.chung@mediatek.com>
    CR-Id: WCNCR00293325
    Feature: reset
    Change-Id: I9b4c8cc67cf4e5673e339818aa0e65224038566c
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8254039
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ks.lim <ks.lim@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: chiahsuan.chuang <chiahsuan.chuang@mediatek.com>
    Test: srv_pf_nep_sanity
    Tested-by: jo-ping.li <jo-ping.li@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: jo-ping.li <jo-ping.li@mediatek.com>

--- os/linux/gl_rst.c
+++ os/linux/gl_rst.c
@@ -103,6 +103,7 @@ uint8_t *apucRstReason[RST_REASON_MAX] = {
 	(uint8_t *) DISP_STRING("RST_SER_L0P5_FAIL"),
 	(uint8_t *) DISP_STRING("RST_CMD_EVT_FAIL"),
 	(uint8_t *) DISP_STRING("RST_WDT"),
+	(uint8_t *) DISP_STRING("RST_SUBSYS_BUS_HANG"),
 	(uint8_t *) DISP_STRING("RST_SMC_CMD_FAIL"),
 	(uint8_t *) DISP_STRING("RST_DEVAPC"),
 	(uint8_t *) DISP_STRING("RST_PCIE_NOT_READY"),
