10c0c8edc551793c8e32223abe2bfacba9cb81ac
Zeno Yang <zeno.yang@mediatek.com>
2024-01-12 22:05:13 +0800

    [ALPS07456740] mlo gc: Handle bc deauth
    Handle bc deauth to do gc disconnection
    
    MTK-Commit-Id: 254b41cf54aec4deedd74039169acc2f6dcf68e6
    
    Signed-off-by: zeno.yang <zeno.yang@mediatek.com>
    CR-Id: ALPS07456740
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I313e79cdba454812a74f9da67bdbf75aa5753ce2
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6282576
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>

--- include/mgmt/p2p_link.h
+++ include/mgmt/p2p_link.h
@@ -121,6 +121,11 @@ void p2pScanFillSecondaryLink(struct ADAPTER *prAdapter,
 	struct BSS_DESC_SET *prBssDescSet);
 #endif
 
+void p2pRxDeauthNoWtbl(
+	struct ADAPTER *prAdapter,
+	struct STA_RECORD *prStaRec,
+	struct SW_RFB *prSwRfb);
+
 void p2pLinkStaRecFree(struct ADAPTER *prAdapter,
 	struct STA_RECORD *prStaRec);
 
