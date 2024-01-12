19a8519acfa4115d9d9766419d1787423a511f61
Rachel Tseng <Rachel.Tseng@mediatek.com>
2024-01-12 22:20:33 +0800

    [ALPS08138740] ais: stop csa timer if disconnected
    Should stop csa timer if disconnected. Otherwise, it may false
    alert timer timeout in next connection.
    
    MTK-Commit-Id: 302fb092605bca01132198201500736226e3435d
    
    Change-Id: I8fb8b8cbefaab7c237e9f035fc5d68e0b3f512b4
    CR-Id: ALPS08138740
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7587537
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Test: srv_mspautosanity

--- mgmt/ais_fsm.c
+++ mgmt/ais_fsm.c
@@ -5741,6 +5741,10 @@ void aisFsmDisconnect(struct ADAPTER *prAdapter,
 	cnmTimerStopTimer(prAdapter,
 		aisGetSecModeChangeTimer(prAdapter, ucBssIndex));
 #endif
+#if CFG_SUPPORT_DFS
+	cnmTimerStopTimer(prAdapter, &prAisBssInfo->rCsaTimer);
+#endif
+
 	nicPmIndicateBssAbort(prAdapter, prAisBssInfo->ucBssIndex);
 
 #if CFG_SUPPORT_ADHOC
