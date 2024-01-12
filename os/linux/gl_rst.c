221d51fd00254a92a4d151ca8cbfddedb0ab468a
Jo-Ping.Li <Jo-Ping.li@mediatek.com>
2024-01-12 22:04:46 +0800

    [ALPS07415267] Fix coverity Dereference before null check
    - Directly dereferencing pointer prGlueInfo
    - Dereference before null check
    
    Solution: deferencing pointer prGlueInfo after null check
    
    MTK-Commit-Id: 3f1d72713e8cce7887a514f96af6eacb6dc9e7b2
    
    CR-Id: ALPS07415267
    Feature: Feature: Wi-Fi Driver CONNAC
    Signed-off-by: jo-ping.li <jo-ping.li@mediatek.com>
    Change-Id: I622b2b8a8d5818d859aeb7c84a8d34c7023ec23c
    (cherry picked from commit 83a6b66f833a0aa1e118ff30390be55162a6e5c3)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6273006
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: chris.hong <chris.hong@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>

--- os/linux/gl_rst.c
+++ os/linux/gl_rst.c
@@ -1458,9 +1458,12 @@ void glResetSubsysRstProcedure(struct GLUE_INFO *prGlueInfo,
 	struct timespec64 *rNowTs,
 	struct timespec64 *rLastTs)
 {
-	struct ADAPTER *prAdapter = prGlueInfo->prAdapter;
+	struct ADAPTER *prAdapter = NULL;
 	struct WIFI_VAR *prWifiVar = NULL;
 	bool fgIsTimeout;
+
+	if (prGlueInfo != NULL)
+		prAdapter = prGlueInfo->prAdapter;
 
 	if (prGlueInfo && prGlueInfo->u4ReadyFlag) {
 		prWifiVar = &prAdapter->rWifiVar;
