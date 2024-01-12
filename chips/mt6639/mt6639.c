70d990ae7ad250476bbfb87806faf94f80938bd8
Ben Lai <ben.lai@mediatek.com>
2024-01-12 22:08:43 +0800

    [ALPS07658361] wlan: Fix coverity issue
    Fix coverity issue:
    Dereference after null check: 11373827
    
    MTK-Commit-Id: fa1847102f67bd87157fec589fa16d0aff649245
    
    Change-Id: I9465fab44cb081bddc8a4fe19a75408bd54861c6
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS07658361
    Feature: Wi-Fi Driver CONNAC
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6674669
    Reviewed-by: tf.huang <tf.huang@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    Test: srv_mspautosanity
    Test: srv_pf_nep_sanity
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>

--- chips/mt6639/mt6639.c
+++ chips/mt6639/mt6639.c
@@ -2010,23 +2010,29 @@ static uint32_t mt6639_mcu_init(struct ADAPTER *ad)
 	uint32_t u4Value = 0, u4PollingCnt = 0;
 	uint32_t rStatus = WLAN_STATUS_SUCCESS;
 
+	if (!ad) {
+		DBGLOG(INIT, ERROR, "NULL ADAPTER.\n");
+		rStatus = WLAN_STATUS_FAILURE;
+		goto exit;
+	}
+
 	set_cbinfra_remap(ad);
 
 	rStatus = mt6639_mcu_reinit(ad);
 	if (rStatus != WLAN_STATUS_SUCCESS)
-		goto exit;
+		goto dump;
 
 #if (CFG_MTK_ANDROID_WMT == 0)
 	rStatus = mt6639_mcu_reset(ad);
 	if (rStatus != WLAN_STATUS_SUCCESS)
-		goto exit;
+		goto dump;
 #endif
 
 	while (TRUE) {
 		if (u4PollingCnt >= 1000) {
 			DBGLOG(INIT, ERROR, "timeout.\n");
 			rStatus = WLAN_STATUS_FAILURE;
-			goto exit;
+			goto dump;
 		}
 
 		HAL_MCR_RD(ad, WF_TOP_CFG_ON_ROMCODE_INDEX_ADDR,
@@ -2042,14 +2048,14 @@ static uint32_t mt6639_mcu_init(struct ADAPTER *ad)
 	if (connv3_ext_32k_on()) {
 		DBGLOG(INIT, ERROR, "connv3_ext_32k_on failed.\n");
 		rStatus = WLAN_STATUS_FAILURE;
-		goto exit;
+		goto dump;
 	}
 #endif
 
 	if (ad->chip_info->coexpccifon)
 		ad->chip_info->coexpccifon();
 
-exit:
+dump:
 	if (rStatus != WLAN_STATUS_SUCCESS) {
 		DBGLOG(INIT, ERROR, "u4Value: 0x%x\n",
 			u4Value);
@@ -2089,6 +2095,7 @@ exit:
 			u4Value);
 	}
 
+exit:
 	return rStatus;
 }
 
