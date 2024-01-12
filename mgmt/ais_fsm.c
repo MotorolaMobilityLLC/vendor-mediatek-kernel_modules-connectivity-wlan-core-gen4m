b6665327b1263c6a232925ed56d6c74f91f0e20c
Rachel Tseng <Rachel.Tseng@mediatek.com>
2024-01-12 21:42:34 +0800

    [ALPS06192763] wlan: reset join req timer for BTO reconnect
    When BTO reconnect, only reset trial count but doesnt reset join
    req timer.
    Also need to reset join req timer to ensure the new reconnection
    runs correctly.
    
    MTK-Commit-Id: f170c077098ff6d9ed4a58c609e1dd7140bb2787
    
    Change-Id: I8823d21d0ee530873dbdb1b441f1df432ba05e80
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    CR-Id: ALPS06192763
    Feature: [Module]Wi-Fi Driver
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/5436232
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Reviewed-by: tf.huang <tf.huang@mediatek.com>
    Tested-by: herman.yang <herman.yang@mediatek.com>
    Build: herman.yang <herman.yang@mediatek.com>
    Test: herman.yang <herman.yang@mediatek.com>

--- mgmt/ais_fsm.c
+++ mgmt/ais_fsm.c
@@ -4724,6 +4724,7 @@ void aisFsmDisconnect(IN struct ADAPTER *prAdapter,
 			 * ensure the new reconnection runs correctly.
 			 */
 			prAisFsmInfo->ucConnTrialCount = 0;
+			GET_CURRENT_SYSTIME(&(prAisFsmInfo->rJoinReqTime));
 
 			switch (prAisFsmInfo->ucReasonOfDisconnect) {
 			case DISCONNECT_REASON_CODE_RADIO_LOST:
