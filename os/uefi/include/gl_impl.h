6f3d5200dab7e29afd3628ef8fa4232b57c10701
allan.wang <allan.wang@mediatek.com>
2024-01-12 22:04:40 +0800

    [WCNCR00244441] wlan: add file for uefi KAL
    [Description]
    Add file for uefi KAL
    
    MTK-Commit-Id: ec51ac1fd70e321dcab0b827d36f85a2efaf33a7
    
    Signed-off-by: allan.wang <allan.wang@mediatek.com>
    CR-Id: WCNCR00244441
    Feature: wlan/os
    Change-Id: I414b7056d12032402486efe178f45eab9b7f1baf
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6246883
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: deren.wu <deren.wu@mediatek.com>
    Reviewed-by: yuchi.wang <yuchi.wang@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>

---
+++ os/uefi/include/gl_impl.h
@@ -0,0 +1,143 @@
+
+/*****************************************************************************
+ *
+ * This file is provided under a dual license.  When you use or
+ * distribute this software, you may choose to be licensed under
+ * version 2 of the GNU General Public License ("GPLv2 License")
+ * or BSD License.
+ *
+ * GPLv2 License
+ *
+ * Copyright(C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ *
+ * BSD LICENSE
+ *
+ * Copyright(C) 2016 MediaTek Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***************************************************************************/
+/*! \file   gl_impl.h
+ * \brief  List the os-dependent structure/API that need to implement
+ * to align with common part
+ *
+ * not to modify *.c while put current used linux-type strucutre/API here
+ * For porting to new OS, the API listed in this file needs to be
+ * implemented
+ */
+#ifndef _GL_IMPL_H
+#define _GL_IMPL_H
+
+/*******************************************************************************
+ *                         C O M P I L E R   F L A G S
+ *******************************************************************************
+ */
+
+/*******************************************************************************
+ *                    E X T E R N A L   R E F E R E N C E S
+ *******************************************************************************
+ */
+#include "config.h"
+#include "gl_os.h"
+#include "gl_typedef.h"
+#include "gl_wext_priv.h"
+#include "link_drv.h"
+#include "nic/mac.h"
+#include "nic/wlan_def.h"
+#include "wlan_lib.h"
+#include "wlan_oid.h"
+#include "debug.h"
+#if CFG_ENABLE_BT_OVER_WIFI
+#include "nic/bow.h"
+#endif
+#include <intrin.h>
+
+/*******************************************************************************
+ *                              C O N S T A N T S
+ *******************************************************************************
+ */
+
+/*******************************************************************************
+ *                             D A T A   T Y P E S
+ *******************************************************************************
+ */
+
+/*******************************************************************************
+ *                            P U B L I C   D A T A
+ *******************************************************************************
+ */
+
+/*******************************************************************************
+ *                           P R I V A T E   D A T A
+ *******************************************************************************
+ */
+
+/*******************************************************************************
+ *                                 M A C R O S
+ *******************************************************************************
+ */
+uint32_t __KAL_INLINE__ kalGetFwVerOffsetAddr(void)
+{
+	DBGLOG(SW4, WARN, "NO firmware version build.\n");
+	return 0;
+}
+
+#if (CFG_SUPPORT_HOST_OFFLOAD == 1)
+uint8_t  __KAL_INLINE__ kalIsSupportMawd(void)
+{
+	KAL_NEED_IMPLEMENT(__FILE__, __func__, __LINE__);
+	return 0;
+}
+
+uint8_t  __KAL_INLINE__ kalIsSupportSdo(void)
+{
+	KAL_NEED_IMPLEMENT(__FILE__, __func__, __LINE__);
+	return 0;
+}
+
+uint8_t  __KAL_INLINE__ kalIsSupportRro(void)
+{
+	KAL_NEED_IMPLEMENT(__FILE__, __func__, __LINE__);
+	return 0;
+}
+#endif
+
+void __KAL_INLINE__ kalGetPlatDev(void **dev)
+{
+	KAL_NEED_IMPLEMENT(__FILE__, __func__, __LINE__);
+}
+
+#endif /* _GL_IMPL_H */
