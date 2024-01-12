1af49b146b3146fe8df64eb34c3cc2f635b43a2b
Wyatt Sun <wyatt.sun@mediatek.com>
2024-01-12 22:10:44 +0800

    [ALPS06471643] Send notification when TX delay over given limit
    Can be set by
    wlanSetTxDelayOverLimitReport() with options:
     - bool enable: enable/disable the report mechanism
     - bool isAverage: report immediately or by counting average
     - uint32_t interval: averaging interval if isAverage is set
     - uint32_t driver_limit: driver delay limit triggering report
     - uint32_t mac_limit: MAC delay limit triggering report
    
    MTK-Commit-Id: f08763a0c01d8d6606e563f6b7199ba3fa6585a7
    
    Change-Id: I753f8c13227f551a19bac0aed8c848a01a5828ef
    CR-Id: ALPS06471643
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Wyatt Sun <wyatt.sun@mediatek.com>
    (cherry picked from commit eab6da517f90407f60b32edb78cc51ceda018724)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6942932
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- include/wlan_lib.h
+++ include/wlan_lib.h
@@ -1580,6 +1580,17 @@ struct RTT_RESULT {
 	int32_t i4NegotiatedBustNum;
 };
 
+enum ENUM_TX_OVER_LIMIT_STATS_TYPE {
+	REPORT_IMMEDIATE,
+	REPORT_AVERAGE,
+};
+
+enum ENUM_TX_OVER_LIMIT_DELAY_TYPE {
+	DRIVER_DELAY,
+	MAC_DELAY,
+	MAX_TX_OVER_LIMIT_TYPE,
+};
+
 enum CHAN_FLAGS {
 	CHAN_RADAR,
 	CHAN_NO_HT40,
@@ -2033,6 +2044,17 @@ u_int8_t wlanIsChipRstRecEnabled(struct ADAPTER *prAdapter);
 u_int8_t wlanIsChipAssert(struct ADAPTER *prAdapter);
 
 void wlanChipRstPreAct(struct ADAPTER *prAdapter);
+
+void wlanCountTxDelayOverLimit(struct ADAPTER *prAdapter,
+		enum ENUM_TX_OVER_LIMIT_DELAY_TYPE type,
+		uint32_t u4Latency);
+
+void wlanReportTxDelayOverLimit(struct ADAPTER *prAdapter,
+	enum ENUM_TX_OVER_LIMIT_DELAY_TYPE type, uint32_t delay);
+
+int wlanSetTxDelayOverLimitReport(struct ADAPTER *prAdapter,
+		bool enable, bool isAverage,
+		uint32_t interval, uint32_t driver_limit, uint32_t mac_limit);
 
 void wlanTxProfilingTagPacket(struct ADAPTER *prAdapter, void *prPacket,
 			      enum ENUM_TX_PROFILING_TAG eTag);
