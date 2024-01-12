859171ad009c46a7070d8363a47e1d8e63c5e10f
Wilson Lien <wilson.lien@mediatek.com>
2024-01-12 22:20:45 +0800

    [ALPS08058821] Get correct SGI for HT/VHT rate
    According to 802.11 Short GI operation spec, it can use SGI bit in
    HT capability BW20 and BW40 for VHT.
    
    MTK-Commit-Id: d5d8185bce3867a706830e1f624f94ee70e40827
    
    CR-Id: ALPS08058821
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: Wilson Lien <wilson.lien@mediatek.com>
    Change-Id: I5cc7a607c28890e98093c0dd35313716c53638ca
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7586916
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: devin.yu <devin.yu@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    (cherry picked from commit e5532a2bc3de3e6fe059c0c78d2afcdd17faaac6)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7600239
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: wyatt.sun <wyatt.sun@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -13065,8 +13065,11 @@ int wlanGetMaxTxRate(struct ADAPTER *prAdapter,
 	if (ucAPBwPermitted < ucBw)
 		ucBw = ucAPBwPermitted;
 
-	/* get Short GI Tx capability */
-	if (ucTxMode == TX_RATE_MODE_HTMIX || ucTxMode == TX_RATE_MODE_HTGF) {
+	/* Get Short GI Tx capability for HT/VHT, check from HT or VHT
+	 * capability BW SGI bit. Refer to 802.11 Short GI operation
+	 */
+	if (ucTxMode == TX_RATE_MODE_HTMIX || ucTxMode == TX_RATE_MODE_HTGF
+	    || ucTxMode == TX_RATE_MODE_VHT) {
 		if (prStaRec->u2HtCapInfo & HT_CAP_INFO_SHORT_GI_20M) {
 			DBGLOG(RLM, TRACE, "HT_CAP_INFO_SHORT_GI_20M\n");
 			ucSgi = 1;
@@ -13075,10 +13078,8 @@ int wlanGetMaxTxRate(struct ADAPTER *prAdapter,
 			DBGLOG(RLM, TRACE, "HT_CAP_INFO_SHORT_GI_40M\n");
 			ucSgi = 1;
 		}
-	}
 
 #if CFG_SUPPORT_802_11AC
-	if (ucTxMode == TX_RATE_MODE_VHT) {
 		if (prStaRec->u4VhtCapInfo & VHT_CAP_INFO_SHORT_GI_80) {
 			DBGLOG(RLM, TRACE, "VHT_CAP_INFO_SHORT_GI_80\n");
 			ucSgi = 1;
@@ -13087,8 +13088,8 @@ int wlanGetMaxTxRate(struct ADAPTER *prAdapter,
 			DBGLOG(RLM, TRACE, "VHT_CAP_INFO_SHORT_GI_160_80P80\n");
 			ucSgi = 1;
 		}
-	}
 #endif
+	}
 
 	if (ucTxMode == TX_RATE_MODE_HE_SU) {
 		DBGLOG(SW4, TRACE, "TX_RATE_MODE_HE_SU\n");
