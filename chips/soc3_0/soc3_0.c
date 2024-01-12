74a962512a694cb9753f349b2c49b54095fd73c7
Ben Lai <ben.lai@mediatek.com>
2024-01-12 21:17:06 +0800

    [ALPS05038778] mddp: Wait for MD off during wifi off
    Wait for MD off during wifi off to prevent MD to access
    registers when consys power is shut down.
    
    MTK-Commit-Id: dda31588ac31ba1b100edd338b9f5f146b1af421
    
    Change-Id: I80076146c64ab1832476cdbbc0046f2e388e366f
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS05038778
    Feature: [Module]Wi-Fi Driver

--- chips/soc3_0/soc3_0.c
+++ chips/soc3_0/soc3_0.c
@@ -1124,39 +1124,6 @@ void soc3_0_DumpWfsyscpupcr(struct ADAPTER *prAdapter)
 			log_buf_lp[4]);
 }
 
-int wf_ioremap_read(size_t addr, unsigned int *val)
-{
-	void *vir_addr = NULL;
-
-	vir_addr = ioremap_nocache(addr, 0x10);
-	if (!vir_addr) {
-		DBGLOG(INIT, ERROR, "%s: Cannot remap address.\n", __func__);
-		return -1;
-	}
-
-	*val = readl(vir_addr);
-	iounmap(vir_addr);
-	DBGLOG(INIT, TRACE, "Read CONSYS 0x%08x=0x%08x.\n", addr, *val);
-
-	return 0;
-}
-int wf_ioremap_write(phys_addr_t addr, unsigned int val)
-{
-	void *vir_addr = NULL;
-
-	vir_addr = ioremap_nocache(addr, 0x10);
-	if (!vir_addr) {
-		DBGLOG(INIT, ERROR, "%s: Cannot remap address.\n", __func__);
-		return -1;
-	}
-
-	writel(val, vir_addr);
-	iounmap(vir_addr);
-	DBGLOG(INIT, TRACE, "Write CONSYS 0x%08x=0x%08x.\n", addr, val);
-
-	return 0;
-}
-
 void soc3_0_CrRead(struct ADAPTER *prAdapter, size_t addr, unsigned int *val)
 {
 	if (prAdapter == NULL)
