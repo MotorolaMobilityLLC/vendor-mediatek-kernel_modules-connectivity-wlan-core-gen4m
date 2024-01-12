1e0d80d9095feecaedb762f02ed6a1cbe13e15e2
Juien Chang <jui-en.chang@mediatek.com>
2024-01-12 22:18:27 +0800

    [ALPS07927498] Coverity Dereference before null check: 11494922
    [Description]
    Fix coverity issue
    
    MTK-Commit-Id: 07c9d8de17d8ad7583647b876672b9bb45515eac
    
    Signed-off-by: jui-en.chang <jui-en.chang@mediatek.com>
    CR-Id: ALPS07927498
    Feature: Wi-Fi NAN
    Change-Id: I0c46469718b6b8c3870841c8ec68e51640b9983f
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7445448
    Test: srv_mspautosanity
    Reviewed-by: fred.shen <fred.shen@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: longlay.ji <longlay.ji@mediatek.com>
    Coverity-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity

--- mgmt/he_rlm.c
+++ mgmt/he_rlm.c
@@ -922,8 +922,7 @@ uint32_t heRlmFillNANHECapIE(
 #if (CFG_RX_PPE_THRESHOLD == 1)
 	uint8_t *pPPEThreshold;
 #endif
-	uint8_t ucSupportedNss =
-		wlanGetSupportNss(prAdapter, prBssInfo->ucBssIndex) - 1;
+	uint8_t ucSupportedNss = 0;
 	u_int8_t fgTxStbcEn = TRUE;
 
 	struct AIS_FSM_INFO *prAisFsmInfo = NULL;
@@ -937,6 +936,9 @@ uint32_t heRlmFillNANHECapIE(
 		DBGLOG(NAN, ERROR, "prBssInfo error!\n");
 		return 0;
 	}
+
+	ucSupportedNss =
+		wlanGetSupportNss(prAdapter, prBssInfo->ucBssIndex) - 1;
 
 	prHeCap = (struct _IE_HE_CAP_T *)pOutBuf;
 
