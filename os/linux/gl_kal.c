c6283cd21cc36279b3b915aa1bdeb1515926d3d0
Holiday Hao <holiday.hao@mediatek.com>
2024-01-12 22:21:07 +0800

    [ALPS07989572] offload: default enable RRO & MAWD on FPGA
    default enable RRO & MAWD on FPGA
    
    MTK-Commit-Id: 17afe2885f59c62f71dc867d9d7464ff2bd1908e
    
    CR-Id: ALPS07989572
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I20b12c1ee57e9e98d41a3c20adfa4ad3c4c61319
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7219222
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Commit-Check: srv_pis <srv_pis@mediatek.com>
    Test: srv_pf_nep_sanity

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -10923,6 +10923,22 @@ int32_t __weak kalPlatOpsInit(void)
 }
 
 #if (CFG_SUPPORT_HOST_OFFLOAD == 1)
+#if (CFG_MTK_FPGA_PLATFORM == 1)
+u_int8_t __weak kalIsSupportMawd(void)
+{
+	return TRUE;
+}
+
+u_int8_t __weak kalIsSupportSdo(void)
+{
+	return FALSE;
+}
+
+u_int8_t __weak kalIsSupportRro(void)
+{
+	return TRUE;
+}
+#else
 u_int8_t __weak kalIsSupportMawd(void)
 {
 	return FALSE;
@@ -10937,7 +10953,8 @@ u_int8_t __weak kalIsSupportRro(void)
 {
 	return FALSE;
 }
-#endif
+#endif /* CFG_MTK_FPGA_PLATFORM */
+#endif /* CFG_SUPPORT_HOST_OFFLOAD */
 
 uint32_t __weak kalGetTxBigCpuMask(void)
 {
