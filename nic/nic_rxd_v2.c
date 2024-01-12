920f9097f4de99c212232e94ab25c9264df24c63
Wyatt Sun <wyatt.sun@mediatek.com>
2024-01-12 21:42:37 +0800

    [ALPS06149076] Explicit cast to unsigned integer
    Coverity might inspect the problem by integral promotion to integers,
    to fix this problem, use both unsigned integer and explicit cast to
    unsigned integer type.
    
    MTK-Commit-Id: 3880beb0c44e71e4ea555cadd3393cc14050d46f
    
    Change-Id: I92a9ea3bf6ff40718b4ff8f4f9f0e66ed1397ab8
    CR-Id: ALPS06149076
    Feature: Wi-Fi Link Layer Statistics
    Signed-off-by: Wyatt Sun <wyatt.sun@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/5436237
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Reviewed-by: tf.huang <tf.huang@mediatek.com>
    Tested-by: herman.yang <herman.yang@mediatek.com>
    Build: herman.yang <herman.yang@mediatek.com>
    Test: herman.yang <herman.yang@mediatek.com>

--- nic/nic_rxd_v2.c
+++ nic/nic_rxd_v2.c
@@ -181,18 +181,28 @@ uint8_t nic_rxd_v2_get_ofld(
 		(struct HW_MAC_CONNAC2X_RX_DESC *)prRxStatus);
 }
 
+static void updateLinkStatsMpduAc(struct ADAPTER *prAdapter,
+		struct SW_RFB *prSwRfb)
+{
 #if CFG_SUPPORT_LLS
-static const u8 Tid2LinkStatsAc[] = {
-	STATS_LLS_WIFI_AC_BE,
-	STATS_LLS_WIFI_AC_BK,
-	STATS_LLS_WIFI_AC_BK,
-	STATS_LLS_WIFI_AC_BE,
-	STATS_LLS_WIFI_AC_VI,
-	STATS_LLS_WIFI_AC_VI,
-	STATS_LLS_WIFI_AC_VO,
-	STATS_LLS_WIFI_AC_VO,
-};
+	static const uint8_t Tid2LinkStatsAc[] = {
+		STATS_LLS_WIFI_AC_BE,
+		STATS_LLS_WIFI_AC_BK,
+		STATS_LLS_WIFI_AC_BK,
+		STATS_LLS_WIFI_AC_BE,
+		STATS_LLS_WIFI_AC_VI,
+		STATS_LLS_WIFI_AC_VI,
+		STATS_LLS_WIFI_AC_VO,
+		STATS_LLS_WIFI_AC_VO,
+	};
+	uint8_t ac;
+
+	ac = Tid2LinkStatsAc[(uint8_t)(prSwRfb->ucTid & 0x7U)];
+	if (prSwRfb->ucPayloadFormat == RX_PAYLOAD_FORMAT_MSDU ||
+	    prSwRfb->ucPayloadFormat == RX_PAYLOAD_FORMAT_FIRST_SUB_AMSDU)
+		prAdapter->u4RxMpduAc[ac]++;
 #endif
+}
 
 /*----------------------------------------------------------------------------*/
 /*!
@@ -305,12 +315,9 @@ void nic_rxd_v2_fill_rfb(
 		HAL_MAC_CONNAC2X_RX_STATUS_GET_RF_BAND(prRxStatus);
 	prSwRfb->ucTcl =
 		HAL_MAC_CONNAC2X_RX_STATUS_GET_TCL(prRxStatus);
-	/* LLS */
-#if CFG_SUPPORT_LLS
-	if (prSwRfb->ucPayloadFormat == RX_PAYLOAD_FORMAT_MSDU ||
-		prSwRfb->ucPayloadFormat == RX_PAYLOAD_FORMAT_FIRST_SUB_AMSDU)
-		prAdapter->u4RxMpduAc[Tid2LinkStatsAc[prSwRfb->ucTid & 0x7]]++;
-#endif
+
+	updateLinkStatsMpduAc(prAdapter, prSwRfb);
+
 #if 0
 	if (prHifRxHdr->ucReorder &
 	    HIF_RX_HDR_80211_HEADER_FORMAT) {
