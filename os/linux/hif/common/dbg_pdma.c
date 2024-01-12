5aa63287d46393303a7431cbf79ed1ab7726a961
devin.yu <devin.yu@mediatek.com>
2024-01-12 22:27:06 +0800

    [ALPS08292807] change module name to Tx timeout
    To simply identify Tx timeout
    
    MTK-Commit-Id: 3d00ff5eb47f0e10b63d25d3312f0cd04ebbd477
    
    CR-Id: ALPS08292807
    Feature: WAPI
    Change-Id: I33a51646e02d122959925b87368388a061fc28a2
    Signed-off-by: devin.yu <devin.yu@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7914295
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: wilson.lien <wilson.lien@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8013518
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Test: srv_pf_nep_sanity

--- os/linux/hif/common/dbg_pdma.c
+++ os/linux/hif/common/dbg_pdma.c
@@ -693,7 +693,8 @@ static bool halIsTxTimeout(struct ADAPTER *prAdapter, uint32_t *u4Token)
 		prAdapter->u4HifChkFlag |= HIF_DRV_SER;
 
 		if (IS_FEATURE_ENABLED(prWifiVar->ucWarningTxTimeout))
-			kalSendAeeWarning("HAL", "Tx timeout > %ds, Warning\n",
+			kalSendAeeWarning("Tx Timeout",
+				"Tx timeout > %ds, Warning\n",
 				prWifiVar->u4MsduReportTimeoutSerTime);
 		DBGLOG(HAL, INFO, "Timeout > %ds, trigger SER\n",
 		       prWifiVar->u4MsduReportTimeoutSerTime);
