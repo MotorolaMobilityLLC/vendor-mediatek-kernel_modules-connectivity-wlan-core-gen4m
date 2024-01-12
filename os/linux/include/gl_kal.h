45d4d551a361e65c8a4f28b19c7393f146b6adfa
Jo-Ping.Li <Jo-Ping.li@mediatek.com>
2024-01-12 22:09:24 +0800

    [ALPS07500089] wlan: 6835 driver porting
    [Description]
    
    1. add 6835 config in makefile
    2. add mt6835 plat_priv.c
    
    MTK-Commit-Id: 01f8f9aee8f726dfb2dc6ae001131e082d0230b0
    
    CR-Id: ALPS07500089
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ibbc9f15fd612b640733fa9d5c8c5edd27c3d256f
    Signed-off-by: jo-ping.li <jo-ping.li@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/6790423
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    CheckForbiddenKeyword: srv_pis <srv_pis@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    CheckPatch: srv_pis <srv_pis@mediatek.com>
    CheckCRInfo: srv_pis <srv_pis@mediatek.com>
    CheckCourse: srv_pis <srv_pis@mediatek.com>
    CheckSWIP: srv_pis <srv_pis@mediatek.com>

--- os/linux/include/gl_kal.h
+++ os/linux/include/gl_kal.h
@@ -136,6 +136,7 @@ extern struct platform_device *g_prPlatDev;
 
 #define PERF_MON_TP_CONDITION (125000)
 #define PERF_MON_COEX_TP_THRESHOLD (100)
+#define PERF_MON_MCC_TP_THRESHOLD	(50)
 
 /* By wifi.cfg first. If it is not set 1s by default; 100ms on more. */
 #define TX_LATENCY_STATS_UPDATE_INTERVAL (0)
