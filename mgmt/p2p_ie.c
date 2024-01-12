7118ec98b8bab8b751353257b785cb8f0021d995
Zeno Yang <zeno.yang@mediatek.com>
2024-01-12 20:45:26 +0800

    [ALPS03645093] wlan: Fix check patch issues
    [Detail]
    Fix check patch warnings and errors.
    (Patch 4)
    
    [Solution]
    Refine the coding style
    
    MTK-Commit-Id: e4d85504c3a6d2303183424b267ac1558067f91e
    
    Feature: Driver
    CR-Id: ALPS03645093
    Change-Id: I8740b757aa4c27b07ff6499d5dca176b7301cf33
    Signed-off-by: Zeno Yang <zeno.yang@mediatek.com>
    Reviewed-on: http://gerrit.mediatek.inc:8080/1287204
    CheckPatch: Check Patch <srv_checkpatch@mediatek.com>
    Reviewed-by: Boforn Lin <boforn.lin@mediatek.com>
    Reviewed-by: Weihung Su <wh.su@mediatek.com>
    Reviewed-by: Ben Lai <ben.lai@mediatek.com>
    Reviewed-by: George Kuo <george.kuo@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: Eddie Chen <eddie.chen@mediatek.com>
    Test: srv_mspautosanity@mediatek.com

--- mgmt/p2p_ie.c
+++ mgmt/p2p_ie.c
@@ -51,11 +51,15 @@
  *****************************************************************************/
 #include "precomp.h"
 
-uint32_t p2pCalculate_IEForAssocReq(IN struct ADAPTER *prAdapter, IN uint8_t ucBssIndex, IN struct STA_RECORD *prStaRec)
+uint32_t p2pCalculate_IEForAssocReq(IN struct ADAPTER *prAdapter,
+		IN uint8_t ucBssIndex,
+		IN struct STA_RECORD *prStaRec)
 {
-	struct P2P_ROLE_FSM_INFO *prP2pRoleFsmInfo = (struct P2P_ROLE_FSM_INFO *) NULL;
+	struct P2P_ROLE_FSM_INFO *prP2pRoleFsmInfo =
+		(struct P2P_ROLE_FSM_INFO *) NULL;
 	struct BSS_INFO *prP2pBssInfo = (struct BSS_INFO *) NULL;
-	struct P2P_CONNECTION_REQ_INFO *prConnReqInfo = (struct P2P_CONNECTION_REQ_INFO *) NULL;
+	struct P2P_CONNECTION_REQ_INFO *prConnReqInfo =
+		(struct P2P_CONNECTION_REQ_INFO *) NULL;
 	uint32_t u4RetValue = 0;
 
 	do {
@@ -63,7 +67,9 @@ uint32_t p2pCalculate_IEForAssocReq(IN struct ADAPTER *prAdapter, IN uint8_t ucB
 
 		prP2pBssInfo = GET_BSS_INFO_BY_INDEX(prAdapter, ucBssIndex);
 
-		prP2pRoleFsmInfo = P2P_ROLE_INDEX_2_ROLE_FSM_INFO(prAdapter, (uint8_t) prP2pBssInfo->u4PrivateData);
+		prP2pRoleFsmInfo =
+			P2P_ROLE_INDEX_2_ROLE_FSM_INFO(prAdapter,
+				(uint8_t) prP2pBssInfo->u4PrivateData);
 
 		prConnReqInfo = &(prP2pRoleFsmInfo->rConnReqInfo);
 
@@ -73,14 +79,16 @@ uint32_t p2pCalculate_IEForAssocReq(IN struct ADAPTER *prAdapter, IN uint8_t ucB
 		u4RetValue += (ELEM_HDR_LEN + ELEM_MAX_LEN_WMM_INFO);
 
 		/* ADD HT Capability */
-		if ((prAdapter->rWifiVar.ucAvailablePhyTypeSet & PHY_TYPE_SET_802_11N) &&
-		    (prStaRec->ucPhyTypeSet & PHY_TYPE_SET_802_11N)) {
+		if ((prAdapter->rWifiVar.ucAvailablePhyTypeSet
+			& PHY_TYPE_SET_802_11N)
+			&& (prStaRec->ucPhyTypeSet & PHY_TYPE_SET_802_11N)) {
 			u4RetValue += (ELEM_HDR_LEN + ELEM_MAX_LEN_HT_CAP);
 		}
 #if CFG_SUPPORT_802_11AC
 		/* ADD VHT Capability */
-		if ((prAdapter->rWifiVar.ucAvailablePhyTypeSet & PHY_TYPE_SET_802_11AC) &&
-		    (prStaRec->ucPhyTypeSet & PHY_TYPE_SET_802_11AC)) {
+		if ((prAdapter->rWifiVar.ucAvailablePhyTypeSet
+			& PHY_TYPE_SET_802_11AC)
+			&& (prStaRec->ucPhyTypeSet & PHY_TYPE_SET_802_11AC)) {
 			u4RetValue += (ELEM_HDR_LEN + ELEM_MAX_LEN_VHT_CAP);
 		}
 #endif
@@ -96,32 +104,41 @@ uint32_t p2pCalculate_IEForAssocReq(IN struct ADAPTER *prAdapter, IN uint8_t ucB
 
 /*----------------------------------------------------------------------------*/
 /*!
-* @brief This function is used to generate P2P IE for Beacon frame.
-*
-* @param[in] prMsduInfo             Pointer to the composed MSDU_INFO_T.
-*
-* @return none
-*/
+ * @brief This function is used to generate P2P IE for Beacon frame.
+ *
+ * @param[in] prMsduInfo             Pointer to the composed MSDU_INFO_T.
+ *
+ * @return none
+ */
 /*----------------------------------------------------------------------------*/
-void p2pGenerate_IEForAssocReq(IN struct ADAPTER *prAdapter, IN struct MSDU_INFO *prMsduInfo)
+void p2pGenerate_IEForAssocReq(IN struct ADAPTER *prAdapter,
+		IN struct MSDU_INFO *prMsduInfo)
 {
 	struct BSS_INFO *prBssInfo = (struct BSS_INFO *) NULL;
-	struct P2P_ROLE_FSM_INFO *prP2pRoleFsmInfo = (struct P2P_ROLE_FSM_INFO *) NULL;
-	struct P2P_CONNECTION_REQ_INFO *prConnReqInfo = (struct P2P_CONNECTION_REQ_INFO *) NULL;
+	struct P2P_ROLE_FSM_INFO *prP2pRoleFsmInfo =
+		(struct P2P_ROLE_FSM_INFO *) NULL;
+	struct P2P_CONNECTION_REQ_INFO *prConnReqInfo =
+		(struct P2P_CONNECTION_REQ_INFO *) NULL;
 	uint8_t *pucIEBuf = (uint8_t *) NULL;
 
 	do {
 		ASSERT_BREAK((prAdapter != NULL) && (prMsduInfo != NULL));
 
-		prBssInfo = GET_BSS_INFO_BY_INDEX(prAdapter, prMsduInfo->ucBssIndex);
+		prBssInfo =
+			GET_BSS_INFO_BY_INDEX(prAdapter,
+				prMsduInfo->ucBssIndex);
 
-		prP2pRoleFsmInfo = P2P_ROLE_INDEX_2_ROLE_FSM_INFO(prAdapter, (uint8_t) prBssInfo->u4PrivateData);
+		prP2pRoleFsmInfo =
+			P2P_ROLE_INDEX_2_ROLE_FSM_INFO(prAdapter,
+				(uint8_t) prBssInfo->u4PrivateData);
 
 		prConnReqInfo = &(prP2pRoleFsmInfo->rConnReqInfo);
 
-		pucIEBuf = (uint8_t *) ((unsigned long) prMsduInfo->prPacket + (unsigned long) prMsduInfo->u2FrameLength);
+		pucIEBuf = (uint8_t *) ((unsigned long) prMsduInfo->prPacket
+			+ (unsigned long) prMsduInfo->u2FrameLength);
 
-		kalMemCopy(pucIEBuf, prConnReqInfo->aucIEBuf, prConnReqInfo->u4BufLength);
+		kalMemCopy(pucIEBuf, prConnReqInfo->aucIEBuf,
+			prConnReqInfo->u4BufLength);
 
 		prMsduInfo->u2FrameLength += prConnReqInfo->u4BufLength;
 
@@ -147,26 +164,34 @@ void p2pGenerate_IEForAssocReq(IN struct ADAPTER *prAdapter, IN struct MSDU_INFO
 
 uint32_t
 wfdFuncAppendAttriDevInfo(IN struct ADAPTER *prAdapter,
-			  IN u_int8_t fgIsAssocFrame, IN uint16_t *pu2Offset, IN uint8_t *pucBuf, IN uint16_t u2BufSize)
+		IN u_int8_t fgIsAssocFrame,
+		IN uint16_t *pu2Offset,
+		IN uint8_t *pucBuf,
+		IN uint16_t u2BufSize)
 {
 	uint32_t u4AttriLen = 0;
 	uint8_t *pucBuffer = NULL;
-	struct WFD_DEVICE_INFORMATION_IE *prWfdDevInfo = (struct WFD_DEVICE_INFORMATION_IE *) NULL;
-	struct WFD_CFG_SETTINGS *prWfdCfgSettings = (struct WFD_CFG_SETTINGS *) NULL;
+	struct WFD_DEVICE_INFORMATION_IE *prWfdDevInfo =
+		(struct WFD_DEVICE_INFORMATION_IE *) NULL;
+	struct WFD_CFG_SETTINGS *prWfdCfgSettings =
+		(struct WFD_CFG_SETTINGS *) NULL;
 
 	do {
-		ASSERT_BREAK((prAdapter != NULL) && (pucBuf != NULL) && (pu2Offset != NULL));
+		ASSERT_BREAK((prAdapter != NULL)
+			&& (pucBuf != NULL) && (pu2Offset != NULL));
 
 		prWfdCfgSettings = &(prAdapter->rWifiVar.rWfdConfigureSettings);
 
 		ASSERT_BREAK((prWfdCfgSettings != NULL));
 
 		if ((prWfdCfgSettings->ucWfdEnable == 0) ||
-		    ((prWfdCfgSettings->u4WfdFlag & WFD_FLAGS_DEV_INFO_VALID) == 0)) {
+			((prWfdCfgSettings->u4WfdFlag
+			& WFD_FLAGS_DEV_INFO_VALID) == 0)) {
 			break;
 		}
 
-		pucBuffer = (uint8_t *) ((unsigned long) pucBuf + (unsigned long) (*pu2Offset));
+		pucBuffer = (uint8_t *) ((unsigned long) pucBuf
+			+ (unsigned long) (*pu2Offset));
 
 		ASSERT_BREAK(pucBuffer != NULL);
 
@@ -174,13 +199,17 @@ wfdFuncAppendAttriDevInfo(IN struct ADAPTER *prAdapter,
 
 		prWfdDevInfo->ucElemID = WFD_ATTRI_ID_DEV_INFO;
 
-		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2WfdDevInfo, prWfdCfgSettings->u2WfdDevInfo);
+		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2WfdDevInfo,
+			prWfdCfgSettings->u2WfdDevInfo);
 
-		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2SessionMgmtCtrlPort, prWfdCfgSettings->u2WfdControlPort);
+		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2SessionMgmtCtrlPort,
+			prWfdCfgSettings->u2WfdControlPort);
 
-		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2WfdDevMaxSpeed, prWfdCfgSettings->u2WfdMaximumTp);
+		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2WfdDevMaxSpeed,
+			prWfdCfgSettings->u2WfdMaximumTp);
 
-		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2Length, WFD_ATTRI_MAX_LEN_DEV_INFO);
+		WLAN_SET_FIELD_BE16(&prWfdDevInfo->u2Length,
+			WFD_ATTRI_MAX_LEN_DEV_INFO);
 
 		u4AttriLen = WFD_ATTRI_MAX_LEN_DEV_INFO + WFD_ATTRI_HDR_LEN;
 
