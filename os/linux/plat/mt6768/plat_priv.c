6c3c96f9ae8fb24a9418e0f2fda311f6810f8f48
Xiang Wu <xiang.wu@mediatek.com>
2024-02-19 14:58:30 +0800

    [ALPS08644469] wlan: Get reserved size and protect
    [Description]
    1. Get reserved mem size by kernel first.
    2. MPU protect reserved memory when init.
    
    [Test]
    1. WiFi on success.
    
    MTK-Commit-Id: 105e5d0cfab2e9f2b0f58af8be904aa4faa24b96
    
    CR-Id: ALPS08644469
    Change-Id: Ia0280d632572684f68fd3ff8a290ff6e857f4332
    Signed-off-by: Xiang Wu <xiang.wu@mediatek.com>
    Feature: Wi-Fi Driver CONNAC
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8596154
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- os/linux/plat/mt6768/plat_priv.c
+++ os/linux/plat/mt6768/plat_priv.c
@@ -10,8 +10,8 @@
 
 #ifdef CONFIG_WLAN_MTK_EMI
 #include <soc/mediatek/emi.h>
-#define WIFI_EMI_MEM_OFFSET    0x140000
-#define WIFI_EMI_MEM_SIZE      0x130000
+#define DOMAIN_AP	0
+#define DOMAIN_CONN	2
 #endif
 
 #define DEFAULT_CPU_FREQ (0)
@@ -118,22 +118,31 @@ void kalSetEmiMpuProtection(phys_addr_t emiPhyBase, bool enable)
 void kalSetDrvEmiMpuProtection(phys_addr_t emiPhyBase, uint32_t offset,
 			       uint32_t size)
 {
-#if KERNEL_VERSION(6, 0, 0) >= LINUX_VERSION_CODE
-	struct emi_region_info_t region_info;
+#if IS_ENABLED(CONFIG_MTK_EMI_LEGACY)
+	struct emimpu_region_t region;
+	unsigned long long start = emiPhyBase + offset;
+	unsigned long long end = emiPhyBase + offset + size - 1;
+	int ret;
 
-	DBGLOG(INIT, INFO, "emiPhyBase: 0x%x, offset: %u, size: %u\n",
-			emiPhyBase, offset, size);
+	DBGLOG(INIT, INFO, "emiPhyBase: 0x%p, offset: %d, size: %d\n",
+				emiPhyBase, offset, size);
 
-	/*set MPU for EMI share Memory */
-	region_info.start = emiPhyBase + offset;
-	region_info.end = emiPhyBase + offset + size - 1;
-	region_info.region = 29;
-	SET_ACCESS_PERMISSION(region_info.apc, LOCK, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, FORBIDDEN,
-			      FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION,
-			      FORBIDDEN, NO_PROTECTION);
-	emi_mpu_set_protection(&region_info);
+	ret = mtk_emimpu_init_region(&region, 29);
+	if (ret) {
+		DBGLOG(INIT, ERROR, "mtk_emimpu_init_region failed, ret: %d\n",
+				ret);
+		return;
+	}
+	mtk_emimpu_set_addr(&region, start, end);
+	mtk_emimpu_set_apc(&region, DOMAIN_AP, MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_set_apc(&region, DOMAIN_CONN, MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_lock_region(&region, MTK_EMIMPU_LOCK);
+	ret = mtk_emimpu_set_protection(&region);
+	if (ret)
+		DBGLOG(INIT, ERROR,
+			"mtk_emimpu_set_protection failed, ret: %d\n",
+			ret);
+	mtk_emimpu_free_region(&region);
 #endif
 }
 #endif
