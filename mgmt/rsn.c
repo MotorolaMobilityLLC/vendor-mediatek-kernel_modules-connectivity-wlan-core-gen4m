f08543b547c2241a7a411d61df99df5cdb01372f
Jim Chuang <jim.chuang@mediatek.com>
2024-02-27 18:57:54 +0800

    [ALPS08647258] wlan: fix kalIndicateStatusAndComplete KE issue
    1. Napi poll shouldnt sleep in interrupt enable status, so send
       message to handle MIC Failure case in main thread.
    
    MTK-Commit-Id: 481527ae3da650f05c588084b2fd01ff4f04db09
    
    Signed-off-by: Jim Chuang <jim.chuang@mediatek.com>
    CR-Id: ALPS08647258
    Feature: Others
    Change-Id: Id9065a101e8c851bb8e09d09b4bff77c3ed7b77b
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8351103
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: wilson.lien <wilson.lien@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: ks.lim <ks.lim@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    (cherry picked from commit 71d045f366c16cf3fb1de5b76d8866092034285a)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8382678
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8652733

--- mgmt/rsn.c
+++ mgmt/rsn.c
@@ -2451,6 +2451,32 @@ void rsnParserCheckForRSNCCMPPSK(struct ADAPTER *prAdapter,
 }
 #endif
 
+static void rsnMicErrorSendMsg(struct ADAPTER *prAdapter,
+	struct STA_RECORD *prSta, u_int8_t fgFlags)
+{
+	struct MSG_MIC_ERROR *prMicErrorMsg;
+
+	prMicErrorMsg = (struct MSG_MIC_ERROR *) cnmMemAlloc(prAdapter,
+		RAM_TYPE_MSG, sizeof(struct MSG_MIC_ERROR));
+	if (!prMicErrorMsg) {
+		DBGLOG(RSN, WARN, "cnmMemAlloc Fail\n");
+		return;
+	}
+
+	prMicErrorMsg->rMsgHdr.eMsgId = MID_RSN_MIC_FAIL;
+	prMicErrorMsg->prStaRec = prSta;
+	prMicErrorMsg->fgFlags = fgFlags;
+
+	DBGLOG(RSN, LOUD,
+		"Handle Msg eMsgId:%u ucBssidx:%u fgFlags:%u\n",
+		prMicErrorMsg->rMsgHdr.eMsgId,
+		prMicErrorMsg->prStaRec->ucBssIndex,
+		prMicErrorMsg->fgFlags);
+
+	mboxSendMsg(prAdapter, MBOX_ID_0,
+		(struct MSG_HDR *) prMicErrorMsg, MSG_SEND_METHOD_BUF);
+}
+
 /*----------------------------------------------------------------------------*/
 /*!
  * \brief This routine is called to generate an authentication event to NDIS.
@@ -2519,11 +2545,12 @@ void rsnTkipHandleMICFailure(struct ADAPTER *prAdapter,
 	DEBUGFUNC("rsnTkipHandleMICFailure");
 
 #if 1
-	rsnGenMicErrorEvent(prAdapter, prSta, fgErrorKeyType);
+	rsnMicErrorSendMsg(prAdapter, prSta, fgErrorKeyType);
 
 	/* Generate authentication request event. */
 	DBGLOG(RSN, INFO,
-	       "Generate TKIP MIC error event (type: 0%d)\n", fgErrorKeyType);
+	       "Send TKIP MIC error event msg to main thread (type: 0%d)\n",
+	       fgErrorKeyType);
 #else
 	ASSERT(prSta);
 
@@ -2561,6 +2588,25 @@ void rsnTkipHandleMICFailure(struct ADAPTER *prAdapter,
 		     u4RsnaCurrentMICFailTime);
 #endif
 }				/* rsnTkipHandleMICFailure */
+
+void rsnMicErrorHandleMsg(struct ADAPTER *prAdapter,
+		struct MSG_HDR *prMsgHdr)
+{
+	struct MSG_MIC_ERROR *prMicErrorMsg;
+
+	prMicErrorMsg = (struct MSG_MIC_ERROR *)prMsgHdr;
+
+	DBGLOG(RSN, LOUD,
+		"Handle Msg eMsgId:%u ucBssidx:%u fgFlags:%u\n",
+		prMicErrorMsg->rMsgHdr.eMsgId,
+		prMicErrorMsg->prStaRec->ucBssIndex,
+		prMicErrorMsg->fgFlags);
+
+	rsnGenMicErrorEvent(prAdapter, prMicErrorMsg->prStaRec,
+				prMicErrorMsg->fgFlags);
+
+	cnmMemFree(prAdapter, prMsgHdr);
+}
 
 /*----------------------------------------------------------------------------*/
 /*!
