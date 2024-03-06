d1e76d527bbec72476478c92f7b535fa010efded
holiday.hao <holiday.hao@mediatek.com>
2024-03-06 10:10:58 +0800

    [ALPS08612625] SER: support SER no mmio read
    using emi read instead of mmio read register directly
    
    MTK-Commit-Id: 6b67368d31600f2d374b85e8b0d39455b8b1518d
    
    Change-Id: I607191013db90c05308ad55f3b76f9f349578229
    CR-Id: ALPS08612625
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: holiday.hao <holiday.hao@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8627152
    Reviewed-by: danny.shih <danny.shih@mediatek.com>
    Reviewed-by: perry.liao <perry.liao@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Tested-by: perry.liao <perry.liao@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Tested-by: danny.shih <danny.shih@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Test: srv_mspautosanity
    Test: srv_pf_nep_sanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- os/linux/hif/common/hif_mem.c
+++ os/linux/hif/common/hif_mem.c
@@ -595,7 +595,8 @@ int halAllocHifMemForWiFiMisc(struct platform_device *pdev,
 		struct mt66xx_hif_driver_data *prDriverData)
 {
 	struct mt66xx_chip_info *prChipInfo;
-	uint32_t u4idx = 0;
+	struct HIF_MEM *prMem = NULL;
+	uint32_t u4idx = 0, u4Size;
 
 	prChipInfo = prDriverData->chip_info;
 
@@ -610,11 +611,13 @@ int halAllocHifMemForWiFiMisc(struct platform_device *pdev,
 
 	/* alloc all memory blocks in wifi_misc */
 	for (u4idx = 0; u4idx < prChipInfo->rsvMemWiFiMiscSize; u4idx++) {
-		if (!halAllocRsvMem(
-				prChipInfo->rsvMemWiFiMisc[u4idx].size,
-				&prChipInfo->rsvMemWiFiMisc[u4idx].rRsvEmiMem,
-				WIFI_RSV_MEM_WIFI_MISC))
+		prMem = &prChipInfo->rsvMemWiFiMisc[u4idx].rRsvEmiMem;
+		u4Size = prChipInfo->rsvMemWiFiMisc[u4idx].size;
+		if (!halAllocRsvMem(u4Size, prMem, WIFI_RSV_MEM_WIFI_MISC)) {
 			DBGLOG(INIT, ERROR, "RsvEmiMem alloc fail\n");
+			continue;
+		}
+		kalMemZero(prMem->va, u4Size);
 	}
 
 	DBGLOG(INIT, INFO,
