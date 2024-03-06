cdcfb9c24def4489650d0fb277c616e54eb2d45d
Holiday Hao <holiday.hao@mediatek.com>
2024-03-06 10:12:09 +0800

    [ALPS08740548] offload: disable mawd & rro
    disable mawd & rro on mt6991
    
    MTK-Commit-Id: 17e23e264603b96cd27b7b824b30f1bc191317bf
    
    CR-Id: ALPS08740548
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    Change-Id: I6727e9af03e17e636d5a9a32346dd163ef720fc4
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8716450
    Reviewed-by: raymond.kuo <raymond.kuo@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_mspautosanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>

--- os/linux/plat/mt6991/plat_priv.c
+++ os/linux/plat/mt6991/plat_priv.c
@@ -1143,7 +1143,7 @@ int32_t kalGetScpDumpInfo(u64 *addr, unsigned int *size)
 #if (CFG_SUPPORT_HOST_OFFLOAD == 1)
 u_int8_t kalIsSupportMawd(void)
 {
-	return TRUE;
+	return FALSE;
 }
 
 u_int8_t kalIsSupportSdo(void)
@@ -1153,7 +1153,7 @@ u_int8_t kalIsSupportSdo(void)
 
 u_int8_t kalIsSupportRro(void)
 {
-	return TRUE;
+	return FALSE;
 }
 
 uint32_t kalGetMawdVer(void)
