3f6af3d124d2d5e203a3bbc92b6d537410076ee7
perry.liao <perry.liao@mediatek.com>
2024-03-19 09:56:24 +0800

    [ALPS08336214] hif: add AER reason
    Problem Statement:
    AER reason need to open log for analysis
    
    Solution:
    Add AER reason string to reset reason
    
    MTK-Commit-Id: 69d55e2bad0780609d1ef71eea1728a59dd322b8
    
    CR-Id: ALPS08336214
    Feature: [Module]Wi-Fi Driver
    Change-Id: I463ae5f98f09b582326caadef84b0a9b8aaa557c
    Signed-off-by: perry.liao <perry.liao@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/7966231
    Test: srv_pf_nep_sanity
    Reviewed-by: wilson.lien <wilson.lien@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit fa20f7e2da00c19cf818cbe4100228c675e1566e)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8739945
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- chips/mt6653/mt6653.c
+++ chips/mt6653/mt6653.c
@@ -3356,7 +3356,7 @@ static u_int8_t mt6653DumpPcieDateFlowStatus(struct GLUE_INFO *prGlueInfo)
 
 		/*1. read pcie cfg.space 0x488 // Readable check*/
 		pci_read_config_dword(pci_dev, 0x488, &u4RegVal[1]);
-		if ((u4RegVal[1] & 0x3c11) != 0x3c11) {
+		if ((u4RegVal[1] & 0x3811) != 0x3811) {
 			pci_read_config_dword(pci_dev, 0x48C, &u4RegVal[2]);
 			DBGLOG(HAL, INFO,
 				"Cb_infra bus fatal error and un-readble 0x488=0x%08x 0x48C=0x%08x\n",
