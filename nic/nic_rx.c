468286b093849e27311ee3cd472f09b520ae1f67
Jim Chuang <jim.chuang@mediatek.com>
2024-04-01 20:30:47 +0800

    [ALPS08815634] nic: rx mgmt packet length check
    [Description]
    1. Check if the rx mgmt packet length is valid.
    
    [Test]
    Sanity test pass
    
    MTK-Commit-Id: 951b1558c88eb7754ac206ec6ce738384d1768e7
    
    Signed-off-by: Jim Chuang <jim.chuang@mediatek.com>
    CR-Id: ALPS08815634
    Feature: Vulnerability Scan
    Change-Id: I5980ed696094a520d2bd8b61f5ad5db3bb21ad63
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8824040
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: devin.yu <devin.yu@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_mspautosanity
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- nic/nic_rx.c
+++ nic/nic_rx.c
@@ -2272,6 +2272,17 @@ void nicRxProcessMgmtPacket(struct ADAPTER *prAdapter,
 		return;
 	}
 
+	if (prSwRfb->u2HeaderLen < sizeof(struct WLAN_MAC_HEADER)
+		|| prSwRfb->u2PacketLen < prSwRfb->u2HeaderLen
+		|| prSwRfb->u2PacketLen > RX_GET_PACKET_MAX_SIZE(prAdapter)) {
+		DBGLOG(RX, WARN,
+			"Mgmt packet length check fail! length[H,P]:%u,%u\n",
+			prSwRfb->u2HeaderLen, prSwRfb->u2PacketLen);
+		RX_INC_CNT(&prAdapter->rRxCtrl, RX_DROP_TOTAL_COUNT);
+		nicRxReturnRFB(prAdapter, prSwRfb);
+		return;
+	}
+
 #if CFG_WIFI_SW_CIPHER_MISMATCH
 	prWlanHeader = (struct WLAN_MAC_HEADER *) prSwRfb->pvHeader;
 #endif
