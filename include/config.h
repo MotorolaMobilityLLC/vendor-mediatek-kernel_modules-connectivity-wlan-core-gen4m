27977699a560cdc5cb0f4d08a41d0d2b5b008e34
Rachel Tseng <Rachel.Tseng@mediatek.com>
2024-04-15 13:22:48 +0800

    [ALPS08819714] wlan: skip H2E check for mobile platform
    [Description]
    Skip WIFI7 WPA3 H2E check for mobile platform.
    
    [Test]
    WIFI7 WPA3 H2E and non-H2E AP test.
    
    MTK-Commit-Id: d98831e2e744bad1d76347400737cd14ee4804b7
    
    Change-Id: I1c66b363f1e23c04331713b01066bb2c32bfaf0e
    CR-Id: ALPS08819714
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <rachel.tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8878362
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_mspautosanity
    Test: srv_pf_nep_sanity
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- include/config.h
+++ include/config.h
@@ -2866,6 +2866,12 @@
 #define CFG_WIFI_IGTK_GTK_SEPARATE	1
 #endif
 
+#if (CFG_MTK_ANDROID_WMT == 1)
+#define CFG_WIFI_EHT_H2E_CHK		0
+#else
+#define CFG_WIFI_EHT_H2E_CHK		1
+#endif
+
 /*------------------------------------------------------------------------------
  * Flags of Retained RX DATA Indicated Buffer
  *------------------------------------------------------------------------------
