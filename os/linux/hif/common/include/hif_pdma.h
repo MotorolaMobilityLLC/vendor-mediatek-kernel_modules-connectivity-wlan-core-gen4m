005d91f1a8e22fb531c8ad1aa2cf56fa7d75a8f8
Holiday Hao <holiday.hao@mediatek.com>
2024-04-26 21:07:22 +0800

    [ALPS08612625] hif: avoid process empty interrupt
    check wfdma rx ring is not empty before process interrupt
    
    MTK-Commit-Id: 4b2f363449d49339058010dd9157060e5bf59840
    
    CR-Id: ALPS08612625
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    Change-Id: I9b78c3fe6bc6ae89eca7fe0084ef64f40b7e8a41
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/8944440
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: soul.huang <soul.huang@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- os/linux/hif/common/include/hif_pdma.h
+++ os/linux/hif/common/include/hif_pdma.h
@@ -1217,6 +1217,7 @@ void halWpdmaProcessCmdDmaDone(struct GLUE_INFO *prGlueInfo,
 void halWpdmaProcessDataDmaDone(struct GLUE_INFO *prGlueInfo,
 				uint16_t u2Port);
 u_int8_t halIsWfdmaRxRingReady(struct GLUE_INFO *prGlueInfo, uint8_t ucRingNum);
+u_int8_t halIsWfdmaRxRingsEmpty(struct GLUE_INFO *prGlueInfo);
 uint32_t halWpdmaGetRxDmaDoneCnt(struct GLUE_INFO *prGlueInfo,
 				 uint8_t ucRingNum);
 uint32_t halGetWfdmaRxCnt(struct ADAPTER *prAdapter);
