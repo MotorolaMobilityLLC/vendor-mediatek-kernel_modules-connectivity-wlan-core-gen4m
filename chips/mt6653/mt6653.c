845d0329bf67e0f8b1f1e50699d683d93a3fb8ff
perry.liao <perry.liao@mediatek.com>
2024-05-13 14:47:23 +0800

    [ALPS08925691] mddp: set MdDrvOwnTimeoutTime to 2s
    Problem Statement:
    md driver own timeout
    
    Solution:
    Adjust md driver own timeout time back to 2 sec
    
    MTK-Commit-Id: 3feb30a76aa4950f550e3491aa368e7e42595e1a
    
    CR-Id: ALPS08925691
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: perry.liao <perry.liao@mediatek.com>
    Change-Id: Id4b556af75fe4563fb9101cf02a7e7cc8309dedf
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9026081
    Test: srv_pf_nep_sanity
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: maker.wang <maker.wang@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- chips/mt6653/mt6653.c
+++ chips/mt6653/mt6653.c
@@ -1218,7 +1218,7 @@ struct mt66xx_chip_info mt66xx_chip_info_mt6653 = {
 	.isSupportMddpAOR = false,
 	.isSupportMddpSHM = true,
 	.u4MdLpctlAddr = CONN_HOST_CSR_TOP_WF_MD_LPCTL_ADDR,
-	.u4MdDrvOwnTimeoutTime = 1500,
+	.u4MdDrvOwnTimeoutTime = 2000,
 #else
 	.isSupportMddpAOR = false,
 	.isSupportMddpSHM = false,
