7fd0126ec86b36a716f979ab866685d265e09c2f
Jake Chung <jake.chung@mediatek.com>
2024-05-28 10:21:31 +0800

    [ALPS08959154] CNM: Add null check
    [Description]
    Add null check
    
    [Test]
    build pass
    
    MTK-Commit-Id: f7943ad907e907a26a2e7cbd75a07b9d6b6d2f5b
    
    Change-Id: I28d28e26783ebe6056c8e788944fbb6920eee7cf
    CR-Id: ALPS08959154
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Jake Chung <jake.chung@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9107202
    Test: srv_pf_nep_sanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: fred.shen <fred.shen@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- mgmt/cnm.c
+++ mgmt/cnm.c
@@ -5048,9 +5048,15 @@ cnmGetOtherSapBssInfo(
 	if (!prAdapter)
 		return NULL;
 
+	if (!prSapBssInfo) {
+		DBGLOG(P2P, WARN, "prSapBssInfo is null");
+		return NULL;
+	}
+
 	for (i = 0; i < prAdapter->ucSwBssIdNum; i++) {
 		prBssInfo = prAdapter->aprBssInfo[i];
-		if ((prSapBssInfo != prBssInfo) &&
+		if (!prBssInfo &&
+			(prSapBssInfo != prBssInfo) &&
 			IS_BSS_P2P(prBssInfo) &&
 			p2pFuncIsAPMode(
 			prAdapter->rWifiVar.prP2PConnSettings
