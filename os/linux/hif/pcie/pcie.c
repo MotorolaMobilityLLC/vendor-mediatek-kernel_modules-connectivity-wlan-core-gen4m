49a9bf327b756d4b1869870bb59370c93d12dddc
mtk04434 <ilex.lin@mediatek.com>
2024-06-12 11:35:45 +0800

    [ALPS08992900] Fix coverity
    [Description]
       Fix coverity 19041545
    
    [Test]
       UT/IT
    
    MTK-Commit-Id: 0284afcc9b5f1be84672662af563bfada5c261e0
    
    CR-Id: ALPS08992900
    Feature: MD Direct Data Path-WiFi hotspot
    Signed-off-by: ilex.lin <ilex.lin@mediatek.com>
    Change-Id: I2a4f1f3e09bcd2b8de17bf99e3dffa36dacd4096
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9155430
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>

--- os/linux/hif/pcie/pcie.c
+++ os/linux/hif/pcie/pcie.c
@@ -3170,6 +3170,9 @@ irqreturn_t pcie_gen_switch_thread_handler(int irq, void *dev_instance)
 			DBGLOG(INIT, ERROR, "prAdapter is NULL.\n");
 			return IRQ_HANDLED;
 		}
+	} else {
+		DBGLOG(INIT, ERROR, "prGlueInfo is NULL.\n");
+		return IRQ_HANDLED;
 	}
 	prAdapter->fgIsGenSwitchProcessing = TRUE;
 
