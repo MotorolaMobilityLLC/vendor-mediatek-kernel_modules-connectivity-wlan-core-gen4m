bbd553a1e332b9721e4cce04f24ec459b2a67953
Jake Chung <jake.chung@mediatek.com>
2024-06-12 11:36:46 +0800

    [ALPS08974258] CNM: fix coverity
    [Description]
    fix coverity
    
    [Test]
    build pass
    
    MTK-Commit-Id: 1e5cd3128d3c1ed7cd3065c59ead5d8fa26a74ef
    
    Change-Id: If8c762f3b833e9b0e3b8c2cafea24f1a99b2776b
    CR-Id: ALPS08974258
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Jake Chung <jake.chung@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9180425
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: fred.shen <fred.shen@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>

--- mgmt/cnm.c
+++ mgmt/cnm.c
@@ -2188,7 +2188,6 @@ uint8_t cnmGetDbdcBwCapability(struct ADAPTER *prAdapter, uint8_t ucBssIndex)
 		eCurrMaxIdx = prBssOpCtrl->rRunning.eRunReq;
 		if (eCurrMaxIdx >= 0 &&
 			eCurrMaxIdx <= CNM_OPMODE_REQ_MAX_CAP &&
-			prBssOpCtrl->rRunning.eReqIdx >= 0 &&
 			prBssOpCtrl->rRunning.eReqIdx <=
 			CNM_OPMODE_REQ_MAX_CAP) {
 			DBGLOG(CNM, INFO,
