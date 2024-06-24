79e8e94c9557cd8f90eb25a4ddb9f50b20ebd692
Jo-Ping.Li <Jo-Ping.li@mediatek.com>
2024-06-24 11:27:35 +0800

    [ALPS09063722] move flag from L2 to mcu init
    [Description]
    move flag from L2 to mcu init
    
    [Test]
    UT
    
    MTK-Commit-Id: 732be4702516ca86a180c375ebb953d757389b5e
    
    CR-Id: ALPS09063722
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: "Jo-Ping.Li" <Jo-Ping.li@mediatek.com>
    Change-Id: Ib90bb2af1ca89d433b308d9185b8499bfe114a46
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9239655
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Tested-by: jo-ping.li <jo-ping.li@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Reviewed-by: jo-ping.li <jo-ping.li@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- chips/mt6653/mt6653.c
+++ chips/mt6653/mt6653.c
@@ -4227,6 +4227,10 @@ static uint32_t mt6653_mcu_init(struct ADAPTER *ad)
 	struct mt66xx_chip_info *prChipInfo = NULL;
 	struct CHIP_DBG_OPS *prDbgOps = NULL;
 
+#if CFG_MTK_WIFI_PCIE_SR
+	fgIsL2Finished = FALSE;
+#endif
+
 	if (!ad) {
 		DBGLOG(INIT, ERROR, "NULL ADAPTER.\n");
 		rStatus = WLAN_STATUS_FAILURE;
@@ -4566,7 +4570,6 @@ static uint32_t mt6653_wlanDownloadPatch(struct ADAPTER *prAdapter)
 		wifi_coredump_set_enable(TRUE);
 
 #if CFG_MTK_WIFI_PCIE_SR
-		fgIsL2Finished = FALSE;
 		/* enter -> keep 100ms -> exit L2 for enabling PCIE SR */
 		if (kalIsSupportPcieL2()) {
 			HAL_MCR_WR(prAdapter,
