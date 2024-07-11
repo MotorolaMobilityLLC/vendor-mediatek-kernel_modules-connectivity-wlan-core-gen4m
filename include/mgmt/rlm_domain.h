1526b59e95bf92bb041e98a088c0b9fa8da96c30
Eddie Lee <eddie-yt.lee@mediatek.com>
2024-07-11 09:29:07 +0800

    [ALPS09098589] wifi: Modify dynamic txpower chl setting memory allocate
    [Description]
      1. Modify dynamic txpower chl setting memory allocate
    
    [Test]
      RD UT/IT pass
    
    MTK-Commit-Id: c97f276291c0d4d5be94492c42a0cc7fe0fb47ed
    
    Feature: CONNAC Driver
    Signed-off-by: Eddie Lee <eddie-yt.lee@mediatek.com>
    CR-Id: ALPS09098589
    Change-Id: I7e6517bcb40df94aeb8b67c50b44fa84f8a654c3
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9301468
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: ian-cc.chuang <ian-cc.chuang@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- include/mgmt/rlm_domain.h
+++ include/mgmt/rlm_domain.h
@@ -866,7 +866,7 @@ struct TX_PWR_CTRL_ELEMENT {
 #if CFG_SUPPORT_DYNAMIC_PWR_LIMIT_ANT_TAG
 	struct TX_PWR_CTRL_ANT_SETTING aiPwrAnt[POWER_ANT_TAG_NUM];
 #endif
-	struct TX_PWR_CTRL_CHANNEL_SETTING rChlSettingList[1];
+	struct TX_PWR_CTRL_CHANNEL_SETTING rChlSettingList[];
 	/* always keep it the last one. */
 };
 
