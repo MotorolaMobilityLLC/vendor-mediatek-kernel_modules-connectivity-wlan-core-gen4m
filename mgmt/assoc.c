f4bcc64023a4d581bf7531e6a0a9ef924b67b4ef
Sticky Chen <sticky.chen@mediatek.com>
2024-07-23 12:42:09 +0800

    [ALPS09108367] Fix FT-9.2.1 RSNXE check
    [Description]
    For FT initial conneciton, auth alg is SAE and RSNXE should be present
    in association request; for FT roaming, auth alg is FT and RSNXE should
    not be present. Adjust skip RSNXE checking for it.
    
    [Test]
    FT-9.2.1
    
    MTK-Commit-Id: 2e826dfbc1876b72f89c6e74dcaa70eb2532b23e
    
    CR-Id: ALPS09108367
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Sticky Chen <sticky.chen@mediatek.com>
    Change-Id: I35af04b73d39ae63285a28cf021a8af638d2845c
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9338886
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>

--- mgmt/assoc.c
+++ mgmt/assoc.c
@@ -794,11 +794,10 @@ static uint8_t assocSkipRSNXIe(struct ADAPTER *prAdapter,
 			aisGetTargetBssDesc(prAdapter, ucBssIndex);
 
 		/* skip rsnxe if target ap doesn't support rsnxe */
-		if (prTargetBss && !prTargetBss->fgIERSNX) {
-			DBGLOG(SAA, LOUD,
-				"Skip RSNXE if AP unsupported");
+		if ((prStaRec->ucAuthAlgNum == AUTH_ALGORITHM_NUM_FT ||
+		    prStaRec->ucAuthAlgNum == AUTH_ALGORITHM_NUM_OPEN_SYSTEM)
+		    && prTargetBss && !prTargetBss->fgIERSNX)
 			return TRUE;
-		}
 	}
 
 	return FALSE;
