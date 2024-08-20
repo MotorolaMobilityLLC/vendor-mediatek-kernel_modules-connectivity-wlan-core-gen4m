ceef36f33cb19126b94e0f712c0f0557628cbe92
mtk09010 <johnny.chiang@mediatek.com>
2024-08-20 09:01:44 +0800

    [ALPS09267009] sap: fix connac 2 sap csa issue
    [Description]
    fix connac 2 sap csa issue
    
    [Test]
    RD UTIT/ Preflight
    
    MTK-Commit-Id: 69db35989a1182733df2e1a9c4d7ab19fd8eaa44
    
    CR-Id: ALPS09267009
    Feature: Wi-Fi Driver
    Signed-off-by: mtk09010 <johnny.chiang@mediatek.com>
    Change-Id: Ifc40d4007f5235afc2ac400cb565db6dc034aeef
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9506977
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Test: srv_pf_nep_sanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- mgmt/p2p_func.c
+++ mgmt/p2p_func.c
@@ -8327,7 +8327,7 @@ void p2pUserPrefChFilter(struct ADAPTER *prAdapter,
 	ucNumAliveNonSapBss = cnmGetAliveNonSapBssInfo(
 						prAdapter, aliveNonSapBss);
 #if (CFG_SUPPORT_CONNAC1X || CFG_SUPPORT_CONNAC2X)
-	/*same hw index scc*/
+	/*same rf band scc*/
 	for (i = *ucChSwitchCandNum; i > 0 ; i--) {
 		for (j = 0; j < ucNumAliveNonSapBss ; j++) {
 			if (prSapSwitchCand[i-1].eRfBand ==
@@ -8413,7 +8413,24 @@ void p2pSetDefaultFilter(struct ADAPTER *prAdapter,
 		enum ENUM_P2P_FILTER_SCENARIO_TYPE eFilterScnario)
 {
 	uint8_t j;
-
+#if (CFG_SUPPORT_CONNAC1X || CFG_SUPPORT_CONNAC2X)
+	for (j = 0; j < *ucChSwithCandNum; j++) {
+		if ((prSapSwitchCand[j].ucChLowerBound <=
+			prP2pBssInfo->ucPrimaryChannel &&
+			prSapSwitchCand[j].ucChUpperBound >=
+			prP2pBssInfo->ucPrimaryChannel) &&
+			prSapSwitchCand[j].eRfBand ==
+			prP2pBssInfo->eBand) {
+			prSapSwitchCand[j].ucChLowerBound =
+				prP2pBssInfo->ucPrimaryChannel;
+			prSapSwitchCand[j].ucChUpperBound =
+				prP2pBssInfo->ucPrimaryChannel;
+			*ucChSwithCandNum = 1;
+			prSapSwitchCand[0] =
+				prSapSwitchCand[j];
+		}
+	}
+#else
 	for (j = 0; j < *ucChSwithCandNum; j++) {
 		if ((prSapSwitchCand[j].ucChLowerBound <=
 			prP2pBssInfo->ucPrimaryChannel &&
@@ -8432,7 +8449,7 @@ void p2pSetDefaultFilter(struct ADAPTER *prAdapter,
 				prSapSwitchCand[j];
 		}
 	}
-
+#endif
 	for (j = 0; j < *ucChSwithCandNum; j++) {
 		if ((prSapSwitchCand[j].ucChLowerBound !=
 			prSapSwitchCand[j].ucChUpperBound) &&
