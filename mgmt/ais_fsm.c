09ff513a22d93887c3389381fe6487dc99ef4cf3
Rachel Tseng <Rachel.Tseng@mediatek.com>
2024-08-20 09:01:44 +0800

    [ALPS09252650] roam: prevent updating illegal BSS info
    [Description]
    Prevent updateing illegal BSS information set from driver
    to FW while roaming.
    
    [Test]
    STA + SAP concurrency, test STA roaming.
    
    MTK-Commit-Id: d7b9121347c0dec654624d0efab0aa5180c77e82
    
    Change-Id: I576c90f20f0b1a20de06f0e02d2a27246bbcc874
    CR-Id: ALPS09252650
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <rachel.tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9497337
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>

--- mgmt/ais_fsm.c
+++ mgmt/ais_fsm.c
@@ -7443,8 +7443,6 @@ void aisFsmRunEventRoamingRoam(struct ADAPTER *prAdapter, uint8_t ucBssIndex)
 		return;
 	}
 
-	aisFillBssInfoFromBssDesc(prAdapter, ais, set);
-
 	ais->ucConnTrialCount++;
 	ais->fgTargetChnlScanIssued = FALSE;
 	ais->ucIsStaRoaming = TRUE;
@@ -7454,6 +7452,8 @@ void aisFsmRunEventRoamingRoam(struct ADAPTER *prAdapter, uint8_t ucBssIndex)
 	ais->ucMlProbeEnable = FALSE;
 	ais->prMlProbeBssDesc = NULL;
 #endif
+
+	aisFillBssInfoFromBssDesc(prAdapter, ais, set);
 
 #if CFG_EXT_ROAMING_WTC
 	aisWtcSearchHandleBssDesc(
