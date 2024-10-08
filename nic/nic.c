825b549f957912299a49edebfa60ead3adc85fcd
Herman Yang <herman.yang@mediatek.com>
2024-10-08 09:30:24 +0800

    [ALPS09348110] wlan: revise cmd and tag to fw
    [Description]
    Revise cmd or tag for connection.
    
    [Test]
    Conn/Roaming.
    
    MTK-Commit-Id: 52e5be6d2b93d50c721922ab339a5a749e24a768
    
    Signed-off-by: Herman Yang <herman.yang@mediatek.com>
    CR-Id: ALPS09348110
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I03e3901a0c272b00601029a51d1191b5b264fded
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9521041
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: jim.chuang <jim.chuang@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>

--- nic/nic.c
+++ nic/nic.c
@@ -2796,8 +2796,9 @@ uint32_t nicUpdateBssEx(struct ADAPTER *prAdapter,
 				}
 			}
 			/*Send Event  to Enable/Disable SG*/
-			wlandioSetSGStatus(prAdapter,
-			ucSGEnable, 0xFF, ucDutNss);
+			if (IS_FEATURE_ENABLED(prAdapter->rWifiVar.ucSGCfg))
+				wlandioSetSGStatus(prAdapter,	ucSGEnable,
+						   0xFF, ucDutNss);
 		}
 #endif
 	}
