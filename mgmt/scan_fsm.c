39fad331599ba2546333779c22b32c661768a1b7
TS Hsu <ts.hsu@mediatek.com>
2024-10-08 09:30:23 +0800

    [ALPS08234751] PNO scan done only deactivate network when AIS is IDLE
    [Description]
    When PNO scan done, only deactivate network when AIS is IDLE.
    Otherwise, deactivate network will cause connection fail if
    connection is on-going.
    
    [Test]
    Pass
    
    MTK-Commit-Id: b715af6a162a827ddb5327ea20900544ea00f7c6
    
    CR-Id: ALPS08234751
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: TS Hsu <ts.hsu@mediatek.com>
    Change-Id: I3b685bc0cc47337ee0244dc53d861b0273e4fbf2
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9376298
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Reviewed-by: zeno.yang <zeno.yang@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit 08635fe42d1c11f1872c5338ff2b3fcdf3948c1b)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9391839
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    (cherry picked from commit 9223641dcbc49ff8cd613cd80266890b1748851a)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9568028
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>

--- mgmt/scan_fsm.c
+++ mgmt/scan_fsm.c
@@ -1925,22 +1925,15 @@ scnFsmSchedScanRequest(struct ADAPTER *prAdapter,
 u_int8_t scnFsmSchedScanStopRequest(struct ADAPTER *prAdapter)
 {
 	uint8_t ucBssIndex = 0;
-	struct BSS_INFO *prAisBssInfo;
+	struct AIS_FSM_INFO *prAisFsmInfo;
 	struct SCAN_INFO *prScanInfo;
 
 	ASSERT(prAdapter);
 
 	ucBssIndex =
 		prAdapter->rWifiVar.rScanInfo.rSchedScanParam.ucBssIndex;
-	prAisBssInfo = aisGetAisBssInfo(prAdapter, ucBssIndex);
+	prAisFsmInfo = aisGetAisFsmInfo(prAdapter, ucBssIndex);
 	prScanInfo = &(prAdapter->rWifiVar.rScanInfo);
-
-	if (prAisBssInfo == NULL) {
-		log_dbg(SCN, WARN,
-			"prAisBssInfo%d is NULL\n",
-			ucBssIndex);
-		return FALSE;
-	}
 
 	if (!scnFsmSchedScanSetAction(prAdapter, SCHED_SCAN_ACT_DISABLE)) {
 		log_dbg(SCN, TRACE, "scnFsmSchedScanSetAction failed\n");
@@ -1950,7 +1943,7 @@ u_int8_t scnFsmSchedScanStopRequest(struct ADAPTER *prAdapter)
 	prAdapter->rWifiVar.rScanInfo.fgSchedScanning = FALSE;
 
 	/* Deactivate network when not connected and not in normal scan */
-	if (prAisBssInfo->eConnectionState == MEDIA_STATE_DISCONNECTED &&
+	if (prAisFsmInfo->eCurrentState == AIS_STATE_IDLE &&
 		prScanInfo->eCurrentState == SCAN_STATE_IDLE) {
 		UNSET_NET_ACTIVE(prAdapter, ucBssIndex);
 		nicDeactivateNetwork(prAdapter, ucBssIndex);
