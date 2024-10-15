f38caf2f1d06bb1740c08e9b9b86f0aee5a2e361
chris.hong <chris.hong@mediatek.com>
2024-10-15 11:40:30 +0800

    [ALPS09423164] wlan: fix variable type
    [Description]
    A retry count with value 300 needs u32 instead of u8 to store.
    
    [Test]
    wifi on/off/reboot
    
    MTK-Commit-Id: 79af0d16f2491260e2c34cb39abd09c6123b43ff
    
    Signed-off-by: chris.hong <chris.hong@mediatek.com>
    CR-Id: ALPS09423164
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I22e954bf1f115783d30fc59b56aa2b57259716bf
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9720545
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: ben.lai <ben.lai@mediatek.com>

--- os/linux/gl_init.c
+++ os/linux/gl_init.c
@@ -9226,7 +9226,7 @@ uint8_t kalGetShutdownState(void)
 #if CFG_MTK_ANDROID_WMT && CFG_WIFI_PLAT_SHUTDOWN_SUPPORT
 void wlanShutdown(void)
 {
-	uint8_t ucRetryCount;
+	uint32_t u4RetryCount;
 
 	/* there are two shutdown entry,
 	 * one is pre_fmd and another is platform
@@ -9236,14 +9236,14 @@ void wlanShutdown(void)
 		goto exit;
 	}
 
-	ucRetryCount = 0;
+	u4RetryCount = 0;
 	uShutdownState = SHUTDOWN_STATE_ONGOING;
 	while (kalIsResetOnEnd()) {
 		DBGLOG(REQ, WARN, "wifi driver is resetting\n");
 		kalMsleep(100);
 
-		ucRetryCount++;
-		if (ucRetryCount > 300) {
+		u4RetryCount++;
+		if (u4RetryCount > 300) {
 			DBGLOG(REQ, ERROR,
 				"Reset not finished more than 30s.\n");
 			goto exit;
