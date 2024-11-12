695b5d0ac10a9a213cfc1ce26aa239e0ba70cb06
Holiday Hao <holiday.hao@mediatek.com>
2024-11-12 12:13:03 +0800

    [ALPS09322828] hif: fix KE cause by napi thread trigger EE
    [Description]
    fix KE cause by napi thread trigger EE
    move trigger EE from rx hif napi to hif_thread
    
    [Test]
    build pass
    
    MTK-Commit-Id: 036f1aea5f1b1f1e947d77a85dcb07d982ef6cbd
    
    CR-Id: ALPS09322828
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    Change-Id: I2031e31e867456b0b3a2aedd420704dd65ee2c77
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9853683
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: perry.liao <perry.liao@mediatek.com>

--- os/linux/hif/pcie/include/hif.h
+++ os/linux/hif/pcie/include/hif.h
@@ -379,6 +379,8 @@ struct GL_HIF_INFO {
 	u_int8_t fgEnablePcieCfgDump;
 #endif /* CFG_MTK_WIFI_PCIE_CONFIG_SPACE_ACCESS_DBG */
 	u_int8_t fgIsDebugSopOnGoing;
+
+	u_int8_t fgIsTriggerRxTimeout;
 };
 
 struct BUS_INFO {
