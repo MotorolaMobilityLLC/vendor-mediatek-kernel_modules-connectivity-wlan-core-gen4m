28f46214f4b343d82b440848a3bf54005e4780fc
Jim Chuang <jim.chuang@mediatek.com>
2024-11-19 12:11:16 +0800

    rx: Fix rx packet processing for null packets
    [Description]
    Added a check for null packets in kalProcessRxPacket to prevent
    processing invalid packets.
    
    [Test]
    UT
    
    Signed-off-by: Jim Chuang <jim.chuang@mediatek.com>
    CR-Id: ALPS09984208
    Mot-CRs-Fixed: (CR)
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ie571051be23cd5c0e3209f0f926f8f5f40f5c6b8
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9906456
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit d898c31401ee00675a8d829f786f16c51410f3c4)
    Signed-off-by: Yue Sun <sunyue5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3378301
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhilu Yin <yinzl1@motorola.com>
    Submit-Approved: Jira Key

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -1770,13 +1770,20 @@ kalProcessRxPacket(struct GLUE_INFO *prGlueInfo,
 	uint32_t rStatus = WLAN_STATUS_SUCCESS;
 	struct sk_buff *skb = (struct sk_buff *)pvPacket;
 
+	if (!skb || !pucPacketStart || u4PacketLen == 0) {
+		RX_INC_CNT(&prGlueInfo->prAdapter->rRxCtrl,
+			RX_NULL_PACKET_COUNT);
+		return WLAN_STATUS_FAILURE;
+	}
+
 	skb->data = (unsigned char *)pucPacketStart;
 
 	/* Reset skb */
 	skb_reset_tail_pointer(skb);
 	skb_trim(skb, 0);
 
-	if (skb_tailroom(skb) < 0 || u4PacketLen > skb_tailroom(skb)) {
+	if (skb_tailroom(skb) < 0 || u4PacketLen > skb_tailroom(skb) ||
+		skb->tail > skb->end) {
 		DBGLOG(RX, ERROR,
 #ifdef NET_SKBUFF_DATA_USES_OFFSET
 			"[skb:0x%p][skb->len:%d][skb->protocol:0x%02X] tail:%u, end:%u, data:%p\n",
