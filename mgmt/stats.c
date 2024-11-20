6885a148813ab17c2f0f5631c44ff57450586180
kehaizhou <haizhou.ke@mediatek.com>
2024-11-21 01:37:19 +0800

    [ALPS09502745] mgmt: Fix kernel panic due to hardware watchdog
    [Description]
    Modified debug logging to prevent kernel panic caused by hardware
    watchdog when handling IRQ from wifi module.
    
    [Test]
    UT
    
    MTK-Commit-Id: ee220b7700144a6d16e0274fede3029aa2543ac3
    
    Signed-off-by: kehaizhou <haizhou.ke@mediatek.com>
    CR-Id: ALPS09502745
    Feature: Others
    Change-Id: I67a0b50a587d1e93c7136a685dcd1b8f0e1f7e89
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9912268
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: shuaishuai.kong <shuaishuai.kong@mediatek.com>

--- mgmt/stats.c
+++ mgmt/stats.c
@@ -333,7 +333,11 @@ void statsParseARPInfo(void *pvPacket,
 				IPV4TOSTR(&pucEthBody[ARP_SENDER_IP_OFFSET]));
 		break;
 	case EVENT_TX:
+#if BUILD_QA_DBG
 		DBGLOG(TX, INFO,
+#else
+		DBGLOG_LIMITED(TX, INFO,
+#endif
 			"ARP %s SRC MAC/IP["
 			MACSTR "]/[" IPV4STR "], TAR MAC/IP["
 			MACSTR "]/[" IPV4STR "], SeqNo: %d\n",
