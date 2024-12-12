21f36ceebfbbb5d5ead637cf45cacd1b05ee9cf1
Eddie Lee <eddie-yt.lee@mediatek.com>
2024-12-12 20:46:31 +0800

    [ALPS09228695] wifi: [soc7]Legacy Event support for HW detect report
    [Description]
      1. wifi: Legacy Event support for HW detect report
    
    [Test]
      UT/IT pass
    
    MTK-Commit-Id: ebb50ca0fa61f269bea8ab9e31101b6ac4d75c87
    
    CR-Id: ALPS09228695
    Feature: CONNAC Driver
    Signed-off-by: Eddie Lee <eddie-yt.lee@mediatek.com>
    Change-Id: I7fc63421420f61348ec0ccdb3e790c2217ea5243
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9473709
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: jim.chuang <jim.chuang@mediatek.com>
    Reviewed-by: xumin.xu <xumin.xu@mediatek.com>
    GAI-Code-Review: srv_ai_review001
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_mspautosanity
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- include/nic_cmd_event.h
+++ include/nic_cmd_event.h
@@ -4054,6 +4054,13 @@ struct CMD_UPDATA_LP_PARAM {
 };
 #endif
 
+#if (CFG_HW_DETECT_REPORT == 1)
+struct EVENT_HW_DETECT_REPORT {
+	bool fgIsReportNode;
+	uint8_t aucReserved[3];
+	uint8_t aucStrBuffer[HW_DETECT_REPORT_STR_MAX_LEN];
+};
+#endif
 /*******************************************************************************
  *                            P U B L I C   D A T A
  *******************************************************************************
@@ -4598,6 +4605,10 @@ void nicEventChannelTime(struct ADAPTER *prAdapter,
 	struct WIFI_EVENT *prEvent);
 #endif
 
+#if (CFG_HW_DETECT_REPORT == 1)
+void nicEventHwDetectReport(struct ADAPTER *prAdapter,
+		struct WIFI_EVENT *prEvent);
+#endif
 /*******************************************************************************
  *                              F U N C T I O N S
  *******************************************************************************
