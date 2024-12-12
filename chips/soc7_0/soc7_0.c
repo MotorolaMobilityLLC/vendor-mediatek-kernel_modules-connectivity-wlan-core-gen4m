dfd10e47719aca7ea9799267e854ac768dae0837
Jake Chung <jake.chung@mediatek.com>
2024-12-12 20:53:46 +0800

    [ALPS09355040] WFSYS: add check bus hang log
    [Description]
    add check bus hang log
    
    [Test]
    build pass
    
    MTK-Commit-Id: 7d6720609db56f091225e947f98a7af3980c915f
    
    Change-Id: I92311a236e7237e618043771eed1bb4db00c4106
    CR-Id: ALPS09355040
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Jake Chung <jake.chung@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9629900
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: chris.hong <chris.hong@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Test: srv_pf_nep_sanity
    (cherry picked from commit 53df7f4247ef7051142db5d799fcd612847cd99f)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9664637
    Test: srv_mspautosanity
    GAI-Code-Review: srv_ai_review001
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>

--- chips/soc7_0/soc7_0.c
+++ chips/soc7_0/soc7_0.c
@@ -2658,6 +2658,10 @@ static int soc7_0_CheckBusHang(void *adapter, uint8_t ucWfResetEnable)
 
 	if (prAdapter == NULL)
 		DBGLOG(HAL, INFO, "prAdapter NULL\n");
+	else
+		DBGLOG(HAL, TRACE, " Start, fgIsFwOwn:%d\n",
+			prAdapter->fgIsFwOwn);
+
 	do {
 /*
  * 1. Check "AP2CONN_INFRA ON step is ok"
