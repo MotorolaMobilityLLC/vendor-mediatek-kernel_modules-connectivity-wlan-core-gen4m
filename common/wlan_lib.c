3ebd5bc8e01ceaf49ea92cc65ba82c750e816d5c
Wyatt Sun <wyatt.sun@mediatek.com>
2024-12-12 20:38:01 +0800

    [ALPS08973317] Fix invalid type to printf format specifier
    [Description]
    Argument u4DelayHifToDone & 0x3ffUL to format specifier %03u was
    expected to have type unsigned int but has type unsigned long.
    
    Argument u4DelayHifToDone & 0x3ffUL to format specifier %03u was
    expected to have type unsigned int but has type unsigned long.
    
    [Test]
    Run coverity test.
    
    MTK-Commit-Id: d58602d387d9a503c750a9d33f7a1056e9c9cc0e
    
    CR-Id: ALPS08973317
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I6b3ddb9aec12f97d14089a1f6ae216c40495a81c
    Signed-off-by: Wyatt Sun <wyatt.sun@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9151999
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: deren.wu <deren.wu@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>

--- common/wlan_lib.c
+++ common/wlan_lib.c
@@ -11661,7 +11661,7 @@ uint32_t wlanPktTxDone(struct ADAPTER *prAdapter,
 
 	/* Unit: TU (1024 micro seconds, 1.024 milliseconds) */
 	kalSnprintf(aucDelayInfo, sizeof(aucDelayInfo),
-		    "Xmit~Hif:%u.%03u Hif~Done:%u.%03u",
+		    "Xmit~Hif:%u.%03lu Hif~Done:%u.%03lu",
 		    u4DelayXmitToHif >> 10, u4DelayXmitToHif & BITS(0, 9),
 		    u4DelayHifToDone >> 10, u4DelayHifToDone & BITS(0, 9));
 #endif
