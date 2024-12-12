63a47e0018660c9760d125318210547873d789ef
ch-howard.chang <ch-howard.chang@mediatek.com>
2024-12-12 20:41:58 +0800

    [ALPS09073722] P2P: set_test_mode 2011 then 0 after role created KE
    [Description]
    If set_test_mode 2011 then 0 after role created, the dev wdev0 will be
    set to role wdev0 when wifi on. Then role wdev0 will be free when
    rtnl_unlock, then case use-after-free when the next wifi off want
    to unregister dev0.
    
    [Test]
    RD local.
    
    MTK-Commit-Id: 898ad1dd94e10b10ecce5dbec3377ee0b86d87c5
    
    Signed-off-by: ch-howard.chang <ch-howard.chang@mediatek.com>
    CR-Id: ALPS09073722
    Feature: Wi-Fi Driver
    Change-Id: I89d0022a10557500be3e7a8c54b13260fe6e6290
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9259549
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Reviewed-by: chris.hong <chris.hong@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit c5c954b15515f4f58b7a06e28a8f38411549915b)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9270846
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: brian-kl.lin <brian-kl.lin@mediatek.com>
    Test: srv_mspautosanity
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>

--- os/linux/gl_p2p.c
+++ os/linux/gl_p2p.c
@@ -1297,15 +1297,15 @@ u_int8_t glRegisterP2P(struct GLUE_INFO *prGlueInfo, const char *prDevName,
 				fgIsApMode = TRUE;
 		}
 
-		if (!gprP2pRoleWdev[i])
+		if (!gprP2pWdev[i])
 			glP2pCreateWirelessDevice(prGlueInfo);
 
-		if (!gprP2pRoleWdev[i]) {
-			DBGLOG(P2P, ERROR, "gprP2pRoleWdev[%d] is NULL\n", i);
+		if (!gprP2pWdev[i]) {
+			DBGLOG(P2P, ERROR, "gprP2pWdev[%d] is NULL\n", i);
 			return FALSE;
 		}
 
-		prP2pWdev = gprP2pRoleWdev[i];
+		prP2pWdev = gprP2pWdev[i];
 
 		/* Reset prP2pWdev for the issue that the prP2pWdev doesn't
 		 * reset when the usb unplug/plug.
