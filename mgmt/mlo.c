ab99b9df8c6dc73503b773a3662f3eeda42fc76f
Sticky Chen <sticky.chen@mediatek.com>
2024-12-12 20:38:17 +0800

    [ALPS09014951] Fix Coverity
    [Description]
    Fix Invalid type in argument to printf format specifier: 19117005
    
    [Test]
    Test Coverity ok
    
    MTK-Commit-Id: 49abbfb15f030222fa8d8010d4a91875c9fb752a
    
    CR-Id: ALPS09014951
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Sticky Chen <sticky.chen@mediatek.com>
    Change-Id: Ice14d56ffd6fd9fe23d01522d925305990bd2156
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9177736
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_mspautosanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- mgmt/mlo.c
+++ mgmt/mlo.c
@@ -1831,7 +1831,7 @@ sta:
 
 		if (prIeSta->aucStaInfo + ucStaInfoLen > tail) {
 			DBGLOG(ML, WARN,
-				"invalid STA profile len=%td\n", ucStaInfoLen);
+				"invalid STA profile len=%d\n", ucStaInfoLen);
 			prMlInfo->ucProfNum--;
 			goto next;
 		}
