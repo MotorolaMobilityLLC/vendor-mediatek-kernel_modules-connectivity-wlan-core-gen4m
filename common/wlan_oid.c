cdc68234512493fd2dedfe771dfa54e5e75fc955
mtk30479 <tzu-meng.wang@mediatek.com>
2024-12-12 20:56:00 +0800

    [ALPS09061056] Fix wlanoidsetP2pMode KE
    [Description]
    issue: wlanoidsetP2pMode wait main thread timeout,
           when finally get main thread, prSetP2Ps memory
           is released, cause KE
    
    solution: memcpy prSetP2p instead of using the pointer,
              As there the value of the  pointer is not
              needed outside wlanoidsetP2pMode
    
    [Test]
    build pass
    
    MTK-Commit-Id: 8ec93d311848547a83da9bf74965a7710eda0095
    
    Signed-off-by: mtk30479 <tzu-meng.wang@mediatek.com>
    CR-Id: ALPS09061056
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Icbacb0d8023670cac49e11ae9adb70dec4dc05e9
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9265635
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_pf_nep_sanity
    (cherry picked from commit cec4c75199ac06c5f26b4fc026d988944417a2a6)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9723436
    GAI-Code-Review: srv_ai_review001
    Test: srv_mspautosanity
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: brian-kl.lin <brian-kl.lin@mediatek.com>
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -13906,7 +13906,7 @@ wlanoidSetP2pMode(struct ADAPTER *prAdapter,
 		  void *pvSetBuffer, uint32_t u4SetBufferLen,
 		  uint32_t *pu4SetInfoLen) {
 	uint32_t status = WLAN_STATUS_SUCCESS;
-	struct PARAM_CUSTOM_P2P_SET_WITH_LOCK_STRUCT *prSetP2P = NULL;
+	struct PARAM_CUSTOM_P2P_SET_WITH_LOCK_STRUCT rSetP2P;
 
 	ASSERT(prAdapter);
 	ASSERT(pu4SetInfoLen);
@@ -13918,11 +13918,10 @@ wlanoidSetP2pMode(struct ADAPTER *prAdapter,
 		return WLAN_STATUS_INVALID_LENGTH;
 	}
 
-	prSetP2P = (struct PARAM_CUSTOM_P2P_SET_WITH_LOCK_STRUCT *)
-		   pvSetBuffer;
+	kalMemCopy(&rSetP2P, pvSetBuffer, *pu4SetInfoLen);
 
 	DBGLOG(P2P, TRACE, "Set P2P enable[%d] mode[%d]\n",
-	       prSetP2P->u4Enable, prSetP2P->u4Mode);
+	       rSetP2P.u4Enable, rSetP2P.u4Mode);
 
 	/*
 	 *    enable = 1, mode = 0  => init P2P network
@@ -13936,25 +13935,25 @@ wlanoidSetP2pMode(struct ADAPTER *prAdapter,
 	DBGLOG(P2P, TRACE, "P2P Compile as (%d)p2p-like interface\n",
 	       KAL_P2P_NUM);
 
-	if (prSetP2P->u4Mode >= RUNNING_P2P_MODE_NUM) {
-		DBGLOG(P2P, ERROR, "P2P interface mode(%d) is wrong\n",
-		       prSetP2P->u4Mode);
+	if (rSetP2P.u4Mode >= RUNNING_P2P_MODE_NUM) {
+		DBGLOG(P2P, ERROR, "mode(%d)\n",
+		       rSetP2P.u4Mode);
 		ASSERT(0);
 	}
 
-	if (prSetP2P->u4Enable) {
-		p2pSetMode(prSetP2P->u4Mode);
+	if (rSetP2P.u4Enable) {
+		p2pSetMode(rSetP2P.u4Mode);
 
 		if (p2pLaunch(prAdapter->prGlueInfo)) {
 			/* ToDo:: ASSERT */
 			ASSERT(prAdapter->fgIsP2PRegistered);
 			if (prAdapter->rWifiVar.ucApUapsd
-			    && (prSetP2P->u4Mode != RUNNING_P2P_MODE)) {
+			    && (rSetP2P.u4Mode != RUNNING_P2P_MODE)) {
 				DBGLOG(OID, INFO,
 				       "wlanoidSetP2pMode Default enable ApUapsd\n");
 				setApUapsdEnable(prAdapter, TRUE);
 			}
-			prAdapter->u4P2pMode = prSetP2P->u4Mode;
+			prAdapter->u4P2pMode = rSetP2P.u4Mode;
 		} else {
 			DBGLOG(P2P, ERROR, "P2P Launch Failed\n");
 			status = WLAN_STATUS_FAILURE;
@@ -13963,7 +13962,7 @@ wlanoidSetP2pMode(struct ADAPTER *prAdapter,
 	} else {
 		if (prAdapter->fgIsP2PRegistered)
 			p2pRemove(prAdapter->prGlueInfo,
-				prSetP2P->fgIsRtnlLockAcquired);
+				rSetP2P.fgIsRtnlLockAcquired);
 
 	}
 
