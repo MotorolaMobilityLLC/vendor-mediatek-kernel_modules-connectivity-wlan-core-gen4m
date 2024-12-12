e53ad43d235afc976726b82f55506cf8adf1055a
TS Hsu <ts.hsu@mediatek.com>
2024-12-12 21:04:13 +0800

    [ALPS09453198] Remove channel 14 for JP
    [Description]
    Remove the unsupported channel 14 configuration for 2.4GHz band
    in JP.
    
    [Test]
    PASS
    
    MTK-Commit-Id: 6543c2c7c6fa4b400ba01207cec3699b74f8e231
    
    CR-Id: ALPS09453198
    Feature: Others
    Signed-off-by: TS Hsu <ts.hsu@mediatek.com>
    Change-Id: Id382174b9a24ccbf6155c385ed2f867bd559b92b
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9762623
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: zeno.yang <zeno.yang@mediatek.com>
    Reviewed-by: facer.pei <facer.pei@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit dd648f54d6650f89d565f19824abd3817e35b67a)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9976093
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Reviewed-by: wyatt.sun <wyatt.sun@mediatek.com>
    Reviewed-by: jeff-kk.zhang <jeff-kk.zhang@mediatek.com>

--- mgmt/rlm_domain.c
+++ mgmt/rlm_domain.c
@@ -953,8 +953,6 @@ struct DOMAIN_INFO_ENTRY arSupportedRegDomains[] = {
 		{
 			{81, BAND_2G4, CHNL_SPAN_5, 1, 13, FALSE}
 			,	/*CH_SET_2G4_1_13 */
-			{82, BAND_2G4, CHNL_SPAN_5, 14, 1, FALSE}
-			,	/*CH_SET_2G4_14_1 */
 			{115, BAND_5G, CHNL_SPAN_20, 36, 4, FALSE}
 			,	/*CH_SET_UNII_LOW_36_48 */
 			{118, BAND_5G, CHNL_SPAN_20, 52, 4, TRUE}
