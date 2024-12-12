3d0cce10f6b1945952b0e4e90f0c7bece9f8b6cd
chris.hong <chris.hong@mediatek.com>
2024-12-12 20:53:53 +0800

    [ALPS09227817] wlan: fix reset timing issue
    [Description]
    Wifi off and reset command executed at the same time.
    Wifi off thread check not in resetting then set wifi off in progress.
    Reset thread check wifi on/off not in progress then set resetting.
    The above design still may has a timing issue that both thread
    think that the other is not in progress.
    
    Fix the set and check timing of the on/off and reset status.
    
    [Test]
    wifi on/off/reset
    
    MTK-Commit-Id: f297358b0801da31acde2dac56c46d809f2457fd
    
    Signed-off-by: chris.hong <chris.hong@mediatek.com>
    CR-Id: ALPS09227817
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I0802c7652e0642068beac8c7068bfed6f6651898
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9459578
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit db2caba7857e8f1eb13818c7f03d5fc3d3033b78)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9470197
    Reviewed-by: jake.chung <jake.chung@mediatek.com>
    Tested-by: jake.chung <jake.chung@mediatek.com>
    (cherry picked from commit 0df948e6d4f56efd95d6435acef4538a5479c0a5)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/9669797
    Build: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>
    GAI-Code-Review: srv_ai_review001
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Test: srv_mspautosanity
    Reviewed-by: yiyu.feng <yiyu.feng@mediatek.com>
    CodeQL-Review-Label: srv_pf_nep_win <srv_pf_nep_win@mediatek.com>

--- os/linux/gl_rst.c
+++ os/linux/gl_rst.c
@@ -696,8 +696,11 @@ uint32_t glResetTrigger(struct ADAPTER *prAdapter,
 	int ret = 0;
 #endif
 
-	if (kalIsResetting())
+
+	if (kalIsResetting() || kalIsResetOnEnd()) {
+		DBGLOG(INIT, INFO, "already in reset\n");
 		goto exit;
+	}
 
 	if (prAdapter) {
 		prChipInfo = prAdapter->chip_info;
@@ -734,18 +737,24 @@ uint32_t glResetTrigger(struct ADAPTER *prAdapter,
 	sysResetTrigger();
 #endif
 #if CFG_MTK_ANDROID_WMT && !CFG_SUPPORT_CONNAC1X
-	/* Avoid doing reset triggered by CMD when WIFI write is processing */
-	if (get_wifi_process_status() &&
-	   (eResetReason == RST_CMD_TRIGGER ||
-	    eResetReason == RST_FWK_TRIGGER))
-		goto exit;
-#endif
-#if CFG_MTK_MDDP_SUPPORT
-	mddpNotifyWifiReset();
-#endif
 
 	glResetUpdateFlag(TRUE);
 	glResetOnEndUpdateFlag(TRUE);
+
+	/* Avoid doing reset triggered by CMD when WIFI write is processing */
+	if (get_wifi_process_status() &&
+	   (eResetReason == RST_CMD_TRIGGER ||
+	    eResetReason == RST_FWK_TRIGGER)) {
+		DBGLOG(INIT, INFO, "cmd trigger in write processing\n");
+		glResetUpdateFlag(FALSE);
+		glResetCleanResetFlag();
+		goto exit;
+	}
+#endif
+
+#if CFG_MTK_MDDP_SUPPORT
+	mddpNotifyWifiReset();
+#endif
 
 	if (kalGetShutdownState()) {
 		DBGLOG(INIT, INFO, "skip in shutdown\n");
@@ -1744,7 +1753,7 @@ int glRstwlanPreWholeChipReset(enum consys_drv_type type, char *reason)
 	if (!get_wifi_powered_status()) {
 		DBGLOG(REQ, WARN, "wifi driver is off now\n");
 		wfsys_unlock();
-		return bRet;
+		goto exit;
 	}
 	wfsys_unlock();
 
@@ -1767,7 +1776,7 @@ int glRstwlanPreWholeChipReset(enum consys_drv_type type, char *reason)
 				DBGLOG(REQ, WARN,
 					"wifi driver reset fail, need whole chip reset.\n");
 				g_IsWholeChipRst = TRUE;
-				return bRet;
+				goto exit;
 			}
 			msleep(100);
 		}
@@ -1787,7 +1796,6 @@ int glRstwlanPreWholeChipReset(enum consys_drv_type type, char *reason)
 			DBGLOG(REQ, WARN, "Wi-Fi driver is resetting\n");
 			msleep(100);
 		}
-		fgIsDrvTriggerWholeChipReset = FALSE;
 		g_IsWholeChipRst = TRUE;
 
 		if (!prGlueInfo->u4ReadyFlag)
@@ -1797,6 +1805,9 @@ int glRstwlanPreWholeChipReset(enum consys_drv_type type, char *reason)
 	}
 	wait_for_completion(&g_RstOffComp);
 	DBGLOG(INIT, INFO, "Wi-Fi is off successfully.\n");
+
+exit:
+	fgIsDrvTriggerWholeChipReset = FALSE;
 
 	return bRet;
 }
@@ -1912,7 +1923,6 @@ int wlan_pre_whole_chip_rst_v3(enum connv3_drv_type drv,
 			DBGLOG(REQ, WARN, "Wi-Fi driver is resetting\n");
 			kalMsleep(100);
 		}
-		fgIsDrvTriggerWholeChipReset = FALSE;
 		g_IsWholeChipRst = TRUE;
 
 		dbg_ops = prAdapter->chip_info->prDebugOps;
@@ -1933,6 +1943,7 @@ int wlan_pre_whole_chip_rst_v3(enum connv3_drv_type drv,
 	wait_for_completion(&g_RstOffComp);
 exit:
 	DBGLOG(INIT, INFO, "Wi-Fi is off successfully\n");
+	fgIsDrvTriggerWholeChipReset = FALSE;
 
 	if (reset_type == ENUM_COREDUMP_BY_CHIP_RST_DFD_DUMP) {
 		glGetChipInfo((void **)&chip);
@@ -1999,7 +2010,7 @@ int wlan_pre_whole_chip_rst_v2(enum consys_drv_type drv,
 
 	if (!get_wifi_powered_status()) {
 		DBGLOG(REQ, WARN, "wifi driver is off now\n");
-		return 0;
+		goto exit;
 	}
 #endif
 	triggerHifDumpIfNeed();
@@ -2023,13 +2034,15 @@ int wlan_pre_whole_chip_rst_v2(enum consys_drv_type drv,
 			DBGLOG(REQ, WARN, "Wi-Fi driver is resetting\n");
 			kalMsleep(100);
 		}
-		fgIsDrvTriggerWholeChipReset = FALSE;
 		g_IsWholeChipRst = TRUE;
 
 		kalSetRstEvent(TRUE);
 	}
 
 	wait_for_completion(&g_RstOffComp);
+exit:
+	fgIsDrvTriggerWholeChipReset = FALSE;
+
 	DBGLOG(INIT, INFO, "Wi-Fi is off successfully.\n");
 
 	return 0;
