413a43e96aa4a8326f922952af5007480816619f
zhanghong20 <zhanghong20@lenovo.com>
2024-12-17 11:12:59 +0800

    [London ROW V] Add wlan power configure
    How:
    Add wlan power configure for London ROW V.
    
    Change-Id: If19d80ee48628905229c2b80165fdad3622ce2a8
    Reviewed-on: https://gerrit.mot.com/3173646
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <lixt11@lenovo.com>
    Reviewed-by: Zhilu Yin <yinzl1@motorola.com>
    Submit-Approved: Jira Key

--- common/mot_config.c
+++ common/mot_config.c
@@ -12,6 +12,8 @@ typedef struct moto_product {
 } moto_product;
 
 static moto_product products_list[] = {
+		{"lyriq",   "JAPAN", "wifi",     "txpowerctrl_JAPAN"},
+		{"lyriq",   "all", "wifi",     "txpowerctrl"},
 		{{0},        {0},   {0},	{0}},
 };
 
