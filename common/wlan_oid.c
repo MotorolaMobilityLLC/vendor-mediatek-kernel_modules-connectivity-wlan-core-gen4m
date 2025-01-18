cffc9cb5ef44a811cbdab95fc47554b76df39358
TS Hsu <ts.hsu@mediatek.com>
2025-01-18 23:52:26 +0800

    [ALPS09664103] wlan: Fix buffer overflow in BSSID list query
    [Description]
    Added a check to prevent buffer overflow when querying BSSID list by
    limiting the number of scan results based on buffer size.
    
    [Test]
    Factory mode pass
    
    MTK-Commit-Id: 9903fb79aac1229a2c5b9071112ade87c88bcfc3
    
    CR-Id: ALPS09664103
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ib5a9757422543b9da2f89a509f0132ac50d692fa
    Signed-off-by: TS Hsu <ts.hsu@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10065363
    Reviewed-by: fred.shen <fred.shen@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit e26f35c1cd76cfd65b6cbbc6eb3bd40350eee0e7)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10072844
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: jeff-kk.zhang <jeff-kk.zhang@mediatek.com>
    GAI-Code-Review: srv_ai_review001 <srv_ai_review001@mediatek.com>

--- common/wlan_oid.c
+++ common/wlan_oid.c
@@ -595,7 +595,7 @@ wlanoidQueryBssidList(struct ADAPTER *prAdapter,
 		      uint32_t *pu4QueryInfoLen)
 {
 	struct GLUE_INFO *prGlueInfo;
-	uint32_t i, u4BssidListExLen;
+	uint32_t i, u4BssidListExLen, u4SavedApCount = 0;
 	struct PARAM_BSSID_LIST_EX *prList;
 	struct PARAM_BSSID_EX *prBssidEx;
 	uint8_t *cp;
@@ -627,8 +627,18 @@ wlanoidQueryBssidList(struct ADAPTER *prAdapter,
 	u4BssidListExLen = 0;
 
 	if (prAdapter->fgIsRadioOff == FALSE) {
-		for (i = 0; i < prWlanInfo->u4ScanResultNum; i++)
+		for (i = 0; i < prWlanInfo->u4ScanResultNum; i++) {
+			/* If scan results size bigger than buffer size,
+			 * not to add further.
+			 */
+			if ((u4BssidListExLen +
+				ALIGN_4(prScanResult[i].u4Length) + 4)
+				> u4QueryBufferLen)
+				break;
+
 			u4BssidListExLen += ALIGN_4(prScanResult[i].u4Length);
+		}
+		u4SavedApCount = i;
 	}
 
 	if (u4BssidListExLen)
@@ -650,7 +660,7 @@ wlanoidQueryBssidList(struct ADAPTER *prAdapter,
 	if (prAdapter->fgIsRadioOff == FALSE &&
 	    prWlanInfo->u4ScanResultNum > 0) {
 		/* fill up for each entry */
-		for (i = 0; i < prWlanInfo->u4ScanResultNum; i++) {
+		for (i = 0; i < u4SavedApCount; i++) {
 			prBssidEx = (struct PARAM_BSSID_EX *) cp;
 
 			/* copy structure */
