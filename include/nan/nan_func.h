3a007ef65201fcd79af032aaf4a6210fd386e025
Zeno Yang <zeno.yang@mediatek.com>
2025-01-18 23:52:59 +0800

    [ALPS08940055] nan unify: API decoupling
    [Description]
    API decoupling
    
    [Test]
    Pass
    
    MTK-Commit-Id: 78d6ab1b10359fc8766da1a7e6c376c80230b445
    
    CR-Id: ALPS08940055
    Feature: Wi-Fi NAN
    Signed-off-by: Zeno Yang <zeno.yang@mediatek.com>
    Change-Id: I2ea023f17b6c6f647554077b1bacc7f358775814
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10088367
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    GAI-Code-Review: srv_ai_review001 <srv_ai_review001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_pf_nep_sanity
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: shin.hung <shin.hung@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    Reviewed-by: xumin.xu <xumin.xu@mediatek.com>

--- include/nan/nan_func.h
+++ include/nan/nan_func.h
@@ -6,208 +6,108 @@
 #ifndef _NAN_FUNC_H
 #define _NAN_FUNC_H
 
-#if !CFG_SUPPORT_NAN_EXT
-static inline
-void nanSetFlashCommunication(struct ADAPTER *prAdapter, u_int8_t fgEnable)
-{
-	DBGLOG(NAN, TRACE, "Set Flash Communication %u\n", fgEnable);
-	/* TODO:
-	 * 1. Set 2.4GHz/5GHz bitmap respectively
-	 * 2. Call reconfigure function to update customized timeline
-	 * 3. Send command to FW to set the bitmap, reuse Instant communication?
-	 */
-}
+void
+nanSetFlashCommunication(struct ADAPTER *prAdapter, u_int8_t fgEnable);
 
-static inline
-void nanExtEnableReq(struct ADAPTER *prAdapter)
-{
-	nanSetFlashCommunication(prAdapter, TRUE);
-}
+void
+nanExtEnableReq(struct ADAPTER *prAdapter);
 
-static inline
-void nanExtDisableReq(struct ADAPTER *prAdapter)
-{
-}
+void
+nanExtDisableReq(struct ADAPTER *prAdapter);
 
-static inline
-void nanExtClearCustomNdpFaw(uint8_t ucIndex)
-{
-}
+void
+nanExtClearCustomNdpFaw(uint8_t ucIndex);
 
-static inline
-void nanPeerReportEhtEvent(struct ADAPTER *prAdapter,
-					 uint8_t enable)
-{
-}
+void
+nanPeerReportEhtEvent(struct ADAPTER *prAdapter,
+					 uint8_t enable);
 
-static inline
-void nanEnableEhtMode(struct ADAPTER *prAdapter, uint8_t mode)
-{
-}
+void
+nanEnableEhtMode(struct ADAPTER *prAdapter, uint8_t mode);
 
-static inline
-void nanEnableEht(struct ADAPTER *prAdapter, uint8_t enable)
-{
-}
+void
+nanEnableEht(struct ADAPTER *prAdapter, uint8_t enable);
 
-static inline
-u_int8_t nanExtHoldNdl(struct _NAN_NDL_INSTANCE_T *prNDL)
-{
-	return FALSE;
-}
+u_int8_t
+nanExtHoldNdl(struct _NAN_NDL_INSTANCE_T *prNDL);
 
-static inline
-void nanExtBackToNormal(struct ADAPTER *prAdapter)
-{
-}
+void
+nanExtBackToNormal(struct ADAPTER *prAdapter);
 
-static inline
-void nanExtResetNdlConfig(struct _NAN_NDL_INSTANCE_T *prNDL)
-{
-}
+void
+nanExtResetNdlConfig(struct _NAN_NDL_INSTANCE_T *prNDL);
 
-static inline
-struct _NAN_NDL_INSTANCE_T *nanExtGetReusedNdl(struct ADAPTER *prAdapter)
-{
-	return NULL;
-}
+struct _NAN_NDL_INSTANCE_T *
+nanExtGetReusedNdl(struct ADAPTER *prAdapter);
 
-static inline
-u32 wlanoidNANExtCmd(struct ADAPTER *prAdapter, void *pvSetBuffer,
-		     uint32_t u4SetBufferLen, uint32_t *pu4SetInfoLen)
-{
-	return -EOPNOTSUPP;
-}
+u32
+wlanoidNANExtCmd(struct ADAPTER *prAdapter, void *pvSetBuffer,
+		     uint32_t u4SetBufferLen, uint32_t *pu4SetInfoLen);
 
-static inline
-u32 wlanoidNANExtCmdRsp(struct ADAPTER *prAdapter, void *pvSetBuffer,
-			uint32_t u4SetBufferLen, uint32_t *pu4SetInfoLen)
-{
-	return -EOPNOTSUPP;
-}
+u32
+wlanoidNANExtCmdRsp(struct ADAPTER *prAdapter, void *pvSetBuffer,
+			uint32_t u4SetBufferLen, uint32_t *pu4SetInfoLen);
 
-static inline
-void nanExtComposeBeaconTrack(struct ADAPTER *prAdapter,
-				struct _NAN_EVENT_REPORT_BEACON *prFwEvt)
-{
-}
+void
+nanExtComposeBeaconTrack(struct ADAPTER *prAdapter,
+				struct _NAN_EVENT_REPORT_BEACON *prFwEvt);
 
-static inline
-void nanExtComposeClusterEvent(struct ADAPTER *prAdapter,
-			       struct NAN_DE_EVENT *prDeEvt)
-{
-}
+void
+nanExtComposeClusterEvent(struct ADAPTER *prAdapter,
+			       struct NAN_DE_EVENT *prDeEvt);
 
-static inline
-uint32_t nanSchedGetVendorEhtAttr(struct ADAPTER *prAdapter,
+uint32_t
+nanSchedGetVendorEhtAttr(struct ADAPTER *prAdapter,
 				  uint8_t **ppucVendorAttr,
-				  uint32_t *pu4VendorAttrLength)
-{
-	return 0;
-}
+				  uint32_t *pu4VendorAttrLength);
 
-static inline
-uint32_t nanSchedGetVendorAttr(struct ADAPTER *prAdapter,
+uint32_t
+nanSchedGetVendorAttr(struct ADAPTER *prAdapter,
 			       uint8_t **ppucVendorAttr,
-			       uint32_t *pu4VendorAttrLength)
-{
-	return 0;
-}
+			       uint32_t *pu4VendorAttrLength);
 
-
-static inline
 uint16_t
 nanDataEngineVendorAttrLength(struct ADAPTER *prAdapter,
 				 struct _NAN_NDL_INSTANCE_T *prNDL,
-				 struct _NAN_NDP_INSTANCE_T *prNDP)
-{
-	return 0;
-}
+				 struct _NAN_NDP_INSTANCE_T *prNDP);
 
-static inline
-void nanDataEngineVendorAttrAppend(struct ADAPTER *prAdapter,
+void
+nanDataEngineVendorAttrAppend(struct ADAPTER *prAdapter,
 				      struct MSDU_INFO *prMsduInfo,
 				      struct _NAN_NDL_INSTANCE_T *prNDL,
-				      struct _NAN_NDP_INSTANCE_T *prNDP)
-{
+				      struct _NAN_NDP_INSTANCE_T *prNDP);
 
-}
-
-static inline
 uint16_t
 nanDataEngineVendorEhtAttrLength(struct ADAPTER *prAdapter,
 				 struct _NAN_NDL_INSTANCE_T *prNDL,
-				 struct _NAN_NDP_INSTANCE_T *prNDP)
-{
-	return 0;
-}
+				 struct _NAN_NDP_INSTANCE_T *prNDP);
 
-static inline
-void nanDataEngineVendorEhtAttrAppend(struct ADAPTER *prAdapter,
+void
+nanDataEngineVendorEhtAttrAppend(struct ADAPTER *prAdapter,
 				      struct MSDU_INFO *prMsduInfo,
 				      struct _NAN_NDL_INSTANCE_T *prNDL,
-				      struct _NAN_NDP_INSTANCE_T *prNDP)
-{
-}
+				      struct _NAN_NDP_INSTANCE_T *prNDP);
 
-static inline
-uint32_t nanGetFcSlots(struct ADAPTER *prAdapter)
-{
-	uint32_t u4Bitmap = 0;
+uint32_t
+nanGetFcSlots(struct ADAPTER *prAdapter);
 
-	DBGLOG(NAN, TEMP,
-	       "FC slots: %02x-%02x-%02x-%02x\n",
-	       ((uint8_t *)&u4Bitmap)[0], ((uint8_t *)&u4Bitmap)[1],
-	       ((uint8_t *)&u4Bitmap)[2], ((uint8_t *)&u4Bitmap)[3]);
+uint32_t
+nanGetTimelineFcSlots(struct ADAPTER *prAdapter, size_t szTimelineIdx,
+			       size_t szSlotIdx);
 
-	return u4Bitmap;
-}
-
-static inline
-uint32_t nanGetTimelineFcSlots(struct ADAPTER *prAdapter, size_t szTimelineIdx,
-			       size_t szSlotIdx)
-{
-	uint32_t u4Bitmap = 0;
-
-	NAN_DW_DBGLOG(NAN, DEBUG, TRUE, szSlotIdx,
-		      "Timeline %u FC slots: %02x-%02x-%02x-%02x\n",
-		      szTimelineIdx,
-		      ((uint8_t *)&u4Bitmap)[0], ((uint8_t *)&u4Bitmap)[1],
-		      ((uint8_t *)&u4Bitmap)[2], ((uint8_t *)&u4Bitmap)[3]);
-
-	return u4Bitmap;
-}
-
-/*
- * @szSlotIdx: slot index [0..512) representing the range in 0~8192 TU
- */
-static inline
-u_int8_t nanIsChnlSwitchSlot(struct ADAPTER *prAdapter,
+u_int8_t
+nanIsChnlSwitchSlot(struct ADAPTER *prAdapter,
 			     unsigned char fgPrintLog,
 			     size_t szTimelineIdx,
-			     size_t szSlotIdx)
-{
-	return FALSE;
-}
+			     size_t szSlotIdx);
 
-static inline
-void nanExtTerminateApNan(struct ADAPTER *prAdapter, uint8_t ucReason)
-{
+void
+nanExtTerminateApNan(struct ADAPTER *prAdapter, uint8_t ucReason);
 
-}
+void
+nanExtTerminateApNanEndPs(struct ADAPTER *prAdapter);
 
-static inline
-void nanExtTerminateApNanEndPs(struct ADAPTER *prAdapter)
-{
-
-}
-
-static inline
-void nanExtTerminateApNanEndLegacy(struct ADAPTER *prAdapter)
-{
-
-}
-
-#endif
+void
+nanExtTerminateApNanEndLegacy(struct ADAPTER *prAdapter);
 
 #endif /* _NAN_FUNC_H */
