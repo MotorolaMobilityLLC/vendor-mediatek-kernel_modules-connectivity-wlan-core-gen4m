cffa657d7f50bf78863fcfc59937807dcf16ddf6
chris.hong <chris.hong@mediatek.com>
2025-02-04 12:32:12 +0800

    [ALPS09463498] wlan: skip reset when fw already asserted
    [Description]
    skip reset when fw already asserted
    
    [Test]
    wifi on/off, reset
    
    MTK-Commit-Id: 5539cdbcdf1fe29ed5cb457c038b4915c267565d
    
    Signed-off-by: chris.hong <chris.hong@mediatek.com>
    CR-Id: ALPS09463498
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I2c5701e87c7d3f2dcd271f8ad1280f477bc27b0e
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10110865
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    GAI-Code-Review: srv_ai_review001 <srv_ai_review001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: yiyu.feng <yiyu.feng@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    (cherry picked from commit 30490d0e5970aacb04dbd701b63213666449d319)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10198011

--- os/linux/gl_init.c
+++ os/linux/gl_init.c
@@ -9201,6 +9201,7 @@ static void wlanRemove(void)
 WLAN_REMOVE_RETURN:
 #if CFG_CHIP_RESET_SUPPORT
 	glResetUpdateFlag(FALSE);
+	glResetUpdateFwAsserted(FALSE);
 #endif
 #if CFG_MTK_MDDP_SUPPORT
 	mddpNotifyWifiOffEnd();
