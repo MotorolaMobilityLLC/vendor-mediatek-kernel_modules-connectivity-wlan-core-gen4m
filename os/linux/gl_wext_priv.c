60643d405bd47951eecb4cc84e76e6015e457c38
chris.hong <chris.hong@mediatek.com>
2025-02-04 12:32:13 +0800

    [ALPS09454455] wlan: skip reset cmd when on/off ongoing
    [Description]
    skip reset cmd when on/off ongoing
    
    [Test]
    wifi on/off, reset
    
    MTK-Commit-Id: ddea60c75d6211688e49d229112b5fe29b12ea48
    
    Signed-off-by: chris.hong <chris.hong@mediatek.com>
    CR-Id: ALPS09454455
    Feature: Wi-Fi Function
    Change-Id: I43e99bd87568005299fd3af696a744c18afad9cb
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10165263
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    GAI-Code-Review: srv_ai_review001 <srv_ai_review001@mediatek.com>
    Reviewed-by: ben.lai <ben.lai@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: jianling.fu <jianling.fu@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    (cherry picked from commit b04bfce51630befc40e6aebc90b58270f9455a0e)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10197998

--- os/linux/gl_wext_priv.c
+++ os/linux/gl_wext_priv.c
@@ -19745,6 +19745,13 @@ int priv_driver_trigger_whole_chip_reset(
 
 	prGlueInfo = *((struct GLUE_INFO **) netdev_priv(prNetDev));
 
+#if CFG_MTK_ANDROID_WMT && !CFG_SUPPORT_CONNAC1X
+	if (get_wifi_process_status()) {
+		DBGLOG(REQ, WARN, "wifi on/off is ongoing, skip reset cmd\n");
+		return -1;
+	}
+#endif
+
 	if ((!prGlueInfo) ||
 	    (prGlueInfo->u4ReadyFlag == 0) ||
 	    kalIsResetting()) {
@@ -19784,6 +19791,13 @@ int priv_driver_trigger_wfsys_reset(
 		return -1;
 
 	prGlueInfo = *((struct GLUE_INFO **) netdev_priv(prNetDev));
+
+#if CFG_MTK_ANDROID_WMT && !CFG_SUPPORT_CONNAC1X
+	if (get_wifi_process_status()) {
+		DBGLOG(REQ, WARN, "wifi on/off is ongoing, skip reset cmd\n");
+		return -1;
+	}
+#endif
 
 	if ((!prGlueInfo) ||
 	    (prGlueInfo->u4ReadyFlag == 0) ||
