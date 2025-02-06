48ab2af227e3e284b30a818c2b87b5d6eac19229
Ruizhi Cai <cairz1@motorola.com>
2025-02-06 14:59:36 +0800

    disable 5G hotspot in the Japan.
    The japan does not support the 5G hotspot,  so
    set 5G channels to DFS channel.
    
    Change-Id: Iabe1a3240132f1032c0a960895f167dea29edbee
    Reviewed-on: https://gerrit.mot.com/3225597
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yue Sun <sunyue5@lenovo.com>
    Reviewed-by: Zhilu Yin <yinzl1@motorola.com>
    Submit-Approved: Jira Key

--- mgmt/rlm_domain.c
+++ mgmt/rlm_domain.c
@@ -451,7 +451,7 @@ struct DOMAIN_INFO_ENTRY arSupportedRegDomains[] = {
 			,			/* CH_SET_2G4_1_13 */
 			{82, BAND_2G4, CHNL_SPAN_5, 14, 1, FALSE}
 			,			/* CH_SET_2G4_14_14 */
-			{115, BAND_5G, CHNL_SPAN_20, 36, 4, FALSE}
+			{115, BAND_5G, CHNL_SPAN_20, 36, 4, TRUE}
 			,			/* CH_SET_UNII_LOW_36_48 */
 			{118, BAND_5G, CHNL_SPAN_20, 52, 4, TRUE}
 			,			/* CH_SET_UNII_MID_52_64 */
