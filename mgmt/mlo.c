52a46f941f8255883b784a977b6a2c83c1104579
Ben Lai <ben.lai@mediatek.com>
2025-02-18 09:58:02 +0800

    [ALPS09705281] sap: Skip ML IE check to adapt older version hostapd
    [Description]
    1. ML IE should be generate from hostapd, and driver will append ML IE
       in beacon frame originally. In 6989v2 project, hostapds ML function
       is NOT complete, so skip ML IE check in driver.
    
    [Test]
    1. Build pass.
    
    MTK-Commit-Id: 4cad7982cb22cc5d0278435dde09967b4908d7bd
    
    Change-Id: I02cc9f6e2bfafd583bb44a4a8acfdbfc1e5ac7c9
    Signed-off-by: Ben Lai <ben.lai@mediatek.com>
    CR-Id: ALPS09705281
    Feature: Wi-Fi Driver CONNAC
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10228412
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: chun-wei.tseng <chun-wei.tseng@mediatek.com>

--- mgmt/mlo.c
+++ mgmt/mlo.c
@@ -4718,7 +4718,8 @@ uint8_t mldSingleLink(struct ADAPTER *prAdapter,
 		enable &= !!(bss->u2RsnSelectedCapInfo & ELEM_WPA_CAP_MFPC);
 #endif
 		enable &= !!(bss->ucPhyTypeSet & PHY_TYPE_BIT_EHT);
-		if (prP2pSpecBssInfo && fgIsApMode)
+		if (!CFG_SAP_SKIP_CHECK_HOSTAPD_ML_IE &&
+		    prP2pSpecBssInfo && fgIsApMode)
 			enable &= prP2pSpecBssInfo->fgMlIeExist;
 	} else if (prStaRec) {
 		enable &= !!(prStaRec->ucDesiredPhyTypeSet & PHY_TYPE_BIT_EHT);
