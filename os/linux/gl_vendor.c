1bd49a1e8e8f6a52a93fcc251b84483cb6151bc7
aaron.chiu <Aaron.Chiu@mediatek.com>
2025-02-18 09:58:10 +0800

    [ALPS09742668] Enable DTIM config support
    [Description]
    Enable DTIM config support
    
    [Test]
    Build pass/Test pass
    
    MTK-Commit-Id: 31895eb82d271733c835574c92d3337c2a5f6a70
    
    CR-Id: ALPS09742668
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: aaron.chiu <aaron.chiu@mediatek.com>
    Change-Id: If19772a56d5f6e02f58505a60d22cdbf2cb9d54d
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10251980
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: sticky.chen <sticky.chen@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: wilson.lien <wilson.lien@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- os/linux/gl_vendor.c
+++ os/linux/gl_vendor.c
@@ -674,7 +674,6 @@ int mtk_cfg80211_vendor_set_dtim_param(struct wiphy *wiphy,
 	uint8_t len;
 	uint32_t u4SetDtimPeriod = 0, rStatus, u4BufLen;
 
-
 	ASSERT(wiphy);
 	ASSERT(wdev);
 	WIPHY_PRIV(wiphy, prGlueInfo);
@@ -709,7 +708,7 @@ int mtk_cfg80211_vendor_set_dtim_param(struct wiphy *wiphy,
 
 	u4SetDtimPeriod = nla_get_u32(attr[WIFI_ATTR_SET_DTIM_PARAMS]);
 
-	len = kalSnprintf(str, sizeof(str), "DtimPeriod %d", u4SetDtimPeriod);
+	len = kalSnprintf(str, sizeof(str), "DtimPeriod 0 %d", u4SetDtimPeriod);
 
 	if (len <= 0 || u4SetDtimPeriod < 0) {
 		DBGLOG(REQ, ERROR,
