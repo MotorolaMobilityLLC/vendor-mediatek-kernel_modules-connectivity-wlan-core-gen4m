fab92d4cbdde3df5ad70f4d8064f637eb31bd18c
Shuaishuai Kong <shuaishuai.kong@mediatek.com>
2025-02-18 09:58:10 +0800

    [ALPS09729813] wlan: call glTxRxUninit when wifi reset fail
    [Description]
    call glTxRxUninit when wifi reset fail
    
    [Test]
    wifi reset test pass
    
    MTK-Commit-Id: 27b3d365df6a3d626fa45061a0773b522fa3e941
    
    Signed-off-by: Shuaishuai Kong <shuaishuai.kong@mediatek.com>
    CR-Id: ALPS09729813
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I0f0030a15a95aaa5b24a9d4612ec6ded3e1e0bd7
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10257420
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Reviewed-by: chris.hong <chris.hong@mediatek.com>

--- os/linux/gl_init.c
+++ os/linux/gl_init.c
@@ -8355,6 +8355,7 @@ int32_t wlanOnAtReset(void)
 				"%d inform disconnected\n", u4Idx);
 		}
 	} else {
+		glTxRxUninit(prGlueInfo);
 		prAdapter->u4HifDbgFlag |= DEG_HIF_DEFAULT_DUMP;
 		halPrintHifDbgInfo(prAdapter);
 		DBGLOG(INIT, WARN, "Fail reason: %d\n", eFailReason);
