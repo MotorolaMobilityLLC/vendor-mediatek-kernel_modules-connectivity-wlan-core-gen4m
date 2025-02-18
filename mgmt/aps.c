9fe92898dde1351bc096c9ad3767e21af63eabeb
Albert Wang <Bh.wang@mediatek.com>
2025-02-18 09:58:07 +0800

    [ALPS09755107] aps: Dont select the last deauth WAPI AP as candidate
    [Description]
    If the WAPI AP that sent the deauth/disassoc is selected as
    candidate AP for SAA roaming, the supplicant may get stuck.
    
    [Test]
    Build pass/Test pass
    
    MTK-Commit-Id: fc11350859bd4f6be4799357c5e39b37520a2f9b
    
    Signed-off-by: Albert Wang <Bh.wang@mediatek.com>
    CR-Id: ALPS09755107
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I9af06e93bcdd5da84b1d6b63b2380c137c9e2a71
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10238630
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: bh.wang <bh.wang@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    Tested-by: bh.wang <bh.wang@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- mgmt/aps.c
+++ mgmt/aps.c
@@ -1564,6 +1564,9 @@ uint8_t apsSanityCheckBssDesc(struct ADAPTER *prAdapter,
 		if (prBssDesc->prBlock->fgDeauthLastTime &&
 		    (apsCanFormMultiLink(prAdapter, prBssDesc, ucBssIndex) ||
 		     prApsInfo->u4EssApNum <= 1 ||
+#if CFG_SUPPORT_WAPI
+		     aisGetWapiMode(prAdapter, ucBssIndex) ||
+#endif
 		     prBssDesc->prBlock->ucDeauthCount >= 2)) {
 			DBGLOG(APS, WARN, MACSTR " is sending deauth [%d].\n",
 				MAC2STR(prBssDesc->aucBSSID),
