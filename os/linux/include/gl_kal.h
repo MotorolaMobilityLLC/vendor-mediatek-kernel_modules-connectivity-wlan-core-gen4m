a5a8e7e3850075366ea0a26cbbd739f22e1802cb
Jim Chuang <jim.chuang@mediatek.com>
2025-03-04 10:13:10 +0800

    [ALPS09770181] net: Add counter and log for Napi schedule check
    [Description]
    Introduced a counter and logging mechanism to track Napi schedule
    checks. This helps diagnose Napi Schedule Timeout issues by providing
    more detailed logs and preventing redundant scheduling.
    
    [Test]
    UT
    
    MTK-Commit-Id: 8f1b88bdb205c8b5dd30307a22be971ae8ec995a
    
    Signed-off-by: Jim Chuang <jim.chuang@mediatek.com>
    CR-Id: ALPS09770181
    Feature: Others
    Change-Id: Id03dd81982628cb48e5cfc1c9f1e26bd395a2ad6
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10290836
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ks.lim <ks.lim@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>

--- os/linux/include/gl_kal.h
+++ os/linux/include/gl_kal.h
@@ -2530,13 +2530,13 @@ kalChannelFormatSwitch(struct cfg80211_chan_def *channel_def,
 		struct RF_CHANNEL_INFO *prRfChnlInfo);
 
 void kal_napi_complete_done(struct napi_struct *n, int work_done);
-void kal_napi_schedule(struct napi_struct *n);
+uint8_t kal_napi_schedule(struct napi_struct *n);
 
 #if CFG_SUPPORT_RX_GRO
 uint8_t kalRxGroInit(struct net_device *prDev);
 uint32_t kal_is_skb_gro(struct ADAPTER *prAdapter, uint8_t ucBssIdx);
 void kal_gro_flush(struct ADAPTER *prAdapter);
-void kal_napi_schedule(struct napi_struct *n);
+uint8_t kal_napi_schedule(struct napi_struct *n);
 int kalNapiPoll(struct napi_struct *napi, int budget);
 uint8_t kalNapiInit(struct GLUE_INFO *prGlueInfo);
 uint8_t kalNapiUninit(struct GLUE_INFO *prGlueInfo);
