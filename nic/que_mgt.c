db64eece808f5aeaaa021f82fe8fa0544c59a3c2
Kok Suan Lim <ks.lim@mediatek.com>
2025-03-07 01:03:51 +0800

    [ALPS09812521] wlan: Fix boundary check to prevent overflow
    [Description]
    Added a boundary check for ucTid to ensure it does not exceed
    CFG_RX_MAX_BA_TID_NUM, preventing potential overflow and security
    vulnerability.
    
    [Test]
    Test Pass
    
    MTK-Commit-Id: ff81673a984864316fcbdaa864cb08f5e88766a6
    
    Signed-off-by: Kok Suan Lim <ks.lim@mediatek.com>
    CR-Id: ALPS09812521
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ifff7a8775581b8ffa37b6817c8291e40a088f46d
    (cherry-pick from f450275ee6501a13d56886e61f4a963496ab65d1)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10343733
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    Tested-by: srv_ssm2_admin <srv_ssm2_admin@mediatek.com>

--- nic/que_mgt.c
+++ nic/que_mgt.c
@@ -10574,6 +10574,9 @@ void qmHandleRxReorderWinShift(struct ADAPTER *prAdapter,
 		return;
 	}
 
+	if (ucTid >= CFG_RX_MAX_BA_TID_NUM)
+		return;
+
 	/* Check whether the BA agreement exists */
 	prReorderQueParm = prStaRec->aprRxReorderParamRefTbl[ucTid];
 	if (!prReorderQueParm) {
