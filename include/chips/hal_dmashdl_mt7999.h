250599860d2bf6d733d8b76080dfceca3ab59dbd
Holiday Hao <holiday.hao@mediatek.com>
2025-03-10 15:40:15 +0800

    [ALPS09322828] dmasch: Update mt7999 ple page size
    [Description]
    Update mt7999 ple page size to 0x960
    
    [Test]
    build pass
    
    MTK-Commit-Id: c4ea1616fcf9feac23233642b72c595b0dc78f38
    
    Change-Id: Icb240d0bb987727314c0a8c066957e0f679f1ded
    CR-Id: ALPS09322828
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Holiday Hao <holiday.hao@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10257425
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    GAI-Code-Review: srv_ai_review001 <srv_ai_review001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- include/chips/hal_dmashdl_mt7999.h
+++ include/chips/hal_dmashdl_mt7999.h
@@ -33,7 +33,7 @@
 
 #define MT7999_DMASHDL_LMAC_QUEUE_NUM                  (32)
 #define MT7999_DMASHDL_SLOT_ARBITER_EN                 (0)
-#define MT7999_DMASHDL_PLE_TOTAL_PAGE_SIZE             (0x6E0)
+#define MT7999_DMASHDL_PLE_TOTAL_PAGE_SIZE             (0x960)
 #define MT7999_DMASHDL_PSE_TOTAL_PAGE_SIZE             (0x20)
 #define MT7999_DMASHDL_PKT_PLE_MAX_PAGE                (0x1)
 #define MT7999_DMASHDL_PKT_PSE_MAX_PAGE                (0x8)
