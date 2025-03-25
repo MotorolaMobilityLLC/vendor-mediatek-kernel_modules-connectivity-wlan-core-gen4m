78d554d3b4e9116d51c10b9a9d72132409e73a04
Jim Chuang <jim.chuang@mediatek.com>
2025-03-25 11:24:20 +0800

    [ALPS09852452] nic: Improve BSS absent queue handling
    [Description]
    Enhanced logging and fixed bitmap handling for BSS absent queue to
    prevent timeout issues during network absence checks.
    
    [Test]
    UT
    
    MTK-Commit-Id: a226f709973b46e510fe0dc075ba9e8c1302506b
    
    Signed-off-by: Jim Chuang <jim.chuang@mediatek.com>
    CR-Id: ALPS09852452
    Feature: Wi-Fi Driver CONNAC
    Change-Id: I15ede8345e2c00559acaa65afe24cda2feb5e631
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10387081
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: boforn.lin <boforn.lin@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    GAI-Code-Review: srv_ai_review001 <srv_ai_review001@mediatek.com>
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_pf_nep_sanity
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- nic/nic_tx.c
+++ nic/nic_tx.c
@@ -5675,18 +5675,23 @@ static void nicTxDirectCheckBssAbsentQ(struct ADAPTER
 	}
 
 	if (isNetAbsent(prAdapter, prBssInfo)) {
-		DBGLOG(TX, TRACE, "fgIsNetAbsent!\n");
 		KAL_ACQUIRE_SPIN_LOCK(prAdapter, SPIN_LOCK_TX_RESOURCE);
 		QUEUE_INSERT_HEAD(
 			&prAdapter->rBssAbsentQueue[ucBssIndex],
 			(struct QUE_ENTRY *) prMsduInfo);
-		KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_TX_RESOURCE);
 		prAdapter->u4BssAbsentTxBufferBitmap |= BIT(ucBssIndex);
+		KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_TX_RESOURCE);
+		DBGLOG(TX, TRACE, "fgIsNetAbsent! BM:0x%02x QC:%u\n",
+			prAdapter->u4BssAbsentTxBufferBitmap,
+			QUEUE_LENGTH(&prAdapter->rBssAbsentQueue[ucBssIndex]));
 		return;
 	}
 
-	if (prAdapter->u4BssAbsentTxBufferBitmap)
-		DBGLOG(TX, TRACE, "fgIsNetAbsent END!\n");
+	if (prAdapter->u4BssAbsentTxBufferBitmap) {
+		DBGLOG(TX, TRACE, "fgIsNetAbsent END! BM:0x%02x QC:%u\n",
+			prAdapter->u4BssAbsentTxBufferBitmap,
+			QUEUE_LENGTH(&prAdapter->rBssAbsentQueue[ucBssIndex]));
+	}
 
 	if (QUEUE_IS_EMPTY(&prAdapter->rBssAbsentQueue[ucBssIndex])) {
 		QUEUE_INSERT_TAIL(prQue, prMsduInfo);
@@ -5697,6 +5702,7 @@ static void nicTxDirectCheckBssAbsentQ(struct ADAPTER
 	KAL_ACQUIRE_SPIN_LOCK(prAdapter, SPIN_LOCK_TX_RESOURCE);
 	QUEUE_CONCATENATE_QUEUES(
 		prTmpQue, &prAdapter->rBssAbsentQueue[ucBssIndex]);
+	prAdapter->u4BssAbsentTxBufferBitmap &= ~BIT(ucBssIndex);
 	KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_TX_RESOURCE);
 
 	u4Size = QUEUE_LENGTH(prTmpQue);
@@ -5726,9 +5732,8 @@ static void nicTxDirectCheckBssAbsentQ(struct ADAPTER
 		QUEUE_CONCATENATE_QUEUES(
 			&prAdapter->rBssAbsentQueue[ucBssIndex],
 			prTmpQue);
+		prAdapter->u4BssAbsentTxBufferBitmap |= BIT(ucBssIndex);
 		KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_TX_RESOURCE);
-	} else {
-		prAdapter->u4BssAbsentTxBufferBitmap &= ~BIT(ucBssIndex);
 	}
 
 	if (QUEUE_IS_NOT_EMPTY(prFreeQue))
