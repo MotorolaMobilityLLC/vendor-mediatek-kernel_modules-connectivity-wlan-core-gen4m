1eaa09df8f12a714e8baf1a245289024e08381b5
Logan <logan.hsiao@mediatek.com>
2025-04-08 22:49:40 +0800

    [WCNCR00401341] misc: add CE_SDK3.7_VLP_CONFIG
    [Description]
    Add 6G power table naming for Falcon
    
    MTK-Commit-Id: e0851a35eed0789ce6779ae5bdd5e0abbe53820b
    
    Signed-off-by: Logan <logan.hsiao@mediatek.com>
    CR-Id: WCNCR00401341
    Feature: WIFI/TXPWR
    Change-Id: I51b7b9f9be6ace23d27bfcfc468303505df7bebb
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10421895
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Reviewed-by: ts.hsu <ts.hsu@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Test: srv_pf_nep_sanity
    GAI-Code-Review: srv_ai_review001 <srv_ai_review001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: jayden.kuo <jayden.kuo@mediatek.com>

--- chips/mt6639/mt6639.c
+++ chips/mt6639/mt6639.c
@@ -1180,6 +1180,10 @@ struct mt66xx_chip_info mt66xx_chip_info_mt6639 = {
 #if (CFG_SUPPORT_SINGLE_SKU_6G_1SS1T == 1)
 	.prTxPwrLimit6G1ss1tFile = "TxPwrLimit6G_MT66x9_1ss1t.dat",
 #endif
+#if (CFG_SUPPORT_CE_6G_PWR_REGULATIONS == 1)
+	.prTxPwrLimit6GVlpFile = "TxPwrLimit6G_MT66x9_VLP.dat",
+	.prTxPwrLimit6GSpFile = "TxPwrLimit6G_MT66x9_SP.dat",
+#endif  /*CFG_SUPPORT_CE_6G_PWR_REGULATIONS == 1*/
 #endif
 
 	.ucTxPwrLimitBatchSize = 3,
