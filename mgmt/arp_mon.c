c322305031ad25b12353cb4d337b2cc96875a4e6
Kok Suan Lim <ks.lim@mediatek.com>
2025-06-03 22:02:27 +0800

    [ALPS09990639] arp_mon: Fix MLO connection issue
    [Description]
    Updated MAC address comparison logic to use cnmStaRecAuthAddr
    function, ensuring accurate MAC address validation and resolving MLO
    connection issue.
    
    [Test]
    Test Pass
    
    MTK-Commit-Id: 1f39ab362cdbc5522017425a38bd1c91b5688eb1
    
    Signed-off-by: Kok Suan Lim <ks.lim@mediatek.com>
    CR-Id: ALPS09990639
    Feature: Wi-Fi Driver CONNAC
    Change-Id: Ib3bb0592ccc912286bf2d6260aeaa09544c28ebb
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10648669
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- mgmt/arp_mon.c
+++ mgmt/arp_mon.c
@@ -466,7 +466,7 @@ void arpMonHandleRxArpPkt(struct ADAPTER *ad,
 
 	if (prAisBssInfo && prAisBssInfo->prStaRecOfAP) {
 		if (EQUAL_MAC_ADDR(prArp->aucSenderMACaddr,
-				   prAisBssInfo->prStaRecOfAP->aucMacAddr)) {
+			cnmStaRecAuthAddr(ad, prAisBssInfo->prStaRecOfAP))) {
 			arpMonResetTxCnt(ad, ucBssIdx);
 			arpMonResetGatewayTxCnt(ad, ucBssIdx);
 			arpMonSetApIp(ad, ucBssIdx, prArp->aucSenderIPaddr);
@@ -476,7 +476,7 @@ void arpMonHandleRxArpPkt(struct ADAPTER *ad,
 				IPV4TOSTR(arpMonGetApIpPtr(ad, ucBssIdx)),
 				MAC2STR(prArp->aucSenderMACaddr));
 		} else if (EQUAL_MAC_ADDR((prArpMonPktInfo->aucTaAddr),
-			prAisBssInfo->prStaRecOfAP->aucMacAddr) &&
+			cnmStaRecAuthAddr(ad, prAisBssInfo->prStaRecOfAP)) &&
 			fgIsFromApIpOrGatewayIp) {
 			arpMonResetTxCnt(ad, ucBssIdx);
 			arpMonResetGatewayTxCnt(ad, ucBssIdx);
