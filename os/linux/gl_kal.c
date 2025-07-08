486b9c0245a0458cc2a11b6d946e9e7d3a32757c
Jim Chuang <jim.chuang@mediatek.com>
2025-07-08 13:05:58 +0800

    [ALPS10127686] wifi: Fix NULL pointer dereference in __kalPerCpuTxXmit
    [Description]
    Added checks for prDev to prevent kernel NULL pointer dereference when
    handling packets, ensuring stability during transmission.
    
    [Test]
    UT
    
    MTK-Commit-Id: 304bff87c0417ea6f6894263aeb6dea04daffebf
    
    Signed-off-by: Jim Chuang <jim.chuang@mediatek.com>
    CR-Id: ALPS10127686
    Feature: Others
    Change-Id: I430a7f64d8d42b9cb5b7ea461dac140aa1fa49d2
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10179317
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: ks.lim <ks.lim@mediatek.com>
    Tested-by: ks.lim <ks.lim@mediatek.com>
    (cherry picked from commit f24b5bf93651ccaaf609da394bf15d4ec8e1d1f2)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/10798613
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: holiday.hao <holiday.hao@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>

--- os/linux/gl_kal.c
+++ os/linux/gl_kal.c
@@ -18683,6 +18683,11 @@ static uint32_t __kalPerCpuTxXmit(struct sk_buff *prSkb, struct GLUE_INFO *pr)
 	if (!prPerCpuTxInfo->fgReady || !prSkb)
 		return WLAN_STATUS_NOT_ACCEPTED;
 
+	ucBssIndex = GLUE_GET_PKT_BSS_IDX(prSkb);
+	prDev = wlanGetNetDev(pr, ucBssIndex);
+	if (!prDev)
+		return WLAN_STATUS_NOT_ACCEPTED;
+
 	prInfo = get_cpu_ptr(prPerCpuTxInfo->prInfo);
 
 	__skb_queue_tail(&prInfo->rSkbQ, prSkb);
@@ -18709,8 +18714,6 @@ static uint32_t __kalPerCpuTxXmit(struct sk_buff *prSkb, struct GLUE_INFO *pr)
 		}
 		tasklet_schedule(&prInfo->rTask);
 	} else {
-		ucBssIndex = GLUE_GET_PKT_BSS_IDX(prSkb);
-		prDev = wlanGetNetDev(pr, ucBssIndex);
 		stopped = netif_subqueue_stopped(prDev, prSkb);
 		if (unlikely(stopped)) {
 			u2QueueIdx = skb_get_queue_mapping(prSkb);
