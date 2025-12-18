920e015b591687b24bf081fbda165c0fab41b8d5
Rachel Tseng <Rachel.Tseng@mediatek.com>
2025-12-18 22:51:22 +0800

    [ALPS10745307] wlan: fix memory leak in cfg80211_disconnected
    [Description]
    If wifi reset during dual STA connection, there is one thread doing wifi
    reset, while another thread doing wlan1 removing. Both threads may
    notify wlan1 cfg80211_disconnected, so dont repeat reporting if main
    link of BssInfo is not exist.
    
    [Test]
    Build pass.
    
    MTK-Commit-Id: e62e547213069f7f6d05f7ce3ac889e7c25bf242
    
    Change-Id: I1378e944b60f793c552c7459195f88115a793248
    CR-Id: ALPS10745307
    Feature: Wi-Fi Driver CONNAC
    Signed-off-by: Rachel Tseng <Rachel.Tseng@mediatek.com>
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/11222900
    ODB-Check: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Test: srv_pf_nep_sanity
    Build: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Commit-Check: srv_check_service <srv_check_service@mediatek.com>
    Reviewed-by: rachel.tseng <rachel.tseng@mediatek.com>
    Tested-by: rachel.tseng <rachel.tseng@mediatek.com>
    Build: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    AutoUT-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Build: srv_preflight_b001 <srv_preflight_b001@mediatek.com>
    Coverity-Review-Label: srv_neptune_adm <srv_neptune_adm@mediatek.com>
    Reviewed-by: herman.yang <herman.yang@mediatek.com>
    (cherry picked from commit 5e1b7ad4ca7d0d3f362069a95d9e55fdf55a2ead)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/11359956
    Test: srv_preflight_a001 <srv_preflight_a001@mediatek.com>
    (cherry picked from commit 61fa78674a2ac16478f911ab8936ffbd1dd10b60)
    Reviewed-on: https://gerrit.mediatek.inc/c/neptune/wlan_driver/gen4m/+/11551603

--- os/linux/gl_init.c
+++ os/linux/gl_init.c
@@ -8875,14 +8875,20 @@ wlanOffNotifyCfg80211Disconnect(struct GLUE_INFO *prGlueInfo)
 	for (u4Idx = 0; u4Idx < KAL_AIS_NUM; u4Idx++) {
 		struct net_device *prDevHandler =
 			wlanGetAisNetDev(prGlueInfo, u4Idx);
-		uint8_t ucBssIndex = 0;
+		struct BSS_INFO *prBssInfo;
 
 		if (!prDevHandler)
 			continue;
 
-		ucBssIndex = AIS_MAIN_BSS_INDEX(prGlueInfo->prAdapter, u4Idx);
- 		if (kalGetMediaStateIndicated(prGlueInfo, ucBssIndex) ==
-		    MEDIA_STATE_CONNECTED) {
+		prBssInfo = AIS_MAIN_BSS_INFO(prGlueInfo->prAdapter, u4Idx);
+		if (!prBssInfo) {
+			DBGLOG(INIT, WARN,
+				"-> ais(%d) main bssinfo null\n", u4Idx);
+			continue;
+		}
+
+		if (kalGetMediaStateIndicated(prGlueInfo,
+		    prBssInfo->ucBssIndex) == MEDIA_STATE_CONNECTED) {
 #if CFG_WPS_DISCONNECT || (KERNEL_VERSION(4, 2, 0) <= CFG80211_VERSION_CODE)
 			cfg80211_disconnected(
 				prDevHandler, 0, NULL, 0,
